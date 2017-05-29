b985c8c93df7c50202f71c553451362838d4e096
yao <myspiritcrazy@gmail.com>
2017-05-29 12:29:11 -0700

    get max num of consecutive coin pairs

--- src/Main.java
+++ src/Main.java
@@ -41,11 +41,38 @@ public class Main {
         return getNumSquares(start, end);
     }
 
+    private static int solution2(int[] A) {
+        int n = A.length;
+        int result = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (A[i] == A[i + 1])
+                result = result + 1;
+        }
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            if (i > 0) {
+                if (A[i - 1] != A[i])
+                    count = count + 1;
+                else
+                    count = count - 1;
+            }
+            if (i < n - 1) {
+                if (A[i + 1] != A[i])
+                    count = count + 1;
+                else
+                    count = count - 1;
+            }
+            r = Math.max(r, count);
+        }
+        return result + r;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
         test();
-
+/*
         // get number of squares
         int left = 4;
         int right = 17;
@@ -96,6 +123,7 @@ public class Main {
         right = 10;
 
         System.out.println("num of squares in [" + left + ", " + right + "]: " +  solution(left, right));
-
+*/
+        System.out.println("max num of consecutive pairs: " + solution2(new int[]{1, 1, 0, 1, 0, 0}));
     }
 }
