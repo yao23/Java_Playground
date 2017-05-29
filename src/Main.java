1525bdbfc273f561edbbcff4b95305fb96a0d4ba
yao <myspiritcrazy@gmail.com>
2017-05-29 10:28:06 -0700

    test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+    public static void test() {
+        System.out.println("test");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        test();
     }
 }
