26244be76bd55383d6d5235314262680a111b2a0
yao <myspiritcrazy@gmail.com>
2017-06-04 19:20:52 -0700

    add a concise solution

--- src/TopKFrequentElements.java
+++ src/TopKFrequentElements.java
@@ -65,7 +65,27 @@ public class TopKFrequentElements {
 
         return resultList;
     }
+/*
+    public List<Integer> topKFrequentV1(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
 
+        PriorityQueue<Map.Entry<Integer, Integer>> maxHeap =
+                new PriorityQueue<>((a,b)->(b.getValue()-a.getValue()));
+        for(Map.Entry<Integer,Integer> entry: map.entrySet()){
+            maxHeap.add(entry);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        while(res.size()<k){
+            Map.Entry<Integer, Integer> entry = maxHeap.poll();
+            res.add(entry.getKey());
+        }
+        return res;
+    }
+*/
     public static void main(String[] args) {
         int [] input = new int[] {1,1,1,2,2,3,4,5,6,6,6,6};
 
