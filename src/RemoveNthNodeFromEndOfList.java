c7beaa338a162141af2a8b70816206aae9d9097f
yao <myspiritcrazy@gmail.com>
2017-06-06 23:05:38 -0700

    add more test cases

--- src/RemoveNthNodeFromEndOfList.java
+++ src/RemoveNthNodeFromEndOfList.java
@@ -43,4 +43,10 @@ public class RemoveNthNodeFromEndOfList {
             return dummy.next;
         }
     }
+
+    // [] 0 => []
+    // [1] 1 => []
+    // [1,2,3,4,5] 2 => [1,2,3,5]
+    // [1,2,3,4,5] 5 => [2,3,4,5]
+    // [1,2,3,4,5] 6 => [1,2,3,4,5] (LeetCode given [2,3,4,5])
 }
