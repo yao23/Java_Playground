9061ff9698de29ba4b70b4a57e56b22bd3899f9f
yao <myspiritcrazy@gmail.com>
2017-06-07 22:20:22 -0700

    use stack

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -1,5 +1,36 @@
 /**
  * Created by liyao on 6/7/17.
  */
+import java.util.*;
+
 public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        TreeNode cur = root;
+
+        while (!stack.isEmpty() || cur != null) {
+            if (cur != null) {
+                if (cur.left == null) {
+                    result.add(cur.val);
+                    if (cur.right != null) {
+                        stack.push(cur.right);
+                    } else {
+                        cur = stack.pop();
+                    }
+                } else {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+            } else {
+                cur = stack.pop();
+            }
+        }
+
+        return result;
+    }
 }
