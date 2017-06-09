0316de24e931d750f174e3c1081287e8e448697d
yao <myspiritcrazy@gmail.com>
2017-06-08 23:27:10 -0700

    add test case leading brute force for time limit exceeded

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -4,9 +4,12 @@
 import java.util.*;
 
 public class ThreeSum {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        Set<List<Integer>> resultSet = new HashSet<List<Integer>>();
+    public List<List<Integer>> threeSum(int[] nums) { // Brute Force: Time Limit Exceeded
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) {
+            return result;
+        }
+        Set<List<Integer>> resultSet = new HashSet<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
             for (int j = i + 1; j < nums.length - 1; j++) {
@@ -31,4 +34,5 @@ public class ThreeSum {
     // [1,2,3]
     // [-1,0,1,2,-1,-4]
     // [-1,-1,-1,2,2,2]
+    // [2,5,5,8,-7,-9,5,-1,-4,2,8,4,-6,-2,-2,9,-2,13,1,0,9,9,4,-13,13,3,-14,11,-5,-13,3,4,7,-15,-11,7,13,1,13,-14,11,-1,5,-10,12,11,14,-13,1,-8,3,-4,-14,14,-10,-15,-6,-9,3,-4,-7,-8,-15,8,-8,12,-8,13,-2,-9,14,-6,5,-3,-9,-6,-7,-10,-3,9,-2,7,-10,-9,-2,-5,13,7,-6,2,-12,-6,1,10,9,0,7,-13,-2,-9,-7,-2,-8,5,10,-1,6,-12,4,10,12,9,2,10,8,-15,12,6,-1,-9,-7,2]
 }
