5cf2c8ea39e760453ddd08e466de5ade7bc586d5
yao <myspiritcrazy@gmail.com>
2017-06-10 16:27:06 -0700

    hashmap and one pass solution

--- src/CopyListWithRandomPointer.java
+++ src/CopyListWithRandomPointer.java
@@ -6,7 +6,61 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CopyListWithRandomPointer {
-    public RandomListNode copyRandomList(RandomListNode head) {
+    private RandomListNode getNode(RandomListNode node, Map<RandomListNode,RandomListNode> hashMap) {
+        RandomListNode newNode = hashMap.get(node);
+        if (newNode == null) { // if not created in hashamp before
+            if (node != null) {
+                newNode = new RandomListNode(node.label);
+            }
+            hashMap.put(node,newNode); // construct old-new node map
+        }
+        return newNode;
+    }
+
+    private RandomListNode processSingleNode(RandomListNode head) {
+        if (head.next == null) {
+            RandomListNode newHead = new RandomListNode(head.label);
+            if (head.random == null) {
+                newHead.random = null;
+            } else {
+                newHead.random = newHead;
+            }
+            return newHead;
+        } else {
+            return null;
+        }
+    }
+
+    public RandomListNode copyRandomList(RandomListNode head) { // hashmap with one pass
+        if (head == null) {
+            return null;
+        }
+
+        RandomListNode tmpNode = processSingleNode(head);
+        if (tmpNode != null) {
+            return tmpNode;
+        }
+
+        Map<RandomListNode,RandomListNode> hashMap = new HashMap<>();
+        RandomListNode cur = head;
+        RandomListNode dummy = new RandomListNode(0);
+        RandomListNode newCur = dummy;
+
+        while (cur != null) { // build new list
+            RandomListNode tmpNewCur = getNode(cur, hashMap);
+            tmpNewCur.next = getNode(cur.next, hashMap);
+            tmpNewCur.random = getNode(cur.random, hashMap);
+
+            newCur.next = tmpNewCur;
+            newCur = newCur.next;
+
+            cur = cur.next;
+        }
+
+        return dummy.next;
+    }
+
+    public RandomListNode copyRandomListV0(RandomListNode head) { // hashmap with two passes
         if (head == null) {
             return null;
         }
