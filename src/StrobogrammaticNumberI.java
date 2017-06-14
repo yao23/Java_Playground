63c86d45de8d7d15845dd20cb5e709acdfb91eeb
yao <myspiritcrazy@gmail.com>
2017-06-13 23:47:02 -0700

    Strobogrammatic Number I

---
+++ src/StrobogrammaticNumberI.java
@@ -0,0 +1,48 @@
+/**
+ * Created by liyao on 6/13/17.
+ */
+public class StrobogrammaticNumberI {
+    private boolean checkOneNumber(int num) {
+        return (num == 0 || num == 1 || num == 8);
+    }
+
+    private boolean checkTwoNumbers(int a, int b) {
+        if ((a == 6 && b == 9) || (a == 9 && b==6)) {
+            return true;
+        } else if (a == b && a == 0) {
+            return true;
+        } else if (a == b && a == 1) {
+            return true;
+        } else if (a == b && a == 8) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isStrobogrammatic(String num) {
+        int len = num.length();
+        if (len == 0) {
+            return true;
+        } else {
+            int left = 0, right = len -1;
+
+            while (left < right) {
+                int leftNum = Character.getNumericValue(num.charAt(left));
+                int rightNum = Character.getNumericValue(num.charAt(right));
+                if (checkTwoNumbers(leftNum, rightNum)) {
+                    left++;
+                    right--;
+                } else {
+                    return false;
+                }
+            }
+
+            if (left == right) {
+                return checkOneNumber(Character.getNumericValue(num.charAt(left)));
+            } else {
+                return true;
+            }
+        }
+    }
+}
