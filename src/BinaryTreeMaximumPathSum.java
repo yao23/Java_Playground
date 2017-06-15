2487a549cd553baf42a484acee7a93827bde6996
yao <myspiritcrazy@gmail.com>
2017-06-14 23:16:58 -0700

    Search Insert Position

---
+++ src/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by liyao on 6/14/17.
+ */
+public class BinaryTreeMaximumPathSum {
+}
---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,30 @@
+/**
+ * Created by liyao on 6/14/17.
+ */
+public class SearchInsertPosition {
+    public int searchInsert(int[] arr, int target) {
+        int start = 0, end = arr.length - 1;
+        if (target < arr[start]) { // smaller than start
+            return start;
+        } else if (target > arr[end]) { // larger than end
+            return (end + 1);
+        }
+
+        while (start + 1 < end) {
+            int mid = start + (end - start)/2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (arr[start] < target) {
+            return (start + 1);
+        } else {
+            return start;
+        }
+    }
+}
