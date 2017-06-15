1f1832df4770e26d31ecc8bf7661d306a79d044d
yao <myspiritcrazy@gmail.com>
2017-06-14 23:18:51 -0700

    add test cases

--- src/SearchInsertPosition.java
+++ src/SearchInsertPosition.java
@@ -27,4 +27,9 @@ public class SearchInsertPosition {
             return start;
         }
     }
+
+    // [1,3,5,6], 5 -> 2
+    // [1,3,5,6], 2 -> 1
+    // [1,3,5,6], 7 -> 4
+    // [1,3,5,6], 0 -> 0
 }
