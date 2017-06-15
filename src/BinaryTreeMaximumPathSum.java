1baa1c98e8c360986392cc5188f210159ced99b0
yao <myspiritcrazy@gmail.com>
2017-06-14 23:17:35 -0700

    Binary Tree Maximum Path Sum

--- src/BinaryTreeMaximumPathSum.java
+++ src/BinaryTreeMaximumPathSum.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by liyao on 6/14/17.
  */
 public class BinaryTreeMaximumPathSum {
+
+    // [[5][3,4][1,2,6,7]] => 21, [2,3,5,4,7], root 5 use both left and right single path
+    // [[5][-3,-4][1,2,6,7]] => 9, [6,-4,7], root 5 don't use left or right single path, arch in root 6 as max
 }
