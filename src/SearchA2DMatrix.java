b1a8398e7e6f81519727382ca9b7b331b27f8b75
yao <myspiritcrazy@gmail.com>
2017-06-16 22:48:38 -0700

    add test cases

--- src/SearchA2DMatrix.java
+++ src/SearchA2DMatrix.java
@@ -3,6 +3,14 @@
  */
 public class SearchA2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-
+        return true;
     }
+
+    // [
+    //    [1,   3,  5,  7],
+    //    [10, 11, 16, 20],
+    //    [23, 30, 34, 50]
+    //  ]
+
+    // Given target = 3, return true.
 }
