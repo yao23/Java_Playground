2f9bee8c20a0e962cf84e43315c348614a37a234
yao <myspiritcrazy@gmail.com>
2017-06-18 15:26:14 -0700

    add test cases

--- src/BinaryTreeMaximumPathSum.java
+++ src/BinaryTreeMaximumPathSum.java
@@ -22,6 +22,9 @@ public class BinaryTreeMaximumPathSum {
         helper(root, 0, result);
         return result[0];
     }
-    // [[5][3,4][1,2,6,7]] => 21, [2,3,5,4,7], root 5 use both left and right single path
-    // [[5][-3,-4][1,2,6,7]] => 9, [6,-4,7], root 5 don't use left or right single path, arch in root 6 as max
+    // [] => -2147483648
+    // [-3] => -3
+    // [1,2,3] => 6
+    // [5,3,4,1,2,6,7] => 21, [2,3,5,4,7], root 5 use both left and right single path
+    // [5,-3,-4,1,2,6,7] => 9, [6,-4,7], root 5 don't use left or right single path, arch in root 6 as max
 }
