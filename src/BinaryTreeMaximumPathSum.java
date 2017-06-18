6bbf0ac2eddb2033107e8dd327346de5071fa0cf
yao <myspiritcrazy@gmail.com>
2017-06-18 15:17:53 -0700

    Binary Tree Maximum Path Sum

--- src/BinaryTreeMaximumPathSum.java
+++ src/BinaryTreeMaximumPathSum.java
@@ -2,7 +2,26 @@
  * Created by liyao on 6/14/17.
  */
 public class BinaryTreeMaximumPathSum {
+    private int helper(TreeNode root, int curSum, int[] result) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = helper(root.left, curSum + root.val, result);
+            int right = helper(root.right, curSum + root.val, result);
+            int single = Math.max(root.val, root.val + Math.max(left,right));
+            int max = Math.max(single, root.val + left + right);
+            if (max > result[0]) {
+                result[0] = max;
+            }
+            return single;
+        }
+    }
 
+    public int maxPathSum(TreeNode root) {
+        int[] result = new int[]{0};
+        helper(root, 0, result);
+        return result[0];
+    }
     // [[5][3,4][1,2,6,7]] => 21, [2,3,5,4,7], root 5 use both left and right single path
     // [[5][-3,-4][1,2,6,7]] => 9, [6,-4,7], root 5 don't use left or right single path, arch in root 6 as max
 }
