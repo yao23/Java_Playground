32c09bca27e2ec59b727c674e789b302a55a3198
yao <myspiritcrazy@gmail.com>
2017-06-18 11:22:51 -0700

    optimal solution and solution distribution

--- src/SumRootToLeafNumbers.java
+++ src/SumRootToLeafNumbers.java
@@ -6,6 +6,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SumRootToLeafNumbers {
+    private void helperV1(TreeNode root, StringBuilder tmpResult, List<Integer> result) { // beats 9.66%
+        tmpResult.append(root.val);
+        if (root.left == null && root.right == null) {
+            result.add(Integer.valueOf(tmpResult.toString()));
+        } else {
+            if (root.left != null) {
+                helperV1(root.left, tmpResult, result);
+                tmpResult.deleteCharAt(tmpResult.length() - 1);
+            }
+            if (root.right != null) {
+                helperV1(root.right, tmpResult, result);
+                tmpResult.deleteCharAt(tmpResult.length() - 1);
+            }
+        }
+    }
+
     private void helper(TreeNode root, List<TreeNode> tmpResult, List<Integer> result) {
         tmpResult.add(root);
         if (root.left == null && root.right == null) {
