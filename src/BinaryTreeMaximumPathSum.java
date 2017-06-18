987da1d87f3071a3deaed87384520ed92ff9aca7
yao <myspiritcrazy@gmail.com>
2017-06-18 15:26:44 -0700

    solution distribution

--- src/BinaryTreeMaximumPathSum.java
+++ src/BinaryTreeMaximumPathSum.java
@@ -27,4 +27,6 @@ public class BinaryTreeMaximumPathSum {
     // [1,2,3] => 6
     // [5,3,4,1,2,6,7] => 21, [2,3,5,4,7], root 5 use both left and right single path
     // [5,-3,-4,1,2,6,7] => 9, [6,-4,7], root 5 don't use left or right single path, arch in root 6 as max
+
+    // beats 48.00%
 }
