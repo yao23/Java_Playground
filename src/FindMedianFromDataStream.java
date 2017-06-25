a7642b25f539235ab142dfdd864ebdf55d4d36a1
yao <myspiritcrazy@gmail.com>
2017-06-25 09:53:20 -0700

    Find Median from Data Stream

---
+++ src/FindMedianFromDataStream.java
@@ -0,0 +1,24 @@
+/**
+ * Created by liyao on 6/25/17.
+ */
+public class FindMedianFromDataStream { // class MedianFinder in LC
+    /** initialize your data structure here. */
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
