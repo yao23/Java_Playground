edcbd4d22a83b623fc52708a07d8320d632343a9
yao <myspiritcrazy@gmail.com>
2017-06-25 12:11:59 -0700

    refactor code to use heap instead of stack

--- src/FindMedianFromDataStream.java
+++ src/FindMedianFromDataStream.java
@@ -3,61 +3,60 @@ import java.util.ArrayDeque;
 /**
  * Created by liyao on 6/25/17.
  */
-import java.util.Deque;
-import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 
 public class FindMedianFromDataStream {
     public class MedianFinder { // class MedianFinder in LC
-        private Deque<Integer> minStack; // right half
-        private Deque<Integer> maxStack; // left half
+        private PriorityQueue<Integer> minQueue; // right half
+        private PriorityQueue<Integer> maxQueue; // left half
 
         /** initialize your data structure here. */
         public MedianFinder() {
-            minStack = new ArrayDeque<>();
-            maxStack = new ArrayDeque<>();
+            minQueue = new PriorityQueue<>();
+            maxQueue = new PriorityQueue<>();
         }
 
         public void addNum(int num) {
-            int minLen = minStack.size();
-            int maxLen = maxStack.size();
+            int minLen = minQueue.size();
+            int maxLen = maxQueue.size();
 
-            if (minLen == 0 && maxLen == 0) { // maxStack and minStack have no num
-                maxStack.push(num);
-            } else if (minLen == 0) { // maxStack has 1 num
-                if (num >= maxStack.peek()) {
-                    minStack.push(num);
+            if (minLen == 0 && maxLen == 0) { // maxQueue and minQueue have no num
+                maxQueue.offer(num);
+            } else if (minLen == 0) { // maxQueue has 1 num
+                if (num >= maxQueue.peek()) {
+                    minQueue.offer(num);
                 } else {
-                    minStack.push(maxStack.peek());
-                    maxStack.push(num);
+                    minQueue.offer(maxQueue.peek());
+                    maxQueue.offer(num);
                 }
-            } else if (maxLen == 0) { // minStack has 1 num
-                if (num <= minStack.peek()) {
-                    maxStack.push(num);
+            } else if (maxLen == 0) { // minQueue has 1 num
+                if (num <= minQueue.peek()) {
+                    maxQueue.offer(num);
                 } else {
-                    maxStack.push(minStack.pop());
-                    minStack.push(num);
+                    maxQueue.offer(minQueue.poll());
+                    minQueue.offer(num);
                 }
-            } else { // maxStack and minStack have more than 1 num
+            } else { // maxQueue and minQueue have more than 1 num
                 if (minLen == maxLen) { // min and max stack sizes are same
-                    if (num > maxStack.peek()) {
-                        minStack.push(num);
+                    if (num > maxQueue.peek()) {
+                        minQueue.offer(num);
                     } else {
-                        maxStack.push(num);
+                        maxQueue.offer(num);
                     }
                 } else { // min and max stack sizes are different
-                    if (minLen < maxLen) { // maxStack has more
-                        if (num > maxStack.peek()) {
-                            minStack.push(num);
+                    if (minLen < maxLen) { // maxQueue has more
+                        if (num > maxQueue.peek()) {
+                            minQueue.offer(num);
                         } else {
-                            maxStack.push(num);
-                            minStack.push(maxStack.pop()); // update to make "balance" (diff as 0 or 1) for 2 stacks
+                            maxQueue.offer(num);
+                            minQueue.offer(maxQueue.poll()); // update to make "balance" (diff as 0 or 1) for 2 stacks
                         }
-                    } else { // minStack has more
-                        if (num > maxStack.peek()) {
-                            minStack.push(num);
-                            maxStack.push(minStack.pop()); // update to make "balance" (diff as 0 or 1) for 2 stacks
+                    } else { // minQueue has more
+                        if (num > maxQueue.peek()) {
+                            minQueue.offer(num);
+                            maxQueue.offer(minQueue.poll()); // update to make "balance" (diff as 0 or 1) for 2 stacks
                         } else {
-                            maxStack.push(num);
+                            maxQueue.offer(num);
                         }
                     }
                 }
@@ -65,16 +64,17 @@ public class FindMedianFromDataStream {
         }
 
         public double findMedian() {
-            int minLen = minStack.size();
-            int maxLen = maxStack.size();
+            int minLen = minQueue.size();
+            int maxLen = maxQueue.size();
 
             if (minLen == maxLen) {
-                return (minStack.peek() + maxStack.peek()) >>> 1;
+                int minHead = minQueue.peek(), maxHead = maxQueue.peek();
+                return (maxHead + (minHead - maxHead) / 2.00); // need double, cannot use >>>
             } else {
                 if (minLen < maxLen) {
-                    return maxStack.peek();
+                    return maxQueue.peek();
                 } else {
-                    return minStack.peek();
+                    return minQueue.peek();
                 }
             }
         }
