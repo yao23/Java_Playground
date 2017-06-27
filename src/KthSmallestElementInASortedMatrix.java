36b075498f5390cb65396fdbd10eeb35a582fdd2
yao <myspiritcrazy@gmail.com>
2017-06-26 22:55:55 -0700

    fix bug to convert 1D index to 2D indices, add test cases

--- src/KthSmallestElementInASortedMatrix.java
+++ src/KthSmallestElementInASortedMatrix.java
@@ -12,9 +12,17 @@ public class KthSmallestElementInASortedMatrix {
             return 0;
         }
         if (k <= row * col) {
-            return matrix[k / col][k % col - 1];
+            return matrix[(k - 1) / col][(k - 1) % col]; // 1D index to 2D indices
         } else {
             return matrix[row - 1][col - 1];
         }
     }
+
+    // [[]] => 0
+    // [[1]], 1 => 1
+    // [[1,5],[10,11]],1 => 1
+    // [[1,5],[10,11]],2 => 5
+    // [[1,5],[10,11]],3 => 10
+    // [[1,5],[10,11]],4 => 11
+    // [[1,5,9],[10,11,13],[12,13,15]],8 => 13
 }
