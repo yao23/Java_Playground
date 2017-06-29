c2927b3f21292810963902632a45788976cd7650
yao <myspiritcrazy@gmail.com>
2017-06-28 23:07:26 -0700

    refactor code to use min heap and helper class

--- src/SuperUglyNumber.java
+++ src/SuperUglyNumber.java
@@ -2,6 +2,7 @@
  * Created by liyao on 6/28/17.
  */
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class SuperUglyNumber {
     public static int nthSuperUglyNumberI(int n, int[] primes) { // 39ms, beats 32.59%
@@ -49,6 +50,43 @@ public class SuperUglyNumber {
         }
 
         return ugly[n - 1];
+    }
+
+    public int nthSuperUglyNumberHeapV2(int n, int[] primes) {
+        int[] ugly = new int[n];
+
+        PriorityQueue<Num> pq = new PriorityQueue<>();
+        for (int i = 0; i < primes.length; i++) {
+            pq.add(new Num(primes[i], 1, primes[i]));
+        }
+        ugly[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            ugly[i] = pq.peek().val;
+            while (pq.peek().val == ugly[i]) {
+                Num nxt = pq.poll();
+                pq.add(new Num(nxt.p * ugly[nxt.idx], nxt.idx + 1, nxt.p));
+            }
+        }
+
+        return ugly[n - 1];
+    }
+
+    private class Num implements Comparable<Num> {
+        int val; // current smallest prime
+        int idx;
+        int p; // input prime num
+
+        public Num(int val, int idx, int p) {
+            this.val = val;
+            this.idx = idx;
+            this.p = p;
+        }
+
+        @Override
+        public int compareTo(Num that) {
+            return this.val - that.val;
+        }
     }
 
     public static void main(String[] args) {
