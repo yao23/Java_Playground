d42908fc934fdebcaf915b37cc4cd672c9c9e465
yao <myspiritcrazy@gmail.com>
2017-07-01 12:40:57 -0700

    refactor code for solution with heap

--- src/MeetingRoomsII.java
+++ src/MeetingRoomsII.java
@@ -6,28 +6,28 @@ import java.util.*;
 
 public class MeetingRoomsII {
     public int minMeetingRooms(Interval[] intervals) {
-        if(intervals==null||intervals.length==0)
+        if (intervals == null || intervals.length == 0) {
             return 0;
+        }
 
-        Arrays.sort(intervals, new Comparator<Interval>(){
-            public int compare(Interval i1, Interval i2){
-                return i1.start-i2.start;
+        Arrays.sort(intervals, new Comparator<Interval>() {
+            public int compare(Interval i1, Interval i2) {
+                return i1.start - i2.start;
             }
         });
 
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
-        int count=1;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        int count = 1;
         queue.offer(intervals[0].end);
 
-        for(int i=1; i<intervals.length; i++){
-            if(intervals[i].start<queue.peek()){
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < queue.peek()) {
                 count++;
-
-            }else{
+            } else {
                 queue.poll();
             }
 
-            queue.offer(intervals[i].end);
+            queue.offer(intervals[i].end); // append end time for next update
         }
 
         return count;
