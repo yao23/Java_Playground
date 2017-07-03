9daa65cc9642ed0d97fd9225c2ac0f68b511c569
yao <myspiritcrazy@gmail.com>
2017-07-03 10:50:45 -0700

    refactor code

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -8,13 +8,11 @@ public class WordBreak {
         return check(s, wordDict);
     }
 
-    private boolean check(String s, List<String> wordDict) {
-        System.out.println("str: " + s);
+    private boolean check(String s, List<String> wordDict) { // Time Limit Exceeded for test case 5
         if (s.equals("")) {
             return true;
         } else {
             for (int i = 0; i < s.length(); i++) {
-                System.out.println("substrs: " + s.substring(0, i + 1) + ", " + s.substring(i + 1));
                 if (wordDict.contains(s.substring(0, i + 1))) {
                     if (check(s.substring(i + 1), wordDict)) {
                         return true;
