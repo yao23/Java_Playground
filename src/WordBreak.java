f28aaced9e45c6fe9144d71c1c096cc4b9f34e34
yao <myspiritcrazy@gmail.com>
2017-07-03 12:04:52 -0700

    refactor code to improve algorithm with DP

--- src/WordBreak.java
+++ src/WordBreak.java
@@ -1,16 +1,39 @@
 /**
  * Created by liyao on 7/3/17.
  */
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return check(s, wordDict, new HashMap<String, Boolean>());
+        return check(s, wordDict);
     }
 
-    private boolean check(String s, List<String> wordDict, Map<String, Boolean> map) { // Time Limit Exceeded for test case 5
+    private boolean check(String s, List<String> wordDict) {
+        if (s.equals("")) {
+            return true;
+        } else {
+            boolean[] result = new boolean[s.length() + 1];
+            Arrays.fill(result, false);
+            result[0] = true;
+
+            // result means segmentation from 0 and length is i can be segmented into dictionary words
+            for (int len = 1; len <= s.length(); len++) {
+                for (int i = 0; i < len; i++) {
+                    String rightStr = s.substring(i, len);
+                    if (result[i] && wordDict.contains(rightStr)) {
+                        result[len] = true;
+                        break;
+                    }
+                }
+            }
+
+            return result[s.length()];
+        }
+    }
+
+    private boolean checkV1(String s, List<String> wordDict, Map<String, Boolean> map) { // Time Limit Exceeded for test case 5
         if (s.equals("")) {
             return true;
         } else {
@@ -26,7 +49,7 @@ public class WordBreak {
                             continue;
                         }
                     } else { // miss the cache
-                        if (check(rightStr, wordDict, map)) {
+                        if (checkV1(rightStr, wordDict, map)) {
                             return true;
                         } else {
                             continue;
