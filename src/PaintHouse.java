7c55ef329dd1f6388682a754a4ef44840d4440c4
yao <myspiritcrazy@gmail.com>
2017-07-03 22:55:40 -0700

    Paint House

---
+++ src/PaintHouse.java
@@ -0,0 +1,20 @@
+/**
+ * Created by liyao on 7/3/17.
+ */
+public class PaintHouse {
+    public int minCost(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        } else {
+            int row = costs.length;
+
+            for (int i = 1; i < row; i++) {
+                costs[i][0] = Math.min(costs[i - 1][1], costs[i - 1][2]);
+                costs[i][1] = Math.min(costs[i - 1][0], costs[i - 1][2]);
+                costs[i][2] = Math.min(costs[i - 1][0], costs[i - 1][1]);
+            }
+
+            return Math.min(costs[row - 1][0], Math.min(costs[row - 1][1], costs[row - 1][2]));
+        }
+    }
+}
