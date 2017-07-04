d523de1b3a67169c92bd2b963cfa39f8893e1e86
yao <myspiritcrazy@gmail.com>
2017-07-03 20:54:28 -0700

    0 1 matrix

---
+++ src/ZeroOneMatrix.java
@@ -0,0 +1,48 @@
+/**
+ * Created by liyao on 7/3/17.
+ */
+public class ZeroOneMatrix {
+    public int[][] updateMatrix(int[][] matrix) {
+        int row = matrix.length;
+        if (row == 0) {
+            return new int[][]{};
+        } else {
+            int col = matrix[0].length;
+            int[][] results = new int[row][col];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    results[i][j] = search(i, j, matrix, results);
+                }
+            }
+
+            return results;
+        }
+    }
+
+    private int search(int x, int y, int[][] matrix, int[][] results) {
+        int row = matrix.length, col = matrix[0].length;
+
+        if (x < 0 || x >= row || y < 0 || y >= col) { // invalid index
+            return -1;
+        } else { // valid index
+            if (matrix[x][y] == 0) {
+                return 0;
+            } else {
+                int[] neighbors = new int[4];
+                neighbors[0] = search(x - 1, y, matrix, results); // up
+                neighbors[1] = search(x + 1, y, matrix, results); // down
+                neighbors[2] = search(x, y - 1, matrix, results); // left
+                neighbors[3] = search(x, y + 1, matrix, results); // right
+
+                int min = Integer.MAX_VALUE;
+                for (int i = 0; i < neighbors.length; i++) {
+                    if (neighbors[i] >= 0 && neighbors[i] < min) {
+                        min = neighbors[i];
+                    }
+                }
+
+                return 1 + min;
+            }
+        }
+    }
+}
