053c7add1fcef9a1b5be8ebc4a3ea39b86df7961
yao <myspiritcrazy@gmail.com>
2017-07-06 00:17:11 -0700

    House Robber II

---
+++ src/HouseRobberII.java
@@ -0,0 +1,26 @@
+/**
+ * Created by liyao on 7/5/17.
+ */
+public class HouseRobberII {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        } else if (len == 1) {
+            return nums[0];
+        } else {
+            return Math.max(robRange(nums, 0, len - 2), robRange(nums, 1, len - 1));
+        }
+    }
+
+    private int robRange(int[]nums, int start, int end) {
+        int preNo = 0, preYes = 0;
+        for (int i = start; i <= end; i++) {
+            int tmp = preNo;
+            preNo = Math.max(preNo, preYes);
+            preYes = nums[i] + tmp;
+        }
+
+        return Math.max(preNo, preYes);
+    }
+}
