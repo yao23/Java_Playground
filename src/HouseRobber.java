a54cec57500f25893c326ddab9a67eba0f77a4d1
yao <myspiritcrazy@gmail.com>
2017-07-05 22:57:13 -0700

    House Robber

---
+++ src/HouseRobber.java
@@ -0,0 +1,24 @@
+/**
+ * Created by liyao on 7/5/17.
+ */
+public class HouseRobber {
+    public int rob(int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        } else {
+            int[] results = new int[len];
+            for (int i = 0; i < len; i++) {
+                int max = 0;
+                for (int j = 0; j < i - 1; j++) {
+                    if (nums[j] > max) {
+                        max = nums[j];
+                    }
+                }
+                results[i] = (nums[i] + max);
+            }
+
+            return results[len - 1];
+        }
+    }
+}
