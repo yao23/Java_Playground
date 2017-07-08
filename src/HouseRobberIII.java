c93f770f7b63fdda25e8efcacdd3a1b9ad607eec
yao <myspiritcrazy@gmail.com>
2017-07-07 23:43:15 -0700

    House Robber III

--- src/HouseRobberIII.java
+++ src/HouseRobberIII.java
@@ -1,7 +1,22 @@
-package PACKAGE_NAME;
-
 /**
  * Created by liyao on 7/6/17.
  */
 public class HouseRobberIII {
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int val = root.val; // rob root level
+            if (root.left != null) {
+                val += (rob(root.left.left) + rob(root.left.right));
+            }
+            if (root.right != null) {
+                val += (rob(root.right.left) + rob(root.right.right));
+            }
+
+            int childrenLevel = rob(root.left) + rob(root.right); // not rob root level
+
+            return Math.max(val, childrenLevel);
+        }
+    }
 }
