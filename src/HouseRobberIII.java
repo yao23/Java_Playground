353350b6582859173f088187d609c47d46156811
yao <myspiritcrazy@gmail.com>
2017-07-07 23:51:01 -0700

    cache previous results and memorized search

--- src/HouseRobberIII.java
+++ src/HouseRobberIII.java
@@ -1,6 +1,8 @@
 /**
  * Created by liyao on 7/6/17.
  */
+import java.util.Map;
+
 public class HouseRobberIII {
     public int rob(TreeNode root) {
         if (root == null) {
@@ -17,6 +19,28 @@ public class HouseRobberIII {
             int childrenLevel = rob(root.left) + rob(root.right); // not rob root level
 
             return Math.max(val, childrenLevel);
+        }
+    }
+
+    private int robV1(TreeNode root, Map<TreeNode, Integer> preRes) { // memorized search
+        if (root == null) {
+            return 0;
+        } else {
+            if (preRes.containsKey(root)) {
+                return preRes.get(root);
+            } else {
+                int val = root.val;
+                if (root.left != null) {
+                    val += (robV1(root.left.left, preRes) + robV1(root.left.right, preRes));
+                }
+                if (root.right != null) {
+                    val += (robV1(root.right.left, preRes) + robV1(root.right.right, preRes));
+                }
+                int childrenLevel = (robV1(root.left, preRes) + robV1(root.right, preRes));
+                val = Math.max(val, childrenLevel);
+                preRes.put(root, val);
+                return val;
+            }
         }
     }
 }
