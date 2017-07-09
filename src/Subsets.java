764100f814b5a1832d0d4ba006234798c73dcc19
yao <myspiritcrazy@gmail.com>
2017-07-08 17:05:39 -0700

    Subsets with bit operation

--- src/Subsets.java
+++ src/Subsets.java
@@ -27,6 +27,34 @@ public class Subsets {
         }
     }
 
+    public static List<List<Integer>> subsetsV1(int[] nums) { // beats 5.98%
+        List<Integer> tmpRes = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < Math.pow(2, nums.length); i++) {
+            int numIdx = 0, resIdx = i;
+            while (resIdx > 0) {
+                if (resIdx % 2 == 1) {
+                    tmpRes.add(nums[numIdx]);
+                }
+                resIdx >>= 1; // right shift, divided by 2 (bit operation)
+                numIdx++;
+            }
+            result.add(new ArrayList<>(tmpRes));
+            tmpRes.clear();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3};
+        List<List<Integer>> subsets = subsetsV1(nums);
+        for (List<Integer> subset : subsets) {
+            System.out.println(subset);
+        }
+    }
+
+
     // [1,2,3] => [[],[1],[1,2],[1,2,3],[1,3],[2],[2,3],[3]]
 
     // beats 28.38%
