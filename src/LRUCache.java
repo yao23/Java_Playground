40e9f1b244589d77ef909e1d191dddbbdd2aa6fa
yao <myspiritcrazy@gmail.com>
2017-07-11 23:49:09 -0700

    add test cases

--- src/LRUCache.java
+++ src/LRUCache.java
@@ -82,6 +82,8 @@ public class LRUCache {
         this.head.next = node;
         node.next.prev = node;
     }
+
+    // ["LRUCache","put","put","get","put","get","put","get","get","get"], [[2],[1,1],[2,2],[1],[3,3],[2],[4,4],[1],[3],[4]] => [null,null,null,1,null,-1,null,-1,3,4]
 }
 
 /**
