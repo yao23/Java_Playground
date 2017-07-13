c0cd41ffabdb661895506006ab2aa8e8dfd26386
yao <myspiritcrazy@gmail.com>
2017-07-12 22:52:44 -0700

    Minimum Depth of Binary Tree

---
+++ src/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+/**
+ * Created by liyao on 7/12/17.
+ */
+public class MinimumDepthOfBinaryTree {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            if (root.left == null && root.right == null) {
+                return 1;
+            } else if (root.left != null && root.right != null) {
+                return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+            } else {
+                return 1 + ((root.left != null) ? minDepth(root.left) : minDepth(root.right));
+            }
+        }
+    }
+
+    // [] => 0
+    // [1,2] => 2
+    // [1,3,4,null,null,5,6] => 2
+
+    // beats 16.35%
+}
