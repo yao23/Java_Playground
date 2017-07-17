c02ff1a58965915815168fbc930abefc6399334b
yao <myspiritcrazy@gmail.com>
2017-07-16 23:33:07 -0700

    fix bugs and solution distribution for Paint House

--- src/PaintHouse.java
+++ src/PaintHouse.java
@@ -9,12 +9,14 @@ public class PaintHouse {
             int row = costs.length;
 
             for (int i = 1; i < row; i++) {
-                costs[i][0] = Math.min(costs[i - 1][1], costs[i - 1][2]);
-                costs[i][1] = Math.min(costs[i - 1][0], costs[i - 1][2]);
-                costs[i][2] = Math.min(costs[i - 1][0], costs[i - 1][1]);
+                costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+                costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+                costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
             }
 
             return Math.min(costs[row - 1][0], Math.min(costs[row - 1][1], costs[row - 1][2]));
         }
     }
+
+    // beats 51.97%
 }
