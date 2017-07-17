0be8bb8295f7829d290ab39dd047620cb2d7a2be
yao <myspiritcrazy@gmail.com>
2017-07-16 22:39:55 -0700

    Refactor code and solution distribution for Best Time to Buy and Sell Stock II

--- src/BestTimetoBuyandSellStock2.java
+++ src/BestTimetoBuyandSellStock2.java
@@ -1,11 +1,14 @@
 
 public class BestTimetoBuyandSellStock2 {
 	public static int maxProfit(int[] prices) {
-		if( prices.length == 0 ) return 0;
+		if (prices.length == 0) {
+			return 0;
+		}
 		int profit = 0;
-		for( int i = 1; i < prices.length; i++ ) {
-			if( prices[i-1] < prices[i] )
-				profit += (prices[i] - prices[i-1]); 
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i - 1] < prices[i]) {
+				profit += (prices[i] - prices[i - 1]);
+			}
 		}
 		return profit;
 	}
@@ -14,4 +17,6 @@ public class BestTimetoBuyandSellStock2 {
 		int[] prices = {1, 5, 3, 2, 6};
 		System.out.println("Max Profit: " + maxProfit(prices));		
 	}
+
+	// beats 61.89%
 }
