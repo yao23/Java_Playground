e6eecb421e4670533ba208a5063533b6fab9207e
yao <myspiritcrazy@gmail.com>
2017-07-16 18:17:11 -0700

    TicTacToe

---
+++ src/TicTacToe.java
@@ -0,0 +1,64 @@
+/**
+ * Created by liyao on 7/16/17.
+ */
+public class TicTacToe {
+    private int n;
+    private int[] rows;
+    private int[] cols;
+    private int diagonal;
+    private int antiDiagonal;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public TicTacToe(int n) {
+        rows = new int[n];
+        cols = new int[n];
+        diagonal = 0;
+        antiDiagonal = 0;
+        this.n = n;
+    }
+
+    /**
+     * Player {player} makes a move at ({row}, {col}).
+     *
+     * @param row    The row of the board.
+     * @param col    The column of the board.
+     * @param player The player, can be either 1 or 2.
+     * @return The current winning condition, can be either:
+     * 0: No one wins.
+     * 1: Player 1 wins.
+     * 2: Player 2 wins.
+     */
+    public int move(int row, int col, int player) {
+        if (player == 1) {
+            if (row == col) {
+                diagonal++;
+            } else if (row + col == n - 1) {
+                antiDiagonal++;
+            } else {
+                rows[row]++;
+                cols[col]++;
+            }
+            if (rows[row] == n || cols[col] == n || diagonal == n || antiDiagonal == n) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (row == col) {
+                diagonal--;
+            } else if (row + col == n - 1) {
+                antiDiagonal--;
+            } else {
+                rows[row]--;
+                cols[col]--;
+            }
+            if (rows[row] == -n || cols[col] == -n || diagonal == -n || antiDiagonal == -n) {
+                return 2;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
