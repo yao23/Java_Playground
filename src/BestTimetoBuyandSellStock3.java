693e1d5348f2a8133a62a8faf87fbae3f85d7e51
yao <myspiritcrazy@gmail.com>
2017-07-16 22:42:55 -0700

    Refactor code and solution distribution for Best Time to Buy and Sell Stock III

--- src/BestTimetoBuyandSellStock3.java
+++ src/BestTimetoBuyandSellStock3.java
@@ -1,31 +1,37 @@
 
 public class BestTimetoBuyandSellStock3 {
 	public static int maxProfit(int[] prices) {
-		if( prices.length == 0 ) return 0;
+		if (prices.length == 0) {
+			return 0;
+		}
 		int profit = 0;
 		int[] profits = new int[prices.length];
 		int MaxUntilNow = Integer.MIN_VALUE;
 		int MinPrice = Integer.MAX_VALUE;
 		int MaxPrice = Integer.MIN_VALUE;
-		for( int i = 0; i < prices.length; i++ ) {
-			if( prices[i] < MinPrice )
+		for (int i = 0; i < prices.length; i++) {
+			if (prices[i] < MinPrice) {
 				MinPrice = prices[i];
-			if( prices[i] - MinPrice > MaxUntilNow )
+			}
+			if (prices[i] - MinPrice > MaxUntilNow) {
 				MaxUntilNow = prices[i] - MinPrice;
+			}
 			profits[i] = MaxUntilNow;
 		}	
 		
-		for( int i = 0; i < prices.length; i++ ) {
+		for (int i = 0; i < prices.length; i++) {
 			System.out.print(profits[i] + " ");
 		}
 		
 		System.out.println();
 		
-		for( int i = prices.length-1; i >= 0; i-- ) {
-			if( prices[i] > MaxPrice )
+		for (int i = prices.length-1; i >= 0; i--) {
+			if (prices[i] > MaxPrice) {
 				MaxPrice = prices[i];
-			if( MaxPrice - prices[i] + profits[i] > profit )
+			}
+			if (MaxPrice - prices[i] + profits[i] > profit) {
 				profit = MaxPrice - prices[i] + profits[i];
+			}
 		}
 		return profit;
 	}
@@ -34,4 +40,6 @@ public class BestTimetoBuyandSellStock3 {
 		int[] prices = {1, 5, 3, 2, 6};
 		System.out.println("Max Profit: " + maxProfit(prices));		
 	}
+
+	// beats 98.87%
 }
