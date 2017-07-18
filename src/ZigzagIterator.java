88b798125177849e1ae620874d4a3d27f78de512
yao <myspiritcrazy@gmail.com>
2017-07-17 19:25:51 -0700

    Zigzag Iterator

---
+++ src/ZigzagIterator.java
@@ -0,0 +1,64 @@
+/**
+ * Created by liyao on 7/17/17.
+ */
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ZigzagIterator {
+    private Iterator<Integer> iterator1;
+    private Iterator<Integer> iterator2;
+    private int mode;
+
+    public ZigzagIterator(List<Integer> v1, List<Integer> v2) {
+        iterator1 = v1.iterator();
+        iterator2 = v2.iterator();
+        if (v1 != null && v1.size() > 0) {
+            mode = 1;
+        } else {
+            if (v2 != null && v2.size() > 0) {
+                mode = 2;
+            } else {
+                mode = 0;
+            }
+        }
+    }
+
+    public int next() {
+        if (mode == 0) {
+            return Integer.MIN_VALUE;
+        } else if (mode == 1) { // list 1
+            if (iterator1 != null && iterator1.hasNext()) {
+                int result = iterator1.next();
+                mode = 2;
+                return result;
+            } else {
+                if (iterator2 != null && iterator2.hasNext()) {
+                    int result = iterator2.next();
+                    mode = 2;
+                    return result;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+        } else { // list 2
+            if (iterator2 != null && iterator2.hasNext()) {
+                int result = iterator2.next();
+                mode = 1;
+                return result;
+            } else {
+                if (iterator1 != null && iterator1.hasNext()) {
+                    int result = iterator1.next();
+                    mode = 1;
+                    return result;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+        }
+    }
+
+    public boolean hasNext() {
+        return iterator1.hasNext() || iterator2.hasNext();
+    }
+}
