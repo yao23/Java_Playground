7b9ed0ebd8101569fccf750ac9cbb623f62e320c
yao <myspiritcrazy@gmail.com>
2017-07-17 21:43:16 -0700

    Flatten Nested List Iterator

---
+++ src/FlattenNestedListIterator.java
@@ -0,0 +1,113 @@
+import java.util.*;
+
+/**
+ * Created by liyao on 7/17/17.
+ */
+public class FlattenNestedListIterator {
+    /**
+     * // This is the interface that allows for creating nested lists.
+     * // You should not implement it, or speculate about its implementation
+     * public interface NestedInteger {
+     *
+     *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+     *     public boolean isInteger();
+     *
+     *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+     *     // Return null if this NestedInteger holds a nested list
+     *     public Integer getInteger();
+     *
+     *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+     *     // Return null if this NestedInteger holds a single integer
+     *     public List<NestedInteger> getList();
+     * }
+     */
+    class NestedIterator implements Iterator<Integer> {
+        private Iterator<NestedInteger> iterator;
+        private Deque<Iterator<NestedInteger>> parentIterators;
+
+        public NestedIterator(List<NestedInteger> nestedList) {
+            iterator = nestedList.iterator();
+            parentIterators = new ArrayDeque<>();
+        }
+
+        @Override
+        public Integer next() {
+            NestedInteger cur = iterator.next();
+            if (cur.isInteger()) {
+                return cur.getInteger();
+            } else {
+                boolean isInt = false;
+                while (cur == null || ((List)cur).size() == 0 && hasNext()) {
+                    cur = iterator.next();
+                    if (cur.isInteger()) {
+                        isInt = true;
+                        break;
+                    }
+                }
+
+                if (isInt) {
+                    return cur.getInteger();
+                } else {
+                    if (hasNext()) {
+                        return getNext(cur.getList());
+                    } else {
+                        return Integer.MIN_VALUE;
+                    }
+                }
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext() || !parentIterators.isEmpty();
+        }
+
+        @Override
+        public void remove() {
+
+        }
+
+        private int getNext(List<NestedInteger> cur) {
+            Iterator<NestedInteger> tmp = cur.iterator();
+
+            while (!tmp.next().isInteger() && hasNext()) {
+                if (tmp == null) {
+                    return Integer.MIN_VALUE;
+                } else {
+                    NestedInteger tmpList = tmp.next();
+                    if (((List)tmpList).size() == 0) {
+                        tmp.next();
+                    } else {
+                        Iterator<NestedInteger> parent = iterator;
+                        parentIterators.push(parent);
+                        iterator = tmp;
+                        tmp.next();
+                    }
+                }
+            }
+
+            if (tmp.next().isInteger()) {
+                return tmp.next().getInteger();
+            } else {
+                return Integer.MIN_VALUE;
+            }
+        }
+
+
+    }
+
+    /**
+     * Your NestedIterator object will be instantiated and called as such:
+     * NestedIterator i = new NestedIterator(nestedList);
+     * while (i.hasNext()) v[f()] = i.next();
+     */
+}
+
+
+interface NestedInteger {
+    public boolean isInteger();
+
+    public Integer getInteger();
+
+    public List<NestedInteger> getList();
+}
