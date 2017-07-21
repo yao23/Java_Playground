01453755207ea2df69fe64c6cfd3b11d4550dbf7
yao <myspiritcrazy@gmail.com>
2017-07-20 23:33:25 -0700

    First Missing Positive

---
+++ src/FirstMissingPositive.java
@@ -0,0 +1,23 @@
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, nums[i], nums[nums[i] - 1]);
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    private void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
