89e5ec28690607a8d97521a5af4797efa0ad5517
yao <myspiritcrazy@gmail.com>
2017-07-21 23:38:52 -0700

    add test cases and fix bugs

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -1,8 +1,12 @@
 public class FirstMissingPositive {
     public int firstMissingPositive(int[] nums) {
+        if (nums.length == 0) {
+            return 1;
+        }
+
         for (int i = 0; i < nums.length; i++) {
             while (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]) {
-                swap(nums, nums[i], nums[nums[i] - 1]);
+                swap(nums, i, nums[i] - 1); // fix bug for test case 2, pass in index, not value
             }
         }
 
@@ -20,4 +24,8 @@ public class FirstMissingPositive {
         nums[left] = nums[right];
         nums[right] = tmp;
     }
+
+    // [] => 1
+    // [1,2,0] => 3
+    // [3,4,-1,1] => 2
 }
