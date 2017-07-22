aec98e4b0cb7bb9de09f5cb7907ebe16fb422d1e
yao <myspiritcrazy@gmail.com>
2017-07-22 15:56:00 -0700

    Longest Substring with At Most Two Distinct Characters

---
+++ src/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,36 @@
+public class LongestSubstringWithAtMostTwoDistinctCharacters {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int k = 2, left = 0, num = 0, res = 0;
+        int[] count = new int[256];
+        for (int right = 0; right < s.length(); right++) {
+            // update cur char
+            char rightChar = s.charAt(right);
+            if (count[rightChar] == 0) {
+                num++;
+            }
+            count[rightChar] += 1;
+
+            // process previous chars if more than k distinct characters
+            if (num > k) {
+                count[s.charAt(left)] -= 1;
+                while (count[s.charAt(left)] > 0) {
+                    left++;
+                    count[s.charAt(left)] -= 1;
+                }
+                left++;
+                num--;
+            }
+
+            int len = right - left + 1;
+            if (len > res) {
+                res = len;
+            }
+        }
+
+        return res;
+    }
+
+    // "eceba", 2 => 3 ("ece")
+
+    // beats 79.44%
+}
