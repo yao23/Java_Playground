04cecd5e69af489cfd7376385f9ee785c3ddb9f0
yao <myspiritcrazy@gmail.com>
2017-07-22 09:19:36 -0700

    Sort Transformed Array

--- src/SortTransformedArray.java
+++ src/SortTransformedArray.java
@@ -1,7 +1,41 @@
 public class SortTransformedArray {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
-        int[] results = new int[nums.length];
-
-        return results;
+        int len = nums.length, left = 0, right = len - 1;
+        int[] results = new int[];
+        if (len == 0) {
+            return results;
+        } else if (len == 1) {
+            results[0] = func(nums[0], a, b, c);
+            return results;
+        } else {
+            if (a > 0) { // mid low, left and right high
+                int cur = len - 1;
+                while (left <= right) {
+                    int leftVal = func(nums[left], a, b, c), rightVal = func(nums[right], a, b, c);
+                    if (leftVal < rightVal) {
+                        results[cur] = rightVal;
+                    } else {
+                        results[cur] = leftVal;
+                    }
+                    cur--;
+                }
+            } else { // mid high, left and right low
+                int cur = 0;
+                while (left <= right) {
+                    int leftVal = func(nums[left], a, b, c), rightVal = func(nums[right], a, b, c);
+                    if (leftVal < rightVal) {
+                        results[cur] = leftVal;
+                    } else {
+                        results[cur] = rightVal;
+                    }
+                    cur++;
+                }
+            }
+            return results;
+        }
+    }
+    
+    private int func(int x, int a , int b, int c) {
+        return a * x * x + b * x + c;
     }
 }
