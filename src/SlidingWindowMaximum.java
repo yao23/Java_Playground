4e212557ec6ceec3674a9d0fbfa04447eea9e17d
yao <myspiritcrazy@gmail.com>
2017-07-22 17:13:05 -0700

    add test cases and fix bugs

--- src/SlidingWindowMaximum.java
+++ src/SlidingWindowMaximum.java
@@ -6,6 +6,8 @@ public class SlidingWindowMaximum {
         int len = nums.length;
         if (len == 0 || k == 0) {
             return new int[]{};
+        } else if (k == 1) {
+            return nums;
         }
         int[] results = new int[len - k + 1];
         Deque<Integer> queue = new ArrayDeque<>(); // index queue (descending order)
@@ -16,7 +18,7 @@ public class SlidingWindowMaximum {
 
             removePassedNums(queue, cur);
 
-            for (int offset = 0; offset < 3 && cur + offset < len; offset++) {
+            for (int offset = 0; offset < k && cur + offset < len; offset++) {
                 int idx = cur + offset, curNum = nums[idx];
                 System.out.println(idx + ", " + curNum);
                 if (queue.isEmpty() || curNum < nums[queue.getLast()]) { System.out.println("[without remove] add last: " + idx);
@@ -43,4 +45,8 @@ public class SlidingWindowMaximum {
             queue.removeFirst();
         }
     }
+
+    // [],0 => []
+    // [1,3,-1,-3,5,3,6,7],3 => [3,3,5,5,6,7]
+    // [5,3,4],1 => [5,3,4]
 }
