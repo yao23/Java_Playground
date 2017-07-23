015a284c9f09b386e38661df9420da21c35f186a
yao <myspiritcrazy@gmail.com>
2017-07-23 09:49:15 -0700

    improve algorithm and use a stack

--- src/RemoveKDigits.java
+++ src/RemoveKDigits.java
@@ -1,8 +1,49 @@
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class RemoveKDigits {
-    public String removeKdigits(String num, int k) {
+    public String removeKdigitsV1(String num, int k) {
+        int len = num.length();
+        //corner case
+        if (k == len)
+            return "0";
+
+        Deque<Character> stack = new ArrayDeque<>();
+        int i = 0;
+        while (i < num.length()) {
+            //whenever meet a digit which is less than the previous digit, discard the previous one
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+
+        // corner case like "1111"
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+
+        //construct the number from the stack
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        sb.reverse();
+
+        //remove all the 0 at the head
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+        return sb.toString();
+    }
+
+    public String removeKdigitsV0(String num, int k) { // not working, pass test cases 26/33
         int len = num.length();
         if (len == 0) {
             return "";
