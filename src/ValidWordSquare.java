b063e449f00aae6ac34aaf0e88cdd7449ca60f2b
yao <myspiritcrazy@gmail.com>
2017-07-24 20:43:17 -0700

    refactor code and improve algorithm

--- src/ValidWordSquare.java
+++ src/ValidWordSquare.java
@@ -3,35 +3,40 @@ import java.util.List;
 public class ValidWordSquare {
     public boolean validWordSquare(List<String> words) {
         int len = words.size();
-        for (int curIdx = 0; curIdx < len; curIdx++) {
-            String str = words.get(curIdx);
-            int strLen = str.length();
-            if (!isLengthSame(words, curIdx, strLen, len) || !isCharSame(words, curIdx, str, len)) {
-                return false;
-            }
-        }
+        if (len == 0) {
+            return true;
+        } else {
+            String firstWord = words.get(0);
+            if (len == firstWord.length()) {
+                for (int curIdx = 0; curIdx < len; curIdx++) {
+                    String str = words.get(curIdx);
+                    if (!isCharSame(words, curIdx, str, len)) {
+                        return false;
+                    }
+                }
 
-        return true;
-    }
-
-    private boolean isLengthSame(List<String> words, int curIdx, int strLen, int len) {
-        if (curIdx < strLen) {
-            if (len - curIdx == strLen - curIdx) {
                 return true;
             } else {
                 return false;
             }
-        } else {
-            return false;
         }
     }
 
     private boolean isCharSame(List<String> words, int curIdx, String str, int len) {
         for (int i = curIdx; i < len; i++) {
-            char horizontalChar =  str.charAt(i);
             String tmp = words.get(i);
-            char verticalChar = tmp.charAt(curIdx);
-            if (horizontalChar != verticalChar) {
+            int strLen = str.length(), tmpLen = tmp.length();
+            if (i < strLen && curIdx < tmpLen) {
+                char horizontalChar =  str.charAt(i);
+                char verticalChar = tmp.charAt(curIdx);
+                if (horizontalChar != verticalChar) {
+                    System.out.println("[hv char diff] curIdx: " + curIdx + ", " + str + ", " + i + ", " + horizontalChar + ", " + verticalChar);
+                    return false;
+                }
+            } else if (i >= strLen && curIdx >= tmpLen) {
+                return true;
+            } else {
+                System.out.println("[hv len diff] curIdx: " + i + ", " + str.length() + ", "+ curIdx  + ", "  + tmpLen);
                 return false;
             }
         }
