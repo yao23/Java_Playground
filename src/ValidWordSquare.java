32f2cd9dcaada81d937369f9e4d15b0f84e83060
yao <myspiritcrazy@gmail.com>
2017-07-24 19:58:28 -0700

    Valid Word Square

---
+++ src/ValidWordSquare.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class ValidWordSquare {
+    public boolean validWordSquare(List<String> words) {
+        int len = words.size();
+        for (int curIdx = 0; curIdx < len; curIdx++) {
+            String str = words.get(curIdx);
+            int strLen = str.length();
+            if (!isLengthSame(words, curIdx, strLen, len) || !isCharSame(words, curIdx, str, len)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isLengthSame(List<String> words, int curIdx, int strLen, int len) {
+        if (curIdx < strLen) {
+            if (len - curIdx == strLen - curIdx) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isCharSame(List<String> words, int curIdx, String str, int len) {
+        for (int i = curIdx; i < len; i++) {
+            char horizontalChar =  str.charAt(i);
+            String tmp = words.get(i);
+            char verticalChar = tmp.charAt(curIdx);
+            if (horizontalChar != verticalChar) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
