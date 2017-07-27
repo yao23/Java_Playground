c15321fe63396157d59e54750b7d190dcc38fcab
yao <myspiritcrazy@gmail.com>
2017-07-26 19:30:04 -0700

    add iterative solution and documentation

--- src/RemoveDuplicateLetters.java
+++ src/RemoveDuplicateLetters.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RemoveDuplicateLetters {
     public String removeDuplicateLetters(String s) { // beats 11.38%
@@ -17,6 +19,48 @@ public class RemoveDuplicateLetters {
         }
         return s.length() == 0 ? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos + 1).replaceAll("" + s.charAt(pos), ""));
     }
+
+    // better explanation: https://discuss.leetcode.com/topic/31413/easy-to-understand-iterative-java-solution
+    public String removeDuplicateLettersV1(String s) {
+        if (s == null || s.length() <= 1) return s;
+
+        Map<Character, Integer> lastPosMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastPosMap.put(s.charAt(i), i);
+        }
+
+        char[] result = new char[lastPosMap.size()];
+        int begin = 0, end = findMinLastPos(lastPosMap);
+
+        for (int i = 0; i < result.length; i++) {
+            char minChar = 'z' + 1;
+            for (int k = begin; k <= end; k++) {
+                if (lastPosMap.containsKey(s.charAt(k)) && s.charAt(k) < minChar) {
+                    minChar = s.charAt(k);
+                    begin = k+1;
+                }
+            }
+
+            result[i] = minChar;
+            if (i == result.length-1) break;
+
+            lastPosMap.remove(minChar);
+            if (s.charAt(end) == minChar) end = findMinLastPos(lastPosMap);
+        }
+
+        return new String(result);
+    }
+
+    private int findMinLastPos(Map<Character, Integer> lastPosMap) {
+        if (lastPosMap == null || lastPosMap.isEmpty()) return -1;
+        int minLastPos = Integer.MAX_VALUE;
+        for (int lastPos : lastPosMap.values()) {
+            minLastPos = Math.min(minLastPos, lastPos);
+        }
+        return minLastPos;
+    }
+
+
 
     private String res = "";
 
