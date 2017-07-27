c329cc966b413eab98914ec067fa2d1837d475f7
yao <myspiritcrazy@gmail.com>
2017-07-26 23:21:03 -0700

    improve algorithm

--- src/ExpressionAddOperators.java
+++ src/ExpressionAddOperators.java
@@ -2,8 +2,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ExpressionAddOperators {
-    public List<String> addOperators(String num, int target) {
+    //  Backtrace
+    //    1. overflow: we use a long type once it is larger than Integer.MAX_VALUE or minimum, we get over it.
+    //    2. 0 sequence: because we can't have numbers with multiple digits started with zero, we have to deal with it too.
+    //    3. a little trick is that we should save the value that is to be multiplied in the next recursion.
+    public List<String> addOperators(String num, int target) { // beats 68.98%
+        List<String> res = new ArrayList<>();
 
+        if (num == null || num.length() == 0) {
+            return res;
+        }
+
+        helper(res, "", num, target, 0, 0, 0);
+
+        return res;
+    }
+
+    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
+        if (pos == num.length()) {
+            if (target == eval) {
+                rst.add(path);
+            }
+            return;
+        }
+        for (int i = pos; i < num.length(); i++) {
+            if (i != pos && num.charAt(pos) == '0') {
+                break;
+            }
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+            if (pos == 0) {
+                helper(rst, path + cur, num, target, i + 1, cur, cur);
+            } else {
+                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
+
+                helper(rst, path + "-" + cur, num, target, i + 1, eval - cur, -cur);
+
+                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+            }
+        }
     }
 
     public List<String> addOperatorsV0(String num, int target) {
@@ -55,6 +91,7 @@ public class ExpressionAddOperators {
         }
     }
 
+    // 0 sequences in head
     private boolean isValidStr(String str) {
         for (int i = 0; i < str.length() - 1; i++) {
             if (str.charAt(i) == '0') {
