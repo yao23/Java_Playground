273c04dbcfc9efd0599271d3133374455fc94678
yao <myspiritcrazy@gmail.com>
2017-07-26 19:33:13 -0700

    add explanation

--- src/RemoveDuplicateLetters.java
+++ src/RemoveDuplicateLetters.java
@@ -159,3 +159,24 @@ public class RemoveDuplicateLetters {
 
 // "bcabc" => "abc"
 // "cbacdcbc" => "acdb"
+
+//    The basic idea is to find out the smallest result letter by letter (one letter at a time). Here is the thinking process for input "cbacdcbc":
+//
+//        find out the last appeared position for each letter;
+//        c - 7
+//        b - 6
+//        a - 2
+//        d - 4
+//        find out the smallest index from the map in step 1 (a - 2);
+//        the first letter in the final result must be the smallest letter from index 0 to index 2;
+//        repeat step 2 to 3 to find out remaining letters.
+//        the smallest letter from index 0 to index 2: a
+//        the smallest letter from index 3 to index 4: c
+//        the smallest letter from index 4 to index 4: d
+//        the smallest letter from index 5 to index 6: b
+//        so the result is "acdb"
+//
+//        Notes:
+//
+//        after one letter is determined in step 3, it need to be removed from the "last appeared position map", and the same letter should be ignored in the following steps
+//        in step 3, the beginning index of the search range should be the index of previous determined letter plus one
