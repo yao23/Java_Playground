4849c82d25ef707b6937fb7fab6cd68b7d820ec3
yao <myspiritcrazy@gmail.com>
2017-07-26 23:25:14 -0700

    add documentation for muliplication statement

--- src/ExpressionAddOperators.java
+++ src/ExpressionAddOperators.java
@@ -40,6 +40,10 @@ public class ExpressionAddOperators {
                 helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
             }
         }
+
+        // If have a sequence of 12345 and you have proceeded to 1 + 2 + 3, now your eval is 6 right?
+        // If you want to add a * between 3 and 4, you would take 3 as the digit to be multiplied,
+        // so you want to take it out from the existing eval. You have 1 + 2 + 3 * 4 and the eval now is (1 + 2 + 3) - 3 + (3 * 4).
     }
 
     public List<String> addOperatorsV0(String num, int target) {
