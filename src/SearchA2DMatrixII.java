eba02432f3df7d4321966d344ea134e4ea88b28c
yao <myspiritcrazy@gmail.com>
2017-07-29 15:14:58 -0700

    Search a 2D Matrix II

---
+++ src/SearchA2DMatrixII.java
@@ -0,0 +1,25 @@
+public class SearchA2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+    }
+
+    private boolean binarySearch(int[][] matrix, int target, int startX, int startY, int endX, int endY) {
+        if (startX > endX || startY > endY) {
+            return false;
+        }
+
+        int midX = startX + (endX - startX) / 2;
+        int midY = startY + (endY - startY) / 2;
+
+        if (matrix[midX][midY] == target) {
+            return true;
+        } else if (matrix[midX][midY] > target) { // larger than target, go left or up
+            return binarySearch(matrix, target, startX, startY, endX, midY - 1) ||
+                    binarySearch(matrix, target, startX, midY,midX - 1,endY);
+        } else { // smaller than target, go right or down
+            return binarySearch(matrix, target, startX, midY + 1, endX, endY) ||
+                    binarySearch(matrix, target, midX + 1, startY, endX, midY);
+        }
+    }
+}
