33d78f82c9be60a05444a476b2ad95a86642c320
yao <myspiritcrazy@gmail.com>
2017-07-29 12:33:24 -0700

    improve algorithm and fix bugs

--- src/DivideTwoIntegers.java
+++ src/DivideTwoIntegers.java
@@ -1,34 +1,43 @@
 public class DivideTwoIntegers {
     public int divide(int dividend, int divisor) {
         if (divisor == 0) {
-            return Integer.MIN_VALUE;
+            return Integer.MAX_VALUE;
         }
-        boolean isNegative = false;
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            } else if (divisor == -1) { // Input:-2147483648, -1, Output:2147483648 Expected:2147483647 (test case 3)
+                return Integer.MAX_VALUE;
+            }
+        }
+
+        long curDividend = Math.abs((long) dividend);
+        long curDivisor = Math.abs((long) divisor);
+        int res = 0;
+
+        while (curDividend >= curDivisor) {
+            long cur = curDivisor; // avoid overflow when left shift
+            int mul = 1;
+
+            while (curDividend >= (cur << 1)) {
+                mul <<= 1;
+                cur <<= 1;
+            }
+
+            curDividend -= cur;
+            res += mul;
+        }
+
         if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) {
-            isNegative = true;
+            return -res;
+        } else {
+            return res;
         }
-        long res = (long)dividend;
-        int counter = 0;
-
-        while (res > 0) {
-            long tmp = (long)divisor; // avoid overflow when left shift
-            while (tmp < res) {
-                tmp <<= 1;
-                counter++;
-            }
-            if (tmp == res) {
-                return counter;
-            } else { // tmp > res
-                tmp >>= 1;
-                res -= tmp;
-                counter--;
-            }
-        }
-
-        if (counter == Integer.MAX_VALUE && isNegative) {
-            counter = Integer.MIN_VALUE;
-        }
-
-        return counter;
     }
 }
+
+// (0, 1) => 0
+// (1, 0) => Integer.MAX_VALUE
+// (Integer.MIN_VALUE, -1) => Integer.MAX_VALUE
+
+// beats 65.41%
