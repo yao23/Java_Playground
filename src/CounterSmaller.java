dc4e33492233f7dc1b0b55bdf03b48c815d1ecd0
yao <myspiritcrazy@gmail.com>
2017-07-28 23:50:52 -0700

    count smaller

---
+++ src/CounterSmaller.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CounterSmaller {
+    public List<Integer> countSmaller(int[] nums) {
+        Integer[] res = new Integer[nums.length];
+
+        mergeSort(0, nums.length - 1, nums, res);
+
+        return Arrays.asList(res);
+    }
+
+    private List<Integer> mergeSort(int start, int end, int[] nums, Integer[] res) {
+        List<Integer> resList = new ArrayList<>();
+        if (start == end) {
+            resList.add(nums[start]);
+            return resList;
+        } else {
+            int mid = start + (end - start) / 2;
+            List<Integer> left = mergeSort(start, mid, nums, res);
+            List<Integer> right = mergeSort(mid + 1, end, nums, res);
+            return merge(left, right, start, mid + 1, res);
+        }
+    }
+
+    private List<Integer> merge(List<Integer> left, List<Integer> right, int leftStart, int rightStart, Integer[] res) {
+        List<Integer> resList = new ArrayList<>();
+        for (int i = 0; i < left.size() + right.size(); i++) {
+            if (leftStart >= left.size()) {
+                resList.add(right.get(rightStart));
+                rightStart++;
+            } else if (rightStart >= right.size()) {
+                resList.add(left.get(leftStart));
+                leftStart++;
+            } else if (left.get(leftStart) <= right.get(rightStart)) {
+                resList.add(left.get(leftStart));
+                leftStart++;
+            } else {
+                res[leftStart] += 1;
+                resList.add(right.get(rightStart));
+                rightStart++;
+            }
+        }
+        return resList;
+    }
+}
---
+++ src/FindKthSmallest.java
@@ -0,0 +1,25 @@
+public class FindKthSmallest {
+    private int findKthSmallest(int[] a, int[] b, int aStart, int aEnd, int bStart, int bEnd, int k) {
+        if (aStart >= a.length) {
+            return b[bStart + k - 1];
+        }
+        if (bStart >= b.length) {
+            return a[aStart + k - 1];
+        }
+        if (k == 1) {
+            return Math.min(a[aStart], b[bStart]);
+        }
+        // throw the smaller half
+        int aMid = aStart + k / 2 - 1;
+        int bMid = bStart + k / 2 - 1;
+        // do not throw the exceeding one
+        int aVal = aMid >= a.length ? Integer.MAX_VALUE : a[aMid];
+        int bVal = bMid >= b.length ? Integer.MAX_VALUE : b[bMid];
+
+        if (aVal <= bVal) {
+            return findKthSmallest(a, b, aMid + 1, aEnd, bStart, bMid - 1, k - k / 2);
+        } else {
+            return findKthSmallest(a, b, aStart, aMid - 1, bMid + 1, bEnd, k - k / 2);
+        }
+    }
+}
