0fac6b486aa620e21351b21b12c5cfa04f8b576d
yao <myspiritcrazy@gmail.com>
2017-07-29 23:15:03 -0700

    another solution for min dpeth of binary tree

--- src/MinimumDepthOfBinaryTree.java
+++ src/MinimumDepthOfBinaryTree.java
@@ -16,6 +16,16 @@ public class MinimumDepthOfBinaryTree {
         }
     }
 
+    public int minDepthV1(TreeNode root) { // beats 14.32
+        if (root == null) {
+            return 0;
+        } else {
+            int left = minDepth(root.left);
+            int right = minDepth(root.right);
+            return (left == 0 || right == 0) ? (left + right + 1) : (Math.min(left, right) + 1);
+        }
+    }
+
     // [] => 0
     // [1,2] => 2
     // [1,3,4,null,null,5,6] => 2
