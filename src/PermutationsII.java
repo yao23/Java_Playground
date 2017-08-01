e08712081fd84e6ece3a576d7b75d0f474b3a832
yao <myspiritcrazy@gmail.com>
2017-07-31 19:48:25 -0700

    use a list without swap

--- src/PermutationsII.java
+++ src/PermutationsII.java
@@ -41,4 +41,36 @@ public class PermutationsII {
         nums[i] = nums[j];
         nums[j] = tmp;
     }
+
+    public List<List<Integer>> permuteUniqueV0(int[] nums) { // beats 63.02%
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        boolean[] used = new boolean[nums.length];
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(nums, used, list, res);
+        return res;
+    }
+
+    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
+        if (list.size() == nums.length) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) {
+                continue;
+            }
+            used[i] = true;
+            list.add(nums[i]);
+            dfs(nums, used, list, res);
+            used[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
 }
