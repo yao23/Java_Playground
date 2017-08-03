53bc4560fa440e52b29e9ef9578fea3b27014603
yao <myspiritcrazy@gmail.com>
2017-08-02 21:28:25 -0700

    add another solution

--- src/LargestBSTSubtree.java
+++ src/LargestBSTSubtree.java
@@ -41,6 +41,40 @@ public class LargestBSTSubtree { // LC 333
             node = null;
         }
     }
+
+    // ---------------------------------------
+
+    class Result {  // (size, rangeLower, rangeUpper) -- size of current tree, range of current tree [rangeLower, rangeUpper]
+        int size;
+        int lower;
+        int upper;
+
+        Result(int size, int lower, int upper) {
+            this.size = size;
+            this.lower = lower;
+            this.upper = upper;
+        }
+    }
+
+    int max = 0;
+
+    public int largestBSTSubtreeV1(TreeNode root) {
+        if (root == null) { return 0; }
+        traverse(root);
+        return max;
+    }
+
+    private Result traverse(TreeNode root) {
+        if (root == null) { return new Result(0, Integer.MAX_VALUE, Integer.MIN_VALUE); }
+        Result left = traverse(root.left);
+        Result right = traverse(root.right);
+        if (left.size == -1 || right.size == -1 || root.val <= left.upper || root.val >= right.lower) {
+            return new Result(-1, 0, 0);
+        }
+        int size = left.size + 1 + right.size;
+        max = Math.max(size, max);
+        return new Result(size, Math.min(left.lower, root.val), Math.max(right.upper, root.val));
+    }
 }
 
 // [10,5,15,1,8,null,7] => 3 ([5,1,8])
