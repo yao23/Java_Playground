4fbccd395fc7ee9f2c1eff0a47f9290ae4b335c3
yao <myspiritcrazy@gmail.com>
2017-08-02 21:07:38 -0700

    Longest BST Subtree

--- src/LargestBSTSubtree.java
+++ src/LargestBSTSubtree.java
@@ -1,5 +1,43 @@
 public class LargestBSTSubtree { // LC 333
     public int largestBSTSubtree(TreeNode root) {
-        return 0;
+        ResNode resNode = helper(root);
+        return resNode.count;
+    }
+
+    private ResNode helper(TreeNode root) {
+        if (root == null) {
+            return new ResNode();
+        }
+        ResNode left = helper(root.left);
+        ResNode right = helper(root.right);
+        if (left.max < root.val && root.val < right.min) {
+            if (left.node == root.left && right.node == root.right) {
+                ResNode resNode = new ResNode();
+                resNode.min = left.min;
+                resNode.max = right.max;
+                resNode.count = left.count + 1 + right.count;
+                resNode.node = root;
+                return resNode;
+            } else {
+                return (left.count < right.count) ? right : left;
+            }
+        } else {
+            return (left.count < right.count) ? right : left;
+        }
+    }
+
+    class ResNode {
+        int min;
+        int max;
+        int count;
+        TreeNode node;
+        public ResNode() {
+            min = Integer.MAX_VALUE;
+            max = Integer.MIN_VALUE;
+            count = 0;
+            node = null;
+        }
     }
 }
+
+// [10,5,15,1,8,null,7] => 3 ([5,1,8])
