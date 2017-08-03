4392311c3ff102622e48b60c1a39310bcfb9d7cd
yao <myspiritcrazy@gmail.com>
2017-08-02 23:59:18 -0700

    Word Squares

---
+++ src/WordSquares.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordSquares { // LC 425
+    public List<List<String>> wordSquares(String[] words) {
+        List<List<String>> res = new ArrayList<>();
+
+        return res;
+    }
+}
+
+// ["area","lead","wall","lady","ball"]
+// [
+//    [ "wall",
+//      "area",
+//      "lead",
+//      "lady"
+//    ],
+//    [ "ball",
+//      "area",
+//      "lead",
+//      "lady"
+//    ]
+// ]
+
