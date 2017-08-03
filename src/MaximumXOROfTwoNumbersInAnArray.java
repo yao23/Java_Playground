7b6c047d175d1c430963ef9189a43f5fa663ec1a
yao <myspiritcrazy@gmail.com>
2017-08-02 22:23:17 -0700

    Maximum XOR of Two Numbers in an Array (bit operation algorithm)

--- src/MaximumXOROfTwoNumbersInAnArray.java
+++ src/MaximumXOROfTwoNumbersInAnArray.java
@@ -1,5 +1,26 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class MaximumXOROfTwoNumbersInAnArray { // LC 421
-    public int findMaximumXOR(int[] nums) {
-        return 0;
+    public int findMaximumXOR(int[] nums) { // beats 81.20%
+        int max = 0, mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask = mask | (1 << i);
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num & mask);
+            }
+            int tmp = max | (1 << i);
+            for (int prefix : set) {
+                if (set.contains(tmp ^ prefix)) {
+                    max = tmp;
+                    break;
+                }
+            }
+        }
+        return max;
     }
 }
+
+// [3, 10, 5, 25, 2, 8] => 28 (5 ^ 25)
+
