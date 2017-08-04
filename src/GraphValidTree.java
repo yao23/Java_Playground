2193c5efb192c651078c6306bc78b6da4ca58365
yao <myspiritcrazy@gmail.com>
2017-08-03 22:43:34 -0700

    Graph Valid Tree

--- src/GraphValidTree.java
+++ src/GraphValidTree.java
@@ -1,4 +1,62 @@
-package PACKAGE_NAME;
-
 public class GraphValidTree {
+    private int[] arr;
+    public boolean validTree(int n, int[][] edges) {
+        if (edges.length == n - 1) {
+            arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = i;
+            }
+
+            for (int i = 0; i < edges.length; i++) {
+                int first = edges[i][0], second = edges[i][1];
+                if (find(first) != find(second)) {
+                    union(first, second);
+                } else {
+                    return false;
+                }
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private int find(int elem) {
+        int parent = arr[elem];
+
+        while (parent != arr[parent]) {
+            parent = arr[parent];
+        }
+
+        return parent;
+    }
+
+    private void union(int first, int second) {
+        int firstParent = compressedFind(first);
+        int secondParent = compressedFind(second);
+        if (firstParent != secondParent) {
+            arr[second] = firstParent;
+        }
+    }
+
+    private int compressedFind(int elem) {
+        int parent = arr[elem];
+
+        // find common parent
+        while (parent != arr[parent]) {
+            parent = arr[parent];
+        }
+
+        int p = arr[elem];
+        // update parent for immediate nodes
+        while (p != arr[p]) {
+            int tmp = arr[p];
+            arr[p] = parent;
+            p = tmp;
+        }
+
+        arr[elem] = parent;
+        return parent;
+    }
 }
