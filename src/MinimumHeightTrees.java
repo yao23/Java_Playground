8e9b47882968cc47edc8176ae4c1fb1215765cd4
yao <myspiritcrazy@gmail.com>
2017-08-05 13:27:24 -0700

    add a solution

--- src/MinimumHeightTrees.java
+++ src/MinimumHeightTrees.java
@@ -1,7 +1,43 @@
 import java.util.*;
 
 public class MinimumHeightTrees { // LC 310
-    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) { // beats 30.33%
+        if (n == 1) {
+            return Collections.singletonList(0);
+        }
+
+        List<Set<Integer>> adj = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            adj.add(new HashSet<>());
+        }
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (adj.get(i).size() == 1) {
+                leaves.add(i);
+            }
+        }
+
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int i : leaves) {
+                int j = adj.get(i).iterator().next();
+                adj.get(j).remove(i);
+                if (adj.get(j).size() == 1) {
+                    newLeaves.add(j);
+                }
+            }
+            leaves = newLeaves;
+        }
+        return leaves;
+    }
+
+    public List<Integer> findMinHeightTreesV0(int n, int[][] edges) {
         // all leaves move to center together, last two or one converged node is the root
         // even as 2, odd as 1 for root
 
