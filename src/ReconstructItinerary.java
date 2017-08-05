8bf935334eec39cde3497c233d0c26a3f4b26987
yao <myspiritcrazy@gmail.com>
2017-08-05 16:34:38 -0700

    add test cases

--- src/ReconstructItinerary.java
+++ src/ReconstructItinerary.java
@@ -32,3 +32,6 @@ public class ReconstructItinerary { // LC 332
         return res;
     }
 }
+
+// [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]] => ["JFK", "MUC", "LHR", "SFO", "SJC"]
+// [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]] => ["JFK","ATL","JFK","SFO","ATL","SFO"]
