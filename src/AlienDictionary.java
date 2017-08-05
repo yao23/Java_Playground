a71e02b6184a2f14e754a35d023e42810c81d13f
yao <myspiritcrazy@gmail.com>
2017-08-05 08:59:37 -0700

    add a solution

--- src/AlienDictionary.java
+++ src/AlienDictionary.java
@@ -1,7 +1,66 @@
 import java.util.*;
 
 public class AlienDictionary { // LC 269
-    public String alienOrder(String[] words) {
+    public String alienOrder(String[] words) { // beats 54.90%
+        Map<Character, Set<Character>> map = new HashMap<>();
+        Map<Character, Integer> degree = new HashMap<>();
+        String result = "";
+
+        if (words == null || words.length == 0) {
+            return result;
+        }
+        for (String s: words) {
+            for (char c: s.toCharArray()) {
+                degree.put(c, 0);
+            }
+        }
+        for (int i = 0; i < words.length - 1; i++) {
+            String cur = words[i];
+            String next = words[i + 1];
+            int length = Math.min(cur.length(), next.length());
+            for (int j = 0; j < length; j++) {
+                char c1 = cur.charAt(j);
+                char c2 = next.charAt(j);
+                if (c1 != c2) {
+                    Set<Character> set = new HashSet<>();
+                    if (map.containsKey(c1)) {
+                        set = map.get(c1);
+                    }
+                    if (!set.contains(c2)) {
+                        set.add(c2);
+                        map.put(c1, set);
+                        degree.put(c2, degree.get(c2) + 1);
+                    }
+                    break;
+                }
+            }
+        }
+        Queue<Character> q = new LinkedList<>();
+        for (char c: degree.keySet()) {
+            if (degree.get(c) == 0) {
+                q.add(c);
+            }
+        }
+        while (!q.isEmpty()) {
+            char c = q.remove();
+            result += c;
+            if (map.containsKey(c)) {
+                for (char c2: map.get(c)) {
+                    degree.put(c2, degree.get(c2) - 1);
+                    if (degree.get(c2) == 0) {
+                        q.add(c2);
+                    }
+                }
+            }
+        }
+        if (result.length()!= degree.size()) {
+            return "";
+        } else {
+            return result;
+        }
+    }
+
+    public String alienOrderV0(String[] words) {
         int len = words.length;
         if (len == 0) {
             return "";
@@ -83,4 +142,5 @@ public class AlienDictionary { // LC 269
 // ["z","x"] => "zx"
 // ["z","x","z"] => ""
 // ["z","z"] => "z"
+// ["zy","zx"] => "yxz"
 
