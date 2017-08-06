4e0f4977948d797a97a4089500240015400eee4e
yao <myspiritcrazy@gmail.com>
2017-08-06 10:26:17 -0700

    add bi-direction BFS solution

--- src/WordLadder.java
+++ src/WordLadder.java
@@ -2,6 +2,51 @@ import java.util.*;
 
 public class WordLadder {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> beginSet = new HashSet<>(), endSet = new HashSet<>();
+
+        int len = 1;
+        HashSet<String> visited = new HashSet<>();
+
+        beginSet.add(beginWord);
+        endSet.add(endWord);
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> set = beginSet;
+                beginSet = endSet;
+                endSet = set;
+            }
+
+            Set<String> temp = new HashSet<>();
+            for (String word : beginSet) {
+                char[] chs = word.toCharArray();
+
+                for (int i = 0; i < chs.length; i++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        char old = chs[i];
+                        chs[i] = c;
+                        String target = String.valueOf(chs);
+
+                        if (endSet.contains(target)) {
+                            return len + 1;
+                        }
+
+                        if (!visited.contains(target) && wordList.contains(target)) {
+                            temp.add(target);
+                            visited.add(target);
+                        }
+                        chs[i] = old;
+                    }
+                }
+            }
+
+            beginSet = temp;
+            len++;
+        }
+
+        return 0;
+    }
+
+    public int ladderLengthV0(String beginWord, String endWord, List<String> wordList) {
         if (beginWord == null || endWord == null || wordList == null) {
             return 0;
         }
