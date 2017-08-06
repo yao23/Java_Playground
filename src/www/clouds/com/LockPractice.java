3544e6510e7c1453d239454e0ef329b3f1c493e7
yao <myspiritcrazy@gmail.com>
2017-08-06 16:45:28 -0700

    Lock Practice (synchronized)

---
+++ src/www/clouds/com/LockPractice.java
@@ -0,0 +1,19 @@
+package www.clouds.com;
+
+public class LockPractice {
+    public static synchronized void getClassValue() {
+        // Class Lock
+    }
+
+    public synchronized void setNewValue() {
+        // Object Lock
+    }
+
+    public synchronized void getNewValue() {
+        // Object Lock
+    }
+
+    public void getNormalValue() {
+        // None Lock
+    }
+}
