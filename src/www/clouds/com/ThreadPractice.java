2fffa3a187e50116cf98c574d95865888b592617
yao <myspiritcrazy@gmail.com>
2017-08-06 16:39:29 -0700

    test Thread join()

--- src/www/clouds/com/ThreadPractice.java
+++ src/www/clouds/com/ThreadPractice.java
@@ -33,6 +33,8 @@ class MyThread extends Thread {
 class MyRunnable implements Runnable {
     @Override
     public void run() {
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
