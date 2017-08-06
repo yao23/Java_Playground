0798ce2eebabe51cc07e7b612c3fcdcd8995db7c
yao <myspiritcrazy@gmail.com>
2017-08-06 16:36:32 -0700

    Thread sleep

--- src/www/clouds/com/ThreadPractice.java
+++ src/www/clouds/com/ThreadPractice.java
@@ -12,7 +12,14 @@ public class ThreadPractice {
 class MyThread extends Thread {
     @Override
     public void run() {
-
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
