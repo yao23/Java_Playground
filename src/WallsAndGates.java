e4b545c2c260b5e97502a378b04b576fe4eeca74
yao <myspiritcrazy@gmail.com>
2017-08-06 11:28:08 -0700

    add solution with multi end BFS

--- src/WallsAndGates.java
+++ src/WallsAndGates.java
@@ -11,13 +11,42 @@ public class WallsAndGates {
     private int INF = Integer.MAX_VALUE; // (2^31 - 1) = 2147483647
 
     public void wallsAndGates(int[][] rooms) { // beats 58.29%
+        checkCornerCase(rooms);
+        Deque<int[]> queue = new ArrayDeque<>();
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                if (rooms[i][j] == 0) { // gates only (sparse in matrix, much less than rooms)
+                    queue.offer(new int[]{i,j});
+                }
+            }
+        }
+        multiEndBFSHelper(rooms, queue);
+    }
 
+    private void multiEndBFSHelper(int[][] rooms, Deque<int[]> queue) {
+        int row = rooms.length;
+        int col = rooms[0].length;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.pollFirst();
+            int x = cur[0];
+            int y = cur[1];
+            int[] dx = new int[]{-1, 1, 0, 0};
+            int[] dy = new int[]{0, 0, -1, 1};
+            for (int k = 0; k < 4; k++) {
+                int newX = x + dx[k];
+                int newY = y + dy[k];
+
+                if (isValid(x, y, newX, newY, row, col, rooms) && rooms[newX][newY] > rooms[x][y] + 1) {
+                    rooms[newX][newY] = rooms[x][y] + 1;
+                    queue.offer(new int[]{newX, newY});
+                }
+            }
+        }
     }
 
     public void wallsAndGatesV0(int[][] rooms) { // beats 58.29%, O(k*mn) and k as # of rooms (INF)
-        if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) {
-            return;
-        }
+        checkCornerCase(rooms);
         for (int i = 0; i < rooms.length; i++) {
             for (int j = 0; j < rooms[0].length; j++) {
                 if (rooms[i][j] >= 0) { // gate or room
@@ -53,6 +82,12 @@ public class WallsAndGates {
     // only update for rooms, INF + 1 = Integer.MIN_VALUE (overflow)
     private boolean isValid(int x, int y, int newX, int newY, int row, int col, int[][] rooms) {
         return newX >= 0 && newY >= 0 && newX < row && newY < col && rooms[newX][newY] > 0 && rooms[x][y] != INF;
+    }
+
+    private void checkCornerCase(int[][] rooms) {
+        if (rooms == null || rooms.length == 0 || rooms[0] == null || rooms[0].length == 0) {
+            return;
+        }
     }
 }
 
