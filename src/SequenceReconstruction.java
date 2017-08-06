de941a24cabdc558260fd1ebf80f46ffd983d658
yao <myspiritcrazy@gmail.com>
2017-08-05 20:16:59 -0700

    add test case and fix bugs

--- src/SequenceReconstruction.java
+++ src/SequenceReconstruction.java
@@ -13,12 +13,16 @@ public class SequenceReconstruction {
             Map<Integer, Integer> degrees = new HashMap<>();
             Map<Integer, Set<Integer>> graph = new HashMap<>();
             for (List<Integer> list : seqs) {
-                for (int i = 0; i < list.size() - 1; i++) {
-                    int start = list.get(i), end = list.get(i + 1);
+                if (list.size() == 1) { // test case 5
+                    updateDegreeForStart(degrees, list.get(0));
+                } else {
+                    for (int i = 0; i < list.size() - 1; i++) {
+                        int start = list.get(i), end = list.get(i + 1);
 
-                    if (updateGraph(graph, start, end)) { // edge from start to end is not existing (test case 4)
-                        updateDegreeForStart(degrees, start);
-                        updateDegreeForEnd(degrees, end);
+                        if (updateGraph(graph, start, end)) { // edge from start to end is not existing (test case 4)
+                            updateDegreeForStart(degrees, start);
+                            updateDegreeForEnd(degrees, end);
+                        }
                     }
                 }
             }
@@ -89,3 +93,4 @@ public class SequenceReconstruction {
 // orig: [1,2,3], seqs: [[1,2]] => false ([1,2])
 // orig: [1,2,3], seqs: [[1,2],[1,3],[2,3]] => true
 // orig: [4,1,5,2,6,3], seqs: [[5,2,6,3],[4,1,5,2]] => true
+// orig: [1], seqs: [[1],[1],[1]] => true
