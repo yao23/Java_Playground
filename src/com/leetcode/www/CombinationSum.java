7bf0ac88ed9ec0f6edf7288c128ae3e7f6ea3919
yao <myspiritcrazy@gmail.com>
2017-08-07 21:28:53 -0700

    documentation

--- src/com/leetcode/www/CombinationSum.java
+++ src/com/leetcode/www/CombinationSum.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class CombinationSum {
+public class CombinationSum { // LC 39
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
--- src/com/leetcode/www/CombinationSumII.java
+++ src/com/leetcode/www/CombinationSumII.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class CombinationSumII {
+public class CombinationSumII { // LC 40
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
