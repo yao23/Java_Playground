d06f0a77f6bd5223020e2e61a9b44c63dc813e17
yao <myspiritcrazy@gmail.com>
2017-08-07 19:27:44 -0700

    Singleton

---
+++ src/www/clouds/com/Singleton.java
@@ -0,0 +1,21 @@
+package www.clouds.com;
+
+public class Singleton {
+    private static Singleton singletonInstance;
+    private static Object object = new Object();
+
+    private Singleton() {
+        // inline code here
+    }
+
+    public static Singleton getInstance() {
+        if (singletonInstance == null) { // all threads check
+            synchronized (object) {
+                if (singletonInstance == null) { // threads hold lock check
+                    singletonInstance = new Singleton();
+                }
+            }
+        }
+        return singletonInstance;
+    }
+}
