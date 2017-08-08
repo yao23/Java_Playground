92a51c370762dbd096915e56e6aa6709bbdc0498
yao <myspiritcrazy@gmail.com>
2017-08-07 21:23:45 -0700

    Combination Sum

---
+++ src/com/leetcode/www/CombinationSum.java
@@ -0,0 +1,32 @@
+package com.leetcode.www;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(res, new ArrayList<Integer>(), candidates, 0, target);
+        return res;
+    }
+
+    private void helper(List<List<Integer>> res, List<Integer> list, int[] nums, int start, int remain) {
+        if (remain == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            list.add(nums[i]);
+            if (remain >= nums[i]) {
+                helper(res, list, nums, i, remain - nums[i]);
+            }
+            list.remove(list.size() - 1);
+        }
+    }
+}
+
+// [2, 3, 6, 7], 7 => [[7],[2, 2, 3]]
+
+// beats 58.30%
