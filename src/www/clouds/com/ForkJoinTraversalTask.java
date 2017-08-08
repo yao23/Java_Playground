b8a6efded3a65dd7c82c299a54d8b77158183848
yao <myspiritcrazy@gmail.com>
2017-08-07 20:52:18 -0700

    Fork/Join Framework solution for ValidateBST

---
+++ src/www/clouds/com/ForkJoinTraversalTask.java
@@ -0,0 +1,39 @@
+package www.clouds.com;
+
+import java.util.concurrent.RecursiveTask;
+
+public class ForkJoinTraversalTask extends RecursiveTask<Boolean> {
+    TreeNode root;
+    int height;
+    long minVal;
+    long maxVal;
+    public ForkJoinTraversalTask(TreeNode root, int height, long minVal, long maxVal) {
+        this.root = root;
+        this.height = height;
+        this.minVal = minVal;
+        this.maxVal = maxVal;
+    }
+    public Boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+    }
+    @Override
+    public Boolean compute() {
+        if (height <= 3) {
+            return isValidBST(root, this.minVal, this.maxVal); // normal recursion
+        } else {
+            ForkJoinTraversalTask left = new ForkJoinTraversalTask(root.left, height - 1, minVal, root.val);
+            ForkJoinTraversalTask right = new ForkJoinTraversalTask(root.right, height - 1, root.val, maxVal);
+
+            left.fork();
+            right.fork();
+
+            return left.join() && right.join();
+        }
+    }
+}
