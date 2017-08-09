86757b47a25cd0daa028c0e8c17072f8008b8fe8
yao <myspiritcrazy@gmail.com>
2017-08-08 23:11:18 -0700

    add memoried search solution

--- src/com/leetcode/www/EditDistance.java
+++ src/com/leetcode/www/EditDistance.java
@@ -2,10 +2,37 @@ package com.leetcode.www;
 
 public class EditDistance {
     public int minDistance(String word1, String word2) {
-        return match(word1, word2, 0, 0);
+        int[][] count = new int[word1.length()][word2.length()];
+        return match(word1, word2, 0, 0, count);
     }
 
-    private int match(String word1, String word2, int i, int j) { // TLE (test case 2)
+    private int match(String word1, String word2, int i, int j, int[][] count) { // TLE (test case 2)
+        if (i == word1.length()) {
+            return word2.length() - j;
+        }
+        if (j == word2.length()) {
+            return word1.length() - i;
+        }
+
+        if (count[i][j] != 0) {
+            return count[i][j];
+        }
+
+        int res;
+        if (word1.charAt(i) == word2.charAt(j)) {
+            res = match(word1, word2, i + 1, j + 1, count);
+        } else {
+            int insert = match(word1, word2, i, j + 1, count);
+            int delete = match(word1, word2, i + 1, j, count);
+            int replace = match(word1, word2, i + 1, j + 1, count);
+            res = Math.min(Math.min(insert, delete), replace) + 1;
+        }
+
+        count[i][j] = res;
+        return res;
+    }
+
+    private int matchV0(String word1, String word2, int i, int j) { // TLE (test case 2)
         if (i == word1.length()) {
             return word2.length() - j;
         }
@@ -15,11 +42,11 @@ public class EditDistance {
 
         int res;
         if (word1.charAt(i) == word2.charAt(j)) {
-            res = match(word1, word2, i + 1, j + 1);
+            res = matchV0(word1, word2, i + 1, j + 1);
         } else {
-            int insert = match(word1, word2, i, j + 1);
-            int delete = match(word1, word2, i + 1, j);
-            int replace = match(word1, word2, i + 1, j + 1);
+            int insert = matchV0(word1, word2, i, j + 1);
+            int delete = matchV0(word1, word2, i + 1, j);
+            int replace = matchV0(word1, word2, i + 1, j + 1);
             res = Math.min(Math.min(insert, delete), replace) + 1;
         }
 
