e6bacb0329fd8d47499aeec6bc3da35c9bf278fc
yao <myspiritcrazy@gmail.com>
2017-08-08 23:37:58 -0700

    add another solution with less space for Paint House

--- src/com/leetcode/www/PaintHouse.java
+++ src/com/leetcode/www/PaintHouse.java
@@ -20,6 +20,27 @@ public class PaintHouse { // LC 256
         }
     }
 
+    public int minCostV0(int[][] costs) { // Time: O(3n), Space: O(3), beats 6.56%
+        if (costs == null || costs.length == 0) {
+            return 0;
+        } else {
+            int[] pre = new int[3];
+            int[] cur = new int[3];
+
+            for (int[] cost : costs) {
+                for (int i = 0; i < 3; i++) {
+                    cur[i] = cost[i] + Math.min(pre[(i + 1) % 3], pre[(i + 2) % 3]);
+                }
+                int[] tmp = cur;
+                cur = pre;
+                pre = tmp;
+            }
+
+
+            return Math.min(pre[0], Math.min(pre[1], pre[2]));
+        }
+    }
+
     // beats 51.97%
 }
 
