605637c982bf2341c4697d498f459f5bb3f24c74
yao <myspiritcrazy@gmail.com>
2017-08-09 21:19:40 -0700

    add greedy solution (1 pass)

--- src/com/leetcode/www/Candy.java
+++ src/com/leetcode/www/Candy.java
@@ -1,7 +1,48 @@
 package com.leetcode.www;
 
 public class Candy { // LC 135
-    public int candy(int[] ratings) { // beats 26.73%
+    public int candy(int[] ratings) { // beats 26.73% (1 pass, greedy), time: O(n), space: O(1)
+        if (ratings == null || ratings.length == 0) {
+            return -1;
+        }
+
+        int sum = 1;
+        int pre = 1;
+        int down = 0;
+
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                down++;
+            } else {
+                // check descending sequence before
+                if (down > 0) {
+                    // step 1: add from 1 to down
+                    sum += (down * (down + 1) / 2); // 1 + 2 + ... + down
+                    // step 2: add enough on prev
+                    if (down >= pre) {
+                        sum += (down - pre + 1);
+                    }
+                    pre = 1;
+                    down = 0;
+                }
+
+                pre = (ratings[i] == ratings[i - 1]) ? 1 : (pre + 1);
+                sum += pre;
+            }
+        }
+
+        // check last descending sequence
+        if (down > 0) {
+            sum += (down * (down + 1) / 2);
+            if (down >= pre) {
+                sum += (down - pre + 1);
+            }
+        }
+
+        return sum;
+    }
+
+    public int candyV0(int[] ratings) { // beats 26.73% (2 passes), time: O(n), space: O(n)
         if (ratings == null || ratings.length == 0) {
             return -1;
         }
