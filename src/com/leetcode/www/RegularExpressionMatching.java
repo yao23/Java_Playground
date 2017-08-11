6e5bc4f7523950d1f11f39144ec808d2df2c53ae
yao <myspiritcrazy@gmail.com>
2017-08-10 22:56:50 -0700

    fix bugs

--- src/com/leetcode/www/RegularExpressionMatching.java
+++ src/com/leetcode/www/RegularExpressionMatching.java
@@ -2,22 +2,33 @@ package com.leetcode.www;
 
 public class RegularExpressionMatching {
     public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
         int lenS = s.length();
         int lenP = p.length();
         boolean[][] dp = new boolean[lenS + 1][lenP + 1];
-
-        for (int j = 1; j <= lenP; j++) {
-            if (p.charAt(j - 1) == '*' && dp[0][j - 2]) {
-                dp[0][j] = true;
+        dp[0][0] = true;
+        for (int j = 0; j < lenP; j++) {
+            if (p.charAt(j) == '*' && dp[0][j - 1]) {
+                dp[0][j + 1] = true;
             }
         }
 
-        for (int i = 0; i <= lenS; i++) {
-            for (int j = 0; j <= lenP; j++) {
-                if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.');
-                } else {
-                    dp[i][j] = dp[i - 1][j - 1] && (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.');
+        for (int i = 0; i < lenS; i++) {
+            for (int j = 0; j < lenP; j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    } else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1]);
+                    }
                 }
             }
         }
