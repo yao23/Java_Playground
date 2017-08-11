77d8b0f1b2c47eeb7f5447d83e03979053b06492
yao <myspiritcrazy@gmail.com>
2017-08-10 23:45:58 -0700

    add a concise solution

--- src/com/leetcode/www/DecodeWays.java
+++ src/com/leetcode/www/DecodeWays.java
@@ -1,9 +1,38 @@
 package com.leetcode.www;
 
-import java.util.Arrays;
-
 public class DecodeWays {
-    public int numDecodings(String s) { // beats 6.01%
+    public int numDecodings(String s) { // beats 54.66%
+        int len = s.length();
+        if (0 == len || s.charAt(0) == '0') {
+            return 0;
+        }
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < len + 1; i++ ) {
+            char curChar = s.charAt(i - 1);
+            int curNum = curChar - '0';
+            if (curNum == 0 ) {	// s[i] is not valid
+                String TwoNum = s.substring(i - 2, i);
+                if( Integer.parseInt(TwoNum) <= 26 && Integer.parseInt(TwoNum) >= 10 ) { // s[i - 1][i] is valid
+                    dp[i] = dp[i - 2];
+                } else {
+                    dp[i] = 0;
+                }
+            } else { // s[i] is valid
+                String TwoNum = s.substring(i - 2, i);
+                if (Integer.parseInt(TwoNum) <= 26 && Integer.parseInt(TwoNum) >= 10 ) { // s[i - 1][i] is valid
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            }
+        }
+
+        return dp[len];
+    }
+
+    public int numDecodingsV0(String s) { // beats 6.01%
         if (s == null || s.length() == 0) {
             return 0;
         }
