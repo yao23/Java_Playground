a71a8441ef06dbc9142db37fe606bc3037054117
yao <myspiritcrazy@gmail.com>
2017-08-10 23:39:19 -0700

    refactor code to improve algorithm

--- src/com/leetcode/www/DecodeWays.java
+++ src/com/leetcode/www/DecodeWays.java
@@ -7,41 +7,22 @@ public class DecodeWays {
         if (s == null || s.length() == 0) {
             return 0;
         }
-        int[] dp = new int[s.length()];
-        for (int i = 0; i < s.length(); i++) {
-            dp[i] = (s.charAt(i) == '0') ? 0 : 1;
-        }
-
-        for (int i = 1; i < s.length(); i++) {
-            char pre = s.charAt(i - 1);
-            char cur = s.charAt(i);
-
-            if (cur == '0') {
-                if (pre == '0') {
-                    return 0;
-                } else {
-                    dp[i] = dp[i - 1];
-                }
-            } else {
-                if (pre == '0') {
-                    dp[i] = dp[i - 1];
-                } else {
-                    String sub = s.substring(i - 1, i + 1);
-                    int num = Integer.valueOf(sub);
-                    if (10 <= num && num <= 26) {
-                        if (i < 2) {
-                            dp[i] = dp[i - 1] + 1;
-                        } else {
-                            dp[i] = dp[i - 1] + dp[i - 2];
-                        }
-                    } else {
-                        dp[i] = dp[i - 1];
-                    }
-                }
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = (s.charAt(0) != '0') ? 1 : 0;
+        for (int i = 2; i <= n; i++) {
+            int first = Integer.valueOf(s.substring(i-1, i));
+            int second = Integer.valueOf(s.substring(i-2, i));
+            if (first >= 1 && first <= 9) {
+                dp[i] += dp[i-1];
+            }
+            if (second >= 10 && second <= 26) {
+                dp[i] += dp[i-2];
             }
         }
-
-        return dp[s.length() - 1];
+        
+        return dp[n];
     }
 }
 
