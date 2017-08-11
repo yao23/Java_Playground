e50a6eb3c6adaa0c7203ad5bf4ad5b57047200b5
yao <myspiritcrazy@gmail.com>
2017-08-10 23:14:14 -0700

    Decode Ways

---
+++ src/com/leetcode/www/DecodeWays.java
@@ -0,0 +1,39 @@
+package com.leetcode.www;
+
+import java.util.Arrays;
+
+public class DecodeWays {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < s.length(); i++) {
+            char pre = s.charAt(i - 1);
+            char cur = s.charAt(i);
+
+            if (cur == '0') {
+                if (pre == '0') {
+                    return 0;
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            } else {
+                if (pre == '0') {
+                    dp[i] = dp[i - 1];
+                } else {
+                    String sub = s.substring(i - 1, i + 1);
+                    int num = Integer.valueOf(sub);
+                    if (10 <= num && num <= 26) {
+                        dp[i] = dp[i - 1] + dp[i - 2];
+                    } else {
+                        dp[i] = dp[i - 1];
+                    }
+                }
+            }
+        }
+
+        return dp[s.length() - 1];
+    }
+}
