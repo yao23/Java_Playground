b743379c0afb4156f3c34acdd16573fef520bdd9
yao <myspiritcrazy@gmail.com>
2017-08-11 23:25:55 -0700

    add bottom up solution

--- src/com/leetcode/www/GuessNumberHigherOrLowerII.java
+++ src/com/leetcode/www/GuessNumberHigherOrLowerII.java
@@ -21,6 +21,21 @@ public class GuessNumberHigherOrLowerII { // LC 375
         t[start][end] = res;
         return res;
     }
+
+    public int getMoneyAmountV0(int n) { // beats 54.44% (bottom up)
+        int[][] table = new int[n + 1][n + 1];
+        for (int j = 2; j <= n; j++) {
+            for (int i = j - 1; i > 0; i--){
+                int globalMin = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++){
+                    int localMax = k + Math.max(table[i][k - 1], table[k + 1][j]);
+                    globalMin = Math.min(globalMin, localMax);
+                }
+                table[i][j] = (i + 1 == j) ? i : globalMin;
+            }
+        }
+        return table[1][n];
+    }
 }
 
 // n = 10, I pick 8. => 21 (5 + 7 + 9)
