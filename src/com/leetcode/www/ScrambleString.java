5e7a8413d143474fb2d636318e42d3727262bfac
yao <myspiritcrazy@gmail.com>
2017-08-12 12:16:27 -0700

    add itreative solution

--- src/com/leetcode/www/ScrambleString.java
+++ src/com/leetcode/www/ScrambleString.java
@@ -1,7 +1,7 @@
 package com.leetcode.www;
 
 public class ScrambleString { // LC 87
-    public boolean isScramble(String s1, String s2) { // beats 64.52%
+    public boolean isScramble(String s1, String s2) { // beats 64.52% (recursion)
         if (s1.equals(s2)) {
             return true;
         }
@@ -29,6 +29,53 @@ public class ScrambleString { // LC 87
             }
         }
         return false;
+    }
+
+    public boolean isScrambleV1(String s1, String s2) { // beats 86.81% (iterative)
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int len = s1.length();
+        /**
+         * Let F(i, j, k) = whether the substring S1[i..i + k - 1] is a scramble of S2[j..j + k - 1] or not
+         * Since each of these substrings is a potential node in the tree, we need to check for all possible cuts.
+         * Let q be the length of a cut (hence, q < k), then we are in the following situation:
+         *
+         * S1 [   x1    |         x2         ]
+         *    i         i + q                i + k - 1
+         *
+         * here we have two possibilities:
+         *
+         * S2 [   y1    |         y2         ]
+         *    j         j + q                j + k - 1
+         *
+         * or
+         *
+         * S2 [       y1        |     y2     ]
+         *    j                 j + k - q    j + k - 1
+         *
+         * which in terms of F means:
+         *
+         * F(i, j, k) = for some 1 <= q < k we have:
+         *  (F(i, j, q) AND F(i + q, j + q, k - q)) OR (F(i, j + k - q, q) AND F(i + q, j, k - q))
+         *
+         * Base case is k = 1, where we simply need to check for S1[i] and S2[j] to be equal
+         * */
+        boolean [][][] F = new boolean[len][len][len + 1];
+        for (int k = 1; k <= len; ++k) {
+            for (int i = 0; i + k <= len; ++i) {
+                for (int j = 0; j + k <= len; ++j) {
+                    if (k == 1) {
+                        F[i][j][k] = s1.charAt(i) == s2.charAt(j);
+                    } else {
+                        for (int q = 1; q < k && !F[i][j][k]; ++q) {
+                            F[i][j][k] = (F[i][j][q] && F[i + q][j + q][k - q]) || (F[i][j + k - q][q] && F[i + q][j][k - q]);
+                        }
+                    }
+                }
+            }
+        }
+        return F[0][0][len];
     }
 
     public boolean isScrambleV0(String s1, String s2) { // not working for test case 5 ("abc", "bca")
