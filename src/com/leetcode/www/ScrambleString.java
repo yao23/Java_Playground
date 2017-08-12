fd76a48be2cfd692633fc413d237ad8a20fe8345
yao <myspiritcrazy@gmail.com>
2017-08-12 12:07:57 -0700

    add resursion solution and solution distribution

--- src/com/leetcode/www/ScrambleString.java
+++ src/com/leetcode/www/ScrambleString.java
@@ -1,6 +1,36 @@
 package com.leetcode.www;
 
 public class ScrambleString { // LC 87
+    public boolean isScramble(String s1, String s2) { // beats 64.52%
+        if (s1.equals(s2)) {
+            return true;
+        }
+
+        int[] letters = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            letters[s1.charAt(i) - 'a']++;
+            letters[s2.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (letters[i] != 0) {
+                return false;
+            }
+        }
+
+        for (int i = 1; i < s1.length(); i++) {
+            if (isScramble(s1.substring(0, i), s2.substring(0, i))
+                    && isScramble(s1.substring(i), s2.substring(i))) {
+                return true;
+            }
+            // root is non-left node, so it has to check 1st level left and right child (test case 5, "abc", "bca")
+            if (isScramble(s1.substring(0, i), s2.substring(s2.length() - i))
+                    && isScramble(s1.substring(i), s2.substring(0, s2.length() - i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public boolean isScrambleV0(String s1, String s2) { // not working for test case 5 ("abc", "bca")
         int len1 = s1.length();
         int len2 = s2.length();
