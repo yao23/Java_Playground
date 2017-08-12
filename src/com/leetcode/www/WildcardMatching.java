10dbce68f3eae9169902d5c8f1ee5813a2d07696
yao <myspiritcrazy@gmail.com>
2017-08-12 10:31:18 -0700

    add another solution with linear runtime and constant space

--- src/com/leetcode/www/WildcardMatching.java
+++ src/com/leetcode/www/WildcardMatching.java
@@ -1,7 +1,64 @@
 package com.leetcode.www;
 
 public class WildcardMatching { // LC 44
-    public boolean isMatch(String s, String p) { // beats 58.79%
+    /**
+     *
+     * @param str
+     * @param pattern
+     * @return
+     *
+     * For each element in s
+     * If *s == *p or *p == ? which means this is a match, then goes to next element s++ p++.
+     * If p == '*', this is also a match, but one or many chars may be available, so let us save this *'s position and the matched s position.
+     * If not match, then we check if there is a * previously showed up,
+     * if there is no *,  return false;
+     * if there is an *,  we set current p to the next element of *, and set current s to the next saved s position.
+
+     * e.g.
+
+       abed
+       ?b*d**
+
+       a=?, go on, b=b, go on,
+       e=*, save * position star=3, save s position ss = 3, p++
+       e!=d,  check if there was a *, yes, ss++, s=ss; p=star+1
+       d=d, go on, meet the end.
+       check the rest element in p, if all are *, true, else false;
+     */
+    public boolean isMatch(String str, String pattern) { // beats 95.12%
+        int s = 0, p = 0, match = 0, starIdx = -1;
+        while (s < str.length()) {
+            // advancing both pointers
+            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))) {
+                s++;
+                p++;
+            }
+            // * found, only advancing pattern pointer
+            else if (p < pattern.length() && pattern.charAt(p) == '*') {
+                starIdx = p;
+                match = s;
+                p++;
+            }
+            // last pattern pointer was *, advancing string pointer
+            else if (starIdx != -1) {
+                p = starIdx + 1;
+                match++;
+                s = match;
+            }
+            //current pattern pointer is not star, last patter pointer was not *
+            //characters do not match
+            else return false;
+        }
+
+        //check for remaining characters in pattern
+        while (p < pattern.length() && pattern.charAt(p) == '*') {
+            p++;
+        }
+
+        return p == pattern.length();
+    }
+
+    public boolean isMatchV2(String s, String p) { // beats 58.79% (from end to beginning)
         int lenS = s.length();
         int lenP = p.length();
         boolean[][] match = new boolean[lenS + 1][lenP + 1];
