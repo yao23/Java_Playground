28b9bcefb2b63a8125b845ecb1e81d940d437b36
yao <myspiritcrazy@gmail.com>
2017-08-12 11:27:54 -0700

    Scramble String

--- src/com/leetcode/www/ScrambleString.java
+++ src/com/leetcode/www/ScrambleString.java
@@ -2,7 +2,49 @@ package com.leetcode.www;
 
 public class ScrambleString { // LC 87
     public boolean isScramble(String s1, String s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        if (len1 != len2) {
+            return false;
+        }
+        boolean[] dp = new boolean[len1 + 1];
+        dp[0] = true;
+        for (int len = 1; len <= len1; len++) {
+            for (int i = 0; i < len; i++) {
+                if (dp[i]) {
+                    String right1 = s1.substring(i, len);
+                    String right2 = s2.substring(i, len);
+                    if (isMatch(right1, right2)) {
+                        dp[len] = true;
+                    }
+                }
+            }
+        }
+        return dp[len1];
+    }
 
+    private boolean isMatch(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return true;
+        } else if (s1.equals(reverseStr(s2))) {
+            return true;
+        } else {
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) == s2.charAt(i) && isMatch(s1.substring(i), s2.substring(i))) {
+                    continue;
+                } else if (isMatch(s1.substring(0, i + 1), s2.substring(0, i + 1)) && isMatch(s1.substring(i),  s2.substring(i))) {
+                    continue;
+                } else {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    private String reverseStr(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 }
 
