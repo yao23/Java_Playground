1adf63e11d076463a65f1f7ec2412a875322efb9
yao <myspiritcrazy@gmail.com>
2017-08-11 22:55:03 -0700

    Russian Doll Envelopes

---
+++ src/com/leetcode/www/RussianDollEnvelopes.java
@@ -0,0 +1,57 @@
+package com.leetcode.www;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class RussianDollEnvelopes { // LC 354
+    public int maxEnvelopes(int[][] envelopes) {
+        int num = envelopes.length;
+        if (num == 0) {
+            return 0;
+        }
+        List<Envelope> list = new ArrayList<>(num);
+        for (int[] envelope : envelopes) {
+            list.add(new Envelope(envelope[0], envelope[1]));
+        }
+        Collections.sort(list);
+        int[] dp = new int[num];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (list.get(i).h > list.get(j).h) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i] > max) {
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+
+    class Envelope implements Comparable<Envelope> {
+        int w;
+        int h;
+        public Envelope(int w, int h) {
+            this.w = w;
+            this.h = h;
+        }
+        public int compareTo(Envelope that) {
+            if (this.w < that.w) {
+                return -1;
+            } else if (this.w == that.w) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+}
+
+// [[5,4],[6,4],[6,7],[2,3]] => 3 (([2,3] => [5,4] => [6,7]))
+// [[4,5],[4,6],[6,7],[2,3],[1,1]] => 4
