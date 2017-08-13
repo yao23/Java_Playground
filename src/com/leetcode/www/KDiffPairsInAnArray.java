4298ce973eff7b6c7b5b2089d14fc16bd237db51
yao <myspiritcrazy@gmail.com>
2017-08-13 11:09:56 -0700

    add a solution with HashMap

--- src/com/leetcode/www/KDiffPairsInAnArray.java
+++ src/com/leetcode/www/KDiffPairsInAnArray.java
@@ -1,9 +1,47 @@
 package com.leetcode.www;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class KDiffPairsInAnArray { // LC 532
-    public int findPairs(int[] nums, int k) {
+    public int findPairs(int[] nums, int k) { // beats 29.75%
+        if (nums == null || nums.length == 0 || k < 0)   return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (k == 0) {
+                //count how many elements in the array that appear more than twice.
+                if (entry.getValue() >= 2) {
+                    count++;
+                }
+            } else {
+                if (map.containsKey(entry.getKey() + k)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public int findPairsV1(int[] nums, int k) {
+        int ans = 0;
+        Arrays.sort(nums);
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            for (j = Math.max(j, i + 1); j < nums.length && (long) nums[j] - nums[i] < k; j++) ;
+            if (j < nums.length && (long) nums[j] - nums[i] == k) ans++;
+            while (i + 1 < nums.length && nums[i] == nums[i + 1]) i++;
+        }
+        return ans;
+    }
+
+    public int findPairsV0(int[] nums, int k) { // not working for test case 5 ([1,1,1,1,1], 0 => 1)
         if (nums == null || nums.length == 0) {
             return 0;
         }
@@ -44,3 +82,5 @@ public class KDiffPairsInAnArray { // LC 532
 // Explanation: There is one 0-diff pair in the array, (1, 1).
 
 // [1,2,3,4,5], -1 => 0
+
+// [1,1,1,1,1], 0 => 1
