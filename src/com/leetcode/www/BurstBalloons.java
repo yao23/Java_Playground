267e9a46143122202c0b9a1ea3d54f772c66566f
yao <myspiritcrazy@gmail.com>
2017-08-12 20:14:14 -0700

    add DP solution

--- src/com/leetcode/www/BurstBalloons.java
+++ src/com/leetcode/www/BurstBalloons.java
@@ -4,78 +4,34 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class BurstBalloons {
-    public int maxCoins(int[] nums) {
+    public int maxCoins(int[] nums) { // beats 53.06% (DP)
         int len = nums.length;
         if (len == 0) {
             return 0;
         } else if (len == 1) {
             return nums[0];
         } else {
-            int sum = 0;
-            int up = 0;
-            int down = 0;
-            Deque<Integer> stack = new ArrayDeque<>();
-            stack.push(nums[0]);
-            for (int i = 1; i < len; i++) {
-                int cur = nums[i], pre = stack.peek();
-                if (cur < pre) { // go down
-                    if (i == 1) {
-                        stack.push(cur);
-                        down++;
-                    } else {
-                        if (i + 1 < len) {
-                            int next = nums[i + 1];
-                            if (cur < next) { // cur < pre && cur < next
-                                sum += (pre * cur * next);
-                            } else { // cur < pre && cur > next
-                                stack.push(cur);
-                                down++;
-                            }
-                        } else { // last element
-                            if (stack.isEmpty()) {
-                                sum += cur;
-                            } else {
-                                int top = stack.pop();
-                                if (stack.isEmpty()) {
-                                    sum += (top * cur);
-                                    if (top < cur) {
-                                        stack.push(cur);
-                                    } else {
-                                        stack.push(top);
-                                    }
-                                } else {
-                                    if (stack.peek() > top && top < cur) {
-                                        sum += (stack.peek() * top * cur);
-                                        stack.push(cur);
-                                    } else {
-
-                                    }
-                                }
-                            }
-                        }
-                    }
-                } else { // cur > pre, go up
-                    if (i == 1) {
-                        stack.push(cur);
-                        up++;
-                    } else {
-                        if (i + 1 < len) {
-                            int next = nums[i + 1];
-                            if (cur < next) { // cur < pre && cur < next
-                                sum += (pre * cur * next);
-                            } else { // cur < pre && cur > next
-                                stack.push(cur);
-                                down++;
-                            }
-                        } else { // last element
-                            stack.push(cur);
-                            down++;
-                        }
-                    }
+            int[] numbers = new int[len + 2];
+            int n = 1;
+            for (int x : nums) {
+                if (x > 0) {
+                    numbers[n++] = x;
                 }
             }
+            numbers[0] = numbers[n++] = 1;
 
-            return sum;
+
+            int[][] dp = new int[n][n];
+            for (int k = 2; k < n; ++k)
+                for (int left = 0; left < n - k; ++left) {
+                    int right = left + k;
+                    for (int i = left + 1; i < right; ++i) {
+                        dp[left][right] = Math.max(dp[left][right],
+                                numbers[left] * numbers[i] * numbers[right] + dp[left][i] + dp[i][right]);
+                    }
+                }
+
+            return dp[0][n - 1];
         }
     }
 }
