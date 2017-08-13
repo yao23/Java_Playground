18d3768fd4a31420fbe876bc6c4daaf6a81bf6d2
yao <myspiritcrazy@gmail.com>
2017-08-12 23:20:25 -0700

    add a clean solution

--- src/com/leetcode/www/ReverseWordsInAString.java
+++ src/com/leetcode/www/ReverseWordsInAString.java
@@ -1,7 +1,36 @@
 package com.leetcode.www;
 
 public class ReverseWordsInAString {
+    public char[] reverse(char[] arr, int i, int j) { // beats 80.56%
+        while (i < j) {
+            char tmp = arr[i];
+            arr[i++] = arr[j];
+            arr[j--] = tmp;
+        }
+        return arr;
+    }
+
     public String reverseWords(String s) {
+        // reverse the whole string and convert to char array
+        char[] str = reverse(s.toCharArray(), 0, s.length()-1);
+        int start = 0, end = 0; // start and end positions of a current word
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] != ' ') { // if the current char is letter
+                str[end++] = str[i]; // just move this letter to the next free pos
+            } else if (i > 0 && str[i-1] != ' ') { // if the first space after word
+                reverse(str, start, end-1); // reverse the word
+                str[end++] = ' '; // and put the space after it
+                start = end; // move start position further for the next word
+            }
+        }
+        reverse(str, start, end-1); // reverse the tail word if it's there
+        // here's an ugly return just because we need to return Java's String
+        // also as there could be spaces at the end of original string
+        // we need to consider redundant space we have put there before
+        return new String(str, 0, end > 0 && str[end-1] == ' ' ? end-1 : end);
+    }
+
+    public String reverseWordsV0(String s) { // not working
         if (s == null || s.length() < 2) {
             if (s.equals(" ")) {
                 return "";
@@ -48,7 +77,7 @@ public class ReverseWordsInAString {
                 if (strArr[right] != ' ') {
                     right++;
                 } else {
-                    String tmp = reverse(strArr, left, right - 1);
+                    String tmp = reverseV0(strArr, left, right - 1);
                     res = assembleStr(tmp, res);
                     right++;
                     left = right;
@@ -56,7 +85,7 @@ public class ReverseWordsInAString {
             }
 
             // reverse the last word
-            String tmp = reverse(strArr, left, right - 1);
+            String tmp = reverseV0(strArr, left, right - 1);
             res = assembleStr(tmp, res);
 
             return res;
@@ -73,7 +102,7 @@ public class ReverseWordsInAString {
         return res;
     }
 
-    private String reverse(char[] ch, int start, int end) {
+    private String reverseV0(char[] ch, int start, int end) {
         while (start < end) {
             char tmp = ch[start];
             ch[start] = ch[end];
