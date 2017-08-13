5f0288a35f41b3152f996931ac4eded63081b0db
yao <myspiritcrazy@gmail.com>
2017-08-13 10:14:17 -0700

    Three Sum Smaller

--- src/com/leetcode/www/ThreeSumSmaller.java
+++ src/com/leetcode/www/ThreeSumSmaller.java
@@ -1,9 +1,11 @@
+package com.leetcode.www;
+
 import java.util.Arrays;
 
 /**
  * Created by liyao on 7/8/17.
  */
-public class ThreeSumSmaller {
+public class ThreeSumSmaller { // LC 259
     public static int threeSumSmaller(int[] nums, int target) {
         int len = nums.length;
         if (len < 3) {
@@ -32,3 +34,7 @@ public class ThreeSumSmaller {
         System.out.println("Three sum smaller: " + threeSumSmaller(arr, target));
     }
 }
+
+// [], 0 => 0
+// [-2, 0, 1, 3], 2 => 2 ([-2, 0, 1], [-2, 0, 3])
+// beats 33.13%
