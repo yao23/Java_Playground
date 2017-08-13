212f8dd03d1c2c9f854770d3b197d16231243714
yao <myspiritcrazy@gmail.com>
2017-08-12 23:25:24 -0700

    refactor code

--- src/com/leetcode/www/ReverseWordsInAString.java
+++ src/com/leetcode/www/ReverseWordsInAString.java
@@ -1,11 +1,13 @@
 package com.leetcode.www;
 
 public class ReverseWordsInAString {
-    public char[] reverse(char[] arr, int i, int j) { // beats 80.56%
-        while (i < j) {
-            char tmp = arr[i];
-            arr[i++] = arr[j];
-            arr[j--] = tmp;
+    public char[] reverse(char[] arr, int start, int end) { // beats 80.56%
+        while (start < end) {
+            char tmp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = tmp;
+            start++;
+            end--;
         }
         return arr;
     }
@@ -13,21 +15,26 @@ public class ReverseWordsInAString {
     public String reverseWords(String s) {
         // reverse the whole string and convert to char array
         char[] str = reverse(s.toCharArray(), 0, s.length()-1);
-        int start = 0, end = 0; // start and end positions of a current word
+
+        // start and end positions of a current word
+        int start = 0, end = 0;
+
         for (int i = 0; i < str.length; i++) {
             if (str[i] != ' ') { // if the current char is letter
-                str[end++] = str[i]; // just move this letter to the next free pos
-            } else if (i > 0 && str[i-1] != ' ') { // if the first space after word
-                reverse(str, start, end-1); // reverse the word
-                str[end++] = ' '; // and put the space after it
+                str[end] = str[i]; // just move this letter to the next free pos
+                end++;
+            } else if (i > 0 && str[i - 1] != ' ') { // if the first space after word
+                reverse(str, start, end - 1); // reverse the word
+                str[end] = ' '; // and put the space after it
+                end++;
                 start = end; // move start position further for the next word
             }
         }
-        reverse(str, start, end-1); // reverse the tail word if it's there
+        reverse(str, start, end - 1); // reverse the tail word if it's there
         // here's an ugly return just because we need to return Java's String
         // also as there could be spaces at the end of original string
         // we need to consider redundant space we have put there before
-        return new String(str, 0, end > 0 && str[end-1] == ' ' ? end-1 : end);
+        return new String(str, 0, end > 0 && str[end - 1] == ' ' ? (end - 1) : end);
     }
 
     public String reverseWordsV0(String s) { // not working
