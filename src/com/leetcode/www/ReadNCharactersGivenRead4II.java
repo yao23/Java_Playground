5b3a0f0146dbe02cfe1e525633f3921d1c15c5db
yao <myspiritcrazy@gmail.com>
2017-08-19 10:20:58 -0700

    Read N Characters Given Read4 II - Call multiple times

---
+++ src/com/leetcode/www/ReadNCharactersGivenRead4II.java
@@ -0,0 +1,53 @@
+package com.leetcode.www;
+
+public class ReadNCharactersGivenRead4II {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     *
+     * The read function may be called multiple times.
+     */
+    public int read(char[] buf, int n) {
+        boolean eof = false;
+        int total = 0;
+        char[] tmp = new char[4];
+        int tmpSize = 0;
+        int tmpIndex = 0;
+        int len = 0;
+
+        while (!eof && total < n) {
+            if (tmpSize == 0) {
+                len = read4(tmp);
+                tmpSize = len;
+            }
+            eof = (len < 4);
+            len = Math.min(tmpSize, n - total);
+
+            for (int i = tmpIndex; i < tmpIndex + len; i++) {
+                buf[total] = tmp[i];
+                total++;
+                tmpIndex++;
+                tmpSize--;
+            }
+            if (tmpSize == 0) {
+                tmpIndex = 0;
+            }
+        }
+
+        return total;
+    }
+    
+    private char[] chars = new char[100];
+    private int offset = 0;
+    private int read4(char[] buf) {
+        int len = 4;
+        if (offset + 4 < chars.length) {
+            len = chars.length - offset;
+        }
+        for (int i = 0; i < len; i++) {
+            buf[i] = chars[offset + i];
+        }
+        return len;
+    }
+}
