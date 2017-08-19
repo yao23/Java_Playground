4c0ebe5efd5a223035488448e492a15cd60f60b5
yao <myspiritcrazy@gmail.com>
2017-08-19 09:47:57 -0700

    add a clean solution

--- src/com/leetcode/www/ReadNCharactersGivenRead4.java
+++ src/com/leetcode/www/ReadNCharactersGivenRead4.java
@@ -10,6 +10,30 @@ public class ReadNCharactersGivenRead4 { // LC 157
      * @return    The number of characters read
      */
     public int read(char[] buf, int n) { // beats 21.61%
+        boolean eof = false;      // end of file flag
+        int total = 0;            // total bytes have read
+        char[] tmp = new char[4]; // temp buffer
+
+        while (!eof && total < n) {
+            int count = read4(tmp);
+
+            // check if it's the end of the file
+            eof = count < 4;
+
+            // get the actual count (test case 1)
+            count = Math.min(count, n - total);
+
+            // copy from temp buffer to buf
+            for (int i = 0; i < count; i++) {
+                buf[total] = tmp[i];
+                total++;
+            }
+        }
+
+        return total;
+    }
+
+    public int readV0(char[] buf, int n) { // beats 21.61%
         int res = 0;
         char[] tmp = new char[4];
 
