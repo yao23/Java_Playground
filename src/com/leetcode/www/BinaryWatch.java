45a9fff37a496a43713fe8df334b935abfb8db3d
yao <myspiritcrazy@gmail.com>
2017-08-20 16:26:26 -0700

    refactor code to fix bug

--- src/com/leetcode/www/BinaryWatch.java
+++ src/com/leetcode/www/BinaryWatch.java
@@ -1,44 +1,54 @@
 package com.leetcode.www;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BinaryWatch { // LC 401
-    private int totalHour = 24;
-    private int totalMinute = 60;
-    private int[] time = new int[] {8, 4, 2, 1, 32, 16, 8, 4, 2, 1};
+    private int totalHour = 12;
+    private int totalMinute = 59;
+    private int[] time = new int[] {1, 2, 4, 8, 16, 32, 1, 2, 4, 8};
 
     public List<String> readBinaryWatch(int num) {
         List<String> res = new ArrayList<>();
-        dfs(totalHour, totalMinute, num, res);
-        return res;
+        if (num == 0) {
+            res.add("0:00");
+            return res;
+        } else {
+            dfs(totalHour, totalMinute, num, res, new HashSet<>());
+            return res;
+        }
     }
 
-    private void dfs(int remainHour, int remainMinute, int num, List<String> res) {
+    private void dfs(int remainHour, int remainMinute, int num, List<String> res, Set<Integer> timeIdx) {
         if (num == 0) {
             int hour = totalHour - remainHour;
             int minute = totalMinute - remainMinute;
-            res.add(hour + ":" + (minute < 10 ? "0" : "") + minute);
+            res.add(hour + ":" + (minute < 10 ? "0" : "") + minute); System.out.println("res: " + res.get(res.size() - 1));System.out.println();
             return;
-        }
-        for (int i = 0; i < 10; i++) {
-            for (int j = 1; j <= num; j++) {
-                int remain = getRemainTime(i, j, remainHour, remainMinute);
-                if (remain > 0) {
-                    if (i < 4) {
-                        dfs(remain, remainMinute, num - j, res);
-                    } else {
-                        dfs(remainHour, remain, num - j, res);
-                    }
-                } else {
-                    return;
+        } System.out.println();
+        for (int i = 0; i < 10 && !timeIdx.contains(i); i++) {
+            timeIdx.add(i);
+            int remain = getRemainTime(i, 1, remainHour, remainMinute);
+            System.out.println("i: " + i + ", " + time[i] + ", count: " + 1 + ", num: " + num);
+            System.out.println(remainHour + ", " + remainMinute + ", " + remain);
+
+            if (remain > 0) {
+                if (i > 5) { // hour
+                    dfs(remain, remainMinute, num - 1, res, timeIdx);
+                } else { // minute
+                    dfs(remainHour, remain, num - 1, res, timeIdx);
                 }
+                timeIdx.remove(i);
+            } else {
+                return;
             }
         }
     }
 
     private int getRemainTime(int i, int j, int remainHour, int remainMinute) {
-        if (i < 4) { // hour
+        if (i > 5) { // hour
             return (remainHour - j * time[i]);
         } else { // minute
             return (remainMinute - j * time[i]);
