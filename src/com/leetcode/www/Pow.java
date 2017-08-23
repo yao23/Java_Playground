d08b1b90ca3a60a7666e845e16027c93d4a5e125
yao <myspiritcrazy@gmail.com>
2017-08-22 21:12:50 -0700

    add iterative solution

--- src/com/leetcode/www/Pow.java
+++ src/com/leetcode/www/Pow.java
@@ -25,6 +25,38 @@ public class Pow { // LC 50
             return res;
         }
     }
+
+    public double myPowV0(double x, int n) { // beast 78.25%
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 1) {
+            return 1;
+        }
+        if (x == -1) {
+            if (n % 2 == 0) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+        if (n == Integer.MIN_VALUE) {
+            return 0;
+        }
+        if (n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+        double ret = 1.0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ret *= x;
+            }
+            x = x * x;
+            n = n >> 1;
+        }
+        return ret;
+    }
 }
 
 // 8.88023, 3 => 700.28148
