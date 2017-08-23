a444565e5a63227341632f4c65919e492b185db5
yao <myspiritcrazy@gmail.com>
2017-08-22 21:06:06 -0700

    solution distribution

--- src/com/leetcode/www/Pow.java
+++ src/com/leetcode/www/Pow.java
@@ -1,8 +1,8 @@
 package com.leetcode.www;
 
 public class Pow { // LC 50
-    public double myPow(double x, int n) {
-        if (n > 0) {
+    public double myPow(double x, int n) { // beats 28.57%
+        if (n >= 0) {
             return power(x, n);
         } else {
             return 1.0/power(x, -n);
@@ -17,7 +17,7 @@ public class Pow { // LC 50
         } else {
             double half = myPow(x, n >>> 1);
             double res = 1;
-            if ((n & 0x1) == 0) { // even
+            if ((n & 0x1) == 0) { // even, pay attention to the (n & 0x1)
                 res = (half * half);
             } else { // odd
                 res = (half * half * x);
