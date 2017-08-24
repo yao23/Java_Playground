57db6c8733925c726f5d4ec9d94df66085cf4ed6
yao <myspiritcrazy@gmail.com>
2017-08-23 19:58:27 -0700

    add a clean solution

--- src/com/leetcode/www/Sqrt.java
+++ src/com/leetcode/www/Sqrt.java
@@ -1,7 +1,7 @@
 package com.leetcode.www;
 
 public class Sqrt { // LC 69
-    public int mySqrt(int x) { // beats 14.13%
+    public int mySqrtV0(int x) { // beats 14.13%
         if (x == 0 || x == 1) {
             return x;
         }
@@ -19,6 +19,17 @@ public class Sqrt { // LC 69
         }
 
         return (start + end) / 2;
+    }
+
+    public int mySqrt(int x) {  // beats 64.54%
+        if (x < 4) {
+            return x == 0 ? 0 : 1;
+        }
+        int res = 2 * mySqrt(x / 4);
+        if ((res + 1) * (res + 1) <= x && (res + 1) * (res + 1) >= 0) {
+            return res + 1;
+        }
+        return res;
     }
 }
 
