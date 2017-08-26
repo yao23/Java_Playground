b87856c8ba4cee1b85b0421432616e9a8578c359
yao <myspiritcrazy@gmail.com>
2017-08-26 15:43:23 -0700

    Reverse Integer

---
+++ src/com/leetcode/www/ReverseInteger.java
@@ -0,0 +1,19 @@
+package com.leetcode.www;
+
+public class ReverseInteger {
+    public int reverse(int x) { // beats 41.73%
+        int result = 0;
+
+        while (x != 0) {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result) {
+                return 0;
+            }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
