4b03ec6ceb254ce8eb7508a2fb4b41d0be30f0e9
yao <myspiritcrazy@gmail.com>
2017-08-26 15:27:29 -0700

    add a clean solution

--- src/com/leetcode/www/LongestPalindromicSubstring.java
+++ src/com/leetcode/www/LongestPalindromicSubstring.java
@@ -1,7 +1,33 @@
 package com.leetcode.www;
 
 public class LongestPalindromicSubstring { // LC 5
-    public String longestPalindrome(String s) { // beats 43.63%
+    private int lo, maxLen;
+
+    public String longestPalindrome(String s) { // beats 91.56%
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+
+        for (int i = 0; i < len - 1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i + 1); //assume even length.
+        }
+        return s.substring(lo, lo + maxLen);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (maxLen < k - j - 1) {
+            lo = j + 1;
+            maxLen = k - j - 1;
+        }
+    }
+
+    public String longestPalindromeV1(String s) { // beats 43.63%
         if (s.length() == 0) {
             return "";
         } else if (s.length() == 1)	{
