ec2d18e06232dfa7db45e9c5e76a865c70f57134
yao <myspiritcrazy@gmail.com>
2017-08-26 15:23:03 -0700

    add a DP solution

--- src/com/leetcode/www/LongestPalindromicSubstring.java
+++ src/com/leetcode/www/LongestPalindromicSubstring.java
@@ -3,7 +3,7 @@ package com.leetcode.www;
 public class LongestPalindromicSubstring { // LC 5
     public String longestPalindrome(String s) { // beats 43.63%
         if (s.length() == 0) {
-            return "";	// try dynamic programming (summary)
+            return "";
         } else if (s.length() == 1)	{
             return s;
         } else {
@@ -44,6 +44,56 @@ public class LongestPalindromicSubstring { // LC 5
         }
 
         return s.substring(l_tmp, r_tmp + 1);
+    }
+
+    public static String longestPalindromeV0(String s) { // beats 23.13% (DP)
+        String tmpS = preprocess(s);
+        int[] p = new int[tmpS.length()];
+        int centerIdx = 0, maxBound = 0;
+        for (int i = 1; i < tmpS.length() - 1; i++ ) {
+            int j = 2 * centerIdx - i; // i's mirror index in left
+            if (maxBound > i) {
+                p[i] = Math.min(maxBound - i, p[j]);
+            } else {
+                p[i] = 0;
+            }
+
+            while (tmpS.charAt(i - 1 - p[i]) == tmpS.charAt(i + 1 + p[i])) {
+                p[i]++;
+            }
+
+            if (i + p[i] > maxBound) {
+                maxBound = i + p[i];
+                centerIdx = i;
+            }
+        }
+        int maxLen = 0;
+        for (int i = 1; i < tmpS.length() - 1; i++) {
+            // System.out.print(p[i] + " ");
+            if (p[i] > maxLen) {
+                maxLen = p[i];
+                centerIdx = i;
+            }
+        }
+        int start = (centerIdx - 1 - maxLen) >> 1; // skip the "^"
+        // System.out.println("\n start: " + start + " MaxLen: " + MaxLen);
+        if (start + maxLen == s.length()) {
+            return s.substring(start);
+        } else {
+            return s.substring(start, start + maxLen);
+        }
+    }
+
+    private static String preprocess(String s) {
+        String res = "";
+        res += "^"; // string start
+        for (int i = 0; i < s.length(); i++) {
+            res = res + "#" + s.charAt(i);
+        }
+        res += "#";
+        res += "$"; // string end
+        //System.out.println("Preprocessed string: " + res);
+        return res;
     }
 }
 
