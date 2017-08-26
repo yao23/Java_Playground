dbf65deedee030d834314e6a5de66e3820fcf093
yao <myspiritcrazy@gmail.com>
2017-08-26 15:35:26 -0700

    ZigZag Conversion

---
+++ src/com/leetcode/www/ZigZagConversion.java
@@ -0,0 +1,39 @@
+package com.leetcode.www;
+
+public class ZigZagConversion {
+    public String convert(String s, int nRows) {
+        if (s == null || s.length() == 0 || nRows <= 1) {
+            return s;
+        } else {
+            return GenerateStr(s, nRows);
+        }
+    }
+    private String GenerateStr(String s, int nRows) {
+        int len = s.length();
+        StringBuilder stringBuilder = new StringBuilder();
+        int diff = 2 * nRows - 2; // ignore 2 elements in top and bottom row
+        for (int i = 0; i < nRows && i < len; i++ ) {
+            if (i == 0 || i == nRows - 1) {
+                stringBuilder.append(s.charAt(i));
+                int index = i;
+                while (index + diff < len) {
+                    stringBuilder.append(s.charAt(index + diff));
+                    index = index + diff;
+                }
+            } else {
+                stringBuilder.append(s.charAt(i));
+                int index = i;
+                while (2 * nRows - 2 * i - 2 + index < len || index + diff < len) {
+                    if (2 * nRows - 2 * i - 2 + index < len) { // exact distance
+                        stringBuilder.append(s.charAt(2 * nRows - 2 * i - 2 + index));
+                    }
+                    if (index + diff < len) {
+                        stringBuilder.append(s.charAt(index + diff));
+                    }
+                    index += diff;
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
