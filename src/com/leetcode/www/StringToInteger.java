d4337b1151afd687d6853f8f24531aee8a6c63f7
yao <myspiritcrazy@gmail.com>
2017-08-27 11:13:28 -0700

    refactor code

--- src/com/leetcode/www/StringToInteger.java
+++ src/com/leetcode/www/StringToInteger.java
@@ -2,21 +2,24 @@ package com.leetcode.www;
 
 public class StringToInteger { // LC 8
     public int myAtoi(String str) {
-        if (str.isEmpty())
+        if (str.isEmpty()) {
             return 0;
+        }
         str = str.trim();
         int i = 0, ans = 0, sign = 1, len = str.length();
-        if (str.charAt(i) == '-' || str.charAt(i) == '+')
+        if (str.charAt(i) == '-' || str.charAt(i) == '+') {
             sign = str.charAt(i++) == '+' ? 1 : -1;
+        }
         for (; i < len; ++i) {
             int tmp = str.charAt(i) - '0';
-            if (tmp < 0 || tmp > 9)
+            if (tmp < 0 || tmp > 9) {
                 break;
-            if (ans > Integer.MAX_VALUE / 10
-                    || (ans == Integer.MAX_VALUE / 10 && Integer.MAX_VALUE % 10 < tmp))
+            }
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && Integer.MAX_VALUE % 10 < tmp)) {
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            else
+            } else {
                 ans = ans * 10 + tmp;
+            }
         }
         return sign * ans;
     }
