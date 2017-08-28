3f30e5dc154df3f74d8829407eb29698576278bf
yao <myspiritcrazy@gmail.com>
2017-08-27 21:14:34 -0700

    add a clean solution

--- src/com/leetcode/www/RomanToInteger.java
+++ src/com/leetcode/www/RomanToInteger.java
@@ -1,7 +1,40 @@
 package com.leetcode.www;
 
 public class RomanToInteger { // LC 13
-    public int romanToInt(String s) { // beats 34.27%
+    public int romanToInt(String s) {
+        if( s == null || s.length() == 0 )
+            return 0;
+        int result = 0;
+        for( int i = 0; i < s.length(); i++ ) {
+            if( (i > 0) && ChartoInt(s.charAt(i-1)) < ChartoInt(s.charAt(i)))
+                result += (ChartoInt(s.charAt(i)) - ChartoInt(s.charAt(i-1))*2);
+            else
+                result += ChartoInt(s.charAt(i));
+        }
+        return result;
+    }
+    private int ChartoInt(char c) {
+        switch(c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+    public int romanToIntV0(String s) { // beats 34.27%
         int nums[] = new int[s.length()];
         for (int i = 0; i < s.length(); i++) {
             switch (s.charAt(i)) {
