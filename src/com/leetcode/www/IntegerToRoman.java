5eba8c34e70edd43237b0b814cde688d6fa31bb8
yao <myspiritcrazy@gmail.com>
2017-08-27 18:10:05 -0700

    Integer to Roman

---
+++ src/com/leetcode/www/IntegerToRoman.java
@@ -0,0 +1,24 @@
+package com.leetcode.www;
+
+public class IntegerToRoman {
+    public String intToRoman(int num) {
+        StringBuilder result = new StringBuilder();
+        String[][] InttoRoman = {
+                {"", "M", "MM", "MMM", "", "", "", "", "", ""}, // 0 - 3000
+                {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}, // 0 - 900
+                {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}, // 0 - 90
+                {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}  // 0 - 9
+        };
+        int digit, i = 0, scale = 1000, num_tmp = num;
+
+        while( num_tmp > 0) {
+            digit = num_tmp / scale;
+            result.append(InttoRoman[i][digit]);
+            i++;
+            num_tmp %= scale;
+            scale /= 10;
+        }
+
+        return result.toString();
+    }
+}
