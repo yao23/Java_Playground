a53fb23c6060d4d7ef11c7eafe32adfe7fcf3ae5
yao <myspiritcrazy@gmail.com>
2017-08-27 19:00:33 -0700

    add a clean solution

--- src/com/leetcode/www/IntegerToRoman.java
+++ src/com/leetcode/www/IntegerToRoman.java
@@ -21,4 +21,12 @@ public class IntegerToRoman { // LC 12
 
         return result.toString();
     }
+
+    public String intToRomanV0(int num) { // beats 28.04%
+        String M[] = {"", "M", "MM", "MMM"};
+        String C[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String X[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String I[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        return M[num/1000] + C[(num%1000)/100] + X[(num%100)/10] + I[num%10];
+    }
 }
