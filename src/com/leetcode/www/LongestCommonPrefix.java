7f6c2e7c6a6a1f2951c8539b01f46f9db910a103
yao <myspiritcrazy@gmail.com>
2017-08-30 23:29:11 -0700

    add a clean solution

--- src/com/leetcode/www/LongestCommonPrefix.java
+++ src/com/leetcode/www/LongestCommonPrefix.java
@@ -1,7 +1,22 @@
 package com.leetcode.www;
 
 public class LongestCommonPrefix { // LC 14
-    public String longestCommonPrefix(String[] strs) { // beats 26.72%
+    public String longestCommonPrefix(String[] strs) { // beast 88.28%
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        String pre = strs[0];
+        int i = 1;
+        while (i < strs.length) {
+            while (strs[i].indexOf(pre) != 0) {
+                pre = pre.substring(0, pre.length() - 1);
+            }
+            i++;
+        }
+        return pre;
+    }
+
+    public String longestCommonPrefixV0(String[] strs) { // beats 26.72%
         String commonPrefix = "";
         if (strs.length == 0) {
             return commonPrefix;
