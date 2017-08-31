600204eb6d4c1332a52392a23b668b0e5c277f12
yao <myspiritcrazy@gmail.com>
2017-08-30 23:25:30 -0700

    Longest Common Prefix

---
+++ src/com/leetcode/www/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package com.leetcode.www;
+
+public class LongestCommonPrefix { // LC 14
+    public String longestCommonPrefix(String[] strs) { // beats 26.72%
+        String CommonPrefix = "";
+        if (strs.length == 0) {
+            return CommonPrefix;
+        }
+        int prefixIndex = 0;
+
+        while (true) {
+            char c = ' ';
+            int i;
+            for (i = 0; i < strs.length; i++ ) {
+                if (prefixIndex == strs[i].length()) {
+                    break;
+                }
+                if (i == 0) {
+                    c = strs[i].charAt(prefixIndex);
+                }
+                if (c != strs[i].charAt(prefixIndex)) {
+                    break;
+                }
+            }
+            if (i != strs.length) {
+                break;
+            }
+            CommonPrefix += c;
+            prefixIndex++;
+        }
+
+        return CommonPrefix;
+    }
+}
