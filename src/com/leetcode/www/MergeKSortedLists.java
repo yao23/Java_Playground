073fc18464dc1c0527bb21218b3fcaddabcc661b
yao <myspiritcrazy@gmail.com>
2017-08-31 20:28:22 -0700

    add a recursive solution

--- src/com/leetcode/www/MergeKSortedLists.java
+++ src/com/leetcode/www/MergeKSortedLists.java
@@ -40,4 +40,38 @@ public class MergeKSortedLists { // LC 23
 
         return head;
     }
+
+    public static ListNode mergeKListsV0(ListNode[] lists) { // beats 78.03%
+        return partion(lists,0,lists.length - 1);
+    }
+
+    private static ListNode partion(ListNode[] lists, int s, int e) {
+        if (s == e) {
+            return lists[s];
+        }
+        if (s < e) {
+            int m = s + (e - s) / 2;
+            ListNode l1 = partion(lists, s, m);
+            ListNode l2 = partion(lists,m + 1, e);
+            return merge(l1, l2);
+        } else {
+            return null;
+        }
+    }
+
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+    }
 }
