8f244bf5309fc0b4f8b07e0706ae2c27c37fe22c
yao <myspiritcrazy@gmail.com>
2017-08-31 20:08:59 -0700

    Generate Parentheses

---
+++ src/com/leetcode/www/GenerateParentheses.java
@@ -0,0 +1,32 @@
+package com.leetcode.www;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> results = new ArrayList<>();
+        if (n <= 0) {
+            return results;
+        }
+        generateParenthesis(n, n, "", results);
+        return results;
+    }
+
+    private void generateParenthesis(int left, int right, String cur, List<String> results) {
+        if (left == 0) {
+            while (right > 0) {
+                cur += ")";
+                right--;
+            }
+            results.add(cur);
+            return;
+        }
+
+        generateParenthesis(left - 1, right, cur + "(", results);
+
+        if (left < right) {
+            generateParenthesis(left, right - 1, cur + ")", results);
+        }
+    }
+}
