aa4787c1325163b9438f08a2926f0e3a3197666d
yao <myspiritcrazy@gmail.com>
2017-08-31 19:57:16 -0700

    documentation for House Robber II

--- src/HouseRobberII.java
+++ src/HouseRobberII.java
@@ -1,7 +1,7 @@
 /**
  * Created by liyao on 7/5/17.
  */
-public class HouseRobberII {
+public class HouseRobberII { // LC 213
     public int rob(int[] nums) {
         int len = nums.length;
         if (len == 0) {
