2c704c86cf7cf404f6bddaea84a95b87a72b2066
yao <myspiritcrazy@gmail.com>
2017-09-02 15:12:47 -0700

    refactor code

--- src/io/bittiger/www/PerfectSubsets.java
+++ src/io/bittiger/www/PerfectSubsets.java
@@ -27,9 +27,9 @@ public class PerfectSubsets {
 
 /**
  * “perfect subset” means the largest number in it equals to the sum of other numbers.
- * [2 3 1 6] -> 2 + 3 + 1 = 6   This   is   perfect
- * [3 1 4] -> 3 + 1 = 4   This   is   perfect.
- * [1 2] -> 1 != 2   This   is   not   perfect.
+ * [2 3 1 6] -> 2 + 3 + 1 = 6   This is perfect
+ * [3 1 4] -> 3 + 1 = 4   This is perfect.
+ * [1 2] -> 1 != 2   This is not perfect.
  * eg. Intput: [2 7 1 3 5]
  * perfet subsets will be [1, 2, 3], [2, 3, 5], [2, 5, 7]  so the return value will be 3.
  */
