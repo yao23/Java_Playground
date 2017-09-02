629f73e5f1a274039f0ae0ec980a006217b8a5ca
yao <myspiritcrazy@gmail.com>
2017-09-02 16:18:16 -0700

    Max Number of Keys Combination

---
+++ src/io/bittiger/www/MaxNumKeysCombination.java
@@ -0,0 +1,14 @@
+package io.bittiger.www;
+
+public class MaxNumKeysCombination {
+    public int maxA(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j <= i - 3; j++) {
+                dp[i] = Math.max(dp[i], dp[j] * (i - j - 1));
+            }
+        }
+        return   dp[n];
+    }
+}
