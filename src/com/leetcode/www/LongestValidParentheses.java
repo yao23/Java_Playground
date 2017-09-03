99b239f204aafd839b9c84cd9ee628e49d7785d4
yao <myspiritcrazy@gmail.com>
2017-09-03 12:02:42 -0700

    Longest Valid Parentheses

---
+++ src/com/leetcode/www/LongestValidParentheses.java
@@ -0,0 +1,36 @@
+package com.leetcode.www;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LongestValidParentheses {
+    public int longestValidParentheses(String s) {
+        int max = 0;
+        Deque<Node> stack = new ArrayDeque<Node>();
+        stack.push(new Node(')', -1));
+        for( int i = 0; i < s.length(); i++ ) {
+            char c = s.charAt(i);
+            if( c == '(' )
+                stack.push(new Node(c, i));
+            else {
+                Node top = stack.peek();
+                if( top.c == '(' ) {
+                    stack.pop();
+                    max = Math.max(max, i - stack.peek().i);
+                }
+                else
+                    stack.push(new Node(c, i));
+            }
+        }
+        return max;
+    }
+
+    class Node {
+        char c;
+        int i;
+        public Node(char c, int i) {
+            this.c = c;
+            this.i = i;
+        }
+    }
+}
