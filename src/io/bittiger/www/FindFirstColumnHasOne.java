7e5eb111af97445e52e796502336c07ab1005335
yao <myspiritcrazy@gmail.com>
2017-09-03 11:12:17 -0700

    documentation

--- src/io/bittiger/www/FindFirstColumnHasOne.java
+++ src/io/bittiger/www/FindFirstColumnHasOne.java
@@ -22,3 +22,24 @@ public class FindFirstColumnHasOne {
         return res == Integer.MAX_VALUE ? -1 : res + 1;
     }
 }
+
+/**
+ * given a 2D (N*M) integer array, find the first column that contains 1. Notice that for each row, if [i, j] = 1 then
+ * every element afterwards in the same row will be 1. like [i, j + 1] [i, j + 2], [i, j + 3]...
+ * Need to solve this in O(N + M)
+ *
+ * eg.
+ * [
+ *   [0   0   0   1   1   1]
+ *   [0   0   1   1   1   1]
+ *   [0   0   0   0   0   1]
+ * ]
+ * The first column that contains 1 is 3rd column. return 3
+ *
+ * [
+ *   [0   0   1   1]
+ *   [1   1   1   1]
+ *   [0   0   0   1]
+ * ]
+ * The first column that contains 1 will be 1st column. return 1
+ */
