0473f46870a098a60d4861fb23ac227c19568827
yao <myspiritcrazy@gmail.com>
2017-09-03 11:28:17 -0700

    refactor code

--- src/com/leetcode/www/NextPermutation.java
+++ src/com/leetcode/www/NextPermutation.java
@@ -3,33 +3,33 @@ package com.leetcode.www;
 public class NextPermutation {
     public void nextPermutation(int[] nums) {
         int index = -1;
-        for( int i = nums.length - 1; i > 0; i-- ) {
-            if( nums[i - 1] < nums[i] ) {
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i - 1] < nums[i]) {
                 index = i;
                 break;
             }
         }
-        if( index == -1 )
-            ReverseArray(0, nums.length - 1, nums);
-        else {
-            int BiggerIndex = FindBig(nums[index - 1], index, nums);
-            swap(BiggerIndex, index - 1, nums);
-            ReverseArray(index, nums.length - 1, nums);
+        if (index == -1) {
+            reverseArray(0, nums.length - 1, nums);
+        } else {
+            int biggerIndex = findBig(nums[index - 1], index, nums);
+            swap(biggerIndex, index - 1, nums);
+            reverseArray(index, nums.length - 1, nums);
         }
     }
-    private int FindBig(int sentinal, int index, int[] num) {
-        int BigIndex = index;
-        int BigValue = num[index];
-        for( int i = index + 1; i < num.length; i++ ) {
-            if( sentinal < num[i] && num[i] <= BigValue ) {
-                BigValue = num[i];
-                BigIndex = i;
+    private int findBig(int sentinal, int index, int[] num) {
+        int bigIndex = index;
+        int bigValue = num[index];
+        for (int i = index + 1; i < num.length; i++) {
+            if (sentinal < num[i] && num[i] <= bigValue) {
+                bigValue = num[i];
+                bigIndex = i;
             }
         }
-        return BigIndex;
+        return bigIndex;
     }
-    private void ReverseArray(int start, int end, int[] num) {
-        while( start < end ) {
+    private void reverseArray(int start, int end, int[] num) {
+        while (start < end) {
             swap(start, end, num);
             start++;
             end--;
