ec9126badd8b13284b3ff0be1f1bd889e58a8f78
yao <myspiritcrazy@gmail.com>
2017-09-03 20:55:41 -0700

    refactor code

--- src/com/leetcode/www/MultiplyStrings.java
+++ src/com/leetcode/www/MultiplyStrings.java
@@ -21,10 +21,10 @@ public class MultiplyStrings { // 43
         int len1 = num1.length();
         int len2 = num2.length();
         int[] num = new int[len1 + len2 + 1];
-        for( int i = 0; i < len2; i++ ) {
+        for (int i = 0; i < len2; i++) {
             int carry = 0;
             int a = num2.charAt(len2 - 1 - i) - '0';
-            for( int j = 0; j < len1; j++ ) {
+            for (int j = 0; j < len1; j++) {
                 int b = num1.charAt(len1 - 1 - j) - '0';
                 num[i + j] += (a * b + carry);
                 carry = num[i + j] / 10;
@@ -34,14 +34,15 @@ public class MultiplyStrings { // 43
         }
         int i = num.length - 1;
 
-        while( i > 0 && num[i] == 0 ) // trim zeroes in top digits
+        // trim zeroes in top digits
+        while (i > 0 && num[i] == 0) {
             i--;
-
+        }
         StringBuilder res = new StringBuilder();
 
-        while( i>= 0 )
+        while (i >= 0) {
             res.append(num[i--]);
-
+        }
         return res.toString();
     }
 }
