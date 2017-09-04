39979cbcb7afe0bc0424c293896baf969ffc8a55
yao <myspiritcrazy@gmail.com>
2017-09-03 20:34:53 -0700

    refactor code

--- src/com/leetcode/www/CountAndSay.java
+++ src/com/leetcode/www/CountAndSay.java
@@ -3,21 +3,23 @@ package com.leetcode.www;
 public class CountAndSay {
     public String countAndSay(int n) {
         String result = "1";
-        for( int i = 1; i < n; i++ )
-            result = CountAndCreate(result);
+        for (int i = 1; i < n; i++) {
+            result = countAndCreate(result);
+        }
         return result;
     }
-    private String CountAndCreate(String result) {
+    private String countAndCreate(String result) {
         String tmp = "";
         int last = 0, len = result.length();
-        for( int i = 0; i < len; i++ ) {
-            if( result.charAt(i) != result.charAt(last) ) {
+        for (int i = 0; i < len; i++) {
+            if (result.charAt(i) != result.charAt(last)) {
                 tmp = tmp + (i - last) + result.charAt(last);
                 last = i;
             }
         }
-        if( last < len )
+        if( last < len ) {
             tmp = tmp + (len - last) + result.charAt(last);
+        }
         return tmp;
     }
 }
