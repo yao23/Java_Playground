f78679f60741a91e702ed611f692bf5a8a4df115
yao <myspiritcrazy@gmail.com>
2017-09-03 20:49:14 -0700

    documentation

--- src/com/leetcode/www/MultiplyStrings.java
+++ src/com/leetcode/www/MultiplyStrings.java
@@ -1,6 +1,22 @@
 package com.leetcode.www;
 
 public class MultiplyStrings { // 43
+    /**
+     *
+     * @param num1
+     * @param num2
+     * @return
+     *
+     * Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
+
+     * Note:
+
+     * The length of both num1 and num2 is < 110.
+     * Both num1 and num2 contains only digits 0-9.
+     * Both num1 and num2 does not contain any leading zero.
+     * You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+     */
     public String multiply(String num1, String num2) { // beats 97.37%
         int len1 = num1.length();
         int len2 = num2.length();
