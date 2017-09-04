fc5e2c876e2e94fd86d95f6c197c2eadbbee02e3
yao <myspiritcrazy@gmail.com>
2017-09-03 20:37:38 -0700

    documentation

--- src/com/leetcode/www/CountAndSay.java
+++ src/com/leetcode/www/CountAndSay.java
@@ -23,3 +23,15 @@ public class CountAndSay { // LC 38
         return tmp;
     }
 }
+
+/**
+ *  1.     1
+ *  2.     11
+ *  3.     21
+ *  4.     1211
+ *  5.     111221
+ *
+ *  1 is read off as "one 1" or 11.
+ *  11 is read off as "two 1s" or 21.
+ *  21 is read off as "one 2, then one 1" or 1211.
+ */
