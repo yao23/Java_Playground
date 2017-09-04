999c2c02cf0de33fc3e945e46f1f31ba91314052
yao <myspiritcrazy@gmail.com>
2017-09-03 20:42:17 -0700

    add a clean solution

--- src/com/leetcode/www/CountAndSay.java
+++ src/com/leetcode/www/CountAndSay.java
@@ -22,6 +22,31 @@ public class CountAndSay { // LC 38
         }
         return tmp;
     }
+
+    public String countAndSayV0(int n) { // beats 54.56%
+        StringBuilder curr = new StringBuilder("1");
+        StringBuilder prev;
+        int count;
+        char say;
+        for (int i = 1; i < n; i++) {
+            prev = curr;
+            curr = new StringBuilder();
+            count = 1;
+            say = prev.charAt(0);
+
+            for (int j = 1, len = prev.length(); j < len; j++) {
+                if (prev.charAt(j) != say) {
+                    curr.append(count).append(say);
+                    count = 1;
+                    say = prev.charAt(j);
+                } else {
+                    count++;
+                }
+            }
+            curr.append(count).append(say);
+        }
+        return curr.toString();
+    }
 }
 
 /**
