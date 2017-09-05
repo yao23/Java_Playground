6b3659c4725378c8863fe35b7358cc1b8adfcde9
yao <myspiritcrazy@gmail.com>
2017-09-04 23:08:29 -0700

    refactor code

--- src/com/leetcode/www/PermutationSequence.java
+++ src/com/leetcode/www/PermutationSequence.java
@@ -4,20 +4,21 @@ public class PermutationSequence {
     public String getPermutation(int n, int k) {
         int[] num = new int[n];
         int count = 1, k_tmp = k;
-        for( int i = 0; i < n; i++ ) {
+        for (int i = 0; i < n; i++) {
             num[i] = i + 1;
             count *= (i + 1);
         }
         k_tmp--;
-        StringBuilder TmpStr = new StringBuilder();
-        for( int i = 0; i < n; i++ ) {
+        StringBuilder tmpStr = new StringBuilder();
+        for (int i = 0; i < n; i++) {
             count /= (n - i);
             int selected = k_tmp / count;
             k_tmp = k_tmp % count;
-            TmpStr.append(num[selected]);
-            for( int j = selected + 1; j < n; j++ )
+            tmpStr.append(num[selected]);
+            for (int j = selected + 1; j < n; j++) {
                 num[j - 1] = num[j];
+            }
         }
-        return TmpStr.toString();
+        return tmpStr.toString();
     }
 }
