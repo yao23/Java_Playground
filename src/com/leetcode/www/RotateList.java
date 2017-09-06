19743e5bac29ee8c708d8e3f17ac8d100b276bff
yao <myspiritcrazy@gmail.com>
2017-09-05 20:16:33 -0700

    refactor code

--- src/com/leetcode/www/RotateList.java
+++ src/com/leetcode/www/RotateList.java
@@ -2,25 +2,29 @@ package com.leetcode.www;
 
 public class RotateList {
     public ListNode rotateRight(ListNode head, int k) {
-        if( head == null || k == 0 )	return head;
+        if (head == null || k == 0)	{
+            return head;
+        }
         int len = 1;
         ListNode it = head;
 
-        while( it.next != null ) {
+        while (it.next != null) {
             len++;
             it = it.next;
         }
 
-        if( k  % len == 0 ) return head;
+        if (k  % len == 0) {
+            return head;
+        }
         it.next = head;
         int start = (len - k) % len;
 
-        while( start < 0 ) {
+        while (start < 0) {
             start += len;
             start %= len;
         }
 
-        while( start > 0 ) {
+        while (start > 0) {
             it = it.next;
             start--;
         }
