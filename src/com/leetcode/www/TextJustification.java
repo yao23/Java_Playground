9cea3c3112e9fe0899996836c7f0556e8b085865
yao <myspiritcrazy@gmail.com>
2017-09-05 20:59:42 -0700

    refactor code

--- src/com/leetcode/www/TextJustification.java
+++ src/com/leetcode/www/TextJustification.java
@@ -5,42 +5,45 @@ import java.util.List;
 
 public class TextJustification {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> res = new ArrayList<String>();
-        int WordsCount = words.length;
-        int CurLen = 0, LastI = 0;
-        for( int i = 0; i <= WordsCount; i++ ) {
-            if( i == WordsCount || CurLen + words[i].length() + i - LastI > maxWidth ) {
-                StringBuffer buf = new StringBuffer();
-                int SpaceCount = maxWidth - CurLen;
-                int SpaceSlots = i - LastI - 1;
-                if( SpaceSlots == 0 || i == WordsCount ) {
-                    for( int j = LastI; j < i; j++ ) {
-                        buf.append(words[j]);
-                        if( j != (i - 1) )
-                            AppendSpace(buf, 1);
+        List<String> res = new ArrayList<>();
+        int wordsCount = words.length;
+        int curLen = 0, lastI = 0;
+        for (int i = 0; i <= wordsCount; i++) {
+            if (i == wordsCount || curLen + words[i].length() + i - lastI > maxWidth ) {
+                StringBuilder sb = new StringBuilder();
+                int spaceCount = maxWidth - curLen;
+                int spaceSlots = i - lastI - 1;
+                if (spaceSlots == 0 || i == wordsCount) {
+                    for (int j = lastI; j < i; j++) {
+                        sb.append(words[j]);
+                        if (j != (i - 1)) {
+                            appendSpace(sb, 1);
+                        }
                     }
-                    AppendSpace(buf, maxWidth - buf.length());
-                }
-                else {
-                    int SpaceEach = SpaceCount / SpaceSlots;
-                    int SpaceExtra = SpaceCount % SpaceSlots;
-                    for( int j = LastI; j < i; j++ ) {
-                        buf.append(words[j]);
-                        if( j != (i - 1) )
-                            AppendSpace(buf, SpaceEach + (j - LastI < SpaceExtra ? 1 : 0));
+                    appendSpace(sb, maxWidth - sb.length());
+                } else {
+                    int spaceEach = spaceCount / spaceSlots;
+                    int spaceExtra = spaceCount % spaceSlots;
+                    for (int j = lastI; j < i; j++) {
+                        sb.append(words[j]);
+                        if (j != (i - 1)) {
+                            appendSpace(sb, spaceEach + (j - lastI < spaceExtra ? 1 : 0));
+                        }
                     }
                 }
-                res.add(buf.toString());
-                LastI = i;
-                CurLen = 0;
+                res.add(sb.toString());
+                lastI = i;
+                curLen = 0;
             }
-            if( i < WordsCount )
-                CurLen += words[i].length();
+            if (i < wordsCount) {
+                curLen += words[i].length();
+            }
         }
         return res;
     }
-    private void AppendSpace(StringBuffer StrBuf, int count) {
-        for( int i = 0; i < count; i++ )
-            StrBuf.append(' ');
+    private void appendSpace(StringBuilder sb, int count) {
+        for (int i = 0; i < count; i++) {
+            sb.append(' ');
+        }
     }
 }
