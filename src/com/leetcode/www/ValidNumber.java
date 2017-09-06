4a354ee92ce324a2e081eb6a5004b029bd42ff9d
yao <myspiritcrazy@gmail.com>
2017-09-05 20:29:28 -0700

    refactor code

--- src/com/leetcode/www/ValidNumber.java
+++ src/com/leetcode/www/ValidNumber.java
@@ -2,84 +2,107 @@ package com.leetcode.www;
 
 public class ValidNumber {
     public boolean isNumber(String s) {
-        if( s == null )	return false;
+        if (s == null) {
+            return false;
+        }
         s = s.trim();
         int len = s.length();
-        if( len == 0 ) return false;
-        boolean HasE = false;
+        if (len == 0) {
+            return false;
+        }
+        boolean hasE = false;
         int eIndex = -1;
-        for( int i = 0; i < len; i++ ) {
-            if( s.charAt(i) == 'e' || s.charAt(i) == 'E' ) {
-                if( HasE ) return false;
-                else {
-                    HasE = true;
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == 'e' || s.charAt(i) == 'E') {
+                if (hasE) {
+                    return false;
+                } else {
+                    hasE = true;
                     eIndex = i;
                 }
             }
         }
-        if( HasE ) {
+        if (hasE) {
             String s1 = s.substring(0, eIndex);
             String s2 = s.substring(eIndex + 1);
-            return IsNumberWithoutE(s1) && IsSignedNumber(s2);
+            return isNumberWithoutE(s1) && isSignedNumber(s2);
+        } else {
+            return isNumberWithoutE(s);
         }
-        else
-            return IsNumberWithoutE(s);
     }
-    private boolean IsNumberWithoutE(String s) {
+    private boolean isNumberWithoutE(String s) {
         int len = s.length();
-        if( len == 0 )	return false;
+        if (len == 0) {
+            return false;
+        }
         int start = 0;
-        if( s.charAt(0) == '+' || s.charAt(0) == '-' )
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
             start++;
-        if( start == len ) return false;
+        }
+        if (start == len) {
+            return false;
+        }
         s = s.substring(start);
         len = s.length();
-        boolean HasDot = false;
-        int DotIndex = -1;
-        for( int i = 0; i < len; i++ ) {
-            if( s.charAt(i) == '.' ) {
-                if( HasDot ) return false;
-                else {
-                    HasDot = true;
-                    DotIndex = i;
+        boolean hasDot = false;
+        int dotIndex = -1;
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == '.') {
+                if (hasDot) {
+                    return false;
+                } else {
+                    hasDot = true;
+                    dotIndex = i;
                 }
             }
         }
-        if( HasDot ) {
-            String s1 = s.substring(0, DotIndex);
-            String s2 = s.substring(DotIndex + 1);
-            if( s1.length() == 0 && s2.length() == 0 )
+        if (hasDot) {
+            String s1 = s.substring(0, dotIndex);
+            String s2 = s.substring(dotIndex + 1);
+            if (s1.length() == 0 && s2.length() == 0) {
                 return false;
-            if( s1.length() == 0 )
-                return IsPureNumber(s2);
-            if( s2.length() == 0 )
-                return IsPureNumber(s1);
-            return IsPureNumber(s1) && IsPureNumber(s2);
-        }
-        else {
-            if( s.length() == 0 )	return false;
-            return IsPureNumber(s);
+            }
+            if (s1.length() == 0) {
+                return isPureNumber(s2);
+            }
+            if (s2.length() == 0) {
+                return isPureNumber(s1);
+            }
+            return isPureNumber(s1) && isPureNumber(s2);
+        } else {
+            if (s.length() == 0) {
+                return false;
+            }
+            return isPureNumber(s);
         }
     }
-    private boolean IsSignedNumber(String s)  {
+    private boolean isSignedNumber(String s)  {
         int len = s.length();
-        if( len == 0 )	return false;
+        if (len == 0) {
+            return false;
+        }
         int start = 0;
-        if( s.charAt(0) == '+' || s.charAt(0) == '-' )
+        if (s.charAt(0) == '+' || s.charAt(0) == '-') {
             start++;
-        if( start == len )	return false;
+        }
+        if (start == len) {
+            return false;
+        }
         s = s.substring(start);
-        return IsPureNumber(s);
+        return isPureNumber(s);
     }
-    private boolean IsPureNumber(String s) {
+    private boolean isPureNumber(String s) {
         int len = s.length();
-        if( len == 0 )	return false;
-        for( int i = 0; i < len; i++ ) {
+        if (len == 0) {
+            return false;
+        }
+        for (int i = 0; i < len; i++) {
             char c = s.charAt(i);
-            if( '0' <= c && c <= '9' )
+            if ('0' <= c && c <= '9') {
                 continue;
-            else
+            } else {
                 return false;
+            }
         }
         return true;
     }
