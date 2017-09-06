5bcaf4a8661bd27152a397e2ffe6ac9e3f454312
yao <myspiritcrazy@gmail.com>
2017-09-05 20:44:52 -0700

    refactor code

--- src/com/leetcode/www/AddBinary.java
+++ src/com/leetcode/www/AddBinary.java
@@ -2,18 +2,20 @@ package com.leetcode.www;
 
 public class AddBinary {
     public String addBinary(String a, String b) {
-        if( a == null || a.equals("") )
+        if (a == null || a.equals("")) {
             return b;
-        if( b == null || b.equals("") )
+        }
+        if (b == null || b.equals("")) {
             return a;
+        }
         StringBuilder a1 = new StringBuilder(a);
         a1.reverse();
         StringBuilder b1 = new StringBuilder(b);
         b1.reverse();
         StringBuilder sum = new StringBuilder();
-        int SumLen = Math.max(a.length(), b.length());
+        int sumLen = Math.max(a.length(), b.length());
         int carry = 0;
-        for( int i = 0; i < SumLen; i++ ) {
+        for (int i = 0; i < sumLen; i++) {
             int t1 = (i >= a.length()? 0 : (a1.charAt(i) - '0'));
             int t2 = (i >= b.length()? 0 : (b1.charAt(i) - '0'));
             int t3 = t1 + t2 + carry;
@@ -21,8 +23,9 @@ public class AddBinary {
             t3 = t3 % 2;
             sum.append(t3);
         }
-        if( carry != 0 )
+        if (carry != 0) {
             sum.append(carry);
+        }
         sum.reverse();
         return sum.toString();
     }
