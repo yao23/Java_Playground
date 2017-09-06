1763b08f34460114dab1c0bae703bf1ba9d5b975
yao <myspiritcrazy@gmail.com>
2017-09-05 20:38:13 -0700

    documentation

--- src/com/leetcode/www/ValidNumber.java
+++ src/com/leetcode/www/ValidNumber.java
@@ -1,6 +1,24 @@
 package com.leetcode.www;
 
 public class ValidNumber { // LC 65
+    /**
+     *
+     * @param s
+     * @return
+     *
+     * We start with trimming.
+
+     * If we see [0-9] we reset the number flags.
+     * We can only see . if we didn't see e or ..
+     * We can only see e if we didn't see e but we did see a number. We reset numberAfterE flag.
+     * We can only see + and - in the beginning and after an e
+     * any other character break the validation.
+     * At the and it is only valid if there was at least 1 number and if we did see an e then a number after it as well.
+
+     * So basically the number should match this regular expression:
+
+     * [-+]?(([0-9]+(.[0-9]*)?)|.[0-9]+)(e[-+]?[0-9]+)?
+     */
     public boolean isNumber(String s) { // beats 77.08%
         s = s.trim();
 
