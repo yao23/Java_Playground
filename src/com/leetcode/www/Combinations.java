1e4cba4d0be910a69aa2db1030aeb1cc51a64cd4
yao <myspiritcrazy@gmail.com>
2017-09-06 22:17:09 -0700

    refactor code

--- src/com/leetcode/www/Combinations.java
+++ src/com/leetcode/www/Combinations.java
@@ -6,23 +6,26 @@ import java.util.List;
 public class Combinations {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-        if( k == 0 )	return result;
+        if (k == 0)	{
+            return result;
+        }
         List<Integer> combinations = new ArrayList<>();
         int depth = 1;
-        GenerateCombination(n, k, depth, combinations, result);
+        generateCombination(n, k, depth, combinations, result);
         return result;
     }
-    public void GenerateCombination(int n, int k, int depth, List<Integer> combinations,
+
+    private void generateCombination(int n, int k, int depth, List<Integer> combinations,
                                     List<List<Integer>> result) {
-        if( combinations.size() == k ) {
-            List<Integer> tmp = new ArrayList<Integer>();
+        if (combinations.size() == k) {
+            List<Integer> tmp = new ArrayList<>();
             tmp.addAll(combinations);
             result.add(tmp);
             return;
         }
-        for( int i = depth; i <= n; i++ ) {
+        for (int i = depth; i <= n; i++) {
             combinations.add(i);
-            GenerateCombination(n, k, i+1, combinations, result);
+            generateCombination(n, k, i + 1, combinations, result);
             combinations.remove(combinations.size() - 1);
         }
     }
