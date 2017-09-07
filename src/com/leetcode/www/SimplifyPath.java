33efc0f2a236107a9ee9158bd71f50080728e46e
yao <myspiritcrazy@gmail.com>
2017-09-06 20:42:05 -0700

    refactor code

--- src/com/leetcode/www/SimplifyPath.java
+++ src/com/leetcode/www/SimplifyPath.java
@@ -6,20 +6,23 @@ import java.util.Deque;
 public class SimplifyPath {
     public String simplifyPath(String path) {
         String[] paths = path.split("/");
-        Deque<String> stack = new ArrayDeque<String>();
-        for( String s : paths ) {
-            if( s.equals("..") ) {
-                if( !stack.isEmpty() )
+        Deque<String> stack = new ArrayDeque<>();
+        for (String s : paths) {
+            if (s.equals("..")) {
+                if (!stack.isEmpty()) {
                     stack.pop();
-            }
-            else if( s.length() > 0 && !s.equals(".") )
+                }
+            } else if (s.length() > 0 && !s.equals(".")) {
                 stack.push(s);
+            }
         }
         StringBuilder res = new StringBuilder();
-        for( String s : stack )
+        for (String s : stack) {
             res.append("/" + s);
-        if( res.length() == 0 )
+        }
+        if (res.length() == 0) {
             res.append("/");
+        }
         return res.toString();
     }
 }
