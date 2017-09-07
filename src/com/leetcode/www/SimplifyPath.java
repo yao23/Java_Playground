bb0e4f521a23fb44274c678c8cd4851e1f6f5eab
yao <myspiritcrazy@gmail.com>
2017-09-06 20:38:05 -0700

    Simplify Path

---
+++ src/com/leetcode/www/SimplifyPath.java
@@ -0,0 +1,25 @@
+package com.leetcode.www;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        String[] paths = path.split("/");
+        Deque<String> stack = new ArrayDeque<String>();
+        for( String s : paths ) {
+            if( s.equals("..") ) {
+                if( !stack.isEmpty() )
+                    stack.pop();
+            }
+            else if( s.length() > 0 && !s.equals(".") )
+                stack.push(s);
+        }
+        StringBuilder res = new StringBuilder();
+        for( String s : stack )
+            res.append("/" + s);
+        if( res.length() == 0 )
+            res.append("/");
+        return res.toString();
+    }
+}
