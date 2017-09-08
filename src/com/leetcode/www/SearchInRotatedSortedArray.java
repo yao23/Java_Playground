7d08b86e4b5313cb9dc78d4a978be0e734e49fc1
yao <myspiritcrazy@gmail.com>
2017-09-07 22:08:47 -0700

    add a clean solution

--- src/com/leetcode/www/SearchInRotatedSortedArray.java
+++ src/com/leetcode/www/SearchInRotatedSortedArray.java
@@ -36,6 +36,42 @@ public class SearchInRotatedSortedArray { // LC 81
         return false;
     }
 
+    public boolean searchV2(int[] nums, int target) { // beats 11.68%
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // System.out.format("start=%d,mid=%d,end=%d\n",start,mid,end);
+            if (nums[mid] == target) return true;
+
+            // need to handle: 1,3,1,1,1
+            while (nums[start] == nums[mid] && start != mid) {
+                start++;
+            }
+            while (nums[mid] == nums[end] && mid != end) {
+                end--;
+            }
+
+            // the following is the same as problem I
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+
     public boolean searchV1(int[] nums, int target) { // not working for [1,1,3,1],3 => true
         int len = nums.length;
         if (len == 0) {
