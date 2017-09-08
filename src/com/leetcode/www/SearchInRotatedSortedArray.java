e58b036d6f4de6c4205f24636c5a30a350b78190
yao <myspiritcrazy@gmail.com>
2017-09-07 21:53:56 -0700

    Search in Rotated Sorted Array II

--- src/com/leetcode/www/SearchInRotatedSortedArray.java
+++ src/com/leetcode/www/SearchInRotatedSortedArray.java
@@ -1,8 +1,65 @@
+package com.leetcode.www;
+
 /**
  * Created by liyao on 6/15/17.
  */
-public class SearchInRotatedSortedArray {
-    public int search(int[] nums, int target) {
+public class SearchInRotatedSortedArray { // LC 81
+    public boolean search(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 0) {
+            return false;
+        } else if (len == 1) {
+            if (nums[0] == target) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            int start = 0, end = len - 1;
+
+            while (start + 1 < end) {
+                int mid = start + (end - start) / 2;
+                if (nums[mid] == target) {
+                    return true;
+                }
+                if (nums[start] < nums[mid]) { // left part is in order
+                    if (nums[mid] < target) {
+                        start = mid;
+                    } else {
+                        if (nums[start] == target) {
+                            return true;
+                        } else if (nums[start] < target) { // target maybe locate in left part
+                            end = mid;
+                        } else { // target maybe locate in right part
+                            start = mid;
+                        }
+                    }
+                } else { // right part is in order
+                    if (nums[mid] < target) {
+                        if (nums[end] == target) {
+                            return true;
+                        } else if (nums[end] < target) { // target maybe locate in left part
+                            end = mid;
+                        } else { // target maybe locate in right part
+                            start = mid;
+                        }
+                    } else {
+                        end = mid;
+                    }
+                }
+            }
+
+            if (nums[start] == target) {
+                return true;
+            } else if (nums[start + 1] == target) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public int searchV0(int[] nums, int target) {
         int index = -1;
         int len = nums.length;
         if (len == 0) {
