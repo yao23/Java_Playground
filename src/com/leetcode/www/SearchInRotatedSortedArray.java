6dda8f1c601c44bd006385837bd9fedc8ed1edae
yao <myspiritcrazy@gmail.com>
2017-09-07 21:59:51 -0700

    add a clean solution

--- src/com/leetcode/www/SearchInRotatedSortedArray.java
+++ src/com/leetcode/www/SearchInRotatedSortedArray.java
@@ -4,7 +4,39 @@ package com.leetcode.www;
  * Created by liyao on 6/15/17.
  */
 public class SearchInRotatedSortedArray { // LC 81
-    public boolean search(int[] nums, int target) {
+    public boolean search(int[] nums, int target) { // beats 11.68%
+        int start = 0, end = nums.length - 1, mid = -1;
+        while(start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            //If we know for sure right side is sorted or left side is unsorted
+            if (nums[mid] < nums[end] || nums[mid] < nums[start]) {
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+                //If we know for sure left side is sorted or right side is unsorted
+            } else if (nums[mid] > nums[start] || nums[mid] > nums[end]) {
+                if (target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+                //If we get here, that means nums[start] == nums[mid] == nums[end], then shifting out
+                //any of the two sides won't change the result but can help remove duplicate from
+                //consideration, here we just use end-- but left++ works too
+            } else {
+                end--;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean searchV1(int[] nums, int target) { // not working for [1,1,3,1],3 => true
         int len = nums.length;
         if (len == 0) {
             return false;
