1fe4f00cc556f5814359695f65d9061e75190065
yao <myspiritcrazy@gmail.com>
2017-09-08 23:01:37 -0700

    Same Tree:

---
+++ src/com/leetcode/www/SameTree.java
@@ -0,0 +1,18 @@
+package com.leetcode.www;
+
+public class SameTree { // LC 100
+    public boolean isSameTree(TreeNode p, TreeNode q) { // beats 1.30%
+        boolean p_null = (p == null);
+        boolean q_null = (q == null);
+
+        if( p_null != q_null ) return false;
+        else if( p == null && q == null )
+            return true;
+        else if( p.val == q.val )
+            return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        else
+            return false;
+    }
+}
+
+// [], [] => true
