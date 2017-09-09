f6cc67dbddb94007c1aa5e486cb837da2a910938
yao <myspiritcrazy@gmail.com>
2017-09-08 22:56:24 -0700

    add a solution with Morris Traversal

--- src/com/leetcode/www/RecoverBinarySearchTree.java
+++ src/com/leetcode/www/RecoverBinarySearchTree.java
@@ -91,4 +91,74 @@ public class RecoverBinarySearchTree { // LC 99
             f2.val = tmp;
         }
     }
+
+    /**
+     *
+     * @param root
+     * 
+     * https://discuss.leetcode.com/topic/29161/share-my-solutions-and-detailed-explanation-with-recursive-iterative-in-order-traversal-and-morris-traversal
+     * https://discuss.leetcode.com/topic/8925/share-my-java-solution-using-morris-traversal
+     */
+    public void recoverTreeV0(TreeNode root) { // beats 26.89%
+        TreeNode pre = null;
+        TreeNode first = null, second = null;
+
+        // Morris Traversal
+        TreeNode temp = null;
+
+        while (root != null) {
+            // If left is not null, we need to find the rightmost node of left subtree,
+            // Set its right child to current node
+            if (root.left != null) {
+                // connect threading for root
+                temp = root.left;
+
+                //find the rightmost
+                while (temp.right != null && temp.right != root) {
+                    temp = temp.right;
+                }
+
+                //There are two cases,
+                //null: first time we access current, set node.right to current and move to left child of current and continue;
+                //current: we accessed current before, thus we've finished traversing left subtree, set node.right back to null;
+
+                if (temp.right != null) { // the threading already exists
+                    if (pre != null && pre.val > root.val) {
+                        if (first == null) {
+                            first = pre;
+                            second = root;
+                        } else {
+                            second = root;
+                        }
+                    }
+                    pre = root;
+
+                    temp.right = null;
+                    root = root.right;
+                } else { // construct the threading
+                    temp.right = root;
+                    root = root.left;
+                }
+            } else {
+                if (pre != null && pre.val > root.val) {
+                    if (first == null) { // first time we encounter reversed order, we set previous node to first
+                        first = pre;
+                        second = root; //In case that two nodes are successive, we set second to current every time.
+                    } else {
+                        second = root; //In case that two nodes are successive, we set second to current every time.
+                    }
+                }
+                pre = root;
+                root = root.right;
+            }
+        }
+        // swap two node values
+        if (first != null && second != null) {
+            int t = first.val;
+            first.val = second.val;
+            second.val = t;
+        }
+    }
 }
+
+// [0,1] => [1,0]
