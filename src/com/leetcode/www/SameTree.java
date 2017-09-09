76b151caa014fdacee41c4148e04009ef6a4c021
yao <myspiritcrazy@gmail.com>
2017-09-08 23:02:34 -0700

    refactor code

--- src/com/leetcode/www/SameTree.java
+++ src/com/leetcode/www/SameTree.java
@@ -5,13 +5,15 @@ public class SameTree { // LC 100
         boolean p_null = (p == null);
         boolean q_null = (q == null);
 
-        if( p_null != q_null ) return false;
-        else if( p == null && q == null )
-            return true;
-        else if( p.val == q.val )
-            return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
-        else
+        if (p_null != q_null) {
             return false;
+        } else if (p == null && q == null) {
+            return true;
+        } else if (p.val == q.val) {
+            return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        } else {
+            return false;
+        }
     }
 }
 
