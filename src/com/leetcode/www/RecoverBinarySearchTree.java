948ccf1af8d41a0786bbbc1ee4bf6c20e4ed6fb0
yao <myspiritcrazy@gmail.com>
2017-09-08 22:43:39 -0700

    refactor code

--- src/com/leetcode/www/RecoverBinarySearchTree.java
+++ src/com/leetcode/www/RecoverBinarySearchTree.java
@@ -6,35 +6,46 @@ import java.util.List;
 
 public class RecoverBinarySearchTree {
     public void recoverTree(TreeNode root) {
-        if( root == null )	return;
-        List<TreeNode> nodes = new ArrayList<TreeNode>();
-        List<Integer> values = new ArrayList<Integer>();
-        InorderTraversal(root, nodes, values);
+        if (root == null) {
+            return;
+        }
+        List<TreeNode> nodes = new ArrayList<>();
+        List<Integer> values = new ArrayList<>();
+
+        inorderTraversal(root, nodes, values);
         Collections.sort(values);
-        for( int i = 0; i < nodes.size(); i++ )
+
+        for (int i = 0; i < nodes.size(); i++) {
             nodes.get(i).val = values.get(i);
+        }
     }
-    private void InorderTraversal(TreeNode root, List<TreeNode> nodes, List<Integer> values) {
-        if( root == null ) return;
-        if( root.left != null )
-            InorderTraversal(root.left, nodes, values);
+    private void inorderTraversal(TreeNode root, List<TreeNode> nodes, List<Integer> values) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            inorderTraversal(root.left, nodes, values);
+        }
         nodes.add(root);
         values.add(root.val);
-        if( root.right != null )
-            InorderTraversal(root.right, nodes, values);
+        if (root.right != null) {
+            inorderTraversal(root.right, nodes, values);
+        }
     }
 
     public void recoverTree2(TreeNode root) {
-        if( root == null )	return;
+        if (root == null) {
+            return;
+        }
         TreeNode f1 = null, f2 = null;
         TreeNode cur, pre, parent = null;
         boolean found = false;
         cur = root;
 
-        while( cur != null ) {
-            if( cur.left == null ) {
-                if( parent != null && parent.val > cur.val ) {
-                    if( !found ) {
+        while (cur != null) {
+            if (cur.left == null) {
+                if (parent != null && parent.val > cur.val) {
+                    if (!found) {
                         f1 = parent;
                         found = true;
                     }
@@ -42,21 +53,20 @@ public class RecoverBinarySearchTree {
                 }
                 parent = cur;
                 cur = cur.right;
-            }
-            else {
+            } else {
                 pre = cur.left;
 
-                while( pre.right != null && pre.right != cur )
+                while (pre.right != null && pre.right != cur) {
                     pre = pre.right;
+                }
 
-                if( pre.right == null ) {
+                if (pre.right == null) {
                     pre.right = cur;
                     cur = cur.left;
-                }
-                else {
+                } else {
                     pre.right = null;
-                    if( parent.val > cur.val ) {
-                        if( !found ) {
+                    if (parent.val > cur.val) {
+                        if (!found) {
                             f1 = parent;
                             found = true;
                         }
@@ -68,7 +78,7 @@ public class RecoverBinarySearchTree {
             }
         }
 
-        if( f1 != null && f2 != null ) {
+        if (f1 != null && f2 != null) {
             int tmp = f1.val;
             f1.val = f2.val;
             f2.val = tmp;
