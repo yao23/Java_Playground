f3c02702570f1812deec1e3d714fb75b1a836016
yao <myspiritcrazy@gmail.com>
2017-09-09 17:56:26 -0700

    add iterative solution{

--- src/com/leetcode/www/ConvertSortedArrayToBinarySearchTree.java
+++ src/com/leetcode/www/ConvertSortedArrayToBinarySearchTree.java
@@ -14,6 +14,39 @@ public class ConvertSortedArrayToBinarySearchTree { // LC 108
         root.right = generateBST(num, mid + 1, end);
         return root;
     }
+
+    public TreeNode sortedArrayToBST(int[] nums) { // beats 1.40%
+        int len = nums.length;
+        if ( len == 0 ) { return null; }
+
+        // 0 as a placeholder
+        TreeNode head = new TreeNode(0);
+
+        Deque<TreeNode> nodeStack       = new LinkedList<TreeNode>() {{ push(head);  }};
+        Deque<Integer>  leftIndexStack  = new LinkedList<Integer>()  {{ push(0);     }};
+        Deque<Integer>  rightIndexStack = new LinkedList<Integer>()  {{ push(len-1); }};
+
+        while ( !nodeStack.isEmpty() ) {
+            TreeNode currNode = nodeStack.pop();
+            int left  = leftIndexStack.pop();
+            int right = rightIndexStack.pop();
+            int mid   = left + (right-left)/2; // avoid overflow
+            currNode.val = nums[mid];
+            if ( left <= mid-1 ) {
+                currNode.left = new TreeNode(0);
+                nodeStack.push(currNode.left);
+                leftIndexStack.push(left);
+                rightIndexStack.push(mid-1);
+            }
+            if ( mid+1 <= right ) {
+                currNode.right = new TreeNode(0);
+                nodeStack.push(currNode.right);
+                leftIndexStack.push(mid+1);
+                rightIndexStack.push(right);
+            }
+        }
+        return head;
+    }
 }
 
 // [] => null
