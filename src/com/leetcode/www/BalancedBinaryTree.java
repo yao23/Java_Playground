47c8b5fdb11f9422e4c93c21e9f870e1b39a9a53
yao <myspiritcrazy@gmail.com>
2017-09-09 18:21:35 -0700

    refactor code

--- src/com/leetcode/www/BalancedBinaryTree.java
+++ src/com/leetcode/www/BalancedBinaryTree.java
@@ -1,29 +1,29 @@
 package com.leetcode.www;
 
 public class BalancedBinaryTree { // LC 110
-    public boolean isBalanced(TreeNode root) {
-        if(root==null){
+    public boolean isBalanced(TreeNode root) { // beats 63.97%
+        if (root == null) {
             return true;
         }
-        return height(root)!=-1;
+        return height(root) != -1;
 
     }
-    public int height(TreeNode node){
-        if(node==null){
+    private int height(TreeNode node) {
+        if (node == null) {
             return 0;
         }
-        int lH=height(node.left);
-        if(lH==-1){
+        int leftHeight = height(node.left);
+        if (leftHeight == -1) {
             return -1;
         }
-        int rH=height(node.right);
-        if(rH==-1){
+        int rightHeight = height(node.right);
+        if (rightHeight == -1) {
             return -1;
         }
-        if(lH-rH<-1 || lH-rH>1){
+        if (leftHeight - rightHeight < -1 || leftHeight - rightHeight > 1) {
             return -1;
         }
-        return Math.max(lH,rH)+1;
+        return Math.max(leftHeight, rightHeight) + 1;
     }
 
     public boolean isBalancedV0(TreeNode root) { // beats 5.02%
