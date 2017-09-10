34a5a31320e28f7674eb01ff07c389448ddb3306
yao <myspiritcrazy@gmail.com>
2017-09-09 18:04:31 -0700

    refactor code

--- src/com/leetcode/www/ConvertSortedListToBinarySearchTree.java
+++ src/com/leetcode/www/ConvertSortedListToBinarySearchTree.java
@@ -1,29 +1,33 @@
 package com.leetcode.www;
 
 public class ConvertSortedListToBinarySearchTree {
-    private static ListNode CurNode;
+    private static ListNode curNode;
 
     public TreeNode sortedListToBST(ListNode head) {
-        if( head == null )	return null;
+        if (head == null) {
+            return null;
+        }
         ListNode cur = head;
         int len = 0;
-        while( cur != null ) {
+        while (cur != null) {
             len++;
             cur = cur.next;
         }
 
-        CurNode = head;
-        return SortedListToBST(0, len - 1);
+        curNode = head;
+        return sortedListToBST(0, len - 1);
     }
-    private TreeNode SortedListToBST(int start, int end) {
-        if( start > end )	return null;
+    private TreeNode sortedListToBST(int start, int end) {
+        if (start > end) {
+            return null;
+        }
         // same as mid = (start + end) / 2, avoid overflow
         int mid = start + (end - start) / 2;
-        TreeNode left = SortedListToBST(start, mid - 1);
-        TreeNode root = new TreeNode(CurNode.val);
+        TreeNode left = sortedListToBST(start, mid - 1);
+        TreeNode root = new TreeNode(curNode.val);
         root.left = left;
-        CurNode = CurNode.next;
-        root.right = SortedListToBST(mid + 1, end);
+        curNode = curNode.next;
+        root.right = sortedListToBST(mid + 1, end);
         return root;
     }
 }
