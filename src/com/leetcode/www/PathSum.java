d6048f65406f5a6cd50c0fc981649d5acc754cb5
yao <myspiritcrazy@gmail.com>
2017-09-09 18:35:25 -0700

    add iterative solution

--- src/com/leetcode/www/PathSum.java
+++ src/com/leetcode/www/PathSum.java
@@ -1,7 +1,10 @@
 package com.leetcode.www;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class PathSum { // LC 112
-    public boolean hasPathSum(TreeNode root, int sum) { // beats 9.74
+    public boolean hasPathSum(TreeNode root, int sum) { // beats 9.74% (recursive)
         if (root == null) {
             return false;
         }
@@ -9,6 +12,33 @@ public class PathSum { // LC 112
             return true;
         }
         return (hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val));
+    }
+
+    public boolean hasPathSumV0(TreeNode root, int sum) { // beats 1.58% (iterative)
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Deque<Integer> sums = new ArrayDeque<>();
+
+        stack.push(root);
+        sums.push(sum);
+
+        while (!stack.isEmpty() && (root!=null)) {
+            int value = sums.pop();
+            TreeNode top = stack.pop();
+
+            if (top.left == null && top.right==null && top.val==value) {
+                return true;
+            }
+            if (top.right != null) {
+                stack.push(top.right);
+                sums.push(value - top.val);
+            }
+            if (top.left != null) {
+                stack.push(top.left);
+                sums.push(value - top.val);
+            }
+
+        }
+        return false;
     }
 }
 
