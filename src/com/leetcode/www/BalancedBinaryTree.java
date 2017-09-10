04ba5c0647e15d1ff48efa570f6a3d5da7f36966
yao <myspiritcrazy@gmail.com>
2017-09-09 18:14:59 -0700

    Balanced Binary Tree

---
+++ src/com/leetcode/www/BalancedBinaryTree.java
@@ -0,0 +1,16 @@
+package com.leetcode.www;
+
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        if( root == null ) return true;
+        if( root.left == null && root.right == null )
+            return true;
+        if( Math.abs(height(root.left) - height(root.right)) > 1 )
+            return false;
+        return (isBalanced(root.left) && isBalanced(root.right));
+    }
+    public int height(TreeNode root) {
+        if( root == null ) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+}
