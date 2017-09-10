1b3407fc139e1850f74f2c1b1a454bedb4028f03
yao <myspiritcrazy@gmail.com>
2017-09-09 17:49:14 -0700

    refactor code

--- src/com/leetcode/www/ConvertSortedArrayToBinarySearchTree.java
+++ src/com/leetcode/www/ConvertSortedArrayToBinarySearchTree.java
@@ -2,15 +2,16 @@ package com.leetcode.www;
 
 public class ConvertSortedArrayToBinarySearchTree {
     public TreeNode sortedArrayToBST(int[] num) {
-        return generate_BST(num, 0, num.length-1);
+        return generateBST(num, 0, num.length-1);
     }
-    public TreeNode generate_BST(int[] num, int start, int end) {
-        if( end < start )
+    private TreeNode generateBST(int[] num, int start, int end) {
+        if (end < start) {
             return null;
-        int mid = (start + end)/2;
+        }
+        int mid = start + (end - start) / 2;
         TreeNode root = new TreeNode(num[mid]);
-        root.left = generate_BST(num, start, mid-1);
-        root.right = generate_BST(num, mid+1, end);
+        root.left = generateBST(num, start, mid - 1);
+        root.right = generateBST(num, mid + 1, end);
         return root;
     }
 }
