8e01d1adbf8ec0acae29d760911b2080608cb6e6
yao <myspiritcrazy@gmail.com>
2017-09-09 18:27:20 -0700

    refactor code

--- src/com/leetcode/www/PathSum.java
+++ src/com/leetcode/www/PathSum.java
@@ -2,11 +2,12 @@ package com.leetcode.www;
 
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
-        if( root == null ) return false;
-        if( root.left == null && root.right == null &&
-                root.val == sum )
+        if (root == null) {
+            return false;
+        }
+        if (root.left == null && root.right == null && root.val == sum) {
             return true;
-        return (hasPathSum(root.left, sum - root.val) ||
-                hasPathSum(root.right, sum - root.val));
+        }
+        return (hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val));
     }
 }
