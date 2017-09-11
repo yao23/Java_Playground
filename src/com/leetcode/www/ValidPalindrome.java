cf028c505d649da57574b45c2e0ce22f2abf3829
yao <myspiritcrazy@gmail.com>
2017-09-10 21:26:03 -0700

    add a clean 'solution'

--- src/com/leetcode/www/ValidPalindrome.java
+++ src/com/leetcode/www/ValidPalindrome.java
@@ -31,6 +31,25 @@ public class ValidPalindrome { // LC 125
         }
         return true;
     }
+
+    public boolean isPalindromeV0(String word) { // not working (TLE) for last test case
+        //Strip out non-alphanumeric characters from string
+        String cleanWord = word.replaceAll("[^a-zA-Z0-9]","");
+        //Check for palindrome quality recursively
+        return checkPalindrome(cleanWord.toLowerCase());
+    }
+    private boolean checkPalindrome(String word) {
+        if (word.length() < 2) {
+            return true;
+        }
+        char first  = word.charAt(0);
+        char last   = word.charAt(word.length() - 1);
+        if (first != last ) {
+            return false;
+        } else {
+            return checkPalindrome(word.substring(1, word.length() - 1));
+        }
+    }
 }
 
 // "" => true
