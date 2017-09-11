a3f271c25d82d302953e907b6232ce8cfbeb9c8f
yao <myspiritcrazy@gmail.com>
2017-09-10 18:52:57 -0700

    refactor code

--- src/com/leetcode/www/DistinctSubsequences.java
+++ src/com/leetcode/www/DistinctSubsequences.java
@@ -1,12 +1,12 @@
 package com.leetcode.www;
 
 public class DistinctSubsequences { // LC 115
-    public int numDistinct(String S, String T) { // beats 80.47%
-        int[] f = new int[T.length() + 1];
-        f[T.length()] = 1;
-        for( int i = S.length() - 1; i >= 0; i-- ) {
-            for(int j = 0; j < T.length(); j++) {
-                f[j] += (S.charAt(i) == T.charAt(j))?  f[j+1] : 0;
+    public int numDistinct(String s, String t) { // beats 80.47%
+        int[] f = new int[t.length() + 1];
+        f[t.length()] = 1;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = 0; j < t.length(); j++) {
+                f[j] += (s.charAt(i) == t.charAt(j)) ? f[j + 1] : 0;
             }
         }
         return f[0];
