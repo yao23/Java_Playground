f8c2419c22465d4b2766ef2592414c6ae5378785
yao <myspiritcrazy@gmail.com>
2017-09-10 21:33:58 -0700

    add a fast solution

--- src/com/leetcode/www/ValidPalindrome.java
+++ src/com/leetcode/www/ValidPalindrome.java
@@ -7,7 +7,37 @@ public class ValidPalindrome { // LC 125
      * @param s
      * @return
      */
-    public boolean isPalindrome(String s) { // beats 25.70%
+    private static final char[]charMap = new char[256];
+    static{
+        for (int i = 0; i < 10; i++) {
+            charMap[i + '0'] = (char)(1 + i);  // numeric
+        }
+        for (int i = 0; i < 26; i++) {
+            charMap[i + 'a'] = charMap[i + 'A'] = (char)(11 + i);  //alphabetic, ignore cases
+        }
+    }
+    public boolean isPalindrome(String s) { // beats 98.64%
+        char[]pChars = s.toCharArray();
+        int start = 0, end = pChars.length - 1;
+        char cS, cE;
+        while (start < end) {
+            cS = charMap[pChars[start]];
+            cE = charMap[pChars[end]];
+            if (cS != 0 && cE != 0) {
+                if (cS != cE) {
+                    return false;
+                }
+                start++;
+                end--;
+            } else {
+                if (cS == 0) start++;
+                if (cE == 0) end--;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindromeV2(String s) { // beats 25.70%
         if (s.length() == 0) {
             return true;
         }
