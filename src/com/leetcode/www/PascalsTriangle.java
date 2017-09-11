54bca8d352479d425d3094e4162d31a12eacee41
yao <myspiritcrazy@gmail.com>
2017-09-10 21:06:23 -0700

    refactor code

--- src/com/leetcode/www/PascalsTriangle.java
+++ src/com/leetcode/www/PascalsTriangle.java
@@ -6,25 +6,31 @@ import java.util.List;
 public class PascalsTriangle {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result = new ArrayList<>();
-        if( numRows <= 0 )	return result;
-        List<Integer> first = new ArrayList<Integer>();
+        if (numRows <= 0) {
+            return result;
+        }
+        List<Integer> first = new ArrayList<>();
         first.add(1);
         result.add(first);
-        if( numRows == 1 ) return result;
-        List<Integer> second = new ArrayList<Integer>();
-        second.add(1);	second.add(1);
+        if (numRows == 1) {
+            return result;
+        }
+        List<Integer> second = new ArrayList<>();
+        second.add(1);
+        second.add(1);
         result.add(second);
-        if( numRows == 2 ) return result;
-        for( int i = 3; i <= numRows; i++ ) {
+        if (numRows == 2) {
+            return result;
+        }
+        for (int i = 3; i <= numRows; i++) {
             int resultSize = result.size();
             List<Integer> previous = result.get(resultSize - 1);
-            List<Integer> current = new ArrayList<Integer>();
-            for( int j = 1; j <= i; j++ ) {
-                if( j== 1 || j == i ) {
+            List<Integer> current = new ArrayList<>();
+            for (int j = 1; j <= i; j++) {
+                if (j== 1 || j == i) {
                     current.add(1);
-                }
-                else {
-                    int num = previous.get(j-2) + previous.get(j-1);
+                } else {
+                    int num = previous.get(j - 2) + previous.get(j - 1);
                     current.add(num);
                 }
             }
