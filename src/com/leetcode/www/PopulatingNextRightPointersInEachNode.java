95aabb413005af5fad35c96ebf70b1723d4b6ec5
yao <myspiritcrazy@gmail.com>
2017-09-10 18:55:54 -0700

    Populating Next Right Pointers in Each Node

---
+++ src/com/leetcode/www/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,17 @@
+package com.leetcode.www;
+
+public class PopulatingNextRightPointersInEachNode {
+    public void connect(TreeLinkNode root) {
+        if( root == null ) return;
+        if( root.left != null ) root.left.next = root.right;
+        if( root.right != null ) root.right.next = ( root.next == null ) ? null : root.next.left;
+        connect(root.left);
+        connect(root.right);
+    }
+}
+
+class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+    TreeLinkNode(int x) { val = x; }
+}
