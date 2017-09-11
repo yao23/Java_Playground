ff8c3c63eac7d70556cde176ed633e235a41deb8
yao <myspiritcrazy@gmail.com>
2017-09-10 18:33:56 -0700

    refactor code

--- src/com/leetcode/www/FlattenBinaryTreeToLinkedList.java
+++ src/com/leetcode/www/FlattenBinaryTreeToLinkedList.java
@@ -2,15 +2,21 @@ package com.leetcode.www;
 
 public class FlattenBinaryTreeToLinkedList {
     public void flatten(TreeNode root) {
-        if( root == null )	return;
+        if (root == null) {
+            return;
+        }
         flatten(root.left);
         flatten(root.right);
         TreeNode tmp = root;
-        if( tmp.left == null )	return; // flattening is done
-        else tmp = tmp.left;
+        if (tmp.left == null) {
+            return; // flattening is done
+        } else {
+            tmp = tmp.left;
+        }
 
-        while( tmp.right != null )
+        while (tmp.right != null) {
             tmp = tmp.right;
+        }
 
         tmp.right = root.right;
         root.right = root.left;
