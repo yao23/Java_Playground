6e054d6a716554e9c12173abee93589b147998ce
yao <myspiritcrazy@gmail.com>
2017-09-10 21:22:03 -0700

    refactor code

--- src/com/leetcode/www/ValidPalindrome.java
+++ src/com/leetcode/www/ValidPalindrome.java
@@ -8,24 +8,26 @@ public class ValidPalindrome {
      * @return
      */
     public boolean isPalindrome(String s) {
-        if( s.length() == 0 )	return true;
-        String FilteredStr = GetFiltered(s.toLowerCase());
-        return IsPalindrome(FilteredStr);
+        if (s.length() == 0) {
+            return true;
+        }
+        String filteredStr = getFiltered(s.toLowerCase());
+        return isPalindromeStr(filteredStr);
     }
-    private String GetFiltered(String s) {
+    private String getFiltered(String s) {
         StringBuilder tmp = new StringBuilder();
-        for( int i = 0; i < s.length(); i++ ) {
-            if( ('0' <= s.charAt(i) && s.charAt(i) <= '9') ||
-                    ('a' <= s.charAt(i) && s.charAt(i) <= 'z') ) {
+        for (int i = 0; i < s.length(); i++) {
+            if (('0' <= s.charAt(i) && s.charAt(i) <= '9') || ('a' <= s.charAt(i) && s.charAt(i) <= 'z')) {
                 tmp.append(s.charAt(i));
             }
         }
         return tmp.toString();
     }
-    private boolean IsPalindrome(String s) {
-        for( int i = 0; i < (s.length() / 2 ); i++ ) {
-            if( s.charAt(i) != s.charAt(s.length() - 1 - i) )
+    private boolean isPalindromeStr(String s) {
+        for (int i = 0; i < (s.length() / 2 ); i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
                 return false;
+            }
         }
         return true;
     }
