3c6c8824c44b45816e18c60ba7d4767a574c405b
yao <myspiritcrazy@gmail.com>
2017-09-10 18:56:54 -0700

    refactor code

--- src/com/leetcode/www/PopulatingNextRightPointersInEachNode.java
+++ src/com/leetcode/www/PopulatingNextRightPointersInEachNode.java
@@ -2,9 +2,15 @@ package com.leetcode.www;
 
 public class PopulatingNextRightPointersInEachNode {
     public void connect(TreeLinkNode root) {
-        if( root == null ) return;
-        if( root.left != null ) root.left.next = root.right;
-        if( root.right != null ) root.right.next = ( root.next == null ) ? null : root.next.left;
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            root.left.next = root.right;
+        }
+        if (root.right != null) {
+            root.right.next = (root.next == null) ? null : root.next.left;
+        }
         connect(root.left);
         connect(root.right);
     }
