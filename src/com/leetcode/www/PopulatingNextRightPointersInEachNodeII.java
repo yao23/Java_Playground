0ca98e71be9ea04b0d2b74a2cfe3113728c0a817
yao <myspiritcrazy@gmail.com>
2017-09-10 20:56:09 -0700

    refactor code

--- src/com/leetcode/www/PopulatingNextRightPointersInEachNodeII.java
+++ src/com/leetcode/www/PopulatingNextRightPointersInEachNodeII.java
@@ -2,44 +2,44 @@ package com.leetcode.www;
 
 public class PopulatingNextRightPointersInEachNodeII { // LC 117
     public void connect(TreeLinkNode root) { // beats 44.84%
-        if( root == null )	return;
-        if( root.left != null ) {
-            if( root.right == null ) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            if (root.right == null) {
                 TreeLinkNode tmp = root.next;
                 boolean flag = true;
-                while( tmp != null && flag ) {
-                    if( tmp.left != null ) {
+                while (tmp != null && flag) {
+                    if (tmp.left != null) {
                         root.left.next = tmp.left;
                         flag = false;
-                    }
-                    else if( tmp.right != null ) {
+                    } else if (tmp.right != null) {
                         root.left.next = tmp.right;
                         flag = false;
-                    }
-                    else
+                    } else {
                         tmp = tmp.next;
+                    }
                 }
-            }
-            else
+            } else {
                 root.left.next = root.right;
+            }
         }
-        if( root.right != null ) {
-            if( root.next == null )
+        if (root.right != null) {
+            if (root.next == null) {
                 root.right.next = null;
-            else {
+            } else {
                 TreeLinkNode tmp = root.next;
                 boolean flag = true;
-                while( tmp != null && flag ) {
-                    if( tmp.left != null ) {
+                while (tmp != null && flag) {
+                    if (tmp.left != null) {
                         root.right.next = tmp.left;
                         flag = false;
-                    }
-                    else if( tmp.right != null ) {
+                    } else if (tmp.right != null) {
                         root.right.next = tmp.right;
                         flag = false;
-                    }
-                    else
+                    } else {
                         tmp = tmp.next;
+                    }
                 }
             }
         }
