4246cf6677e85daf1d8f448e8dd618b26470e16c
yao <myspiritcrazy@gmail.com>
2017-09-10 21:10:43 -0700

    add a DP solution

--- src/com/leetcode/www/PascalsTriangle.java
+++ src/com/leetcode/www/PascalsTriangle.java
@@ -5,6 +5,24 @@ import java.util.List;
 
 public class PascalsTriangle { // LC 118
     public List<List<Integer>> generate(int numRows) { // beats 19.61%
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> row, pre = null;
+        for (int i = 0; i < numRows; ++i) {
+            row = new ArrayList<>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(pre.get(j - 1) + pre.get(j));
+                }
+            }
+            pre = row;
+            res.add(row);
+        }
+        return res;
+    }
+
+    public List<List<Integer>> generateV0(int numRows) { // beats 19.61%
         List<List<Integer>> result = new ArrayList<>();
         if (numRows <= 0) {
             return result;
