b20297a9c6e4c9eaf2b5418f804bdaaf24fb5776
yao <myspiritcrazy@gmail.com>
2017-09-11 22:19:14 -0700

    Linked List Cycle

---
+++ src/com/leetcode/www/LinkedListCycle.java
@@ -0,0 +1,17 @@
+package com.leetcode.www;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if( head == null || head.next == null )
+            return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while( fast != null && fast.next != null ) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if( slow == fast )
+                return true;
+        }
+        return false;
+    }
+}
