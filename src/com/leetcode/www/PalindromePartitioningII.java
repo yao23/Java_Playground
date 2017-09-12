5f867abc79bd474f5a604ccc0d0be88f0f0373dd
yao <myspiritcrazy@gmail.com>
2017-09-11 22:10:52 -0700

    add a clean solution

--- src/com/leetcode/www/PalindromePartitioningII.java
+++ src/com/leetcode/www/PalindromePartitioningII.java
@@ -1,7 +1,38 @@
 package com.leetcode.www;
 
 public class PalindromePartitioningII { // LC 132
-    public int minCut(String s) { // beats 28.44%
+    /**
+     *
+     * @param s
+     * @return
+     *
+     * This can be solved by two points:
+     *
+     * cut[i] is the minimum of cut[j - 1] + 1 (j <= i), if [j, i] is palindrome.
+     * If [j, i] is palindrome, [j + 1, i - 1] is palindrome, and c[j] == c[i].
+     *
+     *
+     */
+    public int minCut(String s) { // beats 69.79%
+        char[] c = s.toCharArray();
+        int n = c.length;
+        int[] cut = new int[n];
+        boolean[][] pal = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            int min = i;
+            for (int j = 0; j <= i; j++) {
+                if (c[j] == c[i] && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+                    pal[j][i] = true;
+                    min = (j == 0) ? 0 : Math.min(min, cut[j - 1] + 1);
+                }
+            }
+            cut[i] = min;
+        }
+        return cut[n - 1];
+    }
+
+    public int minCutV0(String s) { // beats 28.44%
         if (s == null || s.length() <= 1) {
             return 0;
         }
