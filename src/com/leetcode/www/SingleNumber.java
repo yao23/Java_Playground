8788c12931d576c5d8093228d516baa35244fd69
yao <myspiritcrazy@gmail.com>
2017-09-11 22:14:20 -0700

    refactor code

--- src/com/leetcode/www/SingleNumber.java
+++ src/com/leetcode/www/SingleNumber.java
@@ -3,8 +3,9 @@ package com.leetcode.www;
 public class SingleNumber { // LC 136
     public int singleNumber(int[] nums) { // beats 32.11%
         int result = nums[0];
-        for( int i = 1; i < nums.length; i++ )
+        for (int i = 1; i < nums.length; i++) {
             result ^= nums[i];
+        }
         return result;
     }
 }
