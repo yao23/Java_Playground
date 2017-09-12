4543630b1691db502e0ec9fe424a5738c0cdf4b2
yao <myspiritcrazy@gmail.com>
2017-09-11 22:12:51 -0700

    Single Number

---
+++ src/com/leetcode/www/SingleNumber.java
@@ -0,0 +1,12 @@
+package com.leetcode.www;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for( int i = 1; i < nums.length; i++ )
+            result ^= nums[i];
+        return result;
+    }
+}
+
+// [1] => 1
