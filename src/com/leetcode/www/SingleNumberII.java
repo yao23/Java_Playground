797c38843860bf7ae6652db283e8d0c33713af09
yao <myspiritcrazy@gmail.com>
2017-09-11 22:15:59 -0700

    Single Number II

---
+++ src/com/leetcode/www/SingleNumberII.java
@@ -0,0 +1,15 @@
+package com.leetcode.www;
+
+public class SingleNumberII {
+    public int singleNumber(int[] nums) {
+        int ones = 0, twos = 0, threes = 0;
+        for( int i = 0; i < nums.length; i++ ) {
+            twos |= (ones & nums[i]);
+            ones ^= nums[i];
+            threes = ~(ones & twos);
+            ones &= threes;
+            twos &= threes;
+        }
+        return ones;
+    }
+}
