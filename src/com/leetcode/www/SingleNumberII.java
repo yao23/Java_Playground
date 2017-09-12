cbec3aed1b7f4a41602daa33a8fe258c744d80fd
yao <myspiritcrazy@gmail.com>
2017-09-11 22:16:28 -0700

    refactor code

--- src/com/leetcode/www/SingleNumberII.java
+++ src/com/leetcode/www/SingleNumberII.java
@@ -3,7 +3,7 @@ package com.leetcode.www;
 public class SingleNumberII {
     public int singleNumber(int[] nums) {
         int ones = 0, twos = 0, threes = 0;
-        for( int i = 0; i < nums.length; i++ ) {
+        for (int i = 0; i < nums.length; i++) {
             twos |= (ones & nums[i]);
             ones ^= nums[i];
             threes = ~(ones & twos);
