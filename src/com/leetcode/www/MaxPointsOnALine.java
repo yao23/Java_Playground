e5a6fa965b186032087394ffd95965f57760b0ae
yao <myspiritcrazy@gmail.com>
2017-09-12 18:25:39 -0700

    refactor code

--- src/com/leetcode/www/MaxPointsOnALine.java
+++ src/com/leetcode/www/MaxPointsOnALine.java
@@ -5,35 +5,35 @@ import java.util.Map;
 
 public class MaxPointsOnALine {
     public int maxPoints(Point[] points) {
-        Map<Double, Integer> map = new HashMap<Double, Integer>();
+        Map<Double, Integer> map = new HashMap<>();
         int res = 0;
         int len = points.length;
-        for( int i = 0; i < len; i++ ) {
-            int InvalidK = 0;
+        for (int i = 0; i < len; i++) {
+            int invalidK = 0;
             int add = 1;
-            for( int j = i + 1; j < len; j++ ) {
-                if( points[j].x == points[i].x ) {
-                    if( points[j].y == points[i].y )
+            for (int j = i + 1; j < len; j++) {
+                if (points[j].x == points[i].x) {
+                    if (points[j].y == points[i].y) {
                         add++;
-                    else
-                        InvalidK++;
+                    } else {
+                        invalidK++;
+                    }
                     continue;
                 }
-                double k = points[j].y == points[i].y ? 0.0 :
-                        (1.0 * (points[j].y - points[i].y)) /
-                                (points[j].x - points[i].x);
-                if( map.containsKey(k) ) {
+                double k = points[j].y == points[i].y ? 0.0 : (1.0 * (points[j].y - points[i].y)) / (points[j].x - points[i].x);
+                if (map.containsKey(k)) {
                     int count = map.get(k);
                     map.put(k, count + 1);
-                }
-                else
+                } else {
                     map.put(k, 1);
+                }
             }
-            for( Integer it : map.values() ) {
-                if( it + add > res )
+            for (Integer it : map.values()) {
+                if (it + add > res) {
                     res = it.intValue() + add;
+                }
             }
-            res = Math.max(InvalidK + add, res);
+            res = Math.max(invalidK + add, res);
             map.clear();
         }
         return res;
