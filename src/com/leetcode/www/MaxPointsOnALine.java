527f1581fe3074772ddf328cbb5cc3a1539af4c3
yao <myspiritcrazy@gmail.com>
2017-09-12 18:31:08 -0700

    refactor code

--- src/com/leetcode/www/MaxPointsOnALine.java
+++ src/com/leetcode/www/MaxPointsOnALine.java
@@ -5,51 +5,55 @@ import java.util.Map;
 
 public class MaxPointsOnALine { // LC 149
     public int maxPoints(Point[] points) { // beats 51.87%
-        if (points==null) return 0;
-        if (points.length<=2) return points.length;
+        if (points == null) {
+            return 0;
+        }
+        if (points.length <= 2) {
+            return points.length;
+        }
 
-        Map<Integer,Map<Integer,Integer>> map = new HashMap<Integer,Map<Integer,Integer>>();
+        Map<Integer,Map<Integer,Integer>> map = new HashMap<>();
         int result=0;
-        for (int i=0;i<points.length;i++){
+        for (int i = 0; i < points.length; i++) {
             map.clear();
-            int overlap=0,max=0;
-            for (int j=i+1;j<points.length;j++){
-                int x=points[j].x-points[i].x;
-                int y=points[j].y-points[i].y;
-                if (x==0&&y==0){
+            int overlap = 0, max = 0;
+            for (int j = i + 1; j < points.length; j++) {
+                int x = points[j].x - points[i].x;
+                int y = points[j].y - points[i].y;
+                if (x == 0 && y == 0) {
                     overlap++;
                     continue;
                 }
-                int gcd=generateGCD(x,y);
-                if (gcd!=0){
+                int gcd = generateGCD(x, y);
+                if (gcd != 0) {
                     x/=gcd;
                     y/=gcd;
                 }
 
-                if (map.containsKey(x)){
-                    if (map.get(x).containsKey(y)){
-                        map.get(x).put(y, map.get(x).get(y)+1);
-                    }else{
+                if (map.containsKey(x)) {
+                    if (map.get(x).containsKey(y)) {
+                        map.get(x).put(y, map.get(x).get(y) + 1);
+                    } else {
                         map.get(x).put(y, 1);
                     }
-                }else{
-                    Map<Integer,Integer> m = new HashMap<Integer,Integer>();
+                } else {
+                    Map<Integer,Integer> m = new HashMap<>();
                     m.put(y, 1);
                     map.put(x, m);
                 }
                 max=Math.max(max, map.get(x).get(y));
             }
-            result=Math.max(result, max+overlap+1);
+            result=Math.max(result, max+overlap + 1);
         }
         return result;
-
-
     }
-    private int generateGCD(int a,int b){
 
-        if (b==0) return a;
-        else return generateGCD(b,a%b);
-
+    private int generateGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return generateGCD(b,a % b);
+        }
     }
 
     public int maxPointsV0(Point[] points) { // not working for test case 2 (return 3)
