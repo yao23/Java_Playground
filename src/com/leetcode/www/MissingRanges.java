e7d32ea02ac8147b72b1a7cbe21aa1b5a91e198a
yao <myspiritcrazy@gmail.com>
2017-09-12 20:54:40 -0700

    documentation

--- src/com/leetcode/www/MissingRanges.java
+++ src/com/leetcode/www/MissingRanges.java
@@ -1,16 +1,16 @@
-/**
+package com.leetcode.www; /**
  * Created by liyao on 6/11/17.
  */
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MissingRanges {
+public class MissingRanges { // 163
     /**
      *  Given a sorted integer array where the range of elements are [0, 99] inclusive, return its missing ranges.
      *  For example, given [0, 1, 3, 50, 75], return [“2”, “4->49”, “51->74”, “76->99”]
      */
-    public List<String> findMissingRanges(int[] nums, int lo, int hi) {
+    public List<String> findMissingRanges(int[] nums, int lo, int hi) { // beats 13.15%
         List<String> res = new ArrayList<>();
         long lower = (long)lo;
         long upper = (long)hi;
