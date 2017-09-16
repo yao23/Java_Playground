2f01fd945a5fd867b531e34c9bd06556a4b43b45
yao <myspiritcrazy@gmail.com>
2017-09-16 11:04:45 -0700

    Intersection of Two Linked Lists

---
+++ src/com/leetcode/www/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,51 @@
+package com.leetcode.www;
+
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = getLength(headA);
+        int lenB = getLength(headB);
+        if (lenA == lenB) {
+            return findIntersection(headA, headB);
+        } else {
+            ListNode hA = headA;
+            ListNode hB = headB;
+            if (lenA < lenB) {
+                hB = getStartNode(headB, lenB - lenA);
+            } else {
+                hA = getStartNode(headA, lenA - lenB);
+            }
+            return findIntersection(hA, hB);
+        }
+    }
+
+    private ListNode getStartNode(ListNode head, int step) {
+        for (int i = 0; i < step; i++) {
+            head = head.next;
+        }
+        return head;
+    }
+
+    private ListNode findIntersection(ListNode headA, ListNode headB) {
+        while (headA != null && headB != null) {
+            if (headA == headB) {
+                return headA;
+            } else {
+                headA = headA.next;
+                headB = headB.next;
+            }
+        }
+
+        return null;
+    }
+
+    private int getLength(ListNode head) {
+        int len = 0;
+
+        while (head != null) {
+            head = head.next;
+            len++;
+        }
+
+        return len;
+    }
+}
