02c719ee4ae583de0bdb6ba61fba0d7aca525040
yao <myspiritcrazy@gmail.com>
2017-09-16 17:06:33 -0700

    add a clean solution

--- src/com/leetcode/www/OneEditDistance.java
+++ src/com/leetcode/www/OneEditDistance.java
@@ -4,12 +4,13 @@ public class OneEditDistance { // LC 161
     public boolean isOneEditDistance(String s, String t) { // beats 12.49%
         for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
             if (s.charAt(i) != t.charAt(i)) {
-                if (s.length() == t.length()) // s has the same length as t, so the only possibility is replacing one char in s and t
+                if (s.length() == t.length()) {// s has the same length as t, so the only possibility is replacing one char in s and t
                     return s.substring(i + 1).equals(t.substring(i + 1));
-                else if (s.length() < t.length()) // t is longer than s, so the only possibility is deleting one char from t
+                } else if (s.length() < t.length()) { // t is longer than s, so the only possibility is deleting one char from t
                     return s.substring(i).equals(t.substring(i + 1));
-                else // s is longer than t, so the only possibility is deleting one char from s
+                } else { // s is longer than t, so the only possibility is deleting one char from s
                     return t.substring(i).equals(s.substring(i + 1));
+                }
             }
         }
         //All previous chars are the same, the only possibility is deleting the end char in the longer one of s and t
