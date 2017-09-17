0ce6c99730ddd9ea789f2828eae6b31aee33da0f
yao <myspiritcrazy@gmail.com>
2017-09-16 17:17:12 -0700

    add a clean solution

--- src/com/leetcode/www/OneEditDistance.java
+++ src/com/leetcode/www/OneEditDistance.java
@@ -1,7 +1,30 @@
 package com.leetcode.www;
 
 public class OneEditDistance { // LC 161
-    public boolean isOneEditDistance(String s, String t) { // beats 12.49%
+    public boolean isOneEditDistance(String s, String t) { // beats 49.94%
+        if (s == null || t == null)
+            return false;
+
+        if (s.length() > t.length())
+            return isOneEditDistance(t, s);
+
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) != t.charAt(j)) {
+                // we try to replace s[i] with s[j] or insert s[j] to s[i]
+                // then compare the rest and see if they are the same
+                return s.substring(i + 1).equals(t.substring(j + 1)) ||
+                        s.substring(i).equals(t.substring(j + 1));
+            }
+
+            i++; j++;
+        }
+
+        return t.length() - j == 1;
+    }
+
+    public boolean isOneEditDistanceV1(String s, String t) { // beats 12.49%
         for (int i = 0; i < Math.min(s.length(), t.length()); i++) {
             if (s.charAt(i) != t.charAt(i)) {
                 if (s.length() == t.length()) {// s has the same length as t, so the only possibility is replacing one char in s and t
