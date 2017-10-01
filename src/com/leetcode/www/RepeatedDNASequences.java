c06bf6dd4a91409b0d8cffe527fbba64a1f41ded
yao <myspiritcrazy@gmail.com>
2017-10-01 11:55:49 -0700

    refactor code solution for hashset and bitmap

--- src/com/leetcode/www/RepeatedDNASequences.java
+++ src/com/leetcode/www/RepeatedDNASequences.java
@@ -18,25 +18,46 @@ public class RepeatedDNASequences { // LC 187
     }
 
     public List<String> findRepeatedDnaSequencesV0(String s) { // beats 52.60%
-        Set<Integer> words = new HashSet<>();
-        Set<Integer> doubleWords = new HashSet<>();
-        List<String> rv = new ArrayList<>();
-        char[] map = new char[26];
-        //map['A' - 'A'] = 0;
-        map['C' - 'A'] = 1;
-        map['G' - 'A'] = 2;
-        map['T' - 'A'] = 3;
+        Set<Integer> firstTime = new HashSet<>();
+        Set<Integer> secondTime = new HashSet<>();
+        List<String> list = new ArrayList<>();
 
-        for (int i = 0; i < s.length() - 9; i++) {
-            int v = 0;
-            for (int j = i; j < i + 10; j++) {
-                v <<= 2;
-                v |= map[s.charAt(j) - 'A'];
+        char[] map = new char[26];
+        int len = s.length();
+
+        // Hashing function. We have only 4 letters which we can represent by 2 bits.
+        map['A' - 'A'] = 0; // A = 00
+        map['C' - 'A'] = 1; // B = 01
+        map['G' - 'A'] = 2; // C = 10
+        map['T' - 'A'] = 3; // D = 11
+
+        for (int i = 0; i <= len - 10; i++) {
+            int sequence = 0;
+            for (int j = i; j< i + 10; j++) {
+                // Shift existing sequence by two to make space for the new character coming
+                sequence = sequence << 2;
+
+                // Copy the character from the map and paste those two bits in the newly created space. Read bit wise OR.
+                sequence = sequence | map[s.charAt(j) - 'A'];
             }
-            if (!words.add(v) && doubleWords.add(v)) {
-                rv.add(s.substring(i, i + 10));
+
+            // For this number to be added in the list, this should be the second time this number is appearing
+            // For this if condition to be true, firstTime.add() should be false.
+            // firstTime.add() will be false when there is already the same number present.
+            // How it will behave?
+            // First time - firstTime.add(sequence) will return T
+            // !firstTime.add(sequence) will become F
+            // secondTime.add(sequence) will NOT be executed
+
+            // Second time addition:
+            // First time - firstTime.add(sequence) will return F
+            // !firstTime.add(sequence) will become T
+            // secondTime.add(sequence) will be executed
+            if (!firstTime.add(sequence) && secondTime.add(sequence)) {
+                list.add(s.substring(i, i + 10));
             }
         }
-        return rv;
+
+        return list;
     }
 }
