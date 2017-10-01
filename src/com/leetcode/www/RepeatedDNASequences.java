729748ae4e03cc5d6f174212280225adc636f09b
yao <myspiritcrazy@gmail.com>
2017-10-01 11:40:23 -0700

    refactor code

--- src/com/leetcode/www/RepeatedDNASequences.java
+++ src/com/leetcode/www/RepeatedDNASequences.java
@@ -16,13 +16,13 @@ public class RepeatedDNASequences { // LC 187
         map['G' - 'A'] = 2;
         map['T' - 'A'] = 3;
 
-        for(int i = 0; i < s.length() - 9; i++) {
+        for (int i = 0; i < s.length() - 9; i++) {
             int v = 0;
-            for(int j = i; j < i + 10; j++) {
+            for (int j = i; j < i + 10; j++) {
                 v <<= 2;
                 v |= map[s.charAt(j) - 'A'];
             }
-            if(!words.add(v) && doubleWords.add(v)) {
+            if (!words.add(v) && doubleWords.add(v)) {
                 rv.add(s.substring(i, i + 10));
             }
         }
