99de51c1f21eaf3a805b130c6ccd460f09c85ab6
yao <myspiritcrazy@gmail.com>
2017-10-01 11:23:19 -0700

    add solution with array list and hashmap

--- src/com/leetcode/www/TwoSumIII.java
+++ src/com/leetcode/www/TwoSumIII.java
@@ -1,6 +1,8 @@
 package com.leetcode.www;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TwoSumIII { // LC 170
@@ -11,11 +13,42 @@ public class TwoSumIII { // LC 170
      * boolean param_2 = obj.find(value);
      */
 
-    class TwoSum { // beats 12.55%
-        private Map<Integer, Integer> map;
+    class TwoSum { // beats 80.32%
+        private List<Integer> list = new ArrayList<>();
+        private Map<Integer, Integer> map = new HashMap<>();
 
         /** Initialize your data structure here. */
         public TwoSum() {
+            map = new HashMap<>();
+        }
+
+        // Add the number to an internal data structure.
+        public void add(int number) {
+            if (map.containsKey(number)) {
+                map.put(number, map.get(number) + 1);
+            } else {
+                map.put(number, 1);
+                list.add(number);
+            }
+        }
+
+        // Find if there exists any pair of numbers which sum is equal to the value.
+        public boolean find(int value) {
+            for (int i = 0; i < list.size(); i++) { // faster than iterating hashmap
+                int num1 = list.get(i), num2 = value - num1;
+                if ((num1 == num2 && map.get(num1) > 1) || (num1 != num2 && map.containsKey(num2))) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    class TwoSumV0 { // beats 12.55%
+        private Map<Integer, Integer> map;
+
+        /** Initialize your data structure here. */
+        public TwoSumV0() {
             map = new HashMap<>();
         }
 
