f5ad9e7cae8d32ed706fb2010783ec7300f91aba
yao <myspiritcrazy@gmail.com>
2017-10-01 11:47:21 -0700

    add solution with 2 hashset

--- src/com/leetcode/www/RepeatedDNASequences.java
+++ src/com/leetcode/www/RepeatedDNASequences.java
@@ -6,7 +6,18 @@ import java.util.List;
 import java.util.Set;
 
 public class RepeatedDNASequences { // LC 187
-    public List<String> findRepeatedDnaSequences(String s) { // beats 52.60%
+    public List<String> findRepeatedDnaSequences(String s) { // beats 91.81%
+        Set seen = new HashSet(), repeated = new HashSet();
+        for (int i = 0; i + 9 < s.length(); i++) {
+            String ten = s.substring(i, i + 10);
+            if (!seen.add(ten)) {
+                repeated.add(ten);
+            }
+        }
+        return new ArrayList<String>(repeated);
+    }
+
+    public List<String> findRepeatedDnaSequencesV0(String s) { // beats 52.60%
         Set<Integer> words = new HashSet<>();
         Set<Integer> doubleWords = new HashSet<>();
         List<String> rv = new ArrayList<>();
