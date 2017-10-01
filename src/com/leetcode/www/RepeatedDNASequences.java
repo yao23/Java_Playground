2de5232d3b99274a240cc13dafcf62cc4a62cf31
yao <myspiritcrazy@gmail.com>
2017-10-01 11:36:10 -0700

    Repeated DNA Sequences

---
+++ src/com/leetcode/www/RepeatedDNASequences.java
@@ -0,0 +1,31 @@
+package com.leetcode.www;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RepeatedDNASequences { // LC 187
+    public List<String> findRepeatedDnaSequences(String s) { // beats 52.60%
+        Set<Integer> words = new HashSet<>();
+        Set<Integer> doubleWords = new HashSet<>();
+        List<String> rv = new ArrayList<>();
+        char[] map = new char[26];
+        //map['A' - 'A'] = 0;
+        map['C' - 'A'] = 1;
+        map['G' - 'A'] = 2;
+        map['T' - 'A'] = 3;
+
+        for(int i = 0; i < s.length() - 9; i++) {
+            int v = 0;
+            for(int j = i; j < i + 10; j++) {
+                v <<= 2;
+                v |= map[s.charAt(j) - 'A'];
+            }
+            if(!words.add(v) && doubleWords.add(v)) {
+                rv.add(s.substring(i, i + 10));
+            }
+        }
+        return rv;
+    }
+}
