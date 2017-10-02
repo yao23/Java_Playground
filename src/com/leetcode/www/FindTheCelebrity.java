774a4f801081c5fcd27a300070b61586e3c925cf
yao <myspiritcrazy@gmail.com>
2017-10-01 21:10:38 -0700

    documentation

--- src/com/leetcode/www/FindTheCelebrity.java
+++ src/com/leetcode/www/FindTheCelebrity.java
@@ -23,18 +23,18 @@ public class FindTheCelebrity { // LC 277
 
     public int findCelebrity(int n) { // beats 94.21%
         int x = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) { // knows(a, b) is analogy with (a < b)
             if (knows(x, i)) {
                 x = i;
             }
         }
         for (int i = 0; i < x; ++i) {
-            if (knows(x, i)) {
+            if (knows(x, i)) { // candidates knows i
                 return -1;
             }
         }
         for (int i = 0; i < n; ++i) {
-            if (!knows(i, x)) {
+            if (!knows(i, x)) { // i doesn't know candidate
                 return -1;
             }
         }
@@ -49,10 +49,23 @@ public class FindTheCelebrity { // LC 277
             }
         }
         for (int i = 0; i < n; i++) {
-            if (i != candidate && (knows(candidate, i) || !knows(i, candidate))) { // i and candidate don't know each other
+            if (i != candidate && (knows(candidate, i) || !knows(i, candidate))) {
                 return -1;
             }
         }
         return candidate;
     }
 }
+
+// Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity.
+// The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+//
+// Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do
+// is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the
+// celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+//
+// You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int
+// findCelebrity(n), your function should minimize the number of calls to knows.
+//
+// Note: There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is
+// a celebrity in the party. If there is no celebrity, return -1.
