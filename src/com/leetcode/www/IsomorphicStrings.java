16837a8e59deeb2aca6ee848fa713ba89574d294
yao <myspiritcrazy@gmail.com>
2017-10-01 19:19:56 -0700

    add solution with char array

--- src/com/leetcode/www/IsomorphicStrings.java
+++ src/com/leetcode/www/IsomorphicStrings.java
@@ -1,12 +1,37 @@
 package com.leetcode.www;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class IsomorphicStrings { // LC 205
-    public boolean isIsomorphic(String s, String t) { // beats 87.07%
+    public boolean isIsomorphic(String s, String t) { // beats 98.86%
+        char[] sCharArr = s.toCharArray();
+        char[] tCharArr = t.toCharArray();
+
+        int length = sCharArr.length;
+        if (length != tCharArr.length) {
+            return false;
+        }
+
+        char[] sm = new char[256];
+        char[] tm = new char[256];
+
+        for (int i = 0; i < length; i++) {
+            char sc = sCharArr[i];
+            char tc = tCharArr[i];
+            if (sm[sc] == 0 && tm[tc] == 0) {
+                sm[sc] = tc;
+                tm[tc] = sc;
+            } else {
+                if (sm[sc] != tc || tm[tc] != sc){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isIsomorphicV1(String s, String t) { // beats 87.07%
         int lenS = s.length(), lenT = t.length();
         if (lenS != lenT) {
             return false;
