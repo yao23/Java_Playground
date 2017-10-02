df5f6ad537437cfb8ab14d9f9c067f4a7a554ebf
yao <myspiritcrazy@gmail.com>
2017-10-01 19:47:52 -0700

    fix bug for duplicate number in array

--- src/com/leetcode/www/ShortestWordDistance.java
+++ src/com/leetcode/www/ShortestWordDistance.java
@@ -1,21 +1,28 @@
 package com.leetcode.www;
 
 public class ShortestWordDistance { // LC 243
-    public int shortestDistance(String[] words, String word1, String word2) { // not working for test case 1
+    public int shortestDistance(String[] words, String word1, String word2) { // beats 79.02%
         int len = words.length;
+        int res = Integer.MAX_VALUE;
         int idx1 = -1, idx2 = -1;
-        for (int i = 0; i < len && (idx1 < 0 || idx2 < 0); i++) {
+        for (int i = 0; i < len; i++) {
             String s = words[i];
             if (s.equals(word1)) {
                 idx1 = i;
+                if (idx2 >= 0) {
+                    res = Math.min(res, Math.abs(idx2 - idx1));
+                }
             } else if (s.equals(word2)) {
                 idx2 = i;
+                if (idx1 >= 0) {
+                    res = Math.min(res, Math.abs(idx2 - idx1));
+                }
             } else {
                 continue;
             }
         }
 
-        return Math.abs(idx2 - idx1);
+        return res;
     }
 }
 
