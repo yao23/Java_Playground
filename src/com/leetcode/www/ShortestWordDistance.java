843f8c2effde0defba33ecaff42c724535f6137e
yao <myspiritcrazy@gmail.com>
2017-10-01 19:51:13 -0700

    add solution with 1 index

--- src/com/leetcode/www/ShortestWordDistance.java
+++ src/com/leetcode/www/ShortestWordDistance.java
@@ -2,6 +2,19 @@ package com.leetcode.www;
 
 public class ShortestWordDistance { // LC 243
     public int shortestDistance(String[] words, String word1, String word2) { // beats 79.02%
+        int index = -1, minDistance = Integer.MAX_VALUE;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1) || words[i].equals(word2)) {
+                if (index != -1 && !words[index].equals(words[i])) {
+                    minDistance = Math.min(minDistance, i - index);
+                }
+                index = i;
+            }
+        }
+        return minDistance;
+    }
+
+    public int shortestDistanceV0(String[] words, String word1, String word2) { // beats 79.02%
         int len = words.length;
         int res = Integer.MAX_VALUE;
         int idx1 = -1, idx2 = -1;
