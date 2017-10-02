0fea17b912ecf787fc60ae6db95fa300bfb606f7
yao <myspiritcrazy@gmail.com>
2017-10-01 20:27:28 -0700

    add a clean solution

--- src/com/leetcode/www/FactorCombinations.java
+++ src/com/leetcode/www/FactorCombinations.java
@@ -4,7 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FactorCombinations { // LC 254
-    public List<List<Integer>> getFactors(int n) { // beats 32.09%
+    List<List<Integer>> result = new ArrayList<>();
+    public List<List<Integer>> getFactors(int n) { // beats 95.67%
+        getFactors(n, new ArrayList<>(), 2);
+        return result;
+    }
+    private void getFactors(int n, List<Integer> iList, int st){
+        for (int i = st; i * i <= n; ++i) { // only iterate until square root of n
+            if (n % i == 0) {
+                iList.add(i);
+                iList.add(n / i);
+                result.add(new ArrayList<>(iList));
+                iList.remove(iList.size() - 1);
+                getFactors(n / i, iList, i);
+                iList.remove(iList.size() - 1);
+            }
+        }
+    }
+
+    public List<List<Integer>> getFactorsV0(int n) { // beats 32.09%
         List<List<Integer>> result = new ArrayList<>();
         helper(result, new ArrayList<>(), n, 2);
         return result;
