d79ac8af218598b06eb58cf5a06e5e0d2814d201
yao <myspiritcrazy@gmail.com>
2017-10-01 19:31:34 -0700

    add solution with 2 directions scan

--- src/com/leetcode/www/ProductOfArrayExceptSelf.java
+++ src/com/leetcode/www/ProductOfArrayExceptSelf.java
@@ -2,6 +2,19 @@ package com.leetcode.www;
 
 public class ProductOfArrayExceptSelf { // LC 238
     public int[] productExceptSelf(int[] nums) { // beats 27.57%
+        int[] result = new int[nums.length];
+        for (int i = 0, tmp = 1; i < nums.length; i++) { // left to right (previous product)
+            result[i] = tmp;
+            tmp *= nums[i];
+        }
+        for (int i = nums.length - 1, tmp = 1; i >= 0; i--) { // right to left (later product)
+            result[i] *= tmp;
+            tmp *= nums[i];
+        }
+        return result;
+    }
+
+    public int[] productExceptSelfV0(int[] nums) { // beats 27.57%
         int n = nums.length;
         int[] res = new int[n];
         res[0] = 1;
