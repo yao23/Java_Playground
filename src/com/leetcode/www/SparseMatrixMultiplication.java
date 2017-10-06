89b06422031df9b4f5d9a1411350b97c282af15c
yao <myspiritcrazy@gmail.com>
2017-10-05 21:14:57 -0700

    add a clean solution

--- src/com/leetcode/www/SparseMatrixMultiplication.java
+++ src/com/leetcode/www/SparseMatrixMultiplication.java
@@ -1,7 +1,23 @@
 package com.leetcode.www;
 
 public class SparseMatrixMultiplication { // LC 311
-    public int[][] multiply(int[][] A, int[][] B) { // beats 0.29%
+    public int[][] multiply(int[][] A, int[][] B) { // beats 84.43%
+        int m = A.length, n = A[0].length, nB = B[0].length;
+        int[][] C = new int[m][nB];
+
+        for (int i = 0; i < m; i++) {
+            for (int k = 0; k < n; k++) {
+                if (A[i][k] != 0) {
+                    for (int j = 0; j < nB; j++) {
+                        if (B[k][j] != 0) C[i][j] += A[i][k] * B[k][j];
+                    }
+                }
+            }
+        }
+        return C;
+    }
+
+    public int[][] multiplyV0(int[][] A, int[][] B) { // beats 0.29%
         int rowA = A.length;
         if (rowA == 0) {
             return new int[][]{};
