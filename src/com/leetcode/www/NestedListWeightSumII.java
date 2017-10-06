fd7659d13924fef02c4aaa641f7b376d4ea826ae
yao <myspiritcrazy@gmail.com>
2017-10-06 07:09:24 -0700

    documentation and add test case

--- src/com/leetcode/www/NestedListWeightSumII.java
+++ src/com/leetcode/www/NestedListWeightSumII.java
@@ -22,5 +22,5 @@ public class NestedListWeightSumII { // LC 364
     }
 }
 
-// [[1,1],2,[1,1]] => 8
-// [1,[4,[6]]] => 17
+// [[1,1],2,[1,1]] => 8 (four 1's at depth 1, one 2 at depth 2)
+// [1,[4,[6]]] => 17 (one 1 at depth 3, one 4 at depth 2, and one 6 at depth 1; 1*3 + 4*2 + 6*1 = 17)
