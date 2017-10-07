777521cc54561940ee44a969ecf4e5c585b88ba8
yao <myspiritcrazy@gmail.com>
2017-10-06 22:49:36 -0700

    binary search solution

--- src/com/leetcode/www/ValidPerfectSquare.java
+++ src/com/leetcode/www/ValidPerfectSquare.java
@@ -1,8 +1,24 @@
 package com.leetcode.www;
 
 public class ValidPerfectSquare { // LC 367
+    // binary search
+    public boolean isPerfectSquare(int num) { // beats 37.97%, O(logn)
+        int low = 1, high = num;
+        while (low <= high) {
+            long mid = (low + high) >>> 1; // use long to avoid mid*mid from overflow
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                low = (int) mid + 1;
+            } else {
+                high = (int) mid - 1;
+            }
+        }
+        return false;
+    }
+
     // A square number is 1+3+5+7+...
-    public boolean isPerfectSquare(int num) { // beats 20.55%
+    public boolean isPerfectSquareV0(int num) { // beats 20.55%,  O(sqrt(n))
         int i = 1;
         while (num > 0) {
             num -= i;
