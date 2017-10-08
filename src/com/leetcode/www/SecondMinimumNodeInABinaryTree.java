45ccb358a02e23e06afcbdc48e21e7847df96b61
yao <myspiritcrazy@gmail.com>
2017-10-08 12:09:16 -0700

    refactor code

--- src/com/leetcode/www/SecondMinimumNodeInABinaryTree.java
+++ src/com/leetcode/www/SecondMinimumNodeInABinaryTree.java
@@ -37,18 +37,18 @@ public class SecondMinimumNodeInABinaryTree { // LC 671
     // BFS
     public int findSecondMinimumValueV1(TreeNode root) {
         int rootVal = root.val;
-        int secondSmall =Integer.MAX_VALUE;
+        int secondSmall = Integer.MAX_VALUE;
         boolean diffFound = false;
-        Queue<TreeNode> Q= new LinkedList<>();
+        Queue<TreeNode> Q = new LinkedList<>();
         Q.add(root);
 
-        while(!Q.isEmpty()) {
-            TreeNode curr=Q.poll();
+        while (!Q.isEmpty()) {
+            TreeNode curr = Q.poll();
             if (curr.val != rootVal && curr.val < secondSmall) {
                 secondSmall = curr.val;
                 diffFound = true;
             }
-            if(curr.left != null) {
+            if (curr.left != null) {
                 Q.add(curr.left);
                 Q.add(curr.right);
             }
