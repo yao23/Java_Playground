241a703cff017c7b88c17f6ac13d8e96a017a5e2
yao <myspiritcrazy@gmail.com>
2017-10-08 11:45:45 -0700

    Palindromic Substrings

--- src/com/leetcode/www/PalindromicSubstrings.java
+++ src/com/leetcode/www/PalindromicSubstrings.java
@@ -1,7 +1,26 @@
 package com.leetcode.www;
 
-public class PalindromicSubstrings {
-    public int countSubstrings(String s) {
+public class PalindromicSubstrings { // LC 647
+    int count = 0;
 
+    public int countSubstrings(String s) { // beats 89.41%
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i < s.length(); i++) { // i is the mid point
+            extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length
+        }
+
+        return count;
+    }
+
+    private void extendPalindrome(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            count++;
+            left--;
+            right++;
+        }
     }
 }
