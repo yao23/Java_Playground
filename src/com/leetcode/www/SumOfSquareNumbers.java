158d46a4043975ebed88a6dc0e9eb8c4cbb18296
yao <myspiritcrazy@gmail.com>
2017-10-08 11:35:55 -0700

    Fermat Theorem solution

--- src/com/leetcode/www/SumOfSquareNumbers.java
+++ src/com/leetcode/www/SumOfSquareNumbers.java
@@ -3,8 +3,25 @@ package com.leetcode.www;
 public class SumOfSquareNumbers { // LC 633
     // https://leetcode.com/problems/sum-of-square-numbers/solution/
 
-    // binary search
+    // Fermat Theorem
     public boolean judgeSquareSum(int c) { // beats 11.32%
+        for (int i = 2; i * i <= c; i++) {
+            int count = 0;
+            if (c % i == 0) {
+                while (c % i == 0) {
+                    count++;
+                    c /= i;
+                }
+                if (i % 4 == 3 && count % 2 != 0) {
+                    return false;
+                }
+            }
+        }
+        return c % 4 != 3;
+    }
+
+    // binary search
+    public boolean judgeSquareSumV1(int c) { // beats 11.32%
         for (long a = 0; a * a <= c; a++) {
             int b = c - (int)(a * a);
             if (binary_search(0, b, b))
