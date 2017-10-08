ad936cfb700c324ceca7504ac5a95475811fcb12
yao <myspiritcrazy@gmail.com>
2017-10-08 12:03:53 -0700

    add a clean solution

--- src/com/leetcode/www/SecondMinimumNodeInABinaryTree.java
+++ src/com/leetcode/www/SecondMinimumNodeInABinaryTree.java
@@ -30,4 +30,32 @@ public class SecondMinimumNodeInABinaryTree { // LC 671
         helper(root.left);
         helper(root.right);
     }
+
+    public int findSecondMinimumValueV0(TreeNode root) { // beats 45.82%
+        if (root == null) {
+            return -1;
+        }
+        if (root.left == null && root.right == null) {
+            return -1;
+        }
+
+        int left = root.left.val;
+        int right = root.right.val;
+
+        // if value same as root value, need to find the next candidate
+        if (root.left.val == root.val) {
+            left = findSecondMinimumValueV0(root.left);
+        }
+        if (root.right.val == root.val) {
+            right = findSecondMinimumValueV0(root.right);
+        }
+
+        if (left != -1 && right != -1) {
+            return Math.min(left, right);
+        } else if (left != -1) {
+            return left;
+        } else {
+            return right;
+        }
+    }
 }
