5117ed0432818de2772a1387bde5bffbdc08dea4
yao <myspiritcrazy@gmail.com>
2017-10-08 11:33:54 -0700

    binary search solution

--- src/com/leetcode/www/SumOfSquareNumbers.java
+++ src/com/leetcode/www/SumOfSquareNumbers.java
@@ -1,8 +1,30 @@
 package com.leetcode.www;
 
 public class SumOfSquareNumbers { // LC 633
+    // https://leetcode.com/problems/sum-of-square-numbers/solution/
+
+    // binary search
+    public boolean judgeSquareSum(int c) { // beats 11.32%
+        for (long a = 0; a * a <= c; a++) {
+            int b = c - (int)(a * a);
+            if (binary_search(0, b, b))
+                return true;
+        }
+        return false;
+    }
+    private boolean binary_search(long s, long e, int n) {
+        if (s > e)
+            return false;
+        long mid = s + (e - s) / 2;
+        if (mid * mid == n)
+            return true;
+        if (mid * mid > n)
+            return binary_search(s, mid - 1, n);
+        return binary_search(mid + 1, e, n);
+    }
+
     // sqrt function
-    public boolean judgeSquareSum(int c) { // beats 36.68%
+    public boolean judgeSquareSumV0(int c) { // beats 36.68%
         for (long a = 0; a * a <= c; a++) {
             double b = Math.sqrt(c - a * a);
             if (b == (int) b) {
