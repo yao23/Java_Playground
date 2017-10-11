d474872f1044a6df197f63e52b32f3f36bb1415e
yao <myspiritcrazy@gmail.com>
2017-10-10 23:13:00 -0700

    documentation for Super Ugly Number

--- src/SuperUglyNumber.java
+++ src/SuperUglyNumber.java
@@ -4,7 +4,7 @@
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
-public class SuperUglyNumber {
+public class SuperUglyNumber { // LC 313
     public static int nthSuperUglyNumberI(int n, int[] primes) { // 39ms, beats 32.59%
         int[] ugly = new int[n];
         int[] idx = new int[primes.length]; // next index for current prime number to get smallest product, finally [8,3,2,1] for test case 1
