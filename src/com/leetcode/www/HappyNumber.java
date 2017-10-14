36840931f40fa2954130e7747aafcc2796d2fbe6
yao <myspiritcrazy@gmail.com>
2017-10-14 11:42:42 -0700

    add a clean solution

--- src/com/leetcode/www/HappyNumber.java
+++ src/com/leetcode/www/HappyNumber.java
@@ -33,6 +33,49 @@ public class HappyNumber { // LC 202
         return s ;
     }
 
+    public boolean isHappyV2(int n) { // beats 96.12%
+        /* Base cases */
+
+        // A negative or zero value
+        // is not a valid input
+        // Return grumpily
+        if (n < 1) {
+            return false;
+        }
+
+        // You have entered
+        // the chain of despair.
+        // You will never be
+        // happy again
+        if (n == 4 ||  n == 16 ||  n == 20 ||  n == 37 ||  n == 42 ||  n == 58 ||  n == 89 ||  n == 145) {
+            return false;
+        }
+        // Check Wikipedia for
+        // the explanation of despairing numbers
+        // You don't need to remember all of them.
+        // Just remembering one of them will do
+
+        // You have achieved bliss
+        if (n == 1) {
+            return true;
+        }
+
+        // If none of the above
+        // Keep searching
+        return isHappyV2(getSumOfSquares(n));
+    }
+
+    private int getSumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+
+        return sum;
+    }
+
     //this is the bitset representation of happy numbers in range[0,810)
     private static final long[]happyMark = new long[]{580548859274370L, 35812649762896L,
             5764889547766761510L, 158621866461187L, -9061136725337702208L,
@@ -40,7 +83,7 @@ public class HappyNumber { // LC 202
             29290989780729856L, 7566260683533189120L, 1170945809492058640L,
             720593571220033568L, 292095590400L};
 
-    public boolean isHappyV1(int n) {
+    public boolean isHappyV1(int n) { // beats 96.12%
         if( n>810 ){
             int sum = 0, remainder = 0;
             while( n>0 ){
