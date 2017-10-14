7735a63466b14ef14adf283bf4b9b0062635130b
yao <myspiritcrazy@gmail.com>
2017-10-14 11:23:36 -0700

    add a clean solution

--- src/com/leetcode/www/ContainsDuplicate.java
+++ src/com/leetcode/www/ContainsDuplicate.java
@@ -4,7 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ContainsDuplicate { // LC 217
-    public boolean containsDuplicate(int[] nums) { // beats 27.96%
+    public boolean containsDuplicate(int[] nums) { // beats 68.65%
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (!set.add(i)) { // if there is same
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicateV0(int[] nums) { // beats 27.96%
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             if (set.contains(nums[i])) {
