10848f8d679ffbee1173e5ebfe6f190f9f676dbc
yao <myspiritcrazy@gmail.com>
2017-10-14 14:55:00 -0700

    add a clean solution

--- src/com/leetcode/www/ContainsDuplicateIII.java
+++ src/com/leetcode/www/ContainsDuplicateIII.java
@@ -42,6 +42,33 @@ public class ContainsDuplicateIII { // LC 220
         }
         return false;
     }
+
+    public boolean containsNearbyAlmostDuplicateV0(int[] nums, int k, int t) { // beats 70.46%
+        if (t < 0) return false;
+        Map<Long, Long> d = new HashMap<>();
+        long w = (long)t + 1;
+        for (int i = 0; i < nums.length; ++i) {
+            long m = getID(nums[i], w);
+            if (d.containsKey(m)) {
+                return true;
+            }
+            if (d.containsKey(m - 1) && Math.abs(nums[i] - d.get(m - 1)) < w) {
+                return true;
+            }
+            if (d.containsKey(m + 1) && Math.abs(nums[i] - d.get(m + 1)) < w) {
+                return true;
+            }
+            d.put(m, (long)nums[i]);
+            if (i >= k) {
+                d.remove(getID(nums[i - k], w));
+            }
+        }
+        return false;
+    }
+
+    private long getID(long i, long w) {
+        return i < 0 ? (i + 1) / w - 1 : i / w;
+    }
 }
 
 // [], 0, 0 => false
