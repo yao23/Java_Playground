9ebc962a4073f756afc53839991de5f1026e8bed
yao <myspiritcrazy@gmail.com>
2017-10-14 15:15:51 -0700

    refactor code

--- src/com/leetcode/www/MiniParser.java
+++ src/com/leetcode/www/MiniParser.java
@@ -1,19 +1,22 @@
 package com.leetcode.www;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
-import java.util.Stack;
 
 public class MiniParser { // LC 385
     public NestedInteger deserialize(String s) { // beats 53.05%
-        if (s.isEmpty())
+        if (s.isEmpty()) {
             return null;
-        if (s.charAt(0) != '[') // ERROR: special case
+        }
+        if (s.charAt(0) != '[') { // ERROR: special case
             return new NestedInteger(Integer.valueOf(s));
+        }
 
-        Stack<NestedInteger> stack = new Stack<>();
+        Deque<NestedInteger> stack = new ArrayDeque<>();
         NestedInteger curr = null;
         int l = 0; // l shall point to the start of a number substring;
-        // r shall point to the end+1 of a number substring
+        // r shall point to the end + 1 of a number substring
         for (int r = 0; r < s.length(); r++) {
             char ch = s.charAt(r);
             if (ch == '[') {
@@ -21,23 +24,24 @@ public class MiniParser { // LC 385
                     stack.push(curr);
                 }
                 curr = new NestedInteger();
-                l = r+1;
+                l = r + 1;
             } else if (ch == ']') {
                 String num = s.substring(l, r);
-                if (!num.isEmpty())
+                if (!num.isEmpty()) {
                     curr.add(new NestedInteger(Integer.valueOf(num)));
+                }
                 if (!stack.isEmpty()) {
                     NestedInteger pop = stack.pop();
                     pop.add(curr);
                     curr = pop;
                 }
-                l = r+1;
+                l = r + 1;
             } else if (ch == ',') {
-                if (s.charAt(r-1) != ']') {
+                if (s.charAt(r - 1) != ']') {
                     String num = s.substring(l, r);
                     curr.add(new NestedInteger(Integer.valueOf(num)));
                 }
-                l = r+1;
+                l = r + 1;
             }
         }
 
