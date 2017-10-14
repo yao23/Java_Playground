a54ca90e6a0bb9c64bb8cc2385388ac9bb68f3c4
yao <myspiritcrazy@gmail.com>
2017-10-14 16:56:50 -0700

    add a solution with sorting

--- src/com/leetcode/www/MajorityElement.java
+++ src/com/leetcode/www/MajorityElement.java
@@ -1,7 +1,15 @@
 package com.leetcode.www;
 
+import java.util.Arrays;
+
 public class MajorityElement { // LC 169
+    // sorting
     public int majorityElement(int[] nums) { // beats 70.59%
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+
+    public int majorityElementV0(int[] nums) { // beats 70.59%
         int major = nums[0], count = 1;
         for (int i = 1; i < nums.length; i++){
             if (count == 0) {
