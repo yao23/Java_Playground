2a51a70297daa31017a21a2e2e086c34d7de7cb3
yao <myspiritcrazy@gmail.com>
2017-10-14 08:45:20 -0700

    fix bugs

--- src/com/leetcode/www/ContainsDuplicateII.java
+++ src/com/leetcode/www/ContainsDuplicateII.java
@@ -3,13 +3,15 @@ package com.leetcode.www;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ContainsDuplicateII {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
+public class ContainsDuplicateII { // LC 219
+    public boolean containsNearbyDuplicate(int[] nums, int k) { // beats 39.90%
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 if (i - map.get(nums[i]) <= k) {
                     return true;
+                } else {
+                    map.put(nums[i], i);
                 }
             } else {
                 map.put(nums[i], i);
