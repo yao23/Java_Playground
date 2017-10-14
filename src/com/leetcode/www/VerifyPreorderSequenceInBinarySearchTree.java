a4be8624e67ce0a37af16c26d5b27c2a0e063533
yao <myspiritcrazy@gmail.com>
2017-10-14 16:35:21 -0700

    add a solution without stack

--- src/com/leetcode/www/VerifyPreorderSequenceInBinarySearchTree.java
+++ src/com/leetcode/www/VerifyPreorderSequenceInBinarySearchTree.java
@@ -4,7 +4,23 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class VerifyPreorderSequenceInBinarySearchTree { // LC 255
-    public boolean verifyPreorder(int[] preorder) { // beats 77.05%
+    // in place (given array)
+    public boolean verifyPreorder(int[] preorder) { // beats 80.90%
+        int low = Integer.MIN_VALUE, i = -1;
+        for (int p : preorder) {
+            if (p < low) {
+                return false;
+            }
+            while (i >= 0 && p > preorder[i]) {
+                low = preorder[i--];
+            }
+            preorder[++i] = p;
+        }
+        return true;
+    }
+
+    // stack
+    public boolean verifyPreorderV0(int[] preorder) { // beats 77.05%
         int low = Integer.MIN_VALUE;
         Deque<Integer> path = new ArrayDeque<>();
         for (int p : preorder) {
