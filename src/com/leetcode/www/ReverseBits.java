3e64ea6a3938fc9fc94610ad7c44948495169253
yao <myspiritcrazy@gmail.com>
2017-10-14 14:26:39 -0700

    add a clean solution

--- src/com/leetcode/www/ReverseBits.java
+++ src/com/leetcode/www/ReverseBits.java
@@ -4,6 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ReverseBits { // LC 190
+    public int reverseBitsV2(int n) { // beats 27.74%
+        int answer = 0; // initializing answer
+        for (int i = 0; i < 32; i++) { // 32 bit integers
+            answer <<= 1; // shifts answer over 1 to open a space
+            answer |= ((n >> i) & 1); // inserts bits from n
+        }
+        return answer;
+    }
+
     public int reverseBitsV1(int n) { // beats 27.74%
         if (n == 0) {
             return 0;
