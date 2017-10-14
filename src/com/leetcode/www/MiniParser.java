96a5008ba9873dbedde7977b0c089ad6b9db1a4e
yao <myspiritcrazy@gmail.com>
2017-10-14 15:13:31 -0700

    refactor code

--- src/com/leetcode/www/MiniParser.java
+++ src/com/leetcode/www/MiniParser.java
@@ -45,42 +45,42 @@ public class MiniParser { // LC 385
     }
 
     class NestedInteger {
-      // Constructor initializes an empty nested list.
-     public NestedInteger() {
+         // Constructor initializes an empty nested list.
+         public NestedInteger() {
 
-     }
+         }
 
-     // Constructor initializes a single integer.
-     public NestedInteger(int value) {
+         // Constructor initializes a single integer.
+         public NestedInteger(int value) {
 
-     }
+         }
 
-     // @return true if this NestedInteger holds a single integer, rather than a nested list.
-     public boolean isInteger() {
-         return true;
-     }
+         // @return true if this NestedInteger holds a single integer, rather than a nested list.
+         public boolean isInteger() {
+             return true;
+         }
 
-     // @return the single integer that this NestedInteger holds, if it holds a single integer
-     // Return null if this NestedInteger holds a nested list
-     public Integer getInteger() {
-         return 0;
-     }
+         // @return the single integer that this NestedInteger holds, if it holds a single integer
+         // Return null if this NestedInteger holds a nested list
+         public Integer getInteger() {
+             return 0;
+         }
 
-     // Set this NestedInteger to hold a single integer.
-     public void setInteger(int value) {
+         // Set this NestedInteger to hold a single integer.
+         public void setInteger(int value) {
 
-     }
+         }
 
-     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
-     public void add(NestedInteger ni) {
+         // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+         public void add(NestedInteger ni) {
 
-     }
+         }
 
-     // @return the nested list that this NestedInteger holds, if it holds a nested list
-     // Return null if this NestedInteger holds a single integer
-     public List<NestedInteger> getList() {
-         return null;
-     }
+         // @return the nested list that this NestedInteger holds, if it holds a nested list
+         // Return null if this NestedInteger holds a single integer
+         public List<NestedInteger> getList() {
+             return null;
+         }
     }
 }
 
