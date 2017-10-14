ea7582232d22c0c3f4f12b4b365a31a95c53ea60
yao <myspiritcrazy@gmail.com>
2017-10-14 15:01:22 -0700

    add a clean solution

--- src/com/leetcode/www/AddStrings.java
+++ src/com/leetcode/www/AddStrings.java
@@ -12,4 +12,30 @@ public class AddStrings { // LC 415
         }
         return sb.reverse().toString();
     }
+
+    public String addStringsV0(String num1, String num2) { // beats 33.20%
+        if (num1 == null || num2 == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int l1 = num1.length() - 1;
+        int l2 = num2.length() - 1;
+
+        while (l1 >= 0 || l2 >= 0){
+            int n1 = l1 >= 0 ? num1.charAt(l1) - '0' : 0;
+            int n2 = l2 >= 0 ? num2.charAt(l2) - '0' : 0;
+            int sum = n1 + n2 + carry;
+            carry = sum > 9 ? 1 : 0;
+            sb.insert(0, sum % 10);
+            l1--;
+            l2--;
+        }
+
+        if (carry == 1) {
+            sb.insert(0, 1);
+        }
+
+        return sb.toString();
+    }
 }
