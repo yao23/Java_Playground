776271f4c72d50fadffd96d9ebe4514f65f91f95
yao <myspiritcrazy@gmail.com>
2017-10-14 16:59:45 -0700

    add a solution with hashmap

--- src/com/leetcode/www/MajorityElement.java
+++ src/com/leetcode/www/MajorityElement.java
@@ -1,12 +1,32 @@
 package com.leetcode.www;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MajorityElement { // LC 169
     // sorting
     public int majorityElement(int[] nums) { // beats 70.59%
         Arrays.sort(nums);
         return nums[nums.length/2];
+    }
+
+    // Hashtable
+    public int majorityElement2(int[] nums) { // beats 11.57%
+        Map<Integer, Integer> myMap = new HashMap<>();
+        int ret = 0;
+        for (int num: nums) {
+            if (!myMap.containsKey(num)) {
+                myMap.put(num, 1);
+            } else {
+                myMap.put(num, myMap.get(num) + 1);
+            }
+            if (myMap.get(num) > nums.length / 2) {
+                ret = num;
+                break;
+            }
+        }
+        return ret;
     }
 
     public int majorityElementV0(int[] nums) { // beats 70.59%
