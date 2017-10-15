1582778ba4571062b2da730a3e3d9f6ac38eefd1
yao <myspiritcrazy@gmail.com>
2017-10-14 22:53:00 -0700

    add a solution with random function

--- src/com/leetcode/www/EncodeAndDecodeTinyURL.java
+++ src/com/leetcode/www/EncodeAndDecodeTinyURL.java
@@ -2,10 +2,32 @@ package com.leetcode.www;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class EncodeAndDecodeTinyURL { // LC 535
+    /**
+     * https://leetcode.com/articles/encode-and-decode-tinyurl/
+     */
+
+    // random function
+    class Codec {
+        Map<Integer, String> map = new HashMap<>();
+        Random r = new Random();
+        int key = r.nextInt(10000);
+        public String encode(String longUrl) {
+            while (map.containsKey(key)) {
+                key = r.nextInt(10000);
+            }
+            map.put(key, longUrl);
+            return "http://tinyurl.com/" + key;
+        }
+        public String decode(String shortUrl) {
+            return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+        }
+    }
+
     // hashcode
-    class Codec { // beats 29.34%
+    class CodecV1 { // beats 29.34%
         Map<Integer, String> map = new HashMap<>();
         public String encode(String longUrl) {
             map.put(longUrl.hashCode(), longUrl);
