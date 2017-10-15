c5033d3d9b627b975496cee38053412bc3f649d1
yao <myspiritcrazy@gmail.com>
2017-10-15 15:46:51 -0700

    add a clean solution with One-pass Swap

--- src/com/leetcode/www/WiggleSort.java
+++ src/com/leetcode/www/WiggleSort.java
@@ -1,6 +1,24 @@
 package com.leetcode.www;
 
 public class WiggleSort { // LC 280
+    // One-pass Swap
+    public void wiggleSort(int[] nums) { // beats 23.56%
+        for (int i = 1; i < nums.length; i++) {
+            if (i % 2 == 0 && nums[i-1] < nums[i]) {  // at even index, check if it's greater than previous number
+                swap(nums, i-1, i);
+            }
+            if (i % 2 != 0 && nums[i-1] > nums[i]) {  // at odd index, check if it's smaller than previous number
+                swap(nums, i-1, i);
+            }
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     // The basic idea is to make sure that every odd position is greater than its two adjacent even positions.
     // For example, if the current odd position is i, then we need to make sure the nums[i-1] <= nums[i] and
     // nums[i+1] <= nums[i]. If you do that for the entire array, then the result will satisfy Wiggle Sort's requirement.
@@ -18,7 +36,7 @@ public class WiggleSort { // LC 280
     // nums[i] >= nums[i+1]. In addition, whether we swap (a, b) or not, it will still maintain the correct order for
     // the previous group (e.g. if we have d, a, b, c where d >= a. If a >= b then we swap(a, b), but still d >= b).
     // Thus, if we do this operation to each [even, odd, even] index group of the array, then we should have the final solution.
-    public void wiggleSort(int[] nums) { // beats 59.43%
+    public void wiggleSortV1(int[] nums) { // beats 59.43%
         for (int i = 1; i < nums.length; i++) {
             if (((i & 1) == 0) == (nums[i - 1] < nums[i])) {
                 xwap(nums, i);
