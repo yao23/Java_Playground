c9e2dfbfee8b17abfbce2424bf4cb3bd583dfb4e
yao <myspiritcrazy@gmail.com>
2017-10-14 22:47:27 -0700

    Encode and Decode TinyURL

---
+++ src/com/leetcode/www/EncodeAndDecodeTinyURL.java
@@ -0,0 +1,28 @@
+package com.leetcode.www;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EncodeAndDecodeTinyURL { // LC 535
+    // counter
+    class Codec { // beats 29.34%
+        Map<Integer, String> map = new HashMap<>();
+        int i = 0;
+        // Encodes a URL to a shortened URL.
+        public String encode(String longUrl) {
+            map.put(i, longUrl);
+            return "http://tinyurl.com/" + i++;
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public String decode(String shortUrl) {
+            return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+        }
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
+
+// "https://leetcode.com/problems/design-tinyurl" => "https://leetcode.com/problems/design-tinyurl"
