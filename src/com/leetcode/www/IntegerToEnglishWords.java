77d1f88964efe9fef6e4217f700dc200ac6a9670
yao <myspiritcrazy@gmail.com>
2017-10-14 17:39:20 -0700

    add a clean solution

--- src/com/leetcode/www/IntegerToEnglishWords.java
+++ src/com/leetcode/www/IntegerToEnglishWords.java
@@ -36,6 +36,60 @@ public class IntegerToEnglishWords { // LC 273
             return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
         }
     }
+
+    private String[] oneToNineteen = new String[] {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight",
+            "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
+            "Nineteen"};
+
+    private String[] twentyToNinety = new String[] {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy",
+            "Eighty", "Ninety"};
+
+    private String[] hunderedToBillion = new String[] {"Hundred", "Thousand", "Million", "Billion"};
+
+    public String numberToWordsV0(int num) {
+        if (num == 0){
+            return "Zero";
+        }
+
+        StringBuilder builder = new StringBuilder();
+        convert(num, builder);
+        return builder.toString().trim();
+    }
+
+    private void convert(int n, StringBuilder builder){
+        if (n == 0) {
+            return;
+        }
+
+        if (n < 20) {
+            builder.append(oneToNineteen[n]);
+            builder.append(" ");
+        } else if (n < 100) {
+            builder.append(twentyToNinety[n / 10]);
+            builder.append(" ");
+            convert(n%10, builder);
+        } else if (n < 1000) {
+            convert(n / 100, builder);
+            builder.append(hunderedToBillion[0]);
+            builder.append(" ");
+            convert(n % 100, builder);
+        } else if (n < 1000000) {
+            convert(n / 1000, builder);
+            builder.append(hunderedToBillion[1]);
+            builder.append(" ");
+            convert(n % 1000, builder);
+        } else if(n < 1000000000) {
+            convert(n / 1000000, builder);
+            builder.append(hunderedToBillion[2]);
+            builder.append(" ");
+            convert(n % 1000000, builder);
+        } else {
+            convert(n / 1000000000, builder);
+            builder.append(hunderedToBillion[3]);
+            builder.append(" ");
+            convert(n%1000000000, builder);
+        }
+    }
 }
 
 // 123 -> "One Hundred Twenty Three"
