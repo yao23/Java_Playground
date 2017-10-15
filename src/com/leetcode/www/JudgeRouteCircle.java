47cd6102e0b0e5402d7b136e2dfa8066fb51608f
yao <myspiritcrazy@gmail.com>
2017-10-15 15:50:21 -0700

    Judge Route Circle

---
+++ src/com/leetcode/www/JudgeRouteCircle.java
@@ -0,0 +1,20 @@
+package com.leetcode.www;
+
+public class JudgeRouteCircle { // LC 657
+    public boolean judgeCircle(String moves) { // beats 89.42%
+        int x = 0;
+        int y = 0;
+        for (char ch : moves.toCharArray()) {
+            if (ch == 'U') {
+                y++;
+            } else if (ch == 'D') {
+                y--;
+            } else if (ch == 'R') {
+                x++;
+            } else if (ch == 'L') {
+                x--;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
