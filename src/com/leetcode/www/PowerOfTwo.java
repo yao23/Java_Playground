89051b259aa8a065dd8e59737d3b60a1f4b7d521
yao <myspiritcrazy@gmail.com>
2017-10-15 15:11:58 -0700

    add a clean solution

--- src/com/leetcode/www/PowerOfTwo.java
+++ src/com/leetcode/www/PowerOfTwo.java
@@ -1,13 +1,17 @@
 package com.leetcode.www;
 
 public class PowerOfTwo { // LC 231
+    public boolean isPowerOfTwo(int n) { // beats 11.81%
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
     /**
      *  a power of two in binary form has and only has one "1".
      *
      * @param n
      * @return
      */
-    public boolean isPowerOfTwo(int n) { // beats 11.81%
-        return n>0 && Integer.bitCount(n) == 1;
+    public boolean isPowerOfTwoV0(int n) { // beats 11.81%
+        return n > 0 && Integer.bitCount(n) == 1;
     }
 }
