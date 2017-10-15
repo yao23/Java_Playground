f22404534da9e5cc73b16509aa02e661d8a18c41
yao <myspiritcrazy@gmail.com>
2017-10-15 09:44:12 -0700

    add a clean solution

--- src/com/leetcode/www/MergeTwoBinaryTrees.java
+++ src/com/leetcode/www/MergeTwoBinaryTrees.java
@@ -1,6 +1,34 @@
 package com.leetcode.www;
 
 public class MergeTwoBinaryTrees { // LC 617
+    public TreeNode mergeTreesV1(TreeNode t1, TreeNode t2) { // beats 91.81%
+        TreeNode p1 = new TreeNode(0);
+        TreeNode p2 = new TreeNode(0);
+        p1.left = t1;
+        p2.left = t2;
+        dfs(t1, t2, p1, p2);
+
+        return p1.left;
+    }
+
+    public void dfs(TreeNode t1, TreeNode t2, TreeNode p1, TreeNode p2) {
+        if (t2 == null) {
+            return;
+        } else {
+            if (t1 == null) {
+                if (p2.left == t2) {
+                    p1.left = t2;
+                } else {
+                    p1.right = t2;
+                }
+            } else {
+                t1.val += t2.val;
+                dfs(t1.left, t2.left, t1, t2);
+                dfs(t1.right, t2.right, t1, t2);
+            }
+        }
+    }
+
     // tree traversal
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) { // beats 53.18%
         if (t1 == null && t2 == null) {
