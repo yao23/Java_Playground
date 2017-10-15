16488c096f853ed4c837f8a769f2232380597c9a
yao <myspiritcrazy@gmail.com>
2017-10-15 06:26:36 -0700

    Binary Tree Right Side View

---
+++ src/com/leetcode/www/BinaryTreeRightSideView.java
@@ -0,0 +1,25 @@
+package com.leetcode.www;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeRightSideView { // LC 199
+    public List<Integer> rightSideView(TreeNode root) { // beats 22.08%
+        List<Integer> result = new ArrayList<>();
+        rightView(root, result, 0);
+        return result;
+    }
+
+    private void rightView(TreeNode curr, List<Integer> result, int currDepth) {
+        if (curr == null) {
+            return;
+        }
+        if (currDepth == result.size()) {
+            result.add(curr.val);
+        }
+
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+
+    }
+}
