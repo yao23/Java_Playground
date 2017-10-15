d5792d1f715978bf1a97b62e5e0895110d9a99bf
yao <myspiritcrazy@gmail.com>
2017-10-15 06:31:32 -0700

    add a BFS solution

--- src/com/leetcode/www/BinaryTreeRightSideView.java
+++ src/com/leetcode/www/BinaryTreeRightSideView.java
@@ -1,9 +1,39 @@
 package com.leetcode.www;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTreeRightSideView { // LC 199
+    // BFS, reverse level traversal
+    public List<Integer> rightSideView(TreeNode root) { // beats 22.08%
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) {
+            return result;
+        }
+
+        queue.offer(root);
+        while (queue.size() != 0) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                if (i == 0) {
+                    result.add(cur.val);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+            }
+
+        }
+        return result;
+    }
+
     /**
      * 1. Each depth of the tree only select one node.
      * 2. View depth is current size of result list.
@@ -11,7 +41,7 @@ public class BinaryTreeRightSideView { // LC 199
      * @param root
      * @return
      */
-    public List<Integer> rightSideView(TreeNode root) { // beats 22.08%
+    public List<Integer> rightSideViewV0(TreeNode root) { // beats 22.08%
         List<Integer> result = new ArrayList<>();
         rightView(root, result, 0);
         return result;
