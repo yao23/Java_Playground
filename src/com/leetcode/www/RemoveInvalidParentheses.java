d1ff862d3321cfc781ec64f6437e519b9144aa28
yao <myspiritcrazy@gmail.com>
2017-10-14 18:01:10 -0700

    add a BFS solution

--- src/com/leetcode/www/RemoveInvalidParentheses.java
+++ src/com/leetcode/www/RemoveInvalidParentheses.java
@@ -1,7 +1,6 @@
 package com.leetcode.www;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class RemoveInvalidParentheses { // LC 301
     /**
@@ -25,7 +24,7 @@ public class RemoveInvalidParentheses { // LC 301
      *  @param s
      * @return
      */
-
+    // DFS
     public List<String> removeInvalidParentheses(String s) { // beats 92.19%
         List<String> ans = new ArrayList<>();
         remove(s, ans, 0, 0, new char[]{'(', ')'});
@@ -56,6 +55,68 @@ public class RemoveInvalidParentheses { // LC 301
         } else { // finished right to left
             ans.add(reversed);
         }
+    }
+
+    // BFS
+    public List<String> removeInvalidParenthesesV0(String s) { // beats 9.85%
+        List<String> res = new ArrayList<>();
+
+        // sanity check
+        if (s == null) {
+            return res;
+        }
+
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        // initialize
+        queue.add(s);
+        visited.add(s);
+
+        boolean found = false;
+
+        while (!queue.isEmpty()) {
+            s = queue.poll();
+            if (isValid(s)) { // found an answer, add to the result
+                res.add(s);
+                found = true;
+            }
+            if (found) {
+                continue;
+            }
+            // generate all possible states
+            for (int i = 0; i < s.length(); i++) {
+                // we only try to remove left or right paren
+                if (s.charAt(i) != '(' && s.charAt(i) != ')') {
+                    continue;
+                }
+                String t = s.substring(0, i) + s.substring(i + 1);
+                if (!visited.contains(t)) {
+                    // for each state, if it's not visited, add it to the queue
+                    queue.add(t);
+                    visited.add(t);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    // helper function checks if string s contains valid parantheses
+    private boolean isValid(String s) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                count++;
+            }
+            if (c == ')' && count-- == 0) {
+                return false;
+            }
+        }
+
+        return (count == 0);
     }
 }
 
