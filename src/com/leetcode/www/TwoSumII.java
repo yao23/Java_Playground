41470710a680f3ab65ee58ee6495f336049b81ea
yao <myspiritcrazy@gmail.com>
2017-10-15 06:21:08 -0700

    add a solution with binary search

--- src/com/leetcode/www/TwoSumII.java
+++ src/com/leetcode/www/TwoSumII.java
@@ -1,7 +1,55 @@
 package com.leetcode.www;
 
 public class TwoSumII { // Input array is sorted
-    public int[] twoSum(int[] numbers, int target) { // beats 39.70%
+    // binary search
+    public int[] twoSum(int[] numbers, int target) {
+        if (numbers == null || numbers.length == 0) {
+            return new int[2];
+        }
+        int start = 0;
+        int end = numbers.length - 1;
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target) {
+                return new int[]{start + 1, end + 1};
+            } else if (numbers[start] + numbers[end] > target) {
+                // move end forward to the last value that numbers[end] <= target - numbers[start]
+                end = largestSmallerOrLastEqual(numbers, start, end, target - numbers[start]);
+            } else {
+                // move start backward to the first value that numbers[start] >= target - numbers[end]
+                start = smallestLargerOrFirstEqual(numbers, start, end, target - numbers[end]);
+            }
+        }
+        return new int[2];
+    }
+    private int largestSmallerOrLastEqual(int[] numbers, int start, int end, int target) {
+        int left = start;
+        int right = end;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (numbers[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+    private int smallestLargerOrFirstEqual(int[] numbers, int start, int end, int target) {
+        int left = start;
+        int right = end;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (numbers[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    // two pointers
+    public int[] twoSumV0(int[] numbers, int target) { // beats 39.70%
         int[] res = new int[]{-1, -1};
         int l = 0, r = numbers.length - 1;
         while (l < r) {
