16f916922ecbdc27830e861a8dfb37f274be29e1
yao <myspiritcrazy@gmail.com>
2017-10-14 17:28:37 -0700

    add a solution without split

--- src/com/leetcode/www/CompareVersionNumbers.java
+++ src/com/leetcode/www/CompareVersionNumbers.java
@@ -1,7 +1,32 @@
 package com.leetcode.www;
 
 public class CompareVersionNumbers { // LC 165
-    public int compareVersion(String version1, String version2) { // beats 11.74%
+    public int compareVersion(String version1, String version2) { // beats 96.33%
+        int temp1 = 0, temp2 = 0;
+        int len1 = version1.length(), len2 = version2.length();
+        int i = 0, j = 0;
+        while (i < len1 || j < len2) {
+            temp1 = 0;
+            temp2 = 0;
+            while (i < len1 && version1.charAt(i) != '.') {
+                temp1 = temp1 * 10 + version1.charAt(i++) - '0';
+            }
+            while (j < len2 && version2.charAt(j) != '.') {
+                temp2 = temp2 * 10 + version2.charAt(j++) - '0';
+            }
+            if (temp1 > temp2) {
+                return 1;
+            } else if(temp1 < temp2) {
+                return -1;
+            } else {
+                i++;
+                j++;
+            }
+        }
+        return 0;
+    }
+
+    public int compareVersionV0(String version1, String version2) { // beats 11.74%
         String[] levels1 = version1.split("\\.");
         String[] levels2 = version2.split("\\.");
 
