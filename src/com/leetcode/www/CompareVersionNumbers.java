54014231332ba3645ec2895cc5ac988e1bf2377e
yao <myspiritcrazy@gmail.com>
2017-10-14 17:25:19 -0700

    Compare Version Numbers

---
+++ src/com/leetcode/www/CompareVersionNumbers.java
@@ -0,0 +1,22 @@
+package com.leetcode.www;
+
+public class CompareVersionNumbers { // LC 165
+    public int compareVersion(String version1, String version2) { // beats 11.74%
+        String[] levels1 = version1.split("\\.");
+        String[] levels2 = version2.split("\\.");
+
+        int length = Math.max(levels1.length, levels2.length);
+        for (int i = 0; i < length; i++) {
+            Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+            Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+            int compare = v1.compareTo(v2);
+            if (compare != 0) {
+                return compare;
+            }
+        }
+
+        return 0;
+    }
+}
+
+// "1", "0" => 1
