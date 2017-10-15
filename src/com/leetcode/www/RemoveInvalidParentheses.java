92df34ef8c048a88b41c6d18ca38c5dbead71ce5
yao <myspiritcrazy@gmail.com>
2017-10-14 17:57:39 -0700

    documentation

--- src/com/leetcode/www/RemoveInvalidParentheses.java
+++ src/com/leetcode/www/RemoveInvalidParentheses.java
@@ -4,6 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RemoveInvalidParentheses { // LC 301
+    /**
+     * We all know how to check a string of parentheses is valid using a stack. Or even simpler use a counter.
+     * The counter will increase when it is ‘(‘ and decrease when it is ‘)’. Whenever the counter is negative, we have
+     * more ‘)’ than ‘(‘ in the prefix.
+     *
+     * To make the prefix valid, we need to remove a ‘)’. The problem is: which one? The answer is any one in the prefix.
+     * However, if we remove any one, we will generate duplicate results, for example: s = ()), we can remove s[1] or s[2]
+     * but the result is the same (). Thus, we restrict ourself to remove the first ) in a series of consecutive )s.
+     *
+     * After the removal, the prefix is then valid. We then call the function recursively to solve the rest of the string.
+     * However, we need to keep another information: the last removal position. If we do not have this position, we will
+     * generate duplicate by removing two ‘)’ in two steps only with a different order.
+     * For this, we keep tracking the last removal position and only remove ‘)’ after that.
+     *
+     * Now one may ask. What about ‘(‘? What if s = ‘(()(()’ in which we need remove ‘(‘?
+     * The answer is: do the same from right to left.
+     * However a cleverer idea is: reverse the string and reuse the code!
+     *
+     *  @param s
+     * @return
+     */
+
     public List<String> removeInvalidParentheses(String s) { // beats 92.19%
         List<String> ans = new ArrayList<>();
         remove(s, ans, 0, 0, new char[]{'(', ')'});
