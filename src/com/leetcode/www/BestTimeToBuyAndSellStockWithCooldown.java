3eb80cb7496ec93d9768ae2c96fa6bccaf8e7752
yao <myspiritcrazy@gmail.com>
2017-10-15 16:02:45 -0700

    add a clean solution

--- src/com/leetcode/www/BestTimeToBuyAndSellStockWithCooldown.java
+++ src/com/leetcode/www/BestTimeToBuyAndSellStockWithCooldown.java
@@ -1,8 +1,33 @@
 package com.leetcode.www;
 
 public class BestTimeToBuyAndSellStockWithCooldown { // LC 309
+    /**
+     * https://discuss.leetcode.com/topic/31015/very-easy-to-understand-one-pass-o-n-solution-with-no-extra-space/2
+     *
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) { // beats 93.05%
+        int L = prices.length;
+        if (L < 2) {
+            return 0;
+        }
+
+        int has1_doNothing = -prices[0];
+        int has1_Sell = 0;
+        int has0_doNothing = 0;
+        int has0_Buy = -prices[0];
+        for (int i = 1; i < L; i++) {
+            has1_doNothing = has1_doNothing > has0_Buy ? has1_doNothing : has0_Buy;
+            has0_Buy = -prices[i] + has0_doNothing;
+            has0_doNothing = has0_doNothing > has1_Sell ? has0_doNothing : has1_Sell;
+            has1_Sell = prices[i] + has1_doNothing;
+        }
+        return has1_Sell > has0_doNothing ? has1_Sell : has0_doNothing;
+    }
+
     // DP
-    public int maxProfit(int[] prices) { // beats 43.43%
+    public int maxProfitV1(int[] prices) { // beats 43.43%
         if (prices == null || prices.length < 2)  {
             return 0;
         }
