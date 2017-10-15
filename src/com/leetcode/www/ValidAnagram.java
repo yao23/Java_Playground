60fcdc0286d06511bf243b7b43b1a5689db82f88
yao <myspiritcrazy@gmail.com>
2017-10-15 07:22:21 -0700

    add a clean solution

--- src/com/leetcode/www/ValidAnagram.java
+++ src/com/leetcode/www/ValidAnagram.java
@@ -1,7 +1,29 @@
 package com.leetcode.www;
 
 public class ValidAnagram { // LC 242
-    public boolean isAnagram(String s, String t) { // beats 68.93%
+    public boolean isAnagram(String s, String t) { // beats 87.54%
+        int[] charsMap = new int['z' - 'a' + 1];
+
+        for (char c: s.toCharArray()) {
+            int pos = c - 'a';
+            charsMap[pos]++;
+        }
+
+        for(char c: t.toCharArray()) {
+            int pos = c - 'a';
+            charsMap[pos]--;
+        }
+
+        for (int count: charsMap) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isAnagramV0(String s, String t) { // beats 68.93%
         int[] alphabet = new int[26];
         for (int i = 0; i < s.length(); i++) {
             alphabet[s.charAt(i) - 'a']++;
