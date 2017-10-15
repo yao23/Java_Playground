2665d3a951f41969c5839d8d6d1d20f4efe5450b
yao <myspiritcrazy@gmail.com>
2017-10-15 15:17:31 -0700

    add a clean iterative solution

--- src/com/leetcode/www/PowerOfTwo.java
+++ src/com/leetcode/www/PowerOfTwo.java
@@ -1,15 +1,30 @@
 package com.leetcode.www;
 
 public class PowerOfTwo { // LC 231
+    // iteration
+    public boolean isPowerOfTwo(int n) { // beats 11.81, O(logn)
+        if (n == 0) {
+            return false;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return (n == 1);
+    }
 
+    /**
+     * https://discuss.leetcode.com/topic/47195/4-different-ways-to-solve-iterative-recursive-bit-operation-math/2
+     * @param n
+     * @return
+     */
     // the range of an integer = -2147483648 (-2^31) ~ 2147483647 (2^31-1),
     // the max possible power of two = 2^30 = 1073741824
-    public boolean isPowerOfTwo(int n) {
+    public boolean isPowerOfTwoV2(int n) { // beats 11.81%, O(1)
         return n > 0 && (1073741824 % n == 0);
     }
 
     // power of 2 has only one bit 1
-    public boolean isPowerOfTwoV1(int n) { // beats 11.81%
+    public boolean isPowerOfTwoV1(int n) { // beats 11.81%, O(1)
         return n > 0 && (n & (n - 1)) == 0;
     }
 
@@ -19,7 +34,7 @@ public class PowerOfTwo { // LC 231
      * @param n
      * @return
      */
-    public boolean isPowerOfTwoV0(int n) { // beats 11.81%
+    public boolean isPowerOfTwoV0(int n) { // beats 11.81%, O(1)
         return n > 0 && Integer.bitCount(n) == 1;
     }
 }
