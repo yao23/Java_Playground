9ef463953ed83567c6ec3b878467878343623c30
yao <myspiritcrazy@gmail.com>
2017-10-15 15:14:15 -0700

    add a clean solution with Math

--- src/com/leetcode/www/PowerOfTwo.java
+++ src/com/leetcode/www/PowerOfTwo.java
@@ -1,7 +1,15 @@
 package com.leetcode.www;
 
 public class PowerOfTwo { // LC 231
-    public boolean isPowerOfTwo(int n) { // beats 11.81%
+
+    // the range of an integer = -2147483648 (-2^31) ~ 2147483647 (2^31-1),
+    // the max possible power of two = 2^30 = 1073741824
+    public boolean isPowerOfTwo(int n) {
+        return n > 0 && (1073741824 % n == 0);
+    }
+
+    // power of 2 has only one bit 1
+    public boolean isPowerOfTwoV1(int n) { // beats 11.81%
         return n > 0 && (n & (n - 1)) == 0;
     }
 
