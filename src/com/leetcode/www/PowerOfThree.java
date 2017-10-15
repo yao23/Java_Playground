45acf612952b96194f7d801b4c23fa22f303e4d0
yao <myspiritcrazy@gmail.com>
2017-10-15 15:24:50 -0700

    add a clean solution

--- src/com/leetcode/www/PowerOfThree.java
+++ src/com/leetcode/www/PowerOfThree.java
@@ -1,7 +1,14 @@
 package com.leetcode.www;
 
 public class PowerOfThree { // LC 326
-    public boolean isPowerOfThree(int n) { // beats 20.52%
+    // Find the maximum integer that is a power of 3 and check if it is a multiple of the given input.
+    public boolean isPowerOfThree(int n) { // beats 13.45%
+        int maxPowerOfThree = (int)Math.pow(3, (int)(Math.log(0x7fffffff) / Math.log(3)));
+        return n > 0 && maxPowerOfThree % n == 0;
+    }
+
+    // Integer Limitations
+    public boolean isPowerOfThreeV0(int n) { // beats 20.52%
         // 1162261467 is 3^19,  3^20 is bigger than int
         return (n > 0 &&  1162261467 % n == 0);
     }
