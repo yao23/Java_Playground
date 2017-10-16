bca6838cfce2e2d66d2b0dec3e891df1965708bf
yao <myspiritcrazy@gmail.com>
2017-10-15 21:07:31 -0700

    Reverse Vowels of a String

---
+++ src/com/leetcode/www/ReverseVowelsOfAString.java
@@ -0,0 +1,32 @@
+package com.leetcode.www;
+
+public class ReverseVowelsOfAString {
+    public String reverseVowels(String s) { // beats 12.77%
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        String vowels = "aeiouAEIOU";
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = s.length() - 1;
+        while (start < end) {
+            while (start < end && !vowels.contains(chars[start] + "")) {
+                start++;
+            }
+            while (start < end && !vowels.contains(chars[end] + "")){
+                end--;
+            }
+
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+
+            start++;
+            end--;
+        }
+        return new String(chars);
+    }
+}
+
+// Given s = "hello", return "holle".
+// Given s = "leetcode", return "leotcede".
