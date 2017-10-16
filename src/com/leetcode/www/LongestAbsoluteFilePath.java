02ea2c180342a15a23d1e39435be3c384742c8e8
yao <myspiritcrazy@gmail.com>
2017-10-15 21:15:21 -0700

    Longest Absolute File Path

---
+++ src/com/leetcode/www/LongestAbsoluteFilePath.java
@@ -0,0 +1,25 @@
+package com.leetcode.www;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LongestAbsoluteFilePath { // LC 388
+    public int lengthLongestPath(String input) { // beats 26.45%
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0); // "dummy" length
+        int maxLen = 0;
+        for (String s:input.split("\n")) {
+            int lev = s.lastIndexOf("\t") + 1; // number of "\t"
+            while (lev + 1 < stack.size()) {
+                stack.pop(); // find parent
+            }
+            int len = stack.peek() + s.length() - lev + 1; // remove "/t", add"/"
+            stack.push(len);
+            // check if it is file
+            if (s.contains(".")) {
+                maxLen = Math.max(maxLen, len - 1);
+            }
+        }
+        return maxLen;
+    }
+}
