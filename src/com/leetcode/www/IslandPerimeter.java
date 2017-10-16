41b2b28d3d2b7efc7f7a3615e7ea3fdc55981823
yao <myspiritcrazy@gmail.com>
2017-10-15 23:11:11 -0700

    add a clean solution

--- src/com/leetcode/www/IslandPerimeter.java
+++ src/com/leetcode/www/IslandPerimeter.java
@@ -21,6 +21,54 @@ public class IslandPerimeter { // LC 463
         return islands * 4 - neighbours * 2;
     }
 
+    // O(mn) time and O(1) space
+    private int rn ;
+    private int cn ;
+    public int islandPerimeterV1(int[][] grid) { // beats 73.68%
+        if (grid == null || grid.length < 1 || grid[0].length < 1) {
+            return 0;
+        }
+        rn = grid.length - 1;
+        cn = grid[0].length - 1;
+        int perimeter = 0;
+        for (int i = 0; i <= rn; i++) {
+            for (int j = 0; j <= cn; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += findCount(grid, i, j);
+                }
+            }
+        }
+        return perimeter;
+    }
+    private int findCount(int[][] grid, int i, int j) {
+        int sum = 0;
+        //top
+        if (i != 0) {
+            sum += (grid[i - 1][j] == 0) ? 1 : 0;
+        } else {
+            sum += 1;
+        }
+        //bottom
+        if (i != rn) {
+            sum += (grid[i + 1][j] == 0) ? 1 : 0;
+        } else {
+            sum += 1;
+        }
+        //left
+        if (j != 0) {
+            sum += (grid[i][j - 1] == 0) ? 1 : 0;
+        } else {
+            sum += 1;
+        }
+        //right
+        if (j != cn) {
+            sum += (grid[i][j + 1] == 0) ? 1 : 0;
+        } else{
+            sum += 1;
+        }
+        return sum;
+    }
+
     // add 4 for each land and remove 2 for each internal edge
     public int islandPerimeterV0(int[][] grid) { // beats 22.07%
         if (grid == null || grid.length == 0 || grid[0].length == 0) {
