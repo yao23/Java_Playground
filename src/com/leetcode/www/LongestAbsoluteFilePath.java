b202e3db77d7a42ac66c99a5d29e0f58d1138e2c
yao <myspiritcrazy@gmail.com>
2017-10-15 21:17:39 -0700

    add a clean solution with array

--- src/com/leetcode/www/LongestAbsoluteFilePath.java
+++ src/com/leetcode/www/LongestAbsoluteFilePath.java
@@ -4,7 +4,20 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class LongestAbsoluteFilePath { // LC 388
-    public int lengthLongestPath(String input) { // beats 26.45%
+    public int lengthLongestPath(String input) { // beats 85.42%
+        String[] paths = input.split("\n");
+        int[] stack = new int[paths.length + 1];
+        int maxLen = 0;
+        for (String s : paths) {
+            int lev = s.lastIndexOf("\t") + 1, curLen = stack[lev + 1] = stack[lev] + s.length() - lev + 1;
+            if (s.contains(".")) {
+                maxLen = Math.max(maxLen, curLen - 1);
+            }
+        }
+        return maxLen;
+    }
+
+    public int lengthLongestPathV0(String input) { // beats 26.45%
         Deque<Integer> stack = new ArrayDeque<>();
         stack.push(0); // "dummy" length
         int maxLen = 0;
