71c9c39d29f2008731fdf5064c34551d938c6199
yao <myspiritcrazy@gmail.com>
2017-10-15 20:59:49 -0700

    add a clean solution

--- src/com/leetcode/www/GameOfLife.java
+++ src/com/leetcode/www/GameOfLife.java
@@ -1,7 +1,46 @@
 package com.leetcode.www;
 
 public class GameOfLife { // LC 289
-    public void gameOfLife(int[][] board) { // beats 11.75%
+    // use the 1st bit to represent next generation
+    // use the 2nd bit to present current generation
+    public void gameOfLife(int[][] board) { // beats 83.10%
+        int rows = board.length;
+        int cols = board[0].length;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                int neighbors = getNeighbour(board, i, j);
+                if (board[i][j] == 1) {
+                    if (neighbors == 2 || neighbors == 3) {
+                        board[i][j] = 3;
+                    }
+                } else {
+                    if (neighbors == 3) {
+                        board[i][j] = 2;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                board[i][j] >>= 1;
+            }
+        }
+    }
+
+    private int getNeighbour(int[][] board, int row, int col){
+        int cnt = 0;
+        for (int i = row - 1; i <= row + 1; ++i) {
+            for (int j = col - 1; j <= col + 1; ++j) {
+                if (i >= 0 && i < board.length && j >= 0 && j < board[0].length) {
+                    cnt += board[i][j] & 1;
+                }
+            }
+        }
+        cnt -= board[row][col] & 1;
+        return cnt;
+    }
+
+    public void gameOfLifeV0(int[][] board) { // beats 11.75%
         if (board == null || board.length == 0) {
             return;
         }
