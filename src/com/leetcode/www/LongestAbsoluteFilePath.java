5e957046788456787cafc068e0aecff5c3d42e94
yao <myspiritcrazy@gmail.com>
2017-10-15 21:19:18 -0700

    add a clean solution with HashMap

--- src/com/leetcode/www/LongestAbsoluteFilePath.java
+++ src/com/leetcode/www/LongestAbsoluteFilePath.java
@@ -2,6 +2,8 @@ package com.leetcode.www;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestAbsoluteFilePath { // LC 388
     public int lengthLongestPath(String input) { // beats 85.42%
@@ -15,6 +17,23 @@ public class LongestAbsoluteFilePath { // LC 388
             }
         }
         return maxLen;
+    }
+
+    public int lengthLongestPathV1(String input) { // beats 42.41%
+        String[] strs = input.split("\n");
+        int max = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(-1, 0);
+        for (int i = 0; i < strs.length; i++){
+            int level =  strs[i].lastIndexOf('\t')  + 1;
+            int length = map.get(level - 1) + strs[i].length() - level + (level > 0 ? 1 : 0);
+            if (strs[i].indexOf('.') == -1) {
+                map.put(level, length);
+            } else {
+                max = Math.max(length, max);
+            }
+        }
+        return max;
     }
 
     public int lengthLongestPathV0(String input) { // beats 26.45%
