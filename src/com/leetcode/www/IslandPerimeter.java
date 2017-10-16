4147c5c45991ea7651a3818d5e1462ea01b5a107
yao <myspiritcrazy@gmail.com>
2017-10-15 22:59:34 -0700

    add a clean solution

--- src/com/leetcode/www/IslandPerimeter.java
+++ src/com/leetcode/www/IslandPerimeter.java
@@ -20,6 +20,28 @@ public class IslandPerimeter { // LC 463
 
         return islands * 4 - neighbours * 2;
     }
+
+    // add 4 for each land and remove 2 for each internal edge
+    public int islandPerimeterV0(int[][] grid) { // beats 22.07%
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    result += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        result -= 2;
+                    }
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        result -= 2;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
 
 // [[0,1,0,0],
