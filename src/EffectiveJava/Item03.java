910655ee65a51a0df1d825905ebefa24a7d3c098
Yao <myspiritcrazy@gmail.com>
2018-07-12 08:35:07 -0700

    Item 3

--- src/EffectiveJava/Item03.java
+++ src/EffectiveJava/Item03.java
@@ -2,6 +2,9 @@
 public class Elvis {
     public static final Elvis INSTANCE = new Elvis();
     private Elvis() {}
+    public static Elvis getInstance() {
+        return INSTANCE;
+    }
 
     public void leaveTheBuilding() {}
 }
