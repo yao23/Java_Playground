5bf68d329c72272d83cb10da1e6c6a44d2e09650
Yao <myspiritcrazy@gmail.com>
2018-07-18 23:22:36 -0700

    Item 3

--- src/EffectiveJava/Item03.java
+++ src/EffectiveJava/Item03.java
@@ -8,3 +8,10 @@ public class Elvis {
 
     public void leaveTheBuilding() {}
 }
+
+// readResolve method to preserve singleton property
+private Object readResolve() {
+    // Return the one true Elvis and let the garbage collector
+    // take care of the Elvis impersonator
+    return INSTANCE;
+}
