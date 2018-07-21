d98c5f7704d94076068099cea36d0612b88ec8e4
Yao <myspiritcrazy@gmail.com>
2018-07-20 22:32:36 -0700

    Item 4

---
+++ src/EffectiveJava/Item04.java
@@ -0,0 +1,8 @@
+// Noninstantiable utility class
+public class UtilityClass {
+    // Suppress default constructor for noninstantiability
+    private UtilityClass() {
+        throw new AssertionError();
+    }
+    // Remainder omitted
+}
