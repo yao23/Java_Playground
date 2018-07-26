38602d8be0956700cc40194ed34d8ee54c632c02
Yao <myspiritcrazy@gmail.com>
2018-07-25 22:03:17 -0700

    Item 6

--- src/EffectiveJava/Item06.java
+++ src/EffectiveJava/Item06.java
@@ -1,3 +1,9 @@
-static boolean isRomanNumberal(String s) {
-    return s.matches("^(?=.)M*(C[MD]|D?C{0, 3})" + "(X[CL]|L?X{0, 3})(I[XV]|V?I{0, 3})$");
+// Reusing expensive object for improved performance
+public class RomanNumberals {
+    private static final Pattern ROMAN = Pattern.compile(
+        "^(?=.)M*(C[MD]|D?C{0, 3})" + "(X[CL]|L?X{0, 3})(I[XV]|V?I{0, 3})$");
+
+    static boolean isRomanNumberal(String s) {
+        return ROMAN.matcher(s).matches();
+    }
 }
