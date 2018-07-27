85b0be6979160437fcb53b529372c42dcfe51c82
Yao <myspiritcrazy@gmail.com>
2018-07-26 21:56:41 -0700

    Item 6

--- src/EffectiveJava/Item06.java
+++ src/EffectiveJava/Item06.java
@@ -1,9 +1,9 @@
 // Reusing expensive object for improved performance
-public class RomanNumberals {
+public class RomanNumerals {
     private static final Pattern ROMAN = Pattern.compile(
         "^(?=.)M*(C[MD]|D?C{0, 3})" + "(X[CL]|L?X{0, 3})(I[XV]|V?I{0, 3})$");
 
-    static boolean isRomanNumberal(String s) {
+    static boolean isRomanNumeral(String s) {
         return ROMAN.matcher(s).matches();
     }
 }
