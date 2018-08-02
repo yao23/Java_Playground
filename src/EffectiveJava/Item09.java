eae18c9070211d5cdc386a9b4b99a06377a20dcc
Yao <myspiritcrazy@gmail.com>
2018-08-01 22:55:40 -0700

    Item 9

---
+++ src/EffectiveJava/Item09.java
@@ -0,0 +1,7 @@
+// try-with-resources - the best way to close resources!
+static String firstLineOfFile(String path) throws IOException {
+    try (BufferedReader br = new BufferedReader(
+            new FileReader(path))) {
+        return br.readLine();
+    }
+}
