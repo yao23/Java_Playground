a6a507250ca0ab75e3ea2ec7dd775f5ed03f767d
Yao <myspiritcrazy@gmail.com>
2018-08-02 22:32:51 -0700

    Item 9

--- src/EffectiveJava/Item09.java
+++ src/EffectiveJava/Item09.java
@@ -5,3 +5,15 @@ static String firstLineOfFile(String path) throws IOException {
         return br.readLine();
     }
 }
+
+// try-with-resources on mulitple resources - short and sweet
+static void copy(String src, String dst) throws IOException {
+    try (InputStream in = new FileInputStream(src);
+         OutputStream out = new FileOutputStream(dst)) {
+        byte[] buf = new byte[BUFFER_SIZE];
+        int n;
+        while ((n = in.read(buf)) >= 0) {
+            out.write(buf, 0, n);
+        }
+    }
+}
