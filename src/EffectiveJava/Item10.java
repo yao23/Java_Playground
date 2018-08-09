7555eb322f5869022b7bcf5f509543e4f8ecfad6
Yao <myspiritcrazy@gmail.com>
2018-08-08 22:21:35 -0700

    Item 10

--- src/EffectiveJava/Item10.java
+++ src/EffectiveJava/Item10.java
@@ -2,3 +2,12 @@
     return o instanceof CaseInsensitiveString &&
         ((CaseInsensitiveString) o).s.equalsIgnoreCase(s);
 }
+
+// Initialize unitCircle to contain all Points on the unit circle
+private static final Set<Point> unitCircle = Set.of(
+        new Point(1, 0), new Point(0, 1),
+        new Point(-1, 0), new Point(0, -1));
+
+public static boolean onUnitCircle(Point p) {
+    return unitCircle.contains(p);
+}
