9120c6aa48de1094239354fa58317956aff66ef6
Yao <myspiritcrazy@gmail.com>
2018-08-14 20:35:17 -0700

    Item 11

---
+++ src/EffectiveJava/Item11.java
@@ -0,0 +1,7 @@
+// Typical hashCode method
+@Override public int hashCode() {
+    int result = Short.hashCode(areaCode);
+    result = 31 * result + Short.hashCode(prefix);
+    result = 31 * result + Short.hashCode(lineNum);
+    return result;
+}
