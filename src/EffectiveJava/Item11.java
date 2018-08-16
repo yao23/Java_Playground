a687a3f59eb225c0dfbe98bee516e7397cbb7e70
Yao <myspiritcrazy@gmail.com>
2018-08-15 22:45:15 -0700

    Item 11

--- src/EffectiveJava/Item11.java
+++ src/EffectiveJava/Item11.java
@@ -1,7 +1,13 @@
-// Typical hashCode method
+// hashCode method with lazily initialized cached hash code
+private int hashCode; // Automatically initialized to 0
+
 @Override public int hashCode() {
-    int result = Short.hashCode(areaCode);
-    result = 31 * result + Short.hashCode(prefix);
-    result = 31 * result + Short.hashCode(lineNum);
+    int result = hashCode;
+    if (result == 0) {
+        result = Short.hashCode(areaCode);
+        result = 31 * result + Short.hashCode(prefix);
+        result = 31 * result + Short.hashCode(lineNum);
+        hashCode = result;
+    }
     return result;
 }
