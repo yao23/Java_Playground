4c9beb35ecb3dafb06f767c8f6f2456d22d5a09c
Yao <myspiritcrazy@gmail.com>
2019-05-12 17:24:22 -0700

    Reverse LinkedList

--- src/com/leetcode/www/ReverseLinkedList.java
+++ src/com/leetcode/www/ReverseLinkedList.java
@@ -1,6 +1,25 @@
 package com.leetcode.www;
 
 public class ReverseLinkedList { // LC 206
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
+     * Memory Usage: 36 MB, less than 99.25% of Java online submissions for Reverse Linked List.
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) { // recursive
+        if (head == null || head.next == null) {
+            return head;
+        } else {
+            ListNode result = reverseList(head.next);
+            head.next.next = head; // reverse pointing from next to head
+            head.next = null; // remove next pointer from head
+            return result;
+        }
+    }
+
+
     public ListNode reverseListV1(ListNode head) { // beats 27.71% (iterative)
         if (head == null || head.next == null) {
             return head;
@@ -20,7 +39,7 @@ public class ReverseLinkedList { // LC 206
         return head;
     }
 
-    public ListNode reverseList(ListNode head) { // beats 3.85% (recursive)
+    public ListNode reverseListV0(ListNode head) { // beats 3.85% (recursive)
         if (head == null || head.next == null) {
             return head;
         }
