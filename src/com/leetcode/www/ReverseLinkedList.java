260e314080cbd22c4b22b484b109968e019fe310
Yao <myspiritcrazy@gmail.com>
2019-05-12 17:27:14 -0700

    Reverse LinkedList

--- src/com/leetcode/www/ReverseLinkedList.java
+++ src/com/leetcode/www/ReverseLinkedList.java
@@ -5,8 +5,8 @@ public class ReverseLinkedList { // LC 206
      * Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
      * Memory Usage: 36 MB, less than 99.25% of Java online submissions for Reverse Linked List.
      *
-     * @param head
-     * @return
+     * @param head node
+     * @return final reversed head node
      */
     public ListNode reverseList(ListNode head) { // recursive
         if (head == null || head.next == null) {
@@ -19,6 +19,29 @@ public class ReverseLinkedList { // LC 206
         }
     }
 
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Reverse Linked List.
+     * Memory Usage: 36.1 MB, less than 99.25% of Java online submissions for Reverse Linked List.
+     *
+     * @param head node
+     * @return final reversed head node
+     */
+    public ListNode reverseListV2(ListNode head) { // iterative
+        if (head == null || head.next == null) {
+            return head;
+        } else {
+            ListNode nextNode = head.next;
+            head.next = null; // remove next pointing for first node
+            while (nextNode != null) {
+                ListNode nextNextNode = nextNode.next; // handle next next node
+                nextNode.next = head; // reverse pointing
+                head = nextNode;
+                nextNode = nextNextNode;
+            }
+
+            return head;
+        }
+    }
 
     public ListNode reverseListV1(ListNode head) { // beats 27.71% (iterative)
         if (head == null || head.next == null) {
