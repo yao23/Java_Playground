0536401cc658089840a9881f6caa63c8fd0721a3
Yao <myspiritcrazy@gmail.com>
2019-05-14 22:53:55 -0700

    Refactor code for Search For A Range

--- src/com/leetcode/www/SearchForARange.java
+++ src/com/leetcode/www/SearchForARange.java
@@ -2,10 +2,37 @@ package com.leetcode.www;
 
 public class SearchForARange { // LC 34
     public int[] searchRange(int[] nums, int target) {
+        if (nums == null) {
+            return new int[]{-1, -1};
+        }
+        return helper(nums, target, 0, nums.length - 1);
+    }
+
+    private int[] helper(int[] nums, int target, int start, int end) {
+        int leftIndex = -1;
+        int rightIndex = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == nums[mid]) {
+                int[] subLeft =  helper(nums, target, start, mid - 1);
+                int[] subRight =  helper(nums, target, mid + 1, end);
+                leftIndex = subLeft[0] == -1 ? mid : subLeft[0];
+                rightIndex = subRight[1] == -1 ? mid : subRight[1];
+                break;
+            } else if(target>nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return new int[] {leftIndex, rightIndex};
+    }
+
+    public int[] searchRangeV0(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return new int[]{-1, -1};
         }
-        int[] res = {nums.length - 1};
+        int[] res = {nums.length, - 1};
         searchRange(nums, target, 0, nums.length - 1, res);
         return (res[0] <= res[1]) ? res : new int[]{-1, -1};
     }
