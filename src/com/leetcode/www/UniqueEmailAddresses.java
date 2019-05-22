8eaa1571f8baf94602e7ef9cb5a0b8e6c231c703
Yao <myspiritcrazy@gmail.com>
2019-05-22 11:25:47 -0700

    Refactor code for Unique Email Addresses

--- src/com/leetcode/www/UniqueEmailAddresses.java
+++ src/com/leetcode/www/UniqueEmailAddresses.java
@@ -1,8 +1,46 @@
 package com.leetcode.www;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class UniqueEmailAddresses {
+    /**
+     * Runtime: 5 ms, faster than 99.66% of Java online submissions for Unique Email Addresses.
+     * Memory Usage: 36.4 MB, less than 99.86% of Java online submissions for Unique Email Addresses.
+     *
+     * https://leetcode.com/problems/unique-email-addresses/discuss/289396/Java-simple-modular-solution-(beats-100)
+     *
+     * @param emails
+     * @return
+     */
+    public int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmails = new HashSet<>();
+        for (String email: emails) {
+            uniqueEmails.add(processEmail(email));
+        }
+        return uniqueEmails.size();
+    }
+
+
+    private String processEmail(String email){
+        StringBuilder outEmail = new StringBuilder();
+
+        boolean afterPlus = false, endEmail = false;
+        for (char c: email.toCharArray()) {
+            if (c == '+') {
+                afterPlus = true;
+            }
+            if (c == '@') {
+                endEmail = true;
+            }
+            if (endEmail || (!afterPlus && c != '.')) {
+                outEmail.append(c);
+            }
+        }
+
+        return outEmail.toString();
+    }
+
     /**
      * Runtime: 20 ms, faster than 65.22% of Java online submissions for Unique Email Addresses.
      * Memory Usage: 36.9 MB, less than 99.48% of Java online submissions for Unique Email Addresses.
@@ -10,7 +48,7 @@ public class UniqueEmailAddresses {
      * @param emails
      * @return
      */
-    public int numUniqueEmails(String[] emails) {
+    public int numUniqueEmailsV0(String[] emails) {
         String localName = "";
         String domainName = "";
         HashSet<String> uniqueEmails = new HashSet<>();
