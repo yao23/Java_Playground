7a972bcd216d8b3b1bdc8de38efad3f751ab68dd
Yao <myspiritcrazy@gmail.com>
2019-05-22 15:46:53 -0700

    Refactor code for Fibonacci Number

--- src/com/leetcode/www/FibonacciNumber.java
+++ src/com/leetcode/www/FibonacciNumber.java
@@ -22,6 +22,28 @@ public class FibonacciNumber { // LC 509
     }
 
     /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Fibonacci Number.
+     * Memory Usage: 31.8 MB, less than 65.25% of Java online submissions for Fibonacci Number.
+     *
+     * @param N
+     * @return
+     */
+    public int fibV1(int N) {
+        if (N == 0 || N == 1) {
+            return N;
+        }
+        int f1 = 0;
+        int f2 = 1;
+        int sum = 0;
+        for(int i = 2; i <= N; i++){
+            sum = f1 + f2;
+            f1 = f2;
+            f2 = sum;
+        }
+        return sum;
+    }
+
+    /**
      * Runtime: 9 ms, faster than 30.99% of Java online submissions for Fibonacci Number.
      * Memory Usage: 32 MB, less than 60.05% of Java online submissions for Fibonacci Number.
      *
