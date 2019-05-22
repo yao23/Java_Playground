f12017673a9c84e86d74a8003eaf12da874ecc8e
Yao <myspiritcrazy@gmail.com>
2019-05-22 16:15:20 -0700

    Refactor code for K Closest Points to Origin

--- src/com/leetcode/www/KClosestPointsToOrigin.java
+++ src/com/leetcode/www/KClosestPointsToOrigin.java
@@ -1,8 +1,23 @@
 package com.leetcode.www;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class KClosestPointsToOrigin { // LC 973
+    /**
+     * Runtime: 55 ms, faster than 37.01% of Java online submissions for K Closest Points to Origin.
+     * Memory Usage: 56.9 MB, less than 93.02% of Java online submissions for K Closest Points to Origin.
+     *
+     * Arrays.sort()
+     *
+     * @param points
+     * @param K
+     * @return
+     */
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, (p1, p2) -> p1[0] * p1[0] + p1[1] * p1[1] - p2[0] * p2[0] - p2[1] * p2[1]);
+        return Arrays.copyOfRange(points, 0, K);
+    }
     /**
      * Runtime: 63 ms, faster than 24.70% of Java online submissions for K Closest Points to Origin.
      * Memory Usage: 63.4 MB, less than 25.38% of Java online submissions for K Closest Points to Origin.
@@ -13,7 +28,7 @@ public class KClosestPointsToOrigin { // LC 973
      * @param K
      * @return
      */
-    public int[][] kClosest(int[][] points, int K) {
+    public int[][] kClosestV0(int[][] points, int K) {
         PriorityQueue<int[]> pq = new PriorityQueue<>(
                 (p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]
         );
