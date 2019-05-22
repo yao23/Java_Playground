8e9b60eca18da54f0404558ec22906f2feb18a20
Yao <myspiritcrazy@gmail.com>
2019-05-22 09:43:02 -0700

    Refactor code for String Compression

--- src/com/leetcode/www/StringCompression.java
+++ src/com/leetcode/www/StringCompression.java
@@ -3,6 +3,32 @@ package com.leetcode.www;
 public class StringCompression {
     /**
      * Runtime: 1 ms, faster than 98.78% of Java online submissions for String Compression.
+     * Memory Usage: 35.7 MB, less than 99.46% of Java online submissions for String Compression.
+     *
+     * @param chars
+     * @return
+     */
+    public int compress(char[] chars) {
+        int indexAns = 0, index = 0;
+        while(index < chars.length){
+            char currentChar = chars[index];
+            int count = 0;
+            while(index < chars.length && chars[index] == currentChar){
+                index++;
+                count++;
+            }
+            chars[indexAns++] = currentChar;
+            if(count != 1) {
+                for (char c : Integer.toString(count).toCharArray()) {
+                    chars[indexAns++] = c;
+                }
+            }
+        }
+        return indexAns;
+    }
+
+    /**
+     * Runtime: 1 ms, faster than 98.78% of Java online submissions for String Compression.
      * Memory Usage: 35.8 MB, less than 99.40% of Java online submissions for String Compression.
      *
      * two-pointer approach (actually three pointers where write pointer always points to the position that is ready to be written)
@@ -11,7 +37,7 @@ public class StringCompression {
      * @param chars
      * @return
      */
-    public int compress(char[] chars) {
+    public int compressV0(char[] chars) {
         int slow = 0, fast = 0;
         int write = 0;
         while (fast < chars.length) {
