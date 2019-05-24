739417a0daad48c5d8f5a9f58c935e6a0d017c8b
Yao <myspiritcrazy@gmail.com>
2019-05-24 10:05:53 -0700

    Refactor code for Design HashMap

--- src/com/leetcode/www/DesignHashMap.java
+++ src/com/leetcode/www/DesignHashMap.java
@@ -93,4 +93,42 @@ public class DesignHashMap {
      * int param_2 = obj.get(key);
      * obj.remove(key);
      */
+
+    class MyHashMapV0 {
+        /**
+         * Runtime: 90 ms, faster than 13.23% of Java online submissions for Design HashMap.
+         * Memory Usage: 57.8 MB, less than 33.75% of Java online submissions for Design HashMap.
+         */
+
+        //Initialize is false
+        boolean[] isExist = new boolean[1000001];
+        //Initialize a int array
+        int[] intArray = new int[1000001];
+
+        /** Initialize your data structure here. */
+        public MyHashMapV0() {
+
+        }
+
+        /** value will always be non-negative. */
+        public void put(int key, int value) {
+            intArray[key] = value;
+            isExist[key] = true;
+        }
+
+        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+        public int get(int key) {
+            if(isExist[key] == true){
+                return intArray[key];
+            }
+            return -1;
+        }
+
+        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+        public void remove(int key) {
+            intArray[key] = -1;
+            isExist[key] = false;
+        }
+    }
+
 }
