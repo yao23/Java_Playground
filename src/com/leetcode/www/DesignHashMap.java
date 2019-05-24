6a26548d2c220d4d7dcda3f3f9723919bcb851f1
Yao <myspiritcrazy@gmail.com>
2019-05-24 10:17:04 -0700

    Refactor code for Design HashMap

--- src/com/leetcode/www/DesignHashMap.java
+++ src/com/leetcode/www/DesignHashMap.java
@@ -94,10 +94,103 @@ public class DesignHashMap {
      * obj.remove(key);
      */
 
+    class MyHashMapV1 {
+
+        /**
+         * Runtime: 64 ms, faster than 87.94% of Java online submissions for Design HashMap.
+         * Memory Usage: 49.6 MB, less than 94.69% of Java online submissions for Design HashMap.
+         *
+         * https://leetcode.com/problems/design-hashmap/discuss/270685/Java-Separate-Chaining-with-rehashing
+         *
+         * Separate Chaining with rehashing
+         */
+        private static final double LOAD_FACTOR = 0.75;
+        private Node[] nodes;
+        private int size; // number of keys
+
+        /** Initialize your data structure here. */
+        public MyHashMapV1() {
+            nodes = new Node[5];
+            size = 0;
+        }
+
+        /** value will always be non-negative. */
+        public void put(int key, int value) {
+            int idx = hash(key);
+            for (Node x = nodes[idx]; x != null; x = x.next) {
+                if (x.key == key) {
+                    x.value = value;
+                    return;
+                }
+            }
+            nodes[idx] = new Node(key, value, nodes[idx]);
+            size++;
+
+            double loadFactor = (double) size / nodes.length;
+            if (loadFactor > LOAD_FACTOR) {
+                rehash();
+            }
+        }
+
+        /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+        public int get(int key) {
+            int idx = hash(key);
+            for (Node x = nodes[idx]; x != null; x = x.next) {
+                if (x.key == key) {
+                    return x.value;
+                }
+            }
+            return -1;
+        }
+
+        /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+        public void remove(int key) {
+            int idx = hash(key);
+            Node pre = new Node(-1, -1, nodes[idx]); // sentinal node before list head
+            for (Node prev = pre; prev.next != null; prev = prev.next) {
+                if (prev.next.key == key) {
+                    prev.next = prev.next.next;
+                    break;
+                }
+            }
+            nodes[idx] = pre.next;
+            size--;
+        }
+
+        private int hash(int key) {
+            return key % nodes.length;
+        }
+
+        private void rehash() {
+            Node[] tmp = nodes;
+            nodes = new Node[tmp.length * 2];
+            size = 0;
+            for (Node head: tmp) {
+                for (Node x = head; x != null; x = x.next) {
+                    put(x.key, x.value);
+                }
+            }
+        }
+
+        class Node {
+            int key;
+            int value;
+            Node next;
+
+            public Node(int key, int value, Node next) {
+                this.key = key;
+                this.value = value;
+                this.next = next;
+            }
+        }
+    }
+
     class MyHashMapV0 {
         /**
          * Runtime: 90 ms, faster than 13.23% of Java online submissions for Design HashMap.
          * Memory Usage: 57.8 MB, less than 33.75% of Java online submissions for Design HashMap.
+         *
+         * https://leetcode.com/problems/design-hashmap/discuss/297503/Simple-Java-solution-with-one-int-and-one-boolean
          */
 
         //Initialize is false
