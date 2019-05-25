9305fa4c3ae7a940cafab39da414d74e0533b656
Yao <myspiritcrazy@gmail.com>
2019-05-25 10:55:07 -0700

    Refactor code for Shortest Distance in a Plane

--- src/com/leetcode/www/ShortestDistanceInAPlane.sql
+++ src/com/leetcode/www/ShortestDistanceInAPlane.sql
@@ -9,3 +9,14 @@
 select min(round(sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)), 2)) as shortest
 from point_2d p1, point_2d p2
 where p1.x != p2.x or p1.y != p2.y
+
+
+# Runtime: 149 ms, faster than 47.76% of MySQL online submissions for Shortest Distance in a Plane.
+# Memory Usage: N/A
+#
+# https://leetcode.com/problems/shortest-distance-in-a-plane/discuss/286336/Faster-than-100-in-sql-server
+#
+# Write your MySQL query statement below
+select round(min(sqrt(power(a.x - b.x, 2) + power(a.y - b.y, 2))), 2) as shortest
+from point_2d a
+join point_2d b on a.x != b.x or a.y != b.y
