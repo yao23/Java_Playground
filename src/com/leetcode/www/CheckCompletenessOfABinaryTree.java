2ff58e46656aad1ea55a93da030a418693ecbcd9
Yao <myspiritcrazy@gmail.com>
2019-05-25 10:04:57 -0700

    Refactor code for Check Completeness of a Binary Tree

--- src/com/leetcode/www/CheckCompletenessOfABinaryTree.java
+++ src/com/leetcode/www/CheckCompletenessOfABinaryTree.java
@@ -1,5 +1,8 @@
 package com.leetcode.www;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CheckCompletenessOfABinaryTree { // LC 958
     /**
      * Runtime: 0 ms, faster than 100.00% of Java online submissions for Check Completeness of a Binary Tree.
@@ -40,5 +43,38 @@ public class CheckCompletenessOfABinaryTree { // LC 958
             res = Math.max(res, getMaxIndex(root.right,2 * prev + 1));
         }
         return res;
+    }
+
+    /**
+     * Runtime: 1 ms, faster than 95.20% of Java online submissions for Check Completeness of a Binary Tree.
+     * Memory Usage: 35 MB, less than 96.86% of Java online submissions for Check Completeness of a Binary Tree.
+     *
+     * BFS
+     *
+     * https://leetcode.com/problems/check-completeness-of-a-binary-tree/discuss/281891/Java-Short-Solution-!!!
+     *
+     * @param root
+     * @return
+     */
+    public boolean isCompleteTreeV0(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        if (root != null) {
+            q.offer(root);
+        }
+
+        boolean end = false;
+        while (!q.isEmpty()) {
+            TreeNode t = q.poll();
+            if (t == null) {
+                end = true;
+            } else {
+                if(end) { // there are nodes after null in current level
+                    return false;
+                }
+                q.offer(t.left);
+                q.offer(t.right);
+            }
+        }
+        return true;
     }
 }
