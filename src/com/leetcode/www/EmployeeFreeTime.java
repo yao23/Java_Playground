4b6ad1429c4894710c3d33fb7a290c5d7901cc3d
Yao <myspiritcrazy@gmail.com>
2019-05-27 11:53:24 -0700

    Refactor code for Employee Free Time

--- src/com/leetcode/www/EmployeeFreeTime.java
+++ src/com/leetcode/www/EmployeeFreeTime.java
@@ -1,9 +1,6 @@
 package com.leetcode.www;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class EmployeeFreeTime { // LC 759
     /**
@@ -56,5 +53,51 @@ public class EmployeeFreeTime { // LC 759
             this.employeeIndex = employeeIndex;
             this.intervalIndex = intervalIndex;
         }
+    }
+
+    /**
+     * Runtime: 40 ms, faster than 54.99% of Java online submissions for Employee Free Time.
+     * Memory Usage: 48 MB, less than 47.85% of Java online submissions for Employee Free Time.
+     *
+     * https://leetcode.com/problems/employee-free-time/discuss/289293/Java-PriorityQueue-detailed-comments
+     *
+     * @param schedule
+     * @return
+     */
+    public int[][] employeeFreeTimeV0(int[][][] schedule) {
+        // Invariant: pq contains list with postive length
+        PriorityQueue<Deque<Interval>> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x.peek().start));
+
+        for (int[][] intervals : schedule) {
+            Deque<Interval> queue = new ArrayDeque<>();
+            for (int[] interval: intervals) {
+                queue.add(new Interval(interval[0], interval[1]));
+            }
+
+            pq.add(queue);
+        }
+
+        List<Interval> ans = new ArrayList<>();
+        Interval prev = new Interval(0, pq.peek().peek().start);
+
+        while (pq.size() > 0) {
+            Deque<Interval> temp = pq.remove();
+            Interval next = temp.remove();
+            if (temp.size() > 0) pq.add(temp);
+
+            if (prev.end < next.start) {
+                ans.add(new Interval(prev.end, next.start));
+                prev = next;
+            } else {
+                prev.end = Math.max(prev.end, next.end);
+            }
+        }
+
+        int[][] ret = new int[ans.size()][2];
+        for (int i = 0; i < ans.size(); i++) {
+            ret[i][0] = ans.get(i).start;
+            ret[i][1] = ans.get(i).end;
+        }
+        return ret;
     }
 }
