64e1a2eb22b53064611ed175cfa8332e7f957076
Yao <myspiritcrazy@gmail.com>
2019-06-01 23:16:45 -0700

    Refactor code for Lowest Common Ancestor of a Binary Search Tree

--- src/com/leetcode/www/LowestCommonAncestorOfABinarySearchTree.java
+++ src/com/leetcode/www/LowestCommonAncestorOfABinarySearchTree.java
@@ -1,13 +1,22 @@
 package com.leetcode.www;
 
 public class LowestCommonAncestorOfABinarySearchTree { // LC 235
+    /**
+     * Runtime: 4 ms, faster than 100.00% of Java online submissions for Lowest Common Ancestor of a Binary Search Tree.
+     * Memory Usage: 36.1 MB, less than 5.06% of Java online submissions for Lowest Common Ancestor of a Binary Search Tree.
+     *
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root;
         }
-        if(root.val > p.val && root.val > q.val){
+        if (root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left, p, q);
-        } else if(root.val < p.val && root.val < q.val){
+        } else if (root.val < p.val && root.val < q.val){
             return lowestCommonAncestor(root.right, p, q);
         } else{
             return root;
