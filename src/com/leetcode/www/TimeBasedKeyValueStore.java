a05207d295b432f735ba54f774de452a5c954785
Yao <myspiritcrazy@gmail.com>
2019-06-05 14:42:52 -0700

    Refactor code for Time Based Key-Value Store

--- src/com/leetcode/www/TimeBasedKeyValueStore.java
+++ src/com/leetcode/www/TimeBasedKeyValueStore.java
@@ -1,9 +1,6 @@
 package com.leetcode.www;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class TimeBasedKeyValueStore { // LC 981
     /**
@@ -56,6 +53,70 @@ public class TimeBasedKeyValueStore { // LC 981
             return "";
         }
     }
+
+    class TimeMapV0 {
+
+        // Stores key to Node map
+        // where Node stores list of timestamp and list of values both indexed same
+        // since timestamps is strictly increasing, we can do binary search on it
+        Map<String, Node> map;
+
+        /** Initialize your data structure here. */
+        public TimeMapV0() {
+            map = new HashMap<>();
+        }
+
+        public void set(String key, String value, int timestamp) {
+            Node node = map.getOrDefault(key, new Node());
+            node.add(timestamp, value);
+            map.put(key, node);
+        }
+
+        public String get(String key, int timestamp) {
+            if (!map.containsKey(key)) {
+                return "";
+            }
+            Node node = map.get(key);
+            return search(timestamp, node);
+        }
+
+        private String search(int timestamp, Node node) {
+            int i = 0, j = node.times.size() - 1;
+            // some base cases
+            if (timestamp < node.times.get(0)) {
+                return "";
+            }
+            if (timestamp >= node.times.get(j)) {
+                return node.values.get(j);
+            }
+            while (i < j) {
+                int m = (i + j) / 2;
+                if (timestamp == node.times.get(m)) {
+                    return node.values.get(m);
+                } else if (timestamp > node.times.get(m)) {
+                    i = m + 1;
+                } else {
+                    j = m;
+                }
+            }
+            return i == 0 ? "" : node.values.get(i - 1);
+        }
+
+        class Node {
+            List<Integer> times;
+            List<String> values;
+            Node() {
+                times = new ArrayList<>();
+                values = new ArrayList<>();
+            }
+
+            public void add(int time, String value) {
+                times.add(time);
+                values.add(value);
+            }
+        }
+    }
+
 
     /**
      * Your TimeMap object will be instantiated and called as such:
