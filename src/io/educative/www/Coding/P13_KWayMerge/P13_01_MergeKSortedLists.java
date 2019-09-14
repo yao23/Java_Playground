fab082c5846005d52ee583a06684d3013ae02cde
Yao <myspiritcrazy@gmail.com>
2019-09-14 00:01:07 -0700

    Refactor code for Merge K Sorted Lists

---
+++ src/io/educative/www/Coding/P13_KWayMerge/P13_01_MergeKSortedLists.java
@@ -0,0 +1,73 @@
+package io.educative.www.Coding.P13_KWayMerge;
+import java.util.*;
+
+public class P13_01_MergeKSortedLists {
+    /**
+     * Time complexity
+     * Since we’ll be going through all the elements of all arrays and will be removing/adding one element to the heap
+     * in each step, the time complexity of the above algorithm will be O(N*logK), where ‘N’ is the total number of
+     * elements in all the ‘K’ input arrays.
+     *
+     * Space complexity
+     * The space complexity will be O(K) because, at any time, our min-heap will be storing one number from all the ‘K’
+     * input arrays.
+     *
+     * @param lists
+     * @return
+     */
+    public static ListNode merge(ListNode[] lists) {
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(Comparator.comparingInt(n -> n.value));
+
+        // put the root of each list in the min heap
+        for (ListNode root : lists)
+            if (root != null)
+                minHeap.add(root);
+
+        // take the smallest (top) element form the min-heap and add it to the result;
+        // if the top element has a next element add it to the heap
+        ListNode resultHead = null, resultTail = null;
+        while (!minHeap.isEmpty()) {
+            ListNode node = minHeap.poll();
+            if (resultHead == null) {
+                resultHead = resultTail = node;
+            } else {
+                resultTail.next = node;
+                resultTail = resultTail.next;
+            }
+            if (node.next != null)
+                minHeap.add(node.next);
+        }
+
+        return resultHead;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(6);
+        l1.next.next = new ListNode(8);
+
+        ListNode l2 = new ListNode(3);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(7);
+
+        ListNode l3 = new ListNode(1);
+        l3.next = new ListNode(3);
+        l3.next.next = new ListNode(4);
+
+        ListNode result = P13_01_MergeKSortedLists.merge(new ListNode[] { l1, l2, l3 });
+        System.out.print("Here are the elements form the merged list: ");
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
+}
+
+class ListNode {
+    int value;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+}
