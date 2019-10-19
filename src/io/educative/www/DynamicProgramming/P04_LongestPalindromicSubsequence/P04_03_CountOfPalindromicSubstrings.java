ae6f694c84b123c34d31277ed244197d40c512ee
Yao <myspiritcrazy@gmail.com>
2019-10-18 20:37:50 -0700

    Refactor Code for Count of Palindromic Substrings

---
+++ src/io/educative/www/DynamicProgramming/P04_LongestPalindromicSubsequence/P04_03_CountOfPalindromicSubstrings.java
@@ -0,0 +1,43 @@
+package io.educative.www.DynamicProgramming.P04_LongestPalindromicSubsequence;
+
+class P04_03_CountOfPalindromicSubstrings {
+    /**
+     * The time and space complexity of the algorithm is O(n^2), where ‘n’ is the length of the input string.
+     *
+     * @param st
+     * @return
+     */
+    public int findCPS(String st) {
+        // dp[i][j] will be 'true' if the string from index 'i' to index 'j' is a
+        // palindrome
+        boolean[][] dp = new boolean[st.length()][st.length()];
+        int count = 0;
+
+        // every string with one character is a palindrome
+        for (int i = 0; i < st.length(); i++) {
+            dp[i][i] = true;
+            count++;
+        }
+
+        for (int startIndex = st.length() - 1; startIndex >= 0; startIndex--) {
+            for (int endIndex = startIndex + 1; endIndex < st.length(); endIndex++) {
+                if (st.charAt(startIndex) == st.charAt(endIndex)) {
+                    // if it's a two character string or if the remaining string is a palindrome too
+                    if (endIndex - startIndex == 1 || dp[startIndex + 1][endIndex - 1]) {
+                        dp[startIndex][endIndex] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        P04_03_CountOfPalindromicSubstrings cps = new P04_03_CountOfPalindromicSubstrings();
+        System.out.println(cps.findCPS("abdbca"));
+        System.out.println(cps.findCPS("cdpdd"));
+        System.out.println(cps.findCPS("pqr"));
+    }
+}
