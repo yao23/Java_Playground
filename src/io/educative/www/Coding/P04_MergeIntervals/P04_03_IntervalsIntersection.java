0401996e83c314da43d927d91425959def87ff37
Yao <myspiritcrazy@gmail.com>
2020-01-02 22:57:04 -0800

    Refactor Code for Intervals Intersection

---
+++ src/io/educative/www/Coding/P04_MergeIntervals/P04_03_IntervalsIntersection.java
@@ -0,0 +1,66 @@
+package io.educative.www.Coding.P04_MergeIntervals;
+
+import java.util.*;
+
+class Interval {
+    int start;
+    int end;
+
+    public Interval(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+};
+
+class P04_03_IntervalsIntersection {
+    /**
+     * Time complexity
+     * As we are iterating through both the lists once, the time complexity of the above algorithm is O(N + M),
+     * where ‘N’ and ‘M’ are the total number of intervals in the input arrays respectively.
+     *
+     * Space complexity
+     * Ignoring the space needed for the result list, the algorithm runs in constant space O(1).
+     *
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static Interval[] merge(Interval[] arr1, Interval[] arr2) {
+        List<Interval> result = new ArrayList<Interval>();
+        int i = 0, j = 0;
+        while (i < arr1.length && j < arr2.length) {
+            // check if the interval arr[i] intersects with arr2[j]
+            // check if one of the interval's start time lies within the other interval
+            if ((arr1[i].start >= arr2[j].start && arr1[i].start <= arr2[j].end)
+                    || (arr2[j].start >= arr1[i].start && arr2[j].start <= arr1[i].end)) {
+                // store the intersection part
+                result.add(new Interval(Math.max(arr1[i].start, arr2[j].start), Math.min(arr1[i].end, arr2[j].end)));
+            }
+
+            // move next from the interval which is finishing first
+            if (arr1[i].end < arr2[j].end)
+                i++;
+            else
+                j++;
+        }
+
+        return result.toArray(new Interval[result.size()]);
+    }
+
+    public static void main(String[] args) {
+        Interval[] input1 = new Interval[] { new Interval(1, 3), new Interval(5, 6), new Interval(7, 9) };
+        Interval[] input2 = new Interval[] { new Interval(2, 3), new Interval(5, 7) };
+        Interval[] result = P04_03_IntervalsIntersection.merge(input1, input2);
+        System.out.print("Intervals Intersection: ");
+        for (Interval interval : result)
+            System.out.print("[" + interval.start + "," + interval.end + "] ");
+        System.out.println();
+
+        input1 = new Interval[] { new Interval(1, 3), new Interval(5, 7), new Interval(9, 12) };
+        input2 = new Interval[] { new Interval(5, 10) };
+        result = P04_03_IntervalsIntersection.merge(input1, input2);
+        System.out.print("Intervals Intersection: ");
+        for (Interval interval : result)
+            System.out.print("[" + interval.start + "," + interval.end + "] ");
+    }
+}
