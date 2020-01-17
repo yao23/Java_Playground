7be36f21b804e1896206049a377fbe3b4990f7d5
Yao <myspiritcrazy@gmail.com>
2020-01-16 22:10:59 -0800

    Refactor Code for Reverse A Sub List

---
+++ src/io/educative/www/Coding/P06_ReverseLinkedListInPlace/P06_02_ReverseASubList.java
@@ -0,0 +1,69 @@
+package io.educative.www.Coding.P06_InPlaceReversalOfALinkedList;
+
+import java.util.*;
+
+class P06_02_ReverseASubList {
+    /**
+     * Time complexity
+     * The time complexity of our algorithm will be O(N) where ‘N’ is the total number of nodes in the LinkedList.
+     *
+     * Space complexity
+     * We only used constant space, therefore, the space complexity of our algorithm is O(1).
+     *
+     * @param head
+     * @param p
+     * @param q
+     * @return
+     */
+    public static ListNode reverse(ListNode head, int p, int q) {
+        if (p == q)
+            return head;
+
+        // after skipping 'p-1' nodes, current will point to 'p'th node
+        ListNode current = head, previous = null;
+        for (int i = 0; current != null && i < p - 1; ++i) {
+            previous = current;
+            current = current.next;
+        }
+
+        // we are interested in three parts of the LinkedList, part before index 'p', part between 'p' and 
+        // 'q', and the part after index 'q'
+        ListNode lastNodeOfFirstPart = previous; // points to the node at index 'p-1'
+        // after reversing the LinkedList 'current' will become the last node of the sub-list
+        ListNode lastNodeOfSubList = current;
+        ListNode next = null; // will be used to temporarily store the next node
+        // reverse nodes between 'p' and 'q'
+        for (int i = 0; current != null && i < q - p + 1; i++) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        // connect with the first part
+        if (lastNodeOfFirstPart != null)
+            lastNodeOfFirstPart.next = previous; // 'previous' is now the first node of the sub-list
+        else // this means p == 1 i.e., we are changing the first node (head) of the LinkedList
+            head = previous;
+
+        // connect with the last part
+        lastNodeOfSubList.next = current;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ListNode result = P06_02_ReverseASubList.reverse(head, 2, 4);
+        System.out.print("Nodes of the reversed LinkedList are: ");
+        while (result != null) {
+            System.out.print(result.value + " ");
+            result = result.next;
+        }
+    }
+}
