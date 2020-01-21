29bd6662b95dc5dd8dbc9aacbfc6a1f16ed47c63
Yao <myspiritcrazy@gmail.com>
2020-01-20 21:20:56 -0800

    Refactor Code for Binary Tree Level Order Traversal

---
+++ src/io/educative/www/Coding/P07_TreeBFS/P07_01_BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,56 @@
+package io.educative.www.Coding.P07_TreeBFS;
+
+import java.util.*;
+
+class P07_01_BinaryTreeLevelOrderTraversal {
+    /**
+     * Time complexity
+     * The time complexity of the above algorithm is O(N), where ‘N’ is the total number of nodes in the tree.
+     * This is due to the fact that we traverse each node once.
+     *
+     * Space complexity
+     * The space complexity of the above algorithm will be O(N) as we need to return a list containing the level order
+     * traversal. We will also need O(N) space for the queue. Since we can have a maximum of N/2 nodes at any level
+     * (this could happen only at the lowest level), therefore we will need O(N) space to store them in the queue.
+     *
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> traverse(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                // add the node to the current level
+                currentLevel.add(currentNode.val);
+                // insert the children of current node in the queue
+                if (currentNode.left != null)
+                    queue.offer(currentNode.left);
+                if (currentNode.right != null)
+                    queue.offer(currentNode.right);
+            }
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<List<Integer>> result = P07_01_BinaryTreeLevelOrderTraversal.traverse(root);
+        System.out.println("Level order traversal: " + result);
+    }
+}
+
