d7ce270085ecb3bb51aa9317f8c31e6d8805592c
Yao <myspiritcrazy@gmail.com>
2020-02-06 23:56:01 -0800

    Coding Practice for Mural (fix code to parse input for the better algorithm)

--- src/codingcompetition/2019/PracticeRound/Mural.java
+++ src/codingcompetition/2019/PracticeRound/Mural.java
@@ -1,58 +1,40 @@
 package PracticeRound;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Mural {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int testNum = Integer.parseInt(input.nextLine());
+        int testNum = input.nextInt();
 
         for (int i = 0; i < testNum; i++) {
-            int N = Integer.parseInt(input.nextLine());
+            int N = input.nextInt();
 
-            String digits = input.nextLine();
-
-            int score = expand(digits, N);
+            int score = expand(input, N);
 
             System.out.println("Case #" + i + ": " + score);
         }
     }
 
-    private static int expand(String digits, int len) {
+    private static int expand(Scanner input, int len) {
         if (len == 1) {
-            return Integer.parseInt(digits);
+            return input.nextInt();
         } else if (len == 2) {
-            String[] digitsArr = digits.split("\\s+");
-            return Math.max(Integer.parseInt(digitsArr[0]), Integer.parseInt(digitsArr[1]));
+            return Math.max(input.nextInt(), input.nextInt());
         } else {
-            int max = 0, numIdx = 0, sum = 0, sumStart = 0;
+            int max = 0;
             int radius = len / 2 == 0 ? len / 2 : (len + 1) / 2;
-            String tmp = "";
-            List<Integer> nums = new ArrayList<>();
-            for (char ch : digits.toCharArray()) {
-                if (ch != ' ') {
-                    tmp += ch;
-                } else {
-                    int cur = Integer.parseInt(tmp);
-                    nums.add(cur);
-                    if (numIdx < radius) {
-                        sum += cur;
-                    } else {
-                        if (sum > max) {
-                            max = sum;
-                        }
-                        sum += (cur - nums.get(sumStart));
-                        sumStart++;
-                    }
-                    tmp = "";
-                    numIdx++;
-                }
+            int[] digitArr = new int[radius];
+            for (int i = 0; i < radius; i++) {
+                digitArr[i] = input.nextInt();
+                max += digitArr[i];
             }
-
-            if (sum > max) {
-                max = sum;
+            int sum = max;
+            for (int i = radius, j = 0; i < len; i++, j++) {
+                sum += (input.nextInt() - digitArr[j]);
+                if (sum > max) {
+                    max = sum;
+                }
             }
 
             return max;
