924fd143032d9a54d6de1423828885cd868e43ea
Yao <myspiritcrazy@gmail.com>
2020-02-07 22:15:00 -0800

    Coding Practice for Mural (refactor code with better variable name)

--- src/codingcompetition/2019/PracticeRound/Mural.java
+++ src/codingcompetition/2019/PracticeRound/Mural.java
@@ -8,23 +8,23 @@ public class Mural {
         int testNum = input.nextInt();
 
         for (int i = 0; i < testNum; i++) {
-            int score = expand(input);
+            int score = solve(input);
 
             System.out.println("Case #" + i + ": " + score);
         }
     }
 
-    private static int expand(Scanner input) {
-        int len = input.nextInt();
-        String digits = input.next();
+    private static int solve(Scanner input) {
+        int N = input.nextInt();
+        String scores = input.next();
         int max = 0;
-        int radius = len / 2 == 0 ? len / 2 : (len + 1) / 2;
-        for (int i = 0; i < radius; i++) {
-            max += digits.charAt(i) - '0';
+        int resLen = N / 2 == 0 ? N / 2 : (N + 1) / 2;
+        for (int i = 0; i < resLen; i++) {
+            max += scores.charAt(i) - '0';
         }
         int sum = max;
-        for (int i = radius; i < len; i++) {
-            sum += (digits.charAt(i) - digits.charAt(i - radius));
+        for (int i = resLen; i < N; i++) {
+            sum += (scores.charAt(i) - scores.charAt(i - resLen));
             if (sum > max) {
                 max = sum;
             }
