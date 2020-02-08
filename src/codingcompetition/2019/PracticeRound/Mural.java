ebdd030ff2762127381d52763d02257e87748444
Yao <myspiritcrazy@gmail.com>
2020-02-07 22:11:19 -0800

    Coding Practice for Mural (remove base case for the better algorithm)

--- src/codingcompetition/2019/PracticeRound/Mural.java
+++ src/codingcompetition/2019/PracticeRound/Mural.java
@@ -16,27 +16,20 @@ public class Mural {
 
     private static int expand(Scanner input) {
         int len = input.nextInt();
-
-        if (len == 1) {
-            return input.nextInt();
-        } else if (len == 2) {
-            return Math.max(input.nextInt(), input.nextInt());
-        } else {
-            String digits = input.next();
-            int max = 0;
-            int radius = len / 2 == 0 ? len / 2 : (len + 1) / 2;
-            for (int i = 0; i < radius; i++) {
-                max += digits.charAt(i) - '0';
-            }
-            int sum = max;
-            for (int i = radius; i < len; i++) {
-                sum += (digits.charAt(i) - digits.charAt(i - radius));
-                if (sum > max) {
-                    max = sum;
-                }
-            }
-
-            return max;
+        String digits = input.next();
+        int max = 0;
+        int radius = len / 2 == 0 ? len / 2 : (len + 1) / 2;
+        for (int i = 0; i < radius; i++) {
+            max += digits.charAt(i) - '0';
         }
+        int sum = max;
+        for (int i = radius; i < len; i++) {
+            sum += (digits.charAt(i) - digits.charAt(i - radius));
+            if (sum > max) {
+                max = sum;
+            }
+        }
+
+        return max;
     }
 }
