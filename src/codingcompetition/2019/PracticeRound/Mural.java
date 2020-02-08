7711e23a572f9c00b0d17e4b541e83ae37ead5b3
Yao <myspiritcrazy@gmail.com>
2020-02-07 22:01:29 -0800

    Coding Practice for Mural (fix code to parse input for the better algorithm)

--- src/codingcompetition/2019/PracticeRound/Mural.java
+++ src/codingcompetition/2019/PracticeRound/Mural.java
@@ -8,30 +8,29 @@ public class Mural {
         int testNum = input.nextInt();
 
         for (int i = 0; i < testNum; i++) {
-            int N = input.nextInt();
-
-            int score = expand(input, N);
+            int score = expand(input);
 
             System.out.println("Case #" + i + ": " + score);
         }
     }
 
-    private static int expand(Scanner input, int len) {
+    private static int expand(Scanner input) {
+        int len = input.nextInt();
+
         if (len == 1) {
             return input.nextInt();
         } else if (len == 2) {
             return Math.max(input.nextInt(), input.nextInt());
         } else {
+            String digits = input.next();
             int max = 0;
             int radius = len / 2 == 0 ? len / 2 : (len + 1) / 2;
-            int[] digitArr = new int[radius];
             for (int i = 0; i < radius; i++) {
-                digitArr[i] = input.nextInt();
-                max += digitArr[i];
+                max += digits.charAt(i) - '0';
             }
             int sum = max;
-            for (int i = radius, j = 0; i < len; i++, j++) {
-                sum += (input.nextInt() - digitArr[j]);
+            for (int i = radius; i < len; i++) {
+                sum += (digits.charAt(i) - digits.charAt(i - radius));
                 if (sum > max) {
                     max = sum;
                 }
