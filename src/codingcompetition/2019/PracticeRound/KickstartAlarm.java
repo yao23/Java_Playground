106a043f9a0a6203bfaf3d362c674ca4113c299f
Yao <myspiritcrazy@gmail.com>
2020-02-09 23:45:30 -0800

    Coding Practice for Kickstart Alarm (refactor code)

--- src/codingcompetition/2019/PracticeRound/KickstartAlarm.java
+++ src/codingcompetition/2019/PracticeRound/KickstartAlarm.java
@@ -3,8 +3,16 @@ package PracticeRound;
 import java.util.Scanner;
 
 public class KickstartAlarm {
+    /**
+     * https://codingcompetitions.withgoogle.com/kickstart/round/0000000000051060/0000000000058a56
+     *
+     * https://www.topcoder.com/problem-of-the-week-google-kickstart-alarm/
+     * https://blog.csdn.net/IceTeaSet/article/details/80472573
+     * https://blog.csdn.net/ihsin/article/details/81839635
+     * https://github.com/amylmy/KickStart-GoogleCompetition/blob/master/2019_PracticeRound_KickstartAlarm.java
+     */
     private static int mod = 1000000007;
-    private static long[] a = new long[1000001];
+    private static long[] arr = new long[1000001];
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -27,21 +35,21 @@ public class KickstartAlarm {
 
         int i;
         long x, y;
-        a[1] = (x1 + y1) % F;
+        arr[1] = (x1 + y1) % F;
         for (i = 2; i <= N; i++) {
             x = (C * x1 + D * y1 + E1) % F;
             y = (D * x1 + C * y1 + E2) % F;
-            a[i] = (x + y) % F;
+            arr[i] = (x + y) % F;
             x1 = x;
             y1 = y;
         }
-        long ans=0;
+        long ans = 0;
         long la = K;
         for (i = 2; i <= N + 1; i++) {
-            ans = (ans + la * (long)(N + 2 - i) % mod * a[i - 1] % mod) % mod;
-            x = (power(i,K + 1) - 1) * power(i - 1,mod - 2) % mod;
+            ans = (ans + la * (long)(N + 2 - i) % mod * arr[i - 1] % mod) % mod;
+            x = (power(i,K + 1) - 1) * power(i - 1,mod - 2) % mod; // if P is prime, (1 / a) % P = a ^ (P - 2) => (1 / (i - 1) ) % mod = power (i - 1, mod - 2)
             x--;
-            if(x < 0) {
+            if (x < 0) {
                 x += mod;
             }
             la += x;
