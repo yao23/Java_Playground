6ff236b50120e09520ccad37db18e8a9b87e797a
Yao <myspiritcrazy@gmail.com>
2020-02-28 22:30:16 -0800

    Refactor code to add doc for Heaters

--- src/com/leetcode/www/Heaters.java
+++ src/com/leetcode/www/Heaters.java
@@ -3,6 +3,15 @@ package com.leetcode.www;
 import java.util.Arrays;
 
 public class Heaters { // LC 475
+    /**
+     * 1) Input are house and heaters positions, difference is cover radius
+     * 2) Search current heater cover range as much as possible
+     * 3) If current heater has shorter radius than next heater, then go right to next house, otherwise go to next heater
+     *
+     * @param houses
+     * @param heaters
+     * @return
+     */
     public int findRadius(int[] houses, int[] heaters) {
         int res = 0;
         if (houses == null || heaters == null) {
@@ -22,7 +31,7 @@ public class Heaters { // LC 475
             }
 
             res = Math.max(res, Math.abs(heaters[j] - houses[i]));
-            i++;
+            i++; // move to next house
         }
 
         return res;
