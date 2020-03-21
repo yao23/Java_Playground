7776248696d67b679bcdf4bb74a7872406cfb5db
Yao <myspiritcrazy@gmail.com>
2020-03-20 23:53:58 -0700

    Refactor code to add doc for Maximum Sum of 3 Non-Overlapping Subarrays

--- src/com/leetcode/www/MaximumSumOfThreeNonOverlappingSubarrays.java
+++ src/com/leetcode/www/MaximumSumOfThreeNonOverlappingSubarrays.java
@@ -195,4 +195,53 @@ public class MaximumSumOfThreeNonOverlappingSubarrays {
         }
         return sum;
     }
+
+    /**
+     * Runtime: 2 ms, faster than 99.58% of Java online submissions for Maximum Sum of 3 Non-Overlapping Subarrays.
+     * Memory Usage: 43.9 MB, less than 5.88% of Java online submissions for Maximum Sum of 3 Non-Overlapping Subarrays.
+     *
+     * Time Complexity: O(N), where N is the length of the array.
+     * Every loop is bounded in the number of steps by N, and does O(1) work.
+     *
+     * Space complexity: O(N). W, left, and right all take O(N) memory.
+     * @param nums
+     * @param K
+     * @return
+     */
+    public int[] maxSumOfThreeSubarraysV2(int[] nums, int K) {
+        //W is an array of sums of windows
+        int[] W = new int[nums.length - K + 1];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (i >= K) sum -= nums[i-K];
+            if (i >= K-1) W[i-K+1] = sum;
+        }
+
+        int[] left = new int[W.length];
+        int best = 0;
+        for (int i = 0; i < W.length; i++) {
+            if (W[i] > W[best]) best = i;
+            left[i] = best;
+        }
+
+        int[] right = new int[W.length];
+        best = W.length - 1;
+        for (int i = W.length - 1; i >= 0; i--) {
+            if (W[i] >= W[best]) best = i;
+            right[i] = best;
+        }
+
+        int[] ans = new int[]{-1, -1, -1};
+        for (int j = K; j < W.length - K; j++) {
+            int i = left[j - K], k = right[j + K];
+            if (ans[0] == -1 || W[i] + W[j] + W[k] >
+                    W[ans[0]] + W[ans[1]] + W[ans[2]]) {
+                ans[0] = i;
+                ans[1] = j;
+                ans[2] = k;
+            }
+        }
+        return ans;
+    }
 }
