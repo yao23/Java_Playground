3d7111f03db9ff6e940173ad6b9c1946d99793ef
Yao <myspiritcrazy@gmail.com>
2020-03-23 00:19:11 -0700

    Refactor code to add doc for Is Graph Bipartite

--- src/com/leetcode/www/IsGraphBipartite.java
+++ src/com/leetcode/www/IsGraphBipartite.java
@@ -1,8 +1,50 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 public class IsGraphBipartite {
+    /**
+     * Runtime: 1 ms, faster than 69.95% of Java online submissions for Is Graph Bipartite?.
+     * Memory Usage: 42.4 MB, less than 65.85% of Java online submissions for Is Graph Bipartite?.
+     *
+     * Time Complexity: O(N + E), where N is the number of nodes in the graph, and E is the number of edges.
+     * We explore each node once when we transform it from uncolored to colored, traversing all its edges in the process.
+     *
+     * Space Complexity: O(N), the space used to store the color.
+     *
+     * @param graph
+     * @return
+     */
     public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+
+        for (int start = 0; start < n; ++start) {
+            if (color[start] == -1) {
+                Stack<Integer> stack = new Stack();
+                stack.push(start);
+                color[start] = 0;
+
+                while (!stack.empty()) {
+                    Integer node = stack.pop();
+                    for (int nei: graph[node]) {
+                        if (color[nei] == -1) {
+                            stack.push(nei);
+                            color[nei] = color[node] ^ 1;
+                        } else if (color[nei] == color[node]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isBipartiteV2(int[][] graph) {
         Set<Integer> blue = new HashSet<>();
         Set<Integer> red = new HashSet<>();
         int row = graph.length;
