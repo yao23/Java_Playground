45b76b76df6b24ef44150ecbf0256d2ed61ba7ce
Yao <myspiritcrazy@gmail.com>
2020-03-22 17:46:11 -0700

    Refactor code to add doc for Is Graph Bipartite

--- src/com/leetcode/www/IsGraphBipartite.java
+++ src/com/leetcode/www/IsGraphBipartite.java
@@ -11,55 +11,55 @@ public class IsGraphBipartite {
             int col = graph[i].length;
             int index = 0;
             boolean shouldBeBlue = true;
+            Set<Integer> visited = new HashSet<>();
             for (int j = 0; j < col; j++) {
                 int curIdx = graph[i][j];
                 if (blue.contains(curIdx)) {
-                    index = curIdx;
+                    index = j;
+                    break;
                 } else if (red.contains(curIdx)) {
                     shouldBeBlue = false;
-                    index = curIdx;
+                    index = j;
+                    break;
                 }
-                break;
             }
 
-            if (!solve(index, red, blue, shouldBeBlue, col)) {
+            if (!solve(i, index, red, blue, shouldBeBlue, graph, visited)) {
                 return false;
             }
         }
         return true;
     }
 
-    private boolean solve(int index, Set<Integer> red, Set<Integer> blue, boolean shouldBeBlue, int len) {
-        if (index < 0 || index == len) {
+    private boolean solve(int row, int col, Set<Integer> red, Set<Integer> blue, boolean shouldBeBlue, int[][] graph, Set<Integer> visited) {
+        int len = graph[row].length;
+        if (col < 0 || col == len) {
             return true;
         }
-        if (blue.contains(index)) {
-            if (shouldBeBlue) {
-                return true;
-            }
-        } else if (red.contains(index)) {
-            if (!shouldBeBlue) {
-                return true;
-            }
+        int num = graph[row][col];
+        if (visited.contains(num)) {
+            return true;
         }
 
+        visited.add(num);
         if (shouldBeBlue) {
-            if (red.contains(index)) {
+            if (red.contains(num)) {
                 return false;
             } else {
-                blue.add(index);
+                blue.add(num);
             }
         } else {
-            if (blue.contains(index)) {
+            if (blue.contains(num)) {
                 return false;
             } else {
-                red.add(index);
+                red.add(num);
             }
         }
 
-        return solve(index - 1, red, blue, !shouldBeBlue, len) &&
-                solve(index + 1, red, blue, !shouldBeBlue, len);
+        return solve(row, col - 1, red, blue, !shouldBeBlue, graph, visited) &&
+                solve(row, col + 1, red, blue, !shouldBeBlue, graph, visited);
     }
+
 
     public boolean isBipartiteV1(int[][] graph) {
         Set<Integer> blue = new HashSet<>();
