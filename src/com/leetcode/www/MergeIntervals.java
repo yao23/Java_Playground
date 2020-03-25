ce060ece5cceaf102be6a70a3017f48f24abe510
Yao <myspiritcrazy@gmail.com>
2020-03-24 20:40:47 -0700

    Refactor code to add doc for Merge Intervals

--- src/com/leetcode/www/MergeIntervals.java
+++ src/com/leetcode/www/MergeIntervals.java
@@ -1,13 +1,45 @@
-package com.leetcode.www; /**
+package com.leetcode.www;
+/**
  * Created by liyao on 6/26/17.
  */
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Collections;
+import java.util.*;
 
 public class MergeIntervals { // LC 56
-    public List<Interval> merge(List<Interval> intervals) {
+    /**
+     * Runtime: 5 ms, faster than 91.84% of Java online submissions for Merge Intervals.
+     * Memory Usage: 42.1 MB, less than 49.28% of Java online submissions for Merge Intervals.
+     *
+     * @param intervals
+     * @return
+     */
+    public int[][] merge(int[][] intervals) {
+        Collections.sort(Arrays.asList(intervals), new IntervalComparator());
+
+        LinkedList<int[]> merged = new LinkedList<>();
+        for (int[] interval : intervals) {
+            // if the list of merged intervals is empty or if the current
+            // interval does not overlap with the previous, simply append it.
+            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
+                merged.add(interval);
+            }
+            // otherwise, there is overlap, so we merge the current and previous
+            // intervals.
+            else {
+                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    private class IntervalComparator implements Comparator<int[]> {
+        @Override
+        public int compare(int[] a, int[] b) {
+            return a[0] < b[0] ? -1 : a[0] == b[0] ? 0 : 1;
+        }
+    }
+
+    public List<Interval> mergeV0(List<Interval> intervals) {
         int size = intervals.size();
         if (size < 2) {
             return intervals;
