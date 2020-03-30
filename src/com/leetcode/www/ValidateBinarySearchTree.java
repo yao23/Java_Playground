34333740f9e7ba23e13fdf31b1f6b11a7c1fb304
Yao <myspiritcrazy@gmail.com>
2020-03-29 20:38:48 -0700

    Refactor code to add doc for Validate Binary Search Tree

--- src/com/leetcode/www/ValidateBinarySearchTree.java
+++ src/com/leetcode/www/ValidateBinarySearchTree.java
@@ -1,6 +1,13 @@
 package com.leetcode.www;
 
 public class ValidateBinarySearchTree { // LC 98
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Validate Binary Search Tree.
+     * Memory Usage: 39.6 MB, less than 78.61% of Java online submissions for Validate Binary Search Tree.
+     *
+     * @param root
+     * @return
+     */
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return true;
