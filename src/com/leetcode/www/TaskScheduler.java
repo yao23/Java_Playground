a1fc74d1ce776b432f9a5bdccd7785bb996687a0
Yao <myspiritcrazy@gmail.com>
2020-03-30 23:48:23 -0700

    Refactor code to add doc for Task Scheduler

--- src/com/leetcode/www/TaskScheduler.java
+++ src/com/leetcode/www/TaskScheduler.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class TaskScheduler { // 621
     /**
@@ -30,6 +30,48 @@ public class TaskScheduler { // 621
                 i++;
             }
             Arrays.sort(map);
+        }
+        return time;
+    }
+
+    /**
+     * Runtime: 28 ms, faster than 26.86% of Java online submissions for Task Scheduler.
+     * Memory Usage: 40.8 MB, less than 10.29% of Java online submissions for Task Scheduler.
+     *
+     * Time complexity : O(n). Number of iterations will be equal to resultant time time.
+     * Space complexity : O(1). queue and temp size will not exceed O(26).
+     *
+     * @param tasks
+     * @param n
+     * @return
+     */
+    public int leastIntervalV1(char[] tasks, int n) {
+        int[] map = new int[26];
+        for (char c: tasks)
+            map[c - 'A']++;
+        PriorityQueue < Integer > queue = new PriorityQueue< >(26, Collections.reverseOrder());
+        for (int f: map) {
+            if (f > 0)
+                queue.add(f);
+        }
+        int time = 0;
+        while (!queue.isEmpty()) {
+            int i = 0;
+            List< Integer > temp = new ArrayList< >();
+            while (i <= n) {
+                if (!queue.isEmpty()) {
+                    if (queue.peek() > 1)
+                        temp.add(queue.poll() - 1);
+                    else
+                        queue.poll();
+                }
+                time++;
+                if (queue.isEmpty() && temp.size() == 0)
+                    break;
+                i++;
+            }
+            for (int l: temp)
+                queue.add(l);
         }
         return time;
     }
