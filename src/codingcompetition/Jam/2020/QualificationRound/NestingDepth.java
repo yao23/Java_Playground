aa77dd743cc5148be6b8595734499a25aa3c15f3
Yao <myspiritcrazy@gmail.com>
2020-04-03 23:41:58 -0700

    Coding Practice for Nesting Depth

--- src/codingcompetition/Jam/2020/QualificationRound/NestingDepth.java
+++ src/codingcompetition/Jam/2020/QualificationRound/NestingDepth.java
@@ -49,6 +49,39 @@ public class Solution {
         }
         return sb.toString();
     }
+
+    private static void solveV0(Scanner scanner, int tc) {
+        String s = scanner.nextLine();
+        char[] c = s.toCharArray();
+        int n = c.length;
+
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int cur = c[i] - '0';
+            if (i == 0) {
+                sb.append("(".repeat(Math.max(0, cur)));
+                count += cur;
+            }
+            sb.append(cur);
+            if (i + 1 < n) {
+                int next = c[i + 1] - '0';
+                int diff = cur - next;
+                if (diff > 0) {
+                    sb.append(")".repeat(Math.max(0, diff)));
+                    count -= diff;
+                } else if (diff < 0) {
+                    diff *= (-1);
+                    sb.append("(".repeat(Math.max(0, diff)));
+                    count += diff;
+                }
+            }
+        }
+        if (count > 0) {
+            sb.append(")".repeat(Math.max(0, count)));
+        }
+        System.out.println("Case #" + tc +": " + sb.toString());
+    }
 }
 
 // 4
