89323429da5f33acffc95e27b74639618847b79c
Yao <myspiritcrazy@gmail.com>
2020-04-05 16:04:34 -0700

    Refactor code to add doc for House Robber

--- src/com/leetcode/www/HouseRobber.java
+++ src/com/leetcode/www/HouseRobber.java
@@ -4,7 +4,31 @@ package com.leetcode.www;
  * Created by liyao on 7/5/17.
  */
 public class HouseRobber { // LC 198
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
+     * Memory Usage: 38.4 MB, less than 5.26% of Java online submissions for House Robber.
+     *
+     * @param nums
+     * @return
+     */
     public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+
+        if (n > 1) {
+            dp[1] = Math.max(dp[0], nums[1]); // input: [2. 1], output: 2
+        }
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+
+    public int robV0(int[] nums) {
         int len = nums.length;
         if (len == 0) {
             return 0;
