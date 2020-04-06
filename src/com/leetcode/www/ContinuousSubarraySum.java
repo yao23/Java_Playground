ca754ba6cbf71ad75d34824b018d3dc67234441c
Yao <myspiritcrazy@gmail.com>
2020-04-05 23:05:52 -0700

    Refactor code to add doc for Continuous Subarray Sum

--- src/com/leetcode/www/ContinuousSubarraySum.java
+++ src/com/leetcode/www/ContinuousSubarraySum.java
@@ -1,5 +1,35 @@
 public class ContinuousSubarraySum {
     /**
+     * Memory Limit Exceeded
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0 && k != 0) {
+            return false;
+        }
+
+        int[][] dp = new int[n + 1][n]; // int[i][j] - start index j and length i
+        for (int i = 0; i < n; i++) {
+            dp[1][i] = nums[i];
+        }
+
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                dp[l][i] = dp[l - 1][i] + nums[i + l - 1];
+                if (isKMultiple(dp[l][i], k)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Wrong Answer
      * @param nums
      * @param k
