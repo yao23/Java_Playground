57c6ca553b1fad148ec8dd595cdfe269650b2e7d
Yao <myspiritcrazy@gmail.com>
2020-04-05 23:14:54 -0700

    Refactor code to add doc for Continuous Subarray Sum

--- src/com/leetcode/www/ContinuousSubarraySum.java
+++ src/com/leetcode/www/ContinuousSubarraySum.java
@@ -18,13 +18,16 @@ public class ContinuousSubarraySum {
         map.put(0, -1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (k != 0)
+            if (k != 0) {
                 sum = sum % k;
+            }
             if (map.containsKey(sum)) {
-                if (i - map.get(sum) > 1)
+                if (i - map.get(sum) > 1) {
                     return true;
-            } else
+                }
+            } else {
                 map.put(sum, i);
+            }
         }
         return false;
     }
