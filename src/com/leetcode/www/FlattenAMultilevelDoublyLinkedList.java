488655d20ae7d803f36b193e499470b7fd49a0bc
Yao <myspiritcrazy@gmail.com>
2020-04-17 23:12:12 -0700

    Refactor code to add doc for Flatten A Multilevel Doubly Linked List

---
+++ src/com/leetcode/www/FlattenAMultilevelDoublyLinkedList.java
@@ -0,0 +1,164 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class FlattenAMultilevelDoublyLinkedList { // 430
+    /**
+     * DFS by Recursion
+     *
+     * Time Complexity: O(N) where N is the number of nodes in the list. The DFS algorithm traverses each node once and
+     * only once.
+     * Space Complexity: O(N) where N is the number of nodes in the list. In the worst case, the binary tree might be
+     * extremely unbalanced (i.e. the tree leans to the left), which corresponds to the case where nodes are chained with
+     * each other only with the child pointers. In this case, the recursive calls would pile up, and it would take N
+     * space in the function call stack.
+     *
+     * @param head
+     * @return
+     */
+    public Node flatten(Node head) {
+        if (head == null) return head;
+        // pseudo head to ensure the `prev` pointer is never none
+        Node pseudoHead = new Node(0, null, head, null);
+
+        flattenDFS(pseudoHead, head);
+
+        // detach the pseudo head from the real head
+        pseudoHead.next.prev = null;
+        return pseudoHead.next;
+    }
+    /* return the tail of the flatten list */
+    public Node flattenDFS(Node prev, Node curr) {
+        if (curr == null) return prev;
+        curr.prev = prev;
+        prev.next = curr;
+
+        // the curr.next would be tempered in the recursive function
+        Node tempNext = curr.next;
+
+        Node tail = flattenDFS(curr, curr.child);
+        curr.child = null;
+
+        return flattenDFS(tail, tempNext);
+    }
+
+    class Node {
+        public int val;
+        public Node prev;
+        public Node next;
+        public Node child;
+
+        public Node(int val, Node prev, Node next, Node child) {
+            this.val = val;
+            this.prev = prev;
+            this.next = next;
+            this.child = child;
+        }
+    };
+
+    /**
+     * DFS by Iteration
+     *
+     * Time Complexity: O(N). The iterative solution has the same time complexity as the recursive.
+     * Space Complexity: O(N). Again, the iterative solution has the same space complexity as the recursive one.
+     *
+     * @param head
+     * @return
+     */
+    public Node flattenV1(Node head) {
+        if (head == null) return head;
+
+        Node pseudoHead = new Node(0, null, head, null);
+        Node curr, prev = pseudoHead;
+
+        Deque<Node> stack = new ArrayDeque<>();
+        stack.push(head);
+
+        while (!stack.isEmpty()) {
+            curr = stack.pop();
+            prev.next = curr;
+            curr.prev = prev;
+
+            if (curr.next != null) stack.push(curr.next);
+            if (curr.child != null) {
+                stack.push(curr.child);
+                // don't forget to remove all child pointers.
+                curr.child = null;
+            }
+            prev = curr;
+        }
+        // detach the pseudo node from the result
+        pseudoHead.next.prev = null;
+        return pseudoHead.next;
+    }
+}
+
+/**
+ * You are given a doubly linked list which in addition to the next and previous pointers, it could have a child pointer,
+ * which may or may not point to a separate doubly linked list. These child lists may have one or more children of their
+ * own, and so on, to produce a multilevel data structure, as shown in the example below.
+ *
+ * Flatten the list so that all the nodes appear in a single-level, doubly linked list. You are given the head of the
+ * first level of the list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: head = [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+ * Output: [1,2,3,7,8,11,12,9,10,4,5,6]
+ * Explanation:
+ *
+ * The multilevel linked list in the input is as follows:
+ *
+ *
+ *
+ * After flattening the multilevel linked list it becomes:
+ *
+ *
+ * Example 2:
+ *
+ * Input: head = [1,2,null,3]
+ * Output: [1,3,2]
+ * Explanation:
+ *
+ * The input multilevel linked list is as follows:
+ *
+ *   1---2---NULL
+ *   |
+ *   3---NULL
+ * Example 3:
+ *
+ * Input: head = []
+ * Output: []
+ *
+ *
+ * How multilevel linked list is represented in test case:
+ *
+ * We use the multilevel linked list from Example 1 above:
+ *
+ *  1---2---3---4---5---6--NULL
+ *          |
+ *          7---8---9---10--NULL
+ *              |
+ *              11--12--NULL
+ * The serialization of each level is as follows:
+ *
+ * [1,2,3,4,5,6,null]
+ * [7,8,9,10,null]
+ * [11,12,null]
+ * To serialize all levels together we will add nulls in each level to signify no node connects to the upper node of the
+ * previous level. The serialization becomes:
+ *
+ * [1,2,3,4,5,6,null]
+ * [null,null,7,8,9,10,null]
+ * [null,11,12,null]
+ * Merging the serialization of each level and removing trailing nulls we obtain:
+ *
+ * [1,2,3,4,5,6,null,null,null,7,8,9,10,null,null,11,12]
+ *
+ *
+ * Constraints:
+ *
+ * Number of Nodes will not exceed 1000.
+ * 1 <= Node.val <= 10^5
+ */
