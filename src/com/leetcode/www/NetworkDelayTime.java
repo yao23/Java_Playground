32b5fb79d9b2faa52b299945d3c57c50cc0bc555
Yao <myspiritcrazy@gmail.com>
2020-05-06 00:26:04 -0700

    Refactor code to add doc for Network Delay Time

--- src/com/leetcode/www/NetworkDelayTime.java
+++ src/com/leetcode/www/NetworkDelayTime.java
@@ -2,13 +2,25 @@ import java.util.*;
 
 public class NetworkDelayTime { // 743 [Google]
     public int networkDelayTime(int[][] times, int N, int K) {
+        class DelayComparator implements Comparator<int[]>{
+            public int compare(int[] s1, int[] s2) {
+                if (s1[1] < s2[1]) {
+                    return -1;
+                } else if (s1[1] > s2[1]) {
+                    return 1;
+                } else {
+                    return Integer.compare(s1[0], s2[0]);
+                }
+            }
+        }
+
         int count = 0, delayMax = 0;
-        Map<Integer, List<int[]>> graph = new HashMap<>();
+        Map<Integer, PriorityQueue<int[]>> graph = new HashMap<>();
         Map<Integer, Integer> delayMap = new HashMap<>();
         for (int[] time : times) {
             int s = time[0], e = time[1], delay = time[2];
             if (!graph.containsKey(s)) {
-                graph.put(s, new ArrayList<>());
+                graph.put(s, new PriorityQueue<>(new DelayComparator()));
             }
             graph.get(s).add(new int[]{e, delay});
         }
@@ -23,8 +35,9 @@ public class NetworkDelayTime { // 743 [Google]
                 int cur = queue.removeFirst();
                 visited.add(cur);
                 if (graph.containsKey(cur)) {
-                    List<int[]> neighbors = graph.get(cur);
-                    for (int[] neighborNode : neighbors) {
+                    PriorityQueue<int[]> neighbors = graph.get(cur);
+                    while (!neighbors.isEmpty()) {
+                        int[] neighborNode = neighbors.poll();
                         int neighbor = neighborNode[0], delay = neighborNode[1];
                         int neighborDelay = delayMap.get(cur) + delay;
                         if (visited.add(neighbor)) {
