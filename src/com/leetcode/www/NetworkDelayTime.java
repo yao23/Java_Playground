5692c9ad02c44f3aea254e8a6ab569f40556b0d8
Yao <myspiritcrazy@gmail.com>
2020-05-07 00:22:09 -0700

    Refactor code to add doc for Network Delay Time

--- src/com/leetcode/www/NetworkDelayTime.java
+++ src/com/leetcode/www/NetworkDelayTime.java
@@ -2,6 +2,119 @@ import java.util.*;
 
 public class NetworkDelayTime { // 743 [Google]
     Map<Integer, Integer> dist;
+
+    /**
+     * Dijkstra's Algorithm
+     *
+     * Time Complexity: O(N^2 + E) where E is the length of times in the basic implementation.
+     * Space Complexity: O(N + E), the size of the graph (O(E)), plus the size of the other objects used (O(N)).
+     *
+     * Runtime: 12 ms, faster than 86.01% of Java online submissions for Network Delay Time.
+     * Memory Usage: 43.4 MB, less than 90.48% of Java online submissions for Network Delay Time.
+     *
+     * @param times
+     * @param N
+     * @param K
+     * @return
+     */
+    public int networkDelayTime(int[][] times, int N, int K) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] edge: times) {
+            if (!graph.containsKey(edge[0])) {
+                graph.put(edge[0], new ArrayList<>());
+            }
+            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
+        }
+        dist = new HashMap<>();
+        for (int node = 1; node <= N; ++node) {
+            dist.put(node, Integer.MAX_VALUE);
+        }
+
+        dist.put(K, 0);
+        boolean[] seen = new boolean[N+1];
+
+        while (true) {
+            int candNode = -1;
+            int candDist = Integer.MAX_VALUE;
+            for (int i = 1; i <= N; ++i) {
+                if (!seen[i] && dist.get(i) < candDist) {
+                    candDist = dist.get(i);
+                    candNode = i;
+                }
+            }
+
+            if (candNode < 0) break;
+            seen[candNode] = true;
+            if (graph.containsKey(candNode)) {
+                for (int[] info : graph.get(candNode)) {
+                    dist.put(info[0],
+                            Math.min(dist.get(info[0]), dist.get(candNode) + info[1]));
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int cand: dist.values()) {
+            if (cand == Integer.MAX_VALUE) {
+                return -1;
+            }
+            ans = Math.max(ans, cand);
+        }
+        return ans;
+    }
+
+    /**
+     * Time Complexity: O(ElogE) in the heap implementation, as potentially every edge gets added to the heap.
+     * Space Complexity: O(N + E), the size of the graph (O(E)), plus the size of the other objects used (O(N))
+     *
+     * Runtime: 19 ms, faster than 61.01% of Java online submissions for Network Delay Time.
+     * Memory Usage: 42.8 MB, less than 92.86% of Java online submissions for Network Delay Time.
+     *
+     * @param times
+     * @param N
+     * @param K
+     * @return
+     */
+    public int networkDelayTimeV2(int[][] times, int N, int K) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] edge: times) {
+            if (!graph.containsKey(edge[0])) {
+                graph.put(edge[0], new ArrayList<>());
+            }
+            graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
+        }
+        PriorityQueue<int[]> heap = new PriorityQueue<>(
+                Comparator.comparingInt(info -> info[0]));
+        heap.offer(new int[]{0, K});
+
+        Map<Integer, Integer> dist = new HashMap<>();
+
+        while (!heap.isEmpty()) {
+            int[] info = heap.poll();
+            int d = info[0], node = info[1];
+            if (dist.containsKey(node)) {
+                continue;
+            }
+            dist.put(node, d);
+            if (graph.containsKey(node)) {
+                for (int[] edge : graph.get(node)) {
+                    int nei = edge[0], d2 = edge[1];
+                    if (!dist.containsKey(nei)) {
+                        heap.offer(new int[]{d + d2, nei});
+                    }
+                }
+            }
+        }
+
+        if (dist.size() != N) {
+            return -1;
+        }
+        int ans = 0;
+        for (int cand: dist.values())
+            ans = Math.max(ans, cand);
+        return ans;
+    }
+
     /**
      * Time Complexity: O(N^N + E*logE) where E is the length of times. We can only fully visit each node up to N-1 times,
      * one per each other node. Plus, we have to explore every edge and sort them. Sorting each small bucket of outgoing
@@ -9,12 +122,16 @@ public class NetworkDelayTime { // 743 [Google]
      *
      * Space Complexity: O(N + E), the size of the graph (O(E)), plus the size of the implicit call stack in our
      * DFS (O(N)).
+     *
+     * Runtime: 47 ms, faster than 18.20% of Java online submissions for Network Delay Time.
+     * Memory Usage: 42.3 MB, less than 97.62% of Java online submissions for Network Delay Time.
+     *
      * @param times
      * @param N
      * @param K
      * @return
      */
-    public int networkDelayTime(int[][] times, int N, int K) {
+    public int networkDelayTimeV1(int[][] times, int N, int K) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
         for (int[] edge: times) {
             if (!graph.containsKey(edge[0])) {
