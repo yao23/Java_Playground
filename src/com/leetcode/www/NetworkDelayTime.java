55087f5840d8904fd7b15ebc19738e68e2ae0c10
Yao <myspiritcrazy@gmail.com>
2020-05-07 00:06:22 -0700

    Refactor code to add doc for Network Delay Time

--- src/com/leetcode/www/NetworkDelayTime.java
+++ src/com/leetcode/www/NetworkDelayTime.java
@@ -1,6 +1,57 @@
 import java.util.*;
 
 public class NetworkDelayTime { // 743 [Google]
+    Map<Integer, Integer> dist;
+    /**
+     * Time Complexity: O(N^N + E*logE) where E is the length of times. We can only fully visit each node up to N-1 times,
+     * one per each other node. Plus, we have to explore every edge and sort them. Sorting each small bucket of outgoing
+     * edges is bounded by sorting all of them, because of repeated use of the inequality xlogx + ylogy <= (x+y)log(x+y).
+     *
+     * Space Complexity: O(N + E), the size of the graph (O(E)), plus the size of the implicit call stack in our
+     * DFS (O(N)).
+     * @param times
+     * @param N
+     * @param K
+     * @return
+     */
+    public int networkDelayTime(int[][] times, int N, int K) {
+        Map<Integer, List<int[]>> graph = new HashMap<>();
+        for (int[] edge: times) {
+            if (!graph.containsKey(edge[0])) {
+                graph.put(edge[0], new ArrayList<>());
+            }
+            graph.get(edge[0]).add(new int[]{edge[2], edge[1]});
+        }
+        for (int node: graph.keySet()) {
+            graph.get(node).sort(Comparator.comparingInt(a -> a[0]));
+        }
+        dist = new HashMap<>();
+        for (int node = 1; node <= N; ++node) {
+            dist.put(node, Integer.MAX_VALUE);
+        }
+
+        dfs(graph, K, 0);
+        int ans = 0;
+        for (int cand: dist.values()) {
+            if (cand == Integer.MAX_VALUE) {
+                return -1;
+            }
+            ans = Math.max(ans, cand);
+        }
+        return ans;
+    }
+
+    public void dfs(Map<Integer, List<int[]>> graph, int node, int elapsed) {
+        if (elapsed >= dist.get(node)) {
+            return;
+        }
+        dist.put(node, elapsed);
+        if (graph.containsKey(node)) {
+            for (int[] info : graph.get(node)) {
+                dfs(graph, info[1], elapsed + info[0]);
+            }
+        }
+    }
     /**
      * Not work for test case
      * [[15,8,1],[7,10,41],[7,9,34],[9,4,31],[12,13,50],[14,3,52],[4,11,99],[4,7,86],[10,13,57],[9,6,10],[1,7,51],
@@ -30,7 +81,7 @@ public class NetworkDelayTime { // 743 [Google]
      * @param K
      * @return
      */
-    public int networkDelayTime(int[][] times, int N, int K) {
+    public int networkDelayTimeV0(int[][] times, int N, int K) {
         class DelayComparator implements Comparator<int[]>{
             public int compare(int[] s1, int[] s2) {
                 if (s1[1] < s2[1]) {
