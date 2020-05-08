d435e9de77389caf3333d2bc2505c8ef7e89cbeb
Yao <myspiritcrazy@gmail.com>
2020-05-08 00:12:24 -0700

    Refactor code to add doc for Range Sum Of BST

--- src/com/leetcode/www/RangeSumOfBST.java
+++ src/com/leetcode/www/RangeSumOfBST.java
@@ -2,13 +2,39 @@ import com.leetcode.www.TreeNode;
 
 public class RangeSumOfBST { // 938
     /**
-     * Not work, i.e. [15,9,21,7,13,19,23,5,null,11,null,17], 19, 21
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Range Sum of BST.
+     * Memory Usage: 46.9 MB, less than 94.77% of Java online submissions for Range Sum of BST.
+     *
      * @param root
      * @param L
      * @param R
      * @return
      */
     public int rangeSumBST(TreeNode root, int L, int R) {
+        if (root == null) {
+            return 0;
+        } else {
+            int cur = root.val;
+            if (cur < L) {
+                return rangeSumBST(root.right, L, R);
+            } else if (cur > R) {
+                return rangeSumBST(root.left, L, R);
+            } else {
+                int left = rangeSumBST(root.left, L, R);
+                int right = rangeSumBST(root.right, L, R);
+                return cur + left + right;
+            }
+        }
+    }
+
+    /**
+     * Not work, i.e. [15,9,21,7,13,19,23,5,null,11,null,17], 19, 21
+     * @param root
+     * @param L
+     * @param R
+     * @return
+     */
+    public int rangeSumBSTV0(TreeNode root, int L, int R) {
         if (root == null) {
             return 0;
         } else {
