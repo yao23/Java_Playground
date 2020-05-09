4c36097f22f074ee094c0676cc140e14ac4ed71b
Yao <myspiritcrazy@gmail.com>
2020-05-09 11:19:40 -0700

    Refactor code to add doc for Search In Rotated Sorted Array II

--- src/com/leetcode/www/SearchInRotatedSortedArrayII.java
+++ src/com/leetcode/www/SearchInRotatedSortedArrayII.java
@@ -36,4 +36,52 @@ public class SearchInRotatedSortedArrayII { // LC 81
 
         return false;
     }
+
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Search in Rotated Sorted Array II.
+     * Memory Usage: 38.9 MB, less than 81.69% of Java online submissions for Search in Rotated Sorted Array II.
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public boolean searchV1(int[] nums, int target) {
+        if (nums.length == 0) {
+            return false;
+        }
+
+        int low = 0;
+        int high = nums.length - 1;
+        if (nums[low] == target) {
+            return true;
+        }
+
+        while (low < nums.length && nums[low] == nums[high]) {
+            low++;
+        }
+
+        return search(nums, target, low, high);
+    }
+
+    public boolean search(int[] nums, int target, int low, int high) {
+        if (low > high) {
+            return false;
+        }
+
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target) {
+            return true;
+        }
+        if (nums[low] <= nums[mid]) {
+            if (target >= nums[low] && target < nums[mid]) {
+                return search(nums, target, low, mid - 1);
+            }
+            return search(nums, target, mid + 1, high);
+        } else {
+            if (target > nums[mid] && target <= nums[high]) {
+                return search(nums, target, mid + 1, high);
+            }
+            return search(nums, target, low, mid - 1);
+        }
+    }
 }
