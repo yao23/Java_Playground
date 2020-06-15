a4b4f0f1bfff90d6ad36a2d7c76c687cef096596
Yao <myspiritcrazy@gmail.com>
2020-06-14 23:20:57 -0700

    Refactor code to add doc for Integer To English Words

--- src/com/leetcode/www/IntegerToEnglishWords.java
+++ src/com/leetcode/www/IntegerToEnglishWords.java
@@ -1,6 +1,6 @@
 package com.leetcode.www;
 
-public class IntegerToEnglishWords { // LC 273
+public class IntegerToEnglishWords { // LC 273 (Facebook)
     private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
             "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
     private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
