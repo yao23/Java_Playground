be9b1403721bf14f133bc871d100c1ec10f18e0d
Yao <myspiritcrazy@gmail.com>
2020-07-21 23:45:58 -0700

    Refactor code to add doc for Minimum Window Subsequence

--- src/com/leetcode/www/MinimumWindowSubsequence.java
+++ src/com/leetcode/www/MinimumWindowSubsequence.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MinimumWindowSubsequence { // 727
+public class MinimumWindowSubsequence { // 727 [Google]
     class Solution {
         /**
          * Dynamic Programming (Postfix Variation)
