0c3ec6c3261530b0c90f3d22dca18d4769eaadd6
Yao <myspiritcrazy@gmail.com>
2020-07-24 23:16:06 -0700

    Refactor code to add doc for Missing Element In Sorted Array

--- src/com/leetcode/www/MissingElementInSortedArray.java
+++ src/com/leetcode/www/MissingElementInSortedArray.java
@@ -1,4 +1,4 @@
-public class MissingElementInSortedArray { // 1060
+public class MissingElementInSortedArray { // 1060 (Facebook)
     /**
      * Runtime: 1 ms, faster than 41.21% of Java online submissions for Missing Element in Sorted Array.
      * Memory Usage: 46.3 MB, less than 100.00% of Java online submissions for Missing Element in Sorted Array.
@@ -63,3 +63,15 @@ public class MissingElementInSortedArray { // 1060
         return nums[idx] - nums[0] - idx;
     }
 }
+
+// Given a sorted array A of unique numbers, find the K-th missing number starting from the leftmost number of the array.
+//
+// Example 1: Input: A = [4,7,9,10], K = 1 Output: 5 Explanation: The first missing number is 5.
+//
+// Example 2: Input: A = [4,7,9,10], K = 3 Output: 8 Explanation: The missing numbers are [5,6,8,…],
+// hence the third missing number is 8. Example 3:
+//
+// Input: A = [1,2,4], K = 3 Output: 6 Explanation: The missing numbers are [3,5,6,7,…],
+// hence the third missing number is 6.
+//
+// Note: 1 <= A.length <= 50000 1 <= A[i] <= 1e7 1 <= K <= 1e8
