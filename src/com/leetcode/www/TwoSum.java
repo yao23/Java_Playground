faf6416d85237d818f6020307cd646afad33a00e
Yao <myspiritcrazy@gmail.com>
2020-09-21 19:11:53 -0700

    Refactor code to add doc for Two Sum

--- src/com/leetcode/www/TwoSum.java
+++ src/com/leetcode/www/TwoSum.java
@@ -3,7 +3,7 @@ package com.leetcode.www;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum { // LC 1
+public class TwoSum { // LC 1 [Google]
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
