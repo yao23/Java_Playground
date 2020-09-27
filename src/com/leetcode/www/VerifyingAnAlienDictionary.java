b034b8c20066b9e3eac8372bae8c77e89416d980
Yao <myspiritcrazy@gmail.com>
2020-09-27 15:43:36 -0700

    Refactor code to add doc for Verifying An Alien Dictionary

--- src/com/leetcode/www/VerifyingAnAlienDictionary.java
+++ src/com/leetcode/www/VerifyingAnAlienDictionary.java
@@ -1,4 +1,4 @@
-public class VerifyingAnAlienDictionary {
+public class VerifyingAnAlienDictionary { // LC 953 (Facebook)
     /**
      * Time Complexity: O(C), where C is the total content of words.
      * Space Complexity: O(1).
@@ -17,20 +17,20 @@ public class VerifyingAnAlienDictionary {
 
         search: for (int i = 0; i < words.length - 1; ++i) {
             String word1 = words[i];
-            String word2 = words[i+1];
+            String word2 = words[i + 1];
 
             // Find the first difference word1[k] != word2[k].
             for (int k = 0; k < Math.min(word1.length(), word2.length()); ++k) {
                 if (word1.charAt(k) != word2.charAt(k)) {
                     // If they compare badly, it's not sorted.
-                    if (index[word1.charAt(k) - 'a'] > index[word2.charAt(k) - 'a'])
+                    if (index[word1.charAt(k) - 'a'] > index[word2.charAt(k) - 'a']) {
                         return false;
+                    }
                     continue search;
                 }
             }
 
-            // If we didn't find a first difference, the
-            // words are like ("app", "apple").
+            // If we didn't find a first difference, the words are like ("app", "apple").
             if (word1.length() > word2.length())
                 return false;
         }
