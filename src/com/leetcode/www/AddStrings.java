53cbab21b1ea543558ad2f14400e12ca44c26c28
Yao <myspiritcrazy@gmail.com>
2020-10-03 22:42:09 -0700

    Refactor code to add doc for Add Strings

--- src/com/leetcode/www/AddStrings.java
+++ src/com/leetcode/www/AddStrings.java
@@ -1,7 +1,14 @@
 package com.leetcode.www;
 
 public class AddStrings { // LC 415 (Facebook)
-    public String addStrings(String num1, String num2) { // beats 70.81%
+    /**
+     * beats 70.81%
+     *
+     * @param num1
+     * @param num2
+     * @return
+     */
+    public String addStrings(String num1, String num2) {
         StringBuilder sb = new StringBuilder();
         int carry = 0;
         for (int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
