1ad8e1fd925e2e8dbdccfaf071f5b0931e57b279
Yao <myspiritcrazy@gmail.com>
2020-11-02 20:30:09 -0800

    Refactor code to add doc for BinaryTreeMaximumPathSum

--- src/com/leetcode/www/BinaryTreeMaximumPathSum.java
+++ src/com/leetcode/www/BinaryTreeMaximumPathSum.java
@@ -27,6 +27,12 @@ public class BinaryTreeMaximumPathSum { // LC 124 (Facebook)
      *
      * beats 48.00%
      *
+     * [] => -2147483648
+     * [-3] => -3
+     * [1,2,3] => 6
+     * [5,3,4,1,2,6,7] => 21, [2,3,5,4,7], root 5 use both left and right single path
+     * [5,-3,-4,1,2,6,7] => 9, [6,-4,7], root 5 don't use left or right single path, arch in root 6 as max
+     *
      * @param root
      * @return
      */
@@ -39,10 +45,5 @@ public class BinaryTreeMaximumPathSum { // LC 124 (Facebook)
         return result[0];
     }
 
-    // [] => -2147483648
-    // [-3] => -3
-    // [1,2,3] => 6
-    // [5,3,4,1,2,6,7] => 21, [2,3,5,4,7], root 5 use both left and right single path
-    // [5,-3,-4,1,2,6,7] => 9, [6,-4,7], root 5 don't use left or right single path, arch in root 6 as max
 
 }
