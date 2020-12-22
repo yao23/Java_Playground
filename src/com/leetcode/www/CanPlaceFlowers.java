86d94e537f44408a4dd404f01a5060056915e08a
Yao <myspiritcrazy@gmail.com>
2020-12-21 23:32:30 -0800

    Refactor code to add doc for CanPlaceFlowers

--- src/com/leetcode/www/CanPlaceFlowers.java
+++ src/com/leetcode/www/CanPlaceFlowers.java
@@ -25,11 +25,11 @@ public class CanPlaceFlowers { // LC 605
     }
 
     // DP
-    // A Java DP solution runs on
 
     /**
      * beats 0.57%
      *
+     * A Java DP solution runs on
      * K: number of flowers to plant
      * N: length of flower bed
      * O(K) space, O(NK) time
