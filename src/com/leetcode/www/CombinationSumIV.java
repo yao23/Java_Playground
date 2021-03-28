ba963225ff04747f780bd378d32b3d5b678268ce
Yao <myspiritcrazy@gmail.com>
2021-03-27 23:43:58 -0700

    Refactor code to add doc for CombinationSumIV

--- src/com/leetcode/www/CombinationSumIV.java
+++ src/com/leetcode/www/CombinationSumIV.java
@@ -6,6 +6,7 @@ public class CombinationSumIV { // LC 377
     /**
      * nums = [1, 2, 3], target = 4 => 7
      * (1, 1, 1, 1)
+     * // (1, 1, 2)
      * @param nums
      * @param target
      * @return
@@ -48,7 +49,7 @@ public class CombinationSumIV { // LC 377
 
 
 
-// (1, 1, 2)
+
 // (1, 2, 1)
 // (1, 3)
 // (2, 1, 1)
