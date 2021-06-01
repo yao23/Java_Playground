f4abeb1aef31df1ddc41ca8f650a21c3f54820e7
Yao <myspiritcrazy@gmail.com>
2021-05-31 23:37:16 -0700

    Refactor code to add doc for CompareVersionNumbers

--- src/com/leetcode/www/CompareVersionNumbers.java
+++ src/com/leetcode/www/CompareVersionNumbers.java
@@ -40,12 +40,12 @@ public class CompareVersionNumbers { // LC 165
     /**
      * Runtime: 2 ms, faster than 16.34% of Java online submissions for Compare Version Numbers.
      * Memory Usage: 33.4 MB, less than 63.19% of Java online submissions for Compare Version Numbers.
-     *
+     * // beats 11.74%
      * @param version1
      * @param version2
      * @return
      */
-    public int compareVersionV0(String version1, String version2) { // beats 11.74%
+    public int compareVersionV0(String version1, String version2) {
         String[] levels1 = version1.split("\\.");
         String[] levels2 = version2.split("\\.");
 
