7e6a1b7764cedc9c17b7969f0e218f2ba19f9906
Yao <myspiritcrazy@gmail.com>
2021-09-24 23:26:31 -0700

    Refactor code to add doc for ContainsDuplicate

--- src/com/leetcode/www/ContainsDuplicateII.java
+++ src/com/leetcode/www/ContainsDuplicateII.java
@@ -10,7 +10,7 @@ public class ContainsDuplicateII { // LC 219
      * beats 93.93%
      * [], 0 => false
      * [1,0,1,1], 1 => true
-     *  [1,2,1], 1 => false
+     * [1,2,1], 1 => false
      * @param nums
      * @param k
      * @return
