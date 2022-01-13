0af947b818d2cdee8d444501063604da87bdb966
Yao <myspiritcrazy@gmail.com>
2022-01-13 15:20:17 -0800

    Refactor code to add doc for CountAndSay

--- src/com/leetcode/www/CountAndSay.java
+++ src/com/leetcode/www/CountAndSay.java
@@ -8,7 +8,7 @@ public class CountAndSay { // LC 38
      *  2.     11
      *  3.     21
      *  4.     1211
-     *  *  5.     111221
+     *    5.     111221
      *  *
      *  *  1 is read off as "one 1" or 11.
      *  *  11 is read off as "two 1s" or 21.
