87cbb4c7b336d45dfa4143f51a273ef49a2b9b47
Yao <myspiritcrazy@gmail.com>
2022-02-09 14:00:48 -0800

    Refactor code to add doc for CountOfRangeSum

--- src/com/leetcode/www/CountOfRangeSum.java
+++ src/com/leetcode/www/CountOfRangeSum.java
@@ -1,6 +1,28 @@
 package com.leetcode.www;
 
 public class CountOfRangeSum { // LC 327
+    // beats 66.49%
+
+    /**
+     Recall count smaller number after self where we encountered the problem
+
+     count[i] = count of nums[j] - nums[i] < 0 with j > i
+     Here, after we did the preprocess, we need to solve the problem
+
+     count[i] = count of a <= S[j] - S[i] <= b with j > i
+     ans = sum(count[:])
+     Therefore the two problems are almost the same. We can use the same technique used in that problem to solve this problem. One solution is merge sort based; another one is Balanced BST based. The time complexity are both O(n log n).
+
+     The merge sort based solution counts the answer while doing the merge. During the merge stage, we have already sorted the left half [start, mid) and right half [mid, end). We then iterate through the left half with index i. For each i, we need to find two indices k and j in the right half where
+
+     j is the first index satisfy sums[j] - sums[i] > upper and
+     k is the first index satisfy sums[k] - sums[i] >= lower.
+     Then the number of sums in [lower, upper] is j-k. We also use another index t to copy the elements satisfy sums[t] < sums[i] to a cache in order to complete the merge sort.
+
+     Despite the nested loops, the time complexity of the "merge & count" stage is still linear. Because the indices k, j, t will only increase but not decrease, each of them will only traversal the right half once at most. The total time complexity of this divide and conquer solution is then O(n log n).
+
+     One other concern is that the sums may overflow integer. So we use long instead.
+     */
     public static int countRangeSum(int[] nums, int lower, int upper) {
         int n = nums.length;
         long[] sums = new long[n + 1]; // pre-processed sums (hidden condition: sum element has index range info, don't worry about index messed after sorted
@@ -46,26 +68,3 @@ public class CountOfRangeSum { // LC 327
         System.out.println("num of range sum: " + countRangeSum(nums, lower, upper));
     }
 }
-
-// beats 66.49%
-
-/**
- Recall count smaller number after self where we encountered the problem
-
- count[i] = count of nums[j] - nums[i] < 0 with j > i
- Here, after we did the preprocess, we need to solve the problem
-
- count[i] = count of a <= S[j] - S[i] <= b with j > i
- ans = sum(count[:])
- Therefore the two problems are almost the same. We can use the same technique used in that problem to solve this problem. One solution is merge sort based; another one is Balanced BST based. The time complexity are both O(n log n).
-
- The merge sort based solution counts the answer while doing the merge. During the merge stage, we have already sorted the left half [start, mid) and right half [mid, end). We then iterate through the left half with index i. For each i, we need to find two indices k and j in the right half where
-
- j is the first index satisfy sums[j] - sums[i] > upper and
- k is the first index satisfy sums[k] - sums[i] >= lower.
- Then the number of sums in [lower, upper] is j-k. We also use another index t to copy the elements satisfy sums[t] < sums[i] to a cache in order to complete the merge sort.
-
- Despite the nested loops, the time complexity of the "merge & count" stage is still linear. Because the indices k, j, t will only increase but not decrease, each of them will only traversal the right half once at most. The total time complexity of this divide and conquer solution is then O(n log n).
-
- One other concern is that the sums may overflow integer. So we use long instead.
-*/
