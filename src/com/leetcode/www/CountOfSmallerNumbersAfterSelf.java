8fa98a376e1e842dd9fb4bec908d15a5f19e79cb
Yao <myspiritcrazy@gmail.com>
2022-03-03 10:42:49 -0800

    Refactor code to add doc for CountOfSmallerNumbersAfterSelf

--- src/com/leetcode/www/CountOfSmallerNumbersAfterSelf.java
+++ src/com/leetcode/www/CountOfSmallerNumbersAfterSelf.java
@@ -11,6 +11,13 @@ public class CountOfSmallerNumbersAfterSelf { // LC 315 [Google]
      *
      * Binary Search Tree is ordered, have smaller in left and larger in right
      *
+     *
+     // [5,2,6,1] => [2, 1, 1, 0]
+     // [] => []
+     // [5,7,2,2,6,1] => [3,4,1,1,1,0]
+
+     // beats 30.43%
+     *
      * @param nums
      * @return
      */
@@ -132,9 +139,3 @@ public class CountOfSmallerNumbersAfterSelf { // LC 315 [Google]
         }
     }
 }
-
-// [5,2,6,1] => [2, 1, 1, 0]
-// [] => []
-// [5,7,2,2,6,1] => [3,4,1,1,1,0]
-
-// beats 30.43%
