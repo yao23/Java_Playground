36593a6afdf9378c67ce680f4770cbab1bf43923
Yao <myspiritcrazy@gmail.com>
2023-02-14 21:41:58 -0800

    Refactor code to add doc for DesignTinyURL

--- src/com/leetcode/www/DesignTinyURL.java
+++ src/com/leetcode/www/DesignTinyURL.java
@@ -73,7 +73,7 @@ public class DesignTinyURL { // LC 534
  * Daily User: 100M
  * Daily usage per person: (Write) long2short 0.1, (Read) short2long 1
  * Daily request: Write 10M, Read 100M
-* QPS: Since a day is 86400s approximately 100K.
+ * QPS: Since a day is 86400s approximately 100K.
 Write 100, Read 1K
 
 Peak QPS: Write 200, Read 2K
