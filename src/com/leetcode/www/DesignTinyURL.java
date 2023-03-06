882dea1dcfc500dcd923ead8c65b53240f868b18
Yao <myspiritcrazy@gmail.com>
2023-03-06 10:01:19 -0800

    Refactor code to add doc for DesignTinyURL

--- src/com/leetcode/www/DesignTinyURL.java
+++ src/com/leetcode/www/DesignTinyURL.java
@@ -89,7 +89,7 @@ public class DesignTinyURL { // LC 534
  * Through SN analysis, we could have a big picture of the system. In general, this system is not hard and could be handled by a single SSD Machine.
 
  * A: API
-* Only one service: URLService
+ * Only one service: URLService
 
 Core (Business Logic) Layer:
 Class: URLService
