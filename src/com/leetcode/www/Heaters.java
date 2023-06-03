8840f5bcbed871e141b0032f693cd1aba18aa3da
Yao <myspiritcrazy@gmail.com>
2023-06-03 15:49:09 -0700

    Refactor code to add doc for Heaters

--- src/com/leetcode/www/Heaters.java
+++ src/com/leetcode/www/Heaters.java
@@ -36,6 +36,41 @@ public class Heaters { // LC 475
 
         return res;
     }
+
+    public int findRadius2(int[] houses, int[] heaters) {
+        if(houses == null || houses.length == 0 || heaters == null || heaters.length == 0){
+            return 0;
+        }
+
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+
+        int n = houses.length;
+        int m = heaters.length;
+
+        int i = 0;
+        int j = 0;
+
+        int res = 0;
+        while(i < n && j < m){
+            // for each heaters[j],  compare to heaters[j + 1]
+            int dist1 = Math.abs(heaters[j] - houses[i]);
+            int dist2 = Integer.MAX_VALUE;
+            if(j + 1 < m){
+                dist2 = Math.abs(heaters[j + 1] - houses[i]);
+            }
+            // if j closer to i than j + 1, settle and i ++;
+            if(dist1 < dist2){
+                res = Math.max(res, dist1);
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+        return res;
+
+    }
 }
 
 // Input: [1,2,3],[2]
